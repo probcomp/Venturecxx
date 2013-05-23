21c9f725b9e3c31eaa5fcc5a62f8ce21f68e11e1
Zack Drach <hogbait@gmail.com>
2013-05-23 19:56:49 -0400

    implemented ripl rest client

--- lib/ripl/ripl_rest_client.py
+++ lib/ripl/ripl_rest_client.py
@@ -3,6 +3,8 @@
 
 
 from venture.server.utils import RestClient
+from venture.ripl.utils import _RIPL_FUNCTIONS
 
 class RiplRestClient(RestClient):
-    pass
+    def __init__(self, base_url):
+        super(RiplRestClient, self).__init__(base_url, _RIPL_FUNCTIONS)
