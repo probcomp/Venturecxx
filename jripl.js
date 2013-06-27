fabafb3215a1a7be9e3cb0c0d3dce7d12be51693
vladfi1 <vladfi1@mit.edu>
2013-06-26 21:03:15 -0400

    Fixed indentation.

--- jripl.js
+++ jripl.js
@@ -19,7 +19,7 @@ function jripl() {
 
 /* URL utilities */
     var full_url = function(url) {
-	return server_url + port + "/" + url;
+        return server_url + port + "/" + url;
     };
 
 /* Status booleans */
@@ -43,73 +43,73 @@ function jripl() {
      * Asynchronously performs next request in queue.
      */
     var ajax_continue_requests = function() {
-	if (request_queue.length > 0) {
-	    var next_request = request_queue.shift();
-	    ajax_execute_post(next_request.url, next_request.data_in, next_request.on_success);
-	}
-	else {
-	    request_in_progressQ = false;
-	    all_requests_processed_callback();
-	    all_requests_processed_callback = function() {};
-	}
+        if (request_queue.length > 0) {
+            var next_request = request_queue.shift();
+            ajax_execute_post(next_request.url, next_request.data_in, next_request.on_success);
+        }
+        else {
+            request_in_progressQ = false;
+            all_requests_processed_callback();
+            all_requests_processed_callback = function() {};
+        }
     };
 
     /* Send via AJAX in sequence, going through the request queue. */
     var ajax_post_in_sequence = function(URL,data_in,on_success) {
-	if (request_in_progressQ) {
-	    request_queue.push({url: URL, 
-				data_in: data_in,
-		                on_success: on_success});
-	} 
-	else {
-	    ajax_execute_post(URL,data_in,on_success);
-	}
+        if (request_in_progressQ) {
+            request_queue.push({url: URL, 
+                                data_in: data_in,
+                                on_success: on_success});
+        } 
+        else {
+            ajax_execute_post(URL,data_in,on_success);
+        }
     };
 
     /* Perform the actual AJAX request. */
     var ajax_execute_post = function(URL,data_in,on_success) {
-	console.log(data_in);
-	$.ajax({
-	    url: full_url(URL),
-	    type:'POST', 
-	    data: JSON.stringify(data_in),
-	    dataType: 'json',
-	    crossDomain: true,
-	    success:function(data) {
-			a_request_processed_callback();
-			on_success(data);
-			ajax_continue_requests();
-		    },
-		    // TODO this error callback needs updating
-		    error: function(httpRequest, textStatus, errorThrown) { 
-			console.log("ajax_execute_post status=" + textStatus + ",error=" + errorThrown + 'URL:' + URL);
-		},
-		    complete: function() {}
-	    });
+        console.log(data_in);
+        $.ajax({
+            url: full_url(URL),
+            type:'POST', 
+            data: JSON.stringify(data_in),
+            dataType: 'json',
+            crossDomain: true,
+            success:function(data) {
+                        a_request_processed_callback();
+                        on_success(data);
+                        ajax_continue_requests();
+                    },
+                    // TODO this error callback needs updating
+                    error: function(httpRequest, textStatus, errorThrown) { 
+                        console.log("ajax_execute_post status=" + textStatus + ",error=" + errorThrown + 'URL:' + URL);
+                },
+                    complete: function() {}
+            });
     };
 
     
     /* These functions are all supported, and can be called just as in
      * the python layer, except the user cannot receive the return values.
      */
-    var supported_pripl_functions = 
-	[
-        'set_mode','execute_instruction','execute_program','substitute_params','split_program','character_index_to_expression_index','expression_index_to_text_index','configure','infer','clear','rollback','assume','predict','observe','forget','force','sample','start_continuous_inference','stop_continuous_inference','continuous_inference_status'];
+    var supported_pripl_functions = ['set_mode', 'execute_instruction', 'execute_program', 'substitute_params', 'split_program', 'character_index_to_expression_index',
+        'expression_index_to_text_index', 'configure', 'infer', 'clear', 'rollback', 'assume', 'predict', 'observe', 'forget', 'force', 'sample', 'start_continuous_inference',
+        'stop_continuous_inference', 'continuous_inference_status'];
 
     /* Creates a function corresponding to one of the supported pripl functions 
      * listed above. */
     var create_closure = function(name) {
-	return function() {
-	    ajax_post_in_sequence(name,
-				  Array.prototype.slice.call(arguments, 0), 
-				  function() {});
-	};
+        return function() {
+            ajax_post_in_sequence(name,
+                                  Array.prototype.slice.call(arguments, 0), 
+                                  function() {});
+        };
     };
 
     /* Dynamically add supported pripl functions to JRIPL. */  
     for (i = 0; i < supported_pripl_functions.length; i++) {
-	var name = supported_pripl_functions[i];
-	this[name] = create_closure(name);
+        var name = supported_pripl_functions[i];
+        this[name] = create_closure(name);
     };
 
 /* Continuous directives */
@@ -130,46 +130,46 @@ function jripl() {
      * to receive all directives.
      */
     var process_directives = function(time,f,ids) {
-	return function(directives) {
-	    f(directives);
-	    setTimeout(
-		function () {
-		    get_directives_recursively(time,f,ids);
-		},
-		time);
-	};
+        return function(directives) {
+            f(directives);
+            setTimeout(
+                function () {
+                    get_directives_recursively(time,f,ids);
+                },
+                time);
+        };
     };
 
     var get_directives_recursively = function(time,f,ids) {
-	ajax_post_in_sequence("list_directives",
-			      [],
-//			      ids, (see note above)
-			      process_directives(time,f,ids));
+        ajax_post_in_sequence("list_directives",
+                              [],
+//                              ids, (see note above)
+                              process_directives(time,f,ids));
     };
 
     /* This the user-facing function. */
     this.get_directives_continuously = function(list_of_time_f_ids) {
-	if (get_directives_in_progressQ) {
-	    throw "Error: already getting directives continuously!"
-	}
-	else {
-	    get_directives_in_progressQ = true;
-	    for (i = 0; i < list_of_time_f_ids.length; i++) {
-		var time = list_of_time_f_ids[i][0];
-		var f = list_of_time_f_ids[i][1];
-		var ids = list_of_time_f_ids[i][2];
-		get_directives_recursively(time,f,ids);
-	    };
-	};
+        if (get_directives_in_progressQ) {
+            throw "Error: already getting directives continuously!"
+        }
+        else {
+            get_directives_in_progressQ = true;
+            for (i = 0; i < list_of_time_f_ids.length; i++) {
+                var time = list_of_time_f_ids[i][0];
+                var f = list_of_time_f_ids[i][1];
+                var ids = list_of_time_f_ids[i][2];
+                get_directives_recursively(time,f,ids);
+            };
+        };
     };
 
     this.get_directives_once = function(f) {
-	ajax_post_in_sequence("list_directives", [], f);
+        ajax_post_in_sequence("list_directives", [], f);
     };
 
 /* Registering special callback functions */
     this.register_a_request_processed_callback = function(f) {
-	a_request_processed_callback = f;
+        a_request_processed_callback = f;
     }
 
     
@@ -181,13 +181,13 @@ function jripl() {
    directives being received by Venture.
  */
     this.register_all_requests_processed_callback = function(f) {
-	if (request_in_progressQ || request_queue.length > 0) {
-	    all_requests_processed_callback = f;
-	}
-	else {
-	    f();
-	    all_requests_process_callback = function() {};
-	};
+        if (request_in_progressQ || request_queue.length > 0) {
+            all_requests_processed_callback = f;
+        }
+        else {
+            f();
+            all_requests_process_callback = function() {};
+        };
     };
 
 
