6098bb537b993759a2d87a381ee332e31d451e77
vlad <vlad@asus.home>
2013-07-30 19:14:36 -0400

    Added debugging to run_server shortcut.

--- script/run_server.py
+++ script/run_server.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from venture.shortcuts import *
 
-
 server = make_ripl_rest_server()
-server.run(host='0.0.0.0', port=8082)
+server.run(host='0.0.0.0', port=8082, debug=True, use_reloader=False)
