33e421f648621fac05e3cfb8ba30871be758ab6d
Vlad Firoiu <vladfi2@gmail.com>
2013-10-05 22:14:21 +0000

    Port can now be passed to script/run_server.py

--- script/run_server.py
+++ script/run_server.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import sys
 from venture.shortcuts import *
 
+port = 8082
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+
 server = make_ripl_rest_server()
-server.run(host='0.0.0.0', port=8082, debug=True, use_reloader=False)
+server.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
