e88dd0dbe95b9f246c6f53674fb3a2ba3a8af679
dlovell <dlovell@gmail.com>
2013-11-05 21:13:03 -0500

    Don't delete sp's of a specific typeid.name which (currently) correspond to python sps

--- backend/cxx/src/value.cxx
+++ backend/cxx/src/value.cxx
@@ -1,9 +1,20 @@
 #include "value.h"
 #include "sp.h"
+#include <typeinfo>
+
+
+std::string ignore_typeid_name = "2SP";
+
 
 VentureSP::~VentureSP() 
 { 
-  delete sp; 
+  std::string this_typeid_name = (std::string) typeid(*sp).name();
+  if(this_typeid_name == ignore_typeid_name) {
+    // std::cout << "VentureSP::~VentureSP : NOT deleting sp named " << this_typeid_name << std::endl;
+  } else {
+    // std::cout << "VentureSP::~VentureSP : DELETING sp named " << this_typeid_name << std::endl;
+    delete sp;
+  }
 }
 
 
