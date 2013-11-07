97227ffab2963cc7793094af3edb5d1cbd004419
Vikash Mansinghka <vkmvkmvkmvkm@gmail.com>
2013-11-07 06:21:06 +0000

    added FIXME to list_known_issues; not found for some reason

--- list_known_issues.sh
+++ list_known_issues.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-grep -r FIXME . | grep -v site-packages | grep -v Eigen | grep -v include | grep -v fish | grep -v build
+# FIXME improve greps; include in particular seems dangerous; also why isn't this FIXME being found?
+
+grep -r FIXME . | grep -v site-packages | grep -v Eigen | grep -v include | grep -v \.fish | grep -v build | grep -v git
