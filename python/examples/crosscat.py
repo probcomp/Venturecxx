39c28a95a0fd9a29f7573ba9666b0054ce186d09
dlovell <dlovell@gmail.com>
2013-11-07 20:25:53 -0500

    use parameters: set #values, programmatically generate observes

--- python/examples/crosscat.py
+++ python/examples/crosscat.py
@@ -3,7 +3,7 @@ from venture import shortcuts
 
 def makeAssumes(my_ripl, parameters):
     my_ripl.assume("get_component_hyperparameter", "(mem (lambda (col) (gamma 1.0 1.0) ))")
-    my_ripl.assume("get_component_model", "(mem (lambda (col category) (make_sym_dir_mult (get_component_hyperparameter col) 2) ))")
+    my_ripl.assume("get_component_model", "(mem (lambda (col category) (make_sym_dir_mult (get_component_hyperparameter col) %d) ))" % parameters['n_values'])
     my_ripl.assume("view_crp_hyperparameter", "(gamma 1.0 1.0)")
     my_ripl.assume("view_crp", "(make_crp view_crp_hyperparameter)")
     my_ripl.assume("get_view", "(mem (lambda (col) (view_crp) ))")
@@ -15,48 +15,16 @@ def makeAssumes(my_ripl, parameters):
 
 
 def makeObserves(my_ripl, parameters):
-    my_ripl.observe("(get_cell 0 0)", "atom<1>")
-    my_ripl.observe("(get_cell 0 1)", "atom<1>")
-    my_ripl.observe("(get_cell 0 2)", "atom<1>")
-    my_ripl.observe("(get_cell 0 3)", "atom<1>")
-    my_ripl.observe("(get_cell 0 4)", "atom<1>")
-    my_ripl.observe("(get_cell 0 5)", "atom<1>")
-    my_ripl.observe("(get_cell 1 0)", "atom<1>")
-    my_ripl.observe("(get_cell 1 1)", "atom<1>")
-    my_ripl.observe("(get_cell 1 2)", "atom<1>")
-    my_ripl.observe("(get_cell 1 3)", "atom<1>")
-    my_ripl.observe("(get_cell 1 4)", "atom<1>")
-    my_ripl.observe("(get_cell 1 5)", "atom<1>")
-    my_ripl.observe("(get_cell 2 0)", "atom<1>")
-    my_ripl.observe("(get_cell 2 1)", "atom<1>")
-    my_ripl.observe("(get_cell 2 2)", "atom<1>")
-    my_ripl.observe("(get_cell 2 3)", "atom<1>")
-    my_ripl.observe("(get_cell 2 4)", "atom<1>")
-    my_ripl.observe("(get_cell 2 5)", "atom<1>")
-    my_ripl.observe("(get_cell 3 0)", "atom<0>")
-    my_ripl.observe("(get_cell 3 1)", "atom<0>")
-    my_ripl.observe("(get_cell 3 2)", "atom<0>")
-    my_ripl.observe("(get_cell 3 3)", "atom<0>")
-    my_ripl.observe("(get_cell 3 4)", "atom<0>")
-    my_ripl.observe("(get_cell 3 5)", "atom<0>")
-    my_ripl.observe("(get_cell 4 0)", "atom<0>")
-    my_ripl.observe("(get_cell 4 1)", "atom<0>")
-    my_ripl.observe("(get_cell 4 2)", "atom<0>")
-    my_ripl.observe("(get_cell 4 3)", "atom<0>")
-    my_ripl.observe("(get_cell 4 4)", "atom<0>")
-    my_ripl.observe("(get_cell 4 5)", "atom<0>")
-    my_ripl.observe("(get_cell 5 0)", "atom<0>")
-    my_ripl.observe("(get_cell 5 1)", "atom<0>")
-    my_ripl.observe("(get_cell 5 2)", "atom<0>")
-    my_ripl.observe("(get_cell 5 3)", "atom<0>")
-    my_ripl.observe("(get_cell 5 4)", "atom<0>")
-    my_ripl.observe("(get_cell 5 5)", "atom<0>")
+    for r in xrange(parameters['n_rows']):
+        for c in xrange(parameters['n_columns']):
+            cell_value = r >= (parameters['n_rows'] / 2)
+            my_ripl.observe("(get_cell %d %d)" % (r, c), "atom<%d>" % cell_value)
     return
 
 
 def main():
     my_ripl = shortcuts.make_church_prime_ripl() 
-    parameters = {}
+    parameters = {'n_values': 2, 'n_rows': 6, 'n_columns': 6}
     makeAssumes(my_ripl, parameters)
     makeObserves(my_ripl, parameters)
     my_ripl.infer({'transitions': 10})
