ffe5cc62cba968b4b007e5481094f6e975dbc8c6
dlovell <dlovell@gmail.com>
2013-11-11 00:22:25 -0500

    demonstrate multiprocessing

--- python/vmodule/crosscat_asymptotics.py
+++ python/vmodule/crosscat_asymptotics.py
@@ -1,11 +1,16 @@
+import multiprocessing
+#
 from venture import shortcuts
 from venture.vmodule.crosscat_unit import CrossCat
 from venture.vmodule.venture_unit import *
 
 
+parameters = {'n_values': 2, 'n_rows': [8, 16, 32], 'n_columns': [8, 16, 32]}
+cpu_count = multiprocessing.cpu_count()
+
+
 ripl = shortcuts.make_church_prime_ripl()
-parameters = {'n_values': 2, 'n_rows': [16, 32, 64], 'n_columns': [16, 32, 64]}
 def runner(params):
     return CrossCat(ripl, params).runConditionedFromPrior(sweeps=20, runs=2)
-histories = produceHistories(parameters, runner)
+histories = produceHistories(parameters, runner, cpu_count=cpu_count)
 plotAsymptotics(parameters, histories, 'sweep_time', fmt='png', aggregate=True)
