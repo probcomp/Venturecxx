25ff91664fb4ef9043df8d82bba12f8167947c40
Daniel Selsam <daniel.selsam@gmail.com>
2013-11-14 19:40:17 -0500

    Draft of particle.h

--- backend/cxx/inc/particle.h
+++ backend/cxx/inc/particle.h
@@ -3,38 +3,45 @@
 
 struct Particle
 {
-  /* OLD, now we leave the topology of the old trace alone, so that it is easier to 
-     walk it during regen. */
-     
   /* { OldNode => NewNode }
      This is new for this particle, 
      and lookup is environment-lookup semantics. 
-     For RHO, this is the identity on D but we actually copy A, for symmetry.
-     Since we disconnect RHO from the trace on the other end as well, no particle
-     fills the torus during the proposal. */
+     For RHO, this is the identity on D and A. */
   map<Node*,Node*> newNodes;
 
   /* { makerNode => newSPAux } 
      Cloned from parent particle */
   map<Node*,SPAux*> spauxs;
 
-  /* { newNode => (old/new) node that has newNode as child,
+  /* { nodeInR => (old/new) node that has newNode as child,
                   but whose children do not include it yet. } 
      New for this particle, but we iterate over ancestors when
      actually adding these missing edges. 
-     Note: for simplicity, we actually use this same structure
-           for RHO as well, since we disconnect children of the parents
-	   of nodes in R(rho) during detach, so that no nodes in RHO or XI
-	   are pointed to by the parents.
-   */
-  map<Node*,set<Node*> > missingChildren; // for XI, from D => parents in I
-  map<Node*,set<Node*> > missingParents; // for XI, from D => children in A
+     This consists of two types of mappings:
+     For RHO, we add to this whenever we remove an ESR edge or disconnect a lookup.
+              in both cases, we only remove the CHILD, not the PARENT
+	      also, we do not disconnect regular parents of nodes in D.
+     For XI, we add to this whenever we add parents to a new node, which includes ESRs,
+             lookups, and regular parents.
+  */
+  map<Node*,set<Node*> > missingChildren;
 
-  map<Node*,set<Node*> > extraChildren; // for RHO only, for parents in I => D
+  /* { oldAbsorbiNode => newNode }, only applies to XI. */
+  map<Node*,set<Node*> > missingParents;
+
+  /* { parent => oldRhoNode that would need to be removed from children. }
+     We only need to do this for parents in I, but it may be easier to do it for
+     all parents, in D as well. */
+  map<Node*,set<Node*> > extraChildren;
+
+  /* { oldAbsorbiNode => oldNode } */
   map<Node*,set<Node*> > extraParents; // for RHO only, for A => parents in D
 
-  /* Note: missing parents will be handled by inspecting the parents of
-     the NEW absorbing nodes in a given particle. */
+  
+  vector<Node *> unconstrainedRandomChoices;
+  /* Set, because we do not add to unconstrainedRandomChoices if it is constrained,
+     and unconstrain visits first. */
+  set<Node *> constrainedChoices;
 
   Particle * parentParticle;
 };
