24bb7e078863c079d68d4b54230cf2f7fb664ad7
Alexey Radul <axch@mit.edu>
2013-11-26 15:20:54 -0500

    Expand the command language that the console understands.

--- python/lib/ripl/utils.py
+++ python/lib/ripl/utils.py
@@ -38,7 +38,8 @@ _RIPL_FUNCTIONS = [
         ]
 
 def run_venture_console(ripl):
-  while True:
+  done = False
+  while not(done):
     sys.stdout.write('>>> ')
     current_line = sys.stdin.readline()
     current_line = current_line.strip()
@@ -49,31 +50,47 @@ def run_venture_console(ripl):
       current_line = current_line[1:-1]
     current_line = current_line.split(" ", 1)
     directive_name = current_line[0].lower()
-    content = current_line[1]
     sys.stdout.write('')
     try:
-      if (directive_name == "assume"):
-        name_and_expression = content.split(" ", 1)
-        print ripl.assume(name_and_expression[0], name_and_expression[1])
-      elif (directive_name == "predict"):
-        name_and_expression = content.split(" ", 1)
-        print ripl.predict(content)
-      elif (directive_name == "observe"):
-        expression_and_literal_value = content.rsplit(" ", 1)
-        print ripl.observe(expression_and_literal_value[0], expression_and_literal_value[1])
-      elif (directive_name == "infer"):
-        ripl.infer(int(content))
-        print "The engine has made number of inference iterations: " + content
-      elif (directive_name == "forget"):
-        ripl.forget(int(content))
-        print "You have forgotten the directive #" + content
-      elif (directive_name == "report"):
-        print ripl.report_value(int(content))
-      elif (directive_name == "clear"):
-        ripl.clear()
-        print "The trace has been cleared."
+      if (directive_name == "quit"):
+        print "Moriturus te saluto."
+        done = True
+      elif (directive_name == "list-directives"):
+        print ripl.list_directives()
+      elif (directive_name == "get-global-log-score"):
+        print ripl.get_global_logscore()
+      elif (directive_name == "continuous-inference-status"):
+        print ripl.continuous_inference_status()
+      elif (directive_name == "start-continuous-inference"):
+        ripl.start_continuous_inference()
+        print ripl.continuous_inference_status()
+      elif (directive_name == "stop-continuous-inference"):
+        ripl.stop_continuous_inference()
+        print ripl.continuous_inference_status()
       else:
-        print "Sorry, unknown directive."
+        content = current_line[1]
+        if (directive_name == "assume"):
+          name_and_expression = content.split(" ", 1)
+          print ripl.assume(name_and_expression[0], name_and_expression[1])
+        elif (directive_name == "predict"):
+          name_and_expression = content.split(" ", 1)
+          print ripl.predict(content)
+        elif (directive_name == "observe"):
+          expression_and_literal_value = content.rsplit(" ", 1)
+          print ripl.observe(expression_and_literal_value[0], expression_and_literal_value[1])
+        elif (directive_name == "infer"):
+          ripl.infer(int(content))
+          print "The engine has made number of inference iterations: " + content
+        elif (directive_name == "forget"):
+          ripl.forget(int(content))
+          print "You have forgotten the directive #" + content
+        elif (directive_name == "report"):
+          print ripl.report_value(int(content))
+        elif (directive_name == "clear"):
+          ripl.clear()
+          print "The trace has been cleared."
+        else:
+          print "Sorry, unknown directive."
     except Exception, err:
       print "Your query has generated an error: " + str(err)
 
