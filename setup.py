620f02645b8185aab410f044401ee5fc47414211
Alexey Radul <axch@mit.edu>
2013-11-26 14:23:55 -0500

    Install the "venture" program into a globally callable place.

--- setup.py
+++ setup.py
@@ -106,5 +106,6 @@ setup (
     package_dir={"venture":"python/lib/", "venture.test":"python/test/",
         "venture.cxx":"backend/cxx/", "venture.examples":"python/examples/",
         "venture.vmodule":"python/vmodule"},
-    ext_modules = ext_modules
+    ext_modules = ext_modules,
+    scripts = ['script/venture']
 )
