128570e6ade506fdc8e389b15a00705753d333a5
Daniel Selsam <daniel.selsam@gmail.com>
2013-11-26 17:58:35 -0500

    Sprinkler looks great as well.

--- backend/cxx/render_example.py
+++ backend/cxx/render_example.py
@@ -37,5 +37,53 @@ def renderTrickCoin():
 
   renderRIPL(ripl,"graphs/trickycoin2")
 
+def renderSprinkler():
+  ripl = RIPL()
+  ripl.assume("rain","(bernoulli 0.2)")
+  ripl.assume("sprinkler","""
+(tabular_cpt (make_vector rain) 
+  (make_map (list true false) (list (make_vector 0.01 0.99) (make_vector 0.4 0.6)))
+)
+""")
+  ripl.assume("grassWet","""
+(tabular_cpt (make_vector rain sprinkler)
+  (make_map (list (make_vector true true) (make_vector true false) (make_vector false true) (make_vector false false))
+            (list (make_vector 0.99 0.01) (make_vector 0.8 0.2) (make_vector 0.9 0.1) (make_vector 0.00001 0.99999))))
+""")
+
+  sivm.observe("grassWet","true")
+
+  renderRIPL(ripl,"graphs/sprinkler")
+
+
 
  
+def renderSprinkler2():
+  ripl = RIPL()
+  ripl.assume("rain","(bernoulli 0.2)")
+  ripl.assume("sprinkler","(bernoulli (branch_exp rain 0.01 0.4))")
+  ripl.assume("grassWet","""
+(bernoulli (branch_exp rain 
+(branch_exp sprinkler 0.99 0.8)
+(branch_exp sprinkler 0.9 0.00001)))
+""")
+  ripl.observe("grassWet", "true")
+  renderRIPL(ripl,"graphs/sprinkler")
+
+def renderPYMem():
+  ripl = RIPL()
+  ripl.assume("pymem","""
+(lambda (alpha d base_dist)
+  ((lambda (augmented_proc crp)
+     (lambda () (augmented_proc (crp))))
+   (mem (lambda (table) (base_dist)))
+   (make_crp alpha d)))
+""")
+
+  ripl.assume("base_dist","(lambda () (normal 0.0 1.0))")
+
+  ripl.assume("f","(pymem 1.0 0.0 base_dist)")
+  ripl.predict("(f)")
+  ripl.predict("(f)")
+#  ripl.predict("(f)")
+  renderRIPL(ripl,"graphs/pymem")
