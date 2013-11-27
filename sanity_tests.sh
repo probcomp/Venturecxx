dfdcd26147943f49de3e39b397618bf449a53239
Alexey Radul <axch@mit.edu>
2013-11-27 13:26:43 -0500

    Sanity tests should check the venture command.

--- sanity_tests.sh
+++ sanity_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Reset the working directory to the script's path
 my_abs_path=$(readlink -f "$0")
@@ -21,3 +21,7 @@ cd ../..
 echo FIXME test reporting causes some runtime errors to be missed, so watch output carefully
 python -m unittest discover python/test/ "*.py"
 abort_on_error "periphery self-checking"
+
+set -o pipefail # Preserve exit status of venture, per http://stackoverflow.com/questions/6871859/piping-command-output-to-tee-but-also-save-exit-code-of-command
+echo "assume x (uniform_continuous 0.0 0.9)" | venture | grep '>>> 0.'
+abort_on_error "executing the venture command"
