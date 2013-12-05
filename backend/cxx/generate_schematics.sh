f87b03dbec176d88b1fd91a587e417cd62594d27
Daniel Selsam <daniel.selsam@gmail.com>
2013-12-05 16:36:47 -0500

    Added a script to generate schematics

---
+++ backend/cxx/generate_schematics.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Reset the working directory to the script's path
+
+# Actually run the tests
+python -c "import render_example as r; r.renderAllSchematics()"
+cd pdf_graphs
+pdflatex schematics.tex
+cd ..
+cp pdf_graphs/schematics.pdf schematics.pdf
+echo "See schematics.pdf"
