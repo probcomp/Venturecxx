e72cb7f06d9b8c9abb02290c470b18bc9cf46e37
Alexey Radul <axch@mit.edu>
2013-12-06 11:32:08 -0500

    I think I want a lens on a map at an index that errors out if the map doesn't have that key.

--- backend/hs/Utils.hs
+++ backend/hs/Utils.hs
@@ -1,5 +1,8 @@
 module Utils (module Utils, module Unique) where
 
+import Data.Map as M
+import Control.Lens
+
 import Unique
 
 fromJust :: String -> Maybe a -> a
@@ -11,3 +14,11 @@ mapFst f (a,c) = (f a, c)
 
 mapSnd :: (a -> b) -> (c,a) -> (c,b)
 mapSnd f (c,a) = (c, f a)
+
+-- A version of the ix lens that errors out if the value is not there
+hardix :: (Ord k) => String -> k -> Simple Lens (M.Map k a) a
+hardix msg k = lens find replace where
+    find = fromJust msg . M.lookup k
+    replace m a = case M.lookup k m of
+                    (Just _) -> M.insert k a m
+                    Nothing -> error msg
