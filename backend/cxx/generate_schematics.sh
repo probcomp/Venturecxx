6563637c88ddf2982ae06f2e85c195d55e53795e
Daniel Selsam <daniel.selsam@gmail.com>
2013-12-05 20:05:12 -0500

    going to start playing with layouts

--- backend/cxx/generate_schematics.sh
+++ backend/cxx/generate_schematics.sh
@@ -6,6 +6,8 @@
 python -c "import render_example as r; r.renderAllSchematics()"
 cd pdf_graphs
 pdflatex schematics.tex
+pdflatex schematics.tex
+pdflatex schematics.tex
 cd ..
 cp pdf_graphs/schematics.pdf schematics.pdf
 echo "See schematics.pdf"
