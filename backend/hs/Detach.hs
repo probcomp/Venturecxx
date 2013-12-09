9d5c34da7fff75f6d531ab45f5265259f17864e4
Alexey Radul <axch@mit.edu>
2013-12-09 18:44:51 -0500

    Explicitly checking whether the operator is in the DRG is now redundant with canAbsorb knowing the changed parent.

--- backend/hs/Detach.hs
+++ backend/hs/Detach.hs
@@ -58,16 +58,10 @@ collectERG ((a,drg_parent):as) = do
       _ -> case drg_parent of
              Nothing -> resampling a -- principal node
              (Just p_addr) -> do
-               -- TODO Is this operator-drg check redundant with canAbsorb knowing the parent address?
-               let opa = fromJust "DRGing application node with no operator" $ opAddr node
-               -- N.B. This can change as more graph structure is traversed
-               opMember <- uses drg $ O.member opa
-               if opMember then resampling a
-               else do
-                 opCanAbsorb <- lift $ asks $ (canAbsorb node p_addr)
-                                . fromJust "DRGing application node with no operator" . operator node
-                 if opCanAbsorb then absorbing a
-                 else resampling a
+               opCanAbsorb <- lift $ asks $ (canAbsorb node p_addr)
+                              . fromJust "DRGing application node with no operator" . operator node
+               if opCanAbsorb then absorbing a
+               else resampling a
   where resampling :: Address -> StateT Scaffold (Reader (Trace m)) ()
         resampling a = do
           absorbers %= O.delete a
