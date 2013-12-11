4ed575641c2860797c59d64a3362e16a0f04f27c
Alexey Radul <axch@mit.edu>
2013-12-11 14:18:46 -0500

    Underload constructor.

--- backend/lite/env.py
+++ backend/lite/env.py
@@ -1,16 +1,8 @@
 class Env():
-  def __init__(self):
-    self.outerEnv = None
-    self.frame = {}
-
-  def __init__(self,outerEnv):
+  def __init__(self,outerEnv=None,ids=None,nodes=None):
     self.outerEnv = outerEnv
     self.frame = {}
-
-  def __init__(self,outerEnv,ids,nodes):
-    self.outerEnv = outerEnv
-    self.frame = {}
-    self.update(zip(ids,nodes))
+    if ids and nodes: self.frame.update(zip(ids,nodes))
 
   def addBinding(self,sym,val):
     assert not sym in self.frame
