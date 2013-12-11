82cb878798941fc0bddb88627faf0c0deef25dc9
Alexey Radul <axch@mit.edu>
2013-12-11 16:35:46 -0500

    Right.  Make instances of the classes, do not bind the classes themselves in the environment.

--- backend/lite/builtin.py
+++ backend/lite/builtin.py
@@ -11,13 +11,13 @@ import conditionals
 def builtInValues(): return { "true" : True, "false" : False }
 
 def builtInSPs():
-  return { "plus" : SP(NullRequestPSP,number.PlusOutputPSP),
-           "minus" : SP(NullRequestPSP,number.MinusOutputPSP),
-           "bernoulli" : SP(NullRequestPSP,discrete.BernoulliOutputPSP),
-           "normal" : SP(NullRequestPSP,continuous.NormalOutputPSP),
-           "branch" : SP(conditionals.BranchRequestPSP,ESRRefOutputPSP),
-           "make_csp" : SP(NullRequestPSP,csp.MakeCSPOutputPSP),
-           "make_msp" : SP(NullRequestPSP,msp.MakeMSPOutputPSP),
+  return { "plus" : SP(NullRequestPSP(),number.PlusOutputPSP()),
+           "minus" : SP(NullRequestPSP(),number.MinusOutputPSP()),
+           "bernoulli" : SP(NullRequestPSP(),discrete.BernoulliOutputPSP()),
+           "normal" : SP(NullRequestPSP(),continuous.NormalOutputPSP()),
+           "branch" : SP(conditionals.BranchRequestPSP(),ESRRefOutputPSP()),
+           "make_csp" : SP(NullRequestPSP(),csp.MakeCSPOutputPSP()),
+           "make_msp" : SP(NullRequestPSP(),msp.MakeMSPOutputPSP()),
   }
 
 
