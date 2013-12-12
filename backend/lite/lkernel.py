368d6dcdca998a6bf7cf933e01410cc190f2c0c9
Daniel Selsam <daniel.selsam@gmail.com>
2013-12-12 12:13:22 -0500

    Draft of lkernel interface.
    Also:
    
    DefaultAAALKernel
    DeterministicLKernel

---
+++ backend/lite/lkernel.py
@@ -0,0 +1,29 @@
+from abc import ABCMeta, abstractmethod
+
+class LKernel():
+  __metaclass__ = ABCMeta
+
+  @abstractmethod
+  def simulate(self,oldValue,args,latentDB): pass
+
+  def weight(self,newValue,oldValue,args,latentDB): return 0
+
+  def reverseWeight(self,oldValue,args,latentDB): 
+    return self.weight(oldValue,None,args,latentDB)
+
+class DefaultAAALKernel(LKernel):
+  def __init__(self,makerPSP): self.makerPSP = makerPSP
+
+  def simulate(self,oldValue,args,latentDB): return self.makerPSP.simulate(args)
+    
+  def weight(self,newValue,oldValue,args,latentDB):
+    assert isinstance(newValue,SP)
+    return newValue.logDensityOfCounts(args.madeSPAux)
+
+class DeterministicLKernel(LKernel):
+  def __init__(self,value,sp): 
+    self.value = value
+    self.sp = sp
+
+  def simulate(self,oldValue,args,latentDB): return self.value
+  def weight(self,newValue,oldValue,args,latentDB): return self.sp.logDensity(newValue,args)
