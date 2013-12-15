739678eeb44bf84bbf7a95b7c0b1d533a35bac07
Alexey Radul <axch@mit.edu>
2013-12-15 00:12:26 -0500

    Clean up the test example a little more.

--- backend/hs/VentureTest.hs
+++ backend/hs/VentureTest.hs
@@ -5,10 +5,10 @@ import Venture
 
 main = runTestTT $ test
   [ venture_main 1 [Predict 1] >>= (@?= [1])
-  , venture_main 1 [Predict $ App (Lam ["x"] (Var "x")) [1]] >>= (@?= [1])
-  -- (let (id ...) (id 1))
-  , venture_main 1 [Predict $ v_let1 "id" (Lam ["x"] (Var "x")) (App (Var "id") [1])] >>= (@?= [1])
+  , venture_main 1 [Predict $ App v_id [1]] >>= (@?= [1])
+  , venture_main 1 [Predict $ v_let1 "id" v_id (App (Var "id") [1])] >>= (@?= [1])
   -- K combinator
-  , venture_main 1 [Predict $ App (App k [1]) [2]] >>= (@?= [1])
+  , venture_main 1 [Predict $ App (App v_k [1]) [2]] >>= (@?= [1])
   ]
-    where k = (Lam ["x"] (Lam ["y"] (Var "x")))
+    where v_id = (Lam ["x"] (Var "x"))
+          v_k = (Lam ["x"] (Lam ["y"] (Var "x")))
