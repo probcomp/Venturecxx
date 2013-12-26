c7e5b199805a95cfeaed29f3202d1eaf78a37796
Alexey Radul <axch@mit.edu>
2013-12-26 16:52:57 -0500

    We need some help with the code style.

--- examples/crosscat.py
+++ examples/crosscat.py
@@ -19,17 +19,27 @@ from venture.unit import *
 
 class CrossCat(VentureUnit):
   def makeAssumes(self):
-    self.assume("get_component_hyperparameter", "(mem (lambda (col) (gamma 1.0 1.0) ))")
-    self.assume("get_component_model", "(mem (lambda (col category) (make_sym_dir_mult (get_component_hyperparameter col) %d) ))" % self.parameters['n_values'])
+    self.assume("get_component_hyperparameter",
+                "(mem (lambda (col) (gamma 1.0 1.0)))")
+    self.assume("get_component_model", """
+(mem (lambda (col category)
+       (make_sym_dir_mult (get_component_hyperparameter col) %d)))
+""" % self.parameters['n_values'])
     self.assume("view_crp_hyperparameter", "(gamma 1.0 1.0)")
     self.assume("view_crp", "(make_crp view_crp_hyperparameter)")
-    self.assume("get_view", "(mem (lambda (col) (view_crp) ))")
-    self.assume("get_categorization_crp_hyperparameter", "(mem (lambda (view) (gamma 1.0 1.0) ))")
-    self.assume("get_categorization_crp", "(mem (lambda (view) (make_crp (get_categorization_crp_hyperparameter view)) ))")
-    self.assume("get_category", "(mem (lambda (view row) ( (get_categorization_crp view) ) ))")
-    self.assume("get_cell", "(mem (lambda (row col) ( (get_component_model col (get_category (get_view col) row)) ) ))")
+    self.assume("get_view", "(mem (lambda (col) (view_crp)))")
+    self.assume("get_categorization_crp_hyperparameter",
+                "(mem (lambda (view) (gamma 1.0 1.0)))")
+    self.assume("get_categorization_crp", """
+(mem (lambda (view)
+       (make_crp (get_categorization_crp_hyperparameter view))))""")
+    self.assume("get_category", """
+(mem (lambda (view row)
+       ((get_categorization_crp view))))""")
+    self.assume("get_cell", """
+(mem (lambda (row col)
+       ((get_component_model col (get_category (get_view col) row)))))""")
     return
-
 
   def makeObserves(self):
     for r in range(self.parameters['n_rows']):
@@ -37,7 +47,6 @@ class CrossCat(VentureUnit):
         cell_value = r >= (self.parameters['n_rows'] / 2)
         self.observe("(get_cell %d %d)" % (r, c), "atom<%d>" % cell_value)
     return
-
 
 if __name__ == '__main__':
   ripl = shortcuts.make_church_prime_ripl()
