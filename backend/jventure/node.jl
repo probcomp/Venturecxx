6d93cf53ed9b00cc8c7ae76ceb98ee025df100d2
Daniel Selsam <daniel.selsam@gmail.com>
2014-01-13 18:39:53 -0500

    Override all show-related methods on nodes to avoid infinite loops.

--- backend/jventure/node.jl
+++ backend/jventure/node.jl
@@ -47,3 +47,10 @@ end
 OutputNode(requestNode::RequestNode) = OutputNode(requestNode,requestNode.env,Array(Node,0),nothing,0,false)
 
 
+### Printing
+import Base.print
+import Base.show
+Base.print(node::Node) = print("<node>")
+Base.show(io::IO,node::Node) = show("<node>")
+Base.show(node::Node) = show("<node>")
+Base.showcompact(node::Node) = show("<node>")
