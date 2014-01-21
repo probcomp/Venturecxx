c7f68493fa9a3f1240a42130039674ad532657c8
Daniel Selsam <daniel.selsam@gmail.com>
2014-01-21 10:42:07 -0500

    Finished splaying wstests.
    None of the tests run because we need to sort out parameters and
    globals and such.

---
+++ tests/language_features/aux/sivm.py
@@ -0,0 +1,27 @@
+from venture.shortcuts import *
+from stat_helpers import *
+from test_globals import N, globalKernel
+
+def RIPL(): return make_lite_church_prime_ripl()
+
+
+def testForget1():
+  ripl = RIPL()
+
+  ripl.assume("x","(normal 0.0 1.0)")
+  ripl.assume("f","(lambda (y) (normal y 1.0))")
+  ripl.assume("g","(lambda (z) (normal z 2.0))")
+
+  ripl.predict("(f 1.0)",label="id1")
+  ripl.observe("(g 2.0)",3.0,label="id2")
+  ripl.observe("(g 3.0)",3.0,label="id3")
+
+  ripl.forget("id1")
+  ripl.forget("id2")
+  ripl.forget("id3")
+
+  # TODO this line is completely unforgiveable
+  real_sivm = ripl.sivm.core_sivm.engine
+  assert real_sivm.get_entropy_info()["unconstrained_random_choices"] == 1
+  assert real_sivm.logscore() < 0
+  return reportPassage("TestForget1")
