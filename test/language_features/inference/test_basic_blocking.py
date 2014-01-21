ebc40f0f4445ef7b111cf9e2b6aee3b86e8f6f0a
Daniel Selsam <daniel.selsam@gmail.com>
2014-01-21 17:14:49 -0500

    Sketch of inference features.

--- test/language_features/inference/test_basic_blocking.py
+++ test/language_features/inference/test_basic_blocking.py
@@ -1,30 +1,28 @@
-from venture.shortcuts import *
-from stat_helpers import *
-from test_globals import N, globalKernel
+from venture.test.stats import *
+from testconfig import config
 
-def RIPL(): return make_lite_church_prime_ripl()
+def testBlockingExample0():
+  N = config["num_samples"]
+  ripl = config["get_ripl"]()
 
-
-def testBlockingExample0(N):
-  ripl = RIPL()
   ripl.assume("a", "(scope_include 0 0 (normal 10.0 1.0))")
   ripl.assume("b", "(scope_include 1 1 (normal a 1.0))")
   ripl.observe("(normal b 1.0)", 14.0)
 
   # If inference only frobnicates b, then the distribution on a
   # remains the prior.
-  predictions = collectSamplesWith(ripl,1,N,{"transitions":10,"kernel":globalKernel,"scope":1,"block":1})
+  predictions = collectSamplesWith(ripl,1,N,{"transitions":10,"kernel":"mh","scope":1,"block":1})
   cdf = stats.norm(loc=10.0, scale=1.0).cdf
   return reportKnownContinuous("testBlockingExample0", cdf, predictions, "N(10.0,1.0)")
 
 def testBlockingExample1():
-  ripl = RIPL()
+  ripl = config["get_ripl"]()
   ripl.assume("a", "(scope_include 0 0 (normal 0.0 1.0))")
   ripl.assume("b", "(scope_include 0 0 (normal 1.0 1.0))")
   olda = ripl.report(1)
   oldb = ripl.report(2)
   # The point of block proposals is that both things change at once.
-  ripl.sivm.core_sivm.engine.infer({"transitions":1, "kernel":globalKernel, "scope":0, "block":0})
+  ripl.sivm.core_sivm.engine.infer({"transitions":1, "kernel":"mh", "scope":0, "block":0})
   newa = ripl.report(1)
   newb = ripl.report(2)
   assert not(olda == newa)
@@ -32,7 +30,7 @@ def testBlockingExample1():
   return reportPassage("testBlockingExample1")
 
 def testBlockingExample2():
-  ripl = RIPL()
+  ripl = config["get_ripl"]()
   ripl.assume("a", "(scope_include 0 0 (normal 0.0 1.0))")
   ripl.assume("b", "(scope_include 0 0 (normal 1.0 1.0))")
   ripl.assume("c", "(scope_include 0 1 (normal 2.0 1.0))")
@@ -42,7 +40,7 @@ def testBlockingExample2():
   oldc = ripl.report(3)
   oldd = ripl.report(4)
   # Should change everything in one or the other block
-  ripl.sivm.core_sivm.engine.infer({"transitions":1, "kernel":globalKernel, "scope":0, "block":"one"})
+  ripl.sivm.core_sivm.engine.infer({"transitions":1, "kernel":"mh", "scope":0, "block":"one"})
   newa = ripl.report(1)
   newb = ripl.report(2)
   newc = ripl.report(3)
@@ -58,13 +56,13 @@ def testBlockingExample2():
   return reportPassage("testBlockingExample2")
 
 def testBlockingExample3():
-  ripl = RIPL()
+  ripl = config["get_ripl"]()
   ripl.assume("a", "(scope_include 0 0 (normal 0.0 1.0))")
   ripl.assume("b", "(scope_include 0 1 (normal 1.0 1.0))")
   olda = ripl.report(1)
   oldb = ripl.report(2)
   # The point of block proposals is that both things change at once.
-  ripl.sivm.core_sivm.engine.infer({"transitions":1, "kernel":globalKernel, "scope":0, "block":"all"})
+  ripl.sivm.core_sivm.engine.infer({"transitions":1, "kernel":"mh", "scope":0, "block":"all"})
   newa = ripl.report(1)
   newb = ripl.report(2)
   assert not(olda == newa)
--- test/language_features/inference/test_pgibbs.py
+++ test/language_features/inference/test_pgibbs.py
@@ -1,13 +1,11 @@
-from venture.shortcuts import *
-from stat_helpers import *
-from test_globals import N, globalKernel
-
-def RIPL(): return make_lite_church_prime_ripl()
+from venture.test.stats import *
+from testconfig import config
 
 # TODO N needs to be managed here more intelligently
-def testPGibbsBlockingMHHMM1(N):
+def testPGibbsBlockingMHHMM1():
   """The point of this is that it should give reasonable results in very few transitions but with a large number of particles."""
-  ripl = RIPL()
+  N = config["num_samples"]
+  ripl = config["get_ripl"]()
 
   ripl.assume("x0","(scope_include 0 0 (normal 0.0 1.0))")
   ripl.assume("x1","(scope_include 0 1 (normal x0 1.0))")
@@ -26,9 +24,9 @@ def testPGibbsBlockingMHHMM1(N):
   ripl.observe("y2",3.0)
   ripl.observe("y3",4.0)
   ripl.observe("y4",5.0)
-  ripl.predict("x4")
+  ripl.predict("x4",label="pid")
 
-  predictions = collectSamplesWith(ripl,16,N,{"kernel":"pgibbs","transitions":10,"scope":0,"block":"ordered"})
+  predictions = collectSamplesWith(ripl,"pid",N,{"kernel":"pgibbs","transitions":10,"scope":0,"block":"ordered"})
   reportKnownMeanVariance("TestPGibbsBlockingMHHMM1", 390/89.0, 55/89.0, predictions)
   cdf = stats.norm(loc=390/89.0, scale=math.sqrt(55/89.0)).cdf
   return reportKnownContinuous("TestPGibbsBlockingMHHMM1", cdf, predictions, "N(4.382, 0.786)")
