581650891ba0f8792074fba5f29cda842703b908
Daniel Selsam <daniel.selsam@gmail.com>
2014-01-21 16:12:12 -0500

    Sketch of easy hmm particle gibbs asymptotic test.

---
+++ test/performance/asymptotics/test_particles.py
@@ -0,0 +1,50 @@
+from venture.test.stats import *
+from testconfig import config
+import scipy.stats
+
+def loadHMMParticleAsymptoticProgram1(M):
+  """Easiest possible HMM asymptotic test for particles"""
+  N = config["num_samples"]
+  ripl = config["get_ripl"]()
+
+  ripl.assume("f","""
+(mem (lambda (i)
+  (if (eq i 0)
+    (scope_include (quote states) 0 (normal 0.0 1.0))
+    (scope_include (quote states) i (normal (f (minus i 1)) 1.0)))))
+""")
+  ripl.assume("g","""
+(mem (lambda (i)
+  (normal (f i) 1.0)))
+""")
+
+  previousValue = 0.0
+  for m in range(M):
+    newValue = scipy.stats.norm.rvs(previousValue,1.0)
+    ripl.observe("(g %d)" % m,"%d" % newValue)
+    previousValue = newValue
+
+  return ripl
+
+
+# O(N) forwards
+# O(1) to infer
+def testHMMParticleAsymptotics1():
+  from nose import SkipTest
+  raise SkipTest("Skipping testHMMParticleAsymptotics1: no linear regression test")
+
+  Ts = [pow(2,T) for T in range(2,10)]
+
+  inferTimes = []
+  N = 100
+
+  for T in Ts:
+    ripl = loadHMMParticleAsymptoticProgram1(T)
+    start = time.clock()
+    ripl.infer({"kernel":"pgibbs","scope":"states","block":"ordered","transitions":N})
+    end = time.clock()
+    inferTimes.append(end - start)
+
+  # TODO some kind of linear regression R-value 
+  # (the run times should grow linearly in T)
+  assert False
