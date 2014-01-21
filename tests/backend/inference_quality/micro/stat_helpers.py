366b4fad35f48474827d40dabd281def609a6bc5
Daniel Selsam <daniel.selsam@gmail.com>
2014-01-20 20:42:53 -0500

    Example micro inference quality tests w Radul's p-value framework.

---
+++ tests/backend/inference_quality/micro/stat_helpers.py
@@ -0,0 +1,204 @@
+import sys
+import math
+import pdb
+import itertools
+import scipy.stats as stats
+import numpy as np
+
+globalUseGlobalScaffold = False;
+globalAlwaysReport = False;
+globalReportingThreshold = 0.001
+
+def normalizeList(seq):
+  denom = sum(seq)
+  if denom > 0: return [ float(x)/denom for x in seq]
+  else: return [0 for x in seq]
+
+def countPredictions(predictions, seq):
+  return [predictions.count(x) for x in seq]
+
+def rmsDifference(eps,ops): return math.sqrt(sum([ math.pow(x - y,2) for (x,y) in zip(eps,ops)]))
+
+def fmtlst(fmt, lst):
+  return "[" + ", ".join(map((lambda n: fmt % n), lst)) + "]"
+
+def tabulatelst(fmt, lst, width=10, prefix=""):
+  structure = []
+  rest = lst
+  while len(rest) > width + 1: # Avoid length 1 widows
+    structure.append(rest[:width])
+    rest = rest[width:]
+  structure.append(rest)
+  substrs = [", ".join(map((lambda n: fmt % n), l)) for l in structure]
+  bulk = (",\n" + prefix + " ").join(substrs)
+  return prefix + "[" + bulk + "]"
+
+class TestResult(object):
+  def __init__(self, name, pval, report):
+    self.name = name
+    self.pval = pval
+    self.report = report
+
+  def __str__(self):
+    return ("---Test: %s---\n" % self.name) + self.report
+
+def fisherMethod(pvals):
+  if any([p == 0 for p in pvals]):
+    return 0
+  else:
+    chisq = -2 * sum([math.log(p) for p in pvals])
+    return stats.chi2.sf(chisq, 2*len(pvals))
+
+def repeatTest(func, *args):
+  result = func(*args)
+  if result.pval > 0.05:
+    return result
+  elif fisherMethod(result.pval + [1.0]*4) < globalReportingThreshold:
+    return result
+  else:
+    print "Retrying suspicious test %s" % result.name
+    results = [result] + [func(*args) for i in range(1,5)]
+    pval = fisherMethod([r.pval for r in results])
+    assert pval > globalReportingThreshold
+    report = "\n".join([r.report for r in results])
+    report += "\nOverall P value: " + str(pval)
+    return TestResult(result.name + " failing consistently", pval, report)
+
+def reportTest(result):
+  assert result.pval > globalReportingThreshold
+
+
+
+
+# Chi^2 test for agreement with the given discrete distribution.
+# TODO Broken (too stringent?) for small sample sizes; warn?
+# reportKnownDiscrete :: (Eq a) => String -> [(a,Double)] -> [a] -> IO ()
+def reportKnownDiscrete(name, expectedRates, observed):
+  items = [pair[0] for pair in expectedRates]
+  itemsDict = {pair[0]:pair[1] for pair in expectedRates}
+  for o in observed:
+    assert o in itemsDict, "Completely unexpected observation %r" % o
+  # N.B. This is not None test allows observations to be selectively
+  # ignored.  This is useful when information about the right answer
+  # is incomplete.
+  counts = [observed.count(x) for x in items if itemsDict[x] is not None]
+  total = sum(counts)
+  expRates = normalizeList([pair[1] for pair in expectedRates if pair[1] is not None])
+  expCounts = [total * r for r in expRates]
+  (chisq,pval) = stats.chisquare(counts, np.array(expCounts))
+  return TestResult(name, pval, "\n".join([
+    "Expected: " + fmtlst("% 4.1f", expCounts),
+    "Observed: " + fmtlst("% 4d", counts),
+    "Chi^2   : " + str(chisq),
+    "P value : " + str(pval)]))
+
+def explainOneDSample(observed):
+  count = len(observed)
+  mean = np.mean(observed)
+  stddev = np.std(observed)
+  ans = "Observed: % 4d samples with mean %4.3f, stddev %4.3f" % (count, mean, stddev)
+  if count < 101:
+    ans += ", data\n"
+    ans += tabulatelst("%.2f", sorted(observed), width=10, prefix="  ")
+  else:
+    ans += ", percentiles\n"
+    percentiles = [stats.scoreatpercentile(observed, p) for p in range(0,101)]
+    ans += tabulatelst("%.2f", percentiles, width=10, prefix="  ")
+  return ans
+
+# Kolmogorov-Smirnov test for agreement with known 1-D CDF.
+def reportKnownContinuous(name, expectedCDF, observed, descr=None):
+  (K, pval) = stats.kstest(observed, expectedCDF)
+  return TestResult(name, pval, "\n".join([
+    "Expected: %4d samples from %s" % (len(observed), descr),
+    explainOneDSample(observed),
+    "K stat  : " + str(K),
+    "P value : " + str(pval)]))
+
+# Z-score test for known mean, given known variance.
+# Doesn't work for distributions that are fat-tailed enough not to
+# have a mean.
+# TODO Also sensibly compare the variance of the sample to the
+# expected variance (what's the right test statistic when the
+# "population distribution" is not known?  How many samples do I need
+# for it to become effectively normal, if it does?)
+# TODO Can I use the Barry-Esseen theorem to use skewness information
+# for a more precise computation of test validity?  How about
+# comparing sample skewness to expected skewness?
+def reportKnownMeanVariance(name, expMean, expVar, observed):
+  count = len(observed)
+  mean = np.mean(observed)
+  zscore = (mean - expMean) * math.sqrt(count) / math.sqrt(expVar)
+  pval = 2*stats.norm.sf(abs(zscore)) # Two-tailed
+  return TestResult(name, pval, "\n".join([
+    "Expected: % 4d samples with mean %4.3f, stddev %4.3f" % (count, expMean, math.sqrt(expVar)),
+    explainOneDSample(observed),
+    "Z score : " + str(zscore),
+    "P value : " + str(pval)]))
+
+# T-test for known mean, without knowing the variance.
+# Doesn't work for distributions that are fat-tailed enough not to
+# have a mean.
+# TODO This is only valid if there are enough observations; 30 are recommended.
+def reportKnownMean(name, expMean, observed):
+  count = len(observed)
+  (tstat, pval) = stats.ttest_1samp(observed, expMean)
+  return TestResult(name, pval, "\n".join([
+    "Expected: % 4d samples with mean %4.3f" % (count, expMean),
+    explainOneDSample(observed),
+    "T stat  : " + str(tstat),
+    "P value : " + str(pval)]))
+
+# For a deterministic test
+def reportPassage(name):
+  return TestResult("Passed %s" % name, 1.0, "")
+
+def profile(N):
+  import statprof # From sudo pip install statprof
+  statprof.start()
+  try:
+    runAllTests(N)
+  finally:
+    statprof.stop()
+    statprof.display()
+
+def runAllTests(N):
+  print "========= RunAllTests(N) ========"
+  options = [ (make_church_prime_ripl, "mh", False),
+              (make_church_prime_ripl, "mh", True),
+              (make_church_prime_ripl, "pgibbs", False),
+              (make_church_prime_ripl, "pgibbs", True),
+              (make_church_prime_ripl, "meanfield", False),
+              (make_church_prime_ripl, "meanfield", True),
+              (make_church_prime_ripl, "gibbs", False),
+              (make_lite_church_prime_ripl, "mh", False),
+              (make_lite_church_prime_ripl, "meanfield", False)
+              ]
+
+
+  for i in range(len(options)):
+    name = "CXX" if options[i][0] == make_church_prime_ripl else "Lite"
+    print "\n========= %d. (%s, %s, %d) ========" % (i+1,name,options[i][1],options[i][2])
+    global globalBackend
+    global globalKernel
+    global globalUseGlobalScaffold
+    globalBackend = options[i][0]
+    globalKernel = options[i][1]
+    globalUseGlobalScaffold = options[i][2]
+    runTests(N)
+
+def collectSamples(ripl,address,T,kernel=None,use_global_scaffold=None):
+  kernel = kernel if kernel is not None else "mh" # TODO
+  use_global_scaffold = use_global_scaffold if use_global_scaffold is not None else globalUseGlobalScaffold
+  block = "one" if not use_global_scaffold else "all"
+  return collectSamplesWith(ripl, address, T, {"transitions":100, "kernel":kernel, "block":block})
+
+def collectSamplesWith(ripl, address, T, params):
+  predictions = []
+  for t in range(T):
+    # Going direct here saved 5 of 35 seconds on some unscientific
+    # tests, presumably by avoiding the parser.
+    ripl.sivm.core_sivm.engine.infer(params)
+    predictions.append(ripl.report(address))
+    ripl.sivm.core_sivm.engine.reset()
+  return predictions
---
+++ tests/stack/functionality/vim_utils_test.py
@@ -0,0 +1,335 @@
+# Copyright (c) 2013, MIT Probabilistic Computing Project.
+# 
+# This file is part of Venture.
+# 	
+# Venture is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 	
+# Venture is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 	
+# You should have received a copy of the GNU General Public License along with Venture.  If not, see <http://www.gnu.org/licenses/>.
+import unittest
+from venture.exception import VentureException
+from venture.sivm import utils
+
+#Note -- these tests only check for minimum functionality
+
+class TestSivmUtils(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    ######################################
+    # is_valid_symbol
+    ######################################
+
+    def test_is_valid_symbol(self):
+        self.assertTrue(utils.is_valid_symbol('abc123'))
+        self.assertFalse(utils.is_valid_symbol('123abc'))
+        self.assertFalse(utils.is_valid_symbol('+'))
+        self.assertFalse(utils.is_valid_symbol(''))
+
+    ######################################
+    # desugar_expression
+    ######################################
+
+    def test_desugar_expression_if(self):
+        a = ['if','a','b',['if','c','d','e']]
+        b = [['condition_erp','a',['lambda',[],'b'],['lambda',[],
+                [['condition_erp','c',['lambda',[],'d'],['lambda',[],'e']]]]]]
+        self.assertEqual(utils.desugar_expression(a),b)
+    def test_desugar_expression_if_failure(self):
+        a = ['if','a','b',['if',['if'],'d','e']]
+        try:
+            utils.desugar_expression(a)
+        except VentureException as e:
+            self.assertEqual(e.data['expression_index'],[3,1])
+        else:
+            self.fail()
+
+    def test_desugar_expression_and(self):
+        a = ['and','a','b']
+        b = [['condition_erp','a',['lambda',[],'b'],['lambda', [], {"type":"boolean","value":False}]]]
+        self.assertEqual(utils.desugar_expression(a),b)
+    
+    def test_desugar_expression_nested(self):
+        a = [['and','a','b']]
+        b = [[['condition_erp','a',['lambda',[],'b'],['lambda', [], {"type":"boolean","value":False}]]]]
+        self.assertEqual(utils.desugar_expression(a),b)
+
+    def test_desugar_expression_or(self):
+        a = ['or','a','b']
+        b = [['condition_erp','a',['lambda', [], {"type":"boolean","value":True}],['lambda',[],'b']]]
+        self.assertEqual(utils.desugar_expression(a),b)
+
+    def test_desugar_expression_let_1(self):
+        a = ['let',[],'b']
+        b = 'b'
+        self.assertEqual(utils.desugar_expression(a),b)
+    def test_desugar_expression_let_2(self):
+        a = ['let',[['a','b']],'c']
+        b = [['lambda',['a'],'c'],'b']
+        self.assertEqual(utils.desugar_expression(a),b)
+    def test_desugar_expression_let_3(self):
+        a = ['let',[['a','b'],['c','d']],'e']
+        b = [['lambda',['a'],[['lambda',['c'],'e'],'d']],'b']
+        self.assertEqual(utils.desugar_expression(a),b)
+    def test_desugar_expression_let_failure_1(self):
+        a = ['let','a','b']
+        try:
+            utils.desugar_expression(a)
+        except VentureException as e:
+            self.assertEqual(e.data['expression_index'],[1])
+        else:
+            self.fail()
+    def test_desugar_expression_let_failure_2(self):
+        a = ['let',['a'],'b']
+        try:
+            utils.desugar_expression(a)
+        except VentureException as e:
+            self.assertEqual(e.data['expression_index'],[1,0])
+        else:
+            self.fail()
+    def test_desugar_expression_let_failure_3(self):
+        a = ['let',[[object(),'c']],'b']
+        try:
+            utils.desugar_expression(a)
+        except VentureException as e:
+            self.assertEqual(e.data['expression_index'],[1,0,0])
+        else:
+            self.fail()
+
+    def test_desugar_expression_identity(self):
+        a = ['identity','b']
+        b = [['lambda',[],'b']]
+        self.assertEqual(utils.desugar_expression(a),b)
+
+    def test_desugar_nothing(self):
+        a = ['b']
+        b = ['b']
+        self.assertEqual(utils.desugar_expression(a),b)
+
+
+    ######################################
+    # desugar_expression
+    ######################################
+
+    def find_sym(self, exp, sym):
+        if sym==exp:
+            return []
+        if sym in exp:
+            return [exp.index(sym)]
+        for i,e in enumerate(exp):
+            if isinstance(e,(list,tuple)):
+                j = self.find_sym(e,sym)
+                if j != None:
+                    return [i]+j
+        return None
+
+    def test_find_sym(self):
+        a = ['if','a','b',['if','c','d','e']]
+        self.assertEqual(self.find_sym(a,'a'),[1])
+        self.assertEqual(self.find_sym(a,'b'),[2])
+        self.assertEqual(self.find_sym(a,'c'),[3,1])
+        self.assertEqual(self.find_sym(a,'d'),[3,2])
+        self.assertEqual(self.find_sym(a,'e'),[3,3])
+
+    def iter_indices(self,exp):
+        yield []
+        if isinstance(exp,(list,tuple)):
+            for i, e in enumerate(exp):
+                for x in self.iter_indices(e):
+                    yield [i]+x
+
+    def test_iter_indices(self):
+        a = ['a',['b','c'],[]]
+        r = [[],[0],[1],[2],[1,0],[1,1]]
+        for x in self.iter_indices(a):
+            self.assertIn(x,r)
+            r.remove(x)
+        self.assertEqual(r,[])
+
+    fancy_expressions = [
+            ['if','a',['if','b','c','d'],'e'],
+            ['and',['and','a','b'],['and','c','d']],
+            ['or',['or','a','b'],['or','c','d']],
+            [['let',[],[['a'],'b']],['c'],'d'],
+            ['let',[['a',['b','c']]],'d'],
+            ['let',[['a','b'],['c','d']],'e'],
+            ['identity',[['identity',['a','b']],'c','d']]
+            ]
+    def test_sugar_expression_index_standard_cases(self):
+        """make sure that all sugared locations are properly translated"""
+        msg_string ="\n\nsym: {}\nsugared_exp: {}\ndesugared_exp: {}\n"\
+                    "desugared_index: {}\nexpected_index: {}\n"\
+                    "got_index: {}"
+        for a in self.fancy_expressions:
+            s = utils.desugar_expression(a)
+            for sym in ('a','b','c','d'):
+                i1 = self.find_sym(a,sym)
+                i2 = self.find_sym(s,sym)
+                try:
+                    i3 = utils.sugar_expression_index(a,i2)
+                except:
+                    print msg_string.format(sym,a,s,i2,i1,None)
+                    raise
+                self.assertEqual(i3,i1,msg=msg_string.format(sym,a,s,i2,i1,i3))
+    def test_sugar_expression_index_all_cases(self):
+        """make sure that none of the edge cases crash the thingy"""
+        msg_string ="\n\nsugared_exp: {}\ndesugared_exp: {}\n"\
+                    "desugared_index: {}"
+        for a in self.fancy_expressions:
+            s = utils.desugar_expression(a)
+            for i in self.iter_indices(s):
+                try:
+                    self.assertIsNotNone(utils.sugar_expression_index(a,i),
+                            msg=msg_string.format(a,s,i))
+                except:
+                    print msg_string.format(a,s,i)
+                    raise
+
+    def test_desugar_expression_index_standard_cases(self):
+        """make sure that all sugared locations are properly translated"""
+        msg_string ="\n\nsym: {}\nsugared_exp: {}\ndesugared_exp: {}\n"\
+                    "sugared_index: {}\nexpected_index: {}\n"\
+                    "got_index: {}"
+        for a in self.fancy_expressions:
+            s = utils.desugar_expression(a)
+            for sym in ('a','b','c','d'):
+                i1 = self.find_sym(a,sym)
+                i2 = self.find_sym(s,sym)
+                try:
+                    i3 = utils.desugar_expression_index(a,i1)
+                except:
+                    print msg_string.format(sym,a,s,i1,i2,None)
+                    raise
+                self.assertEqual(i3,i2,msg=msg_string.format(sym,a,s,i1,i2,i3))
+    def test_desugar_expression_index_all_cases(self):
+        """make sure that none of the edge cases crash the thingy"""
+        msg_string ="\n\nsugared_exp: {}\ndesugared_exp: {}\n"\
+                    "sugared_index: {}"
+        for a in self.fancy_expressions:
+            s = utils.desugar_expression(a)
+            for i in self.iter_indices(a):
+                try:
+                    self.assertIsNotNone(utils.desugar_expression_index(a,i),
+                            msg=msg_string.format(a,s,i))
+                except VentureException as e:
+                    self.assertEquals(e.exception,'expression_index_desugaring')
+                except:
+                    print msg_string.format(a,s,i)
+                    raise
+
+    def test_validate_instruction_1(self):
+        try:
+            utils.validate_instruction({},[])
+        except VentureException as e:
+            self.assertEqual(e.exception,'malformed_instruction')
+    def test_validate_instruction_2(self):
+        try:
+            utils.validate_instruction({'instruction':"moo"},[])
+        except VentureException as e:
+            self.assertEqual(e.exception,'unrecognized_instruction')
+    def test_validate_instruction_3(self):
+        i = {'instruction':"moo"}
+        self.assertEqual(utils.validate_instruction(i,['moo']),i)
+
+    def test_require_state_1(self):
+        utils.require_state('default','red','default')
+    def test_require_state_2(self):
+        try:
+            utils.require_state('moo','default')
+        except VentureException as e:
+            self.assertEqual(e.exception,'invalid_state')
+            self.assertEqual(e.data['state'],'moo')
+
+    def test_validate_symbol_1(self):
+        self.assertEqual(utils.validate_symbol('add'),'add')
+    def test_validate_symbol_2(self):
+        try:
+            utils.validate_symbol('9ab')
+        except VentureException as e:
+            self.assertEqual(e.exception, 'parse')
+
+    def test_validate_value_1(self):
+        v = {
+            "type":"real",
+            "value":1,
+            }
+        self.assertEqual(utils.validate_value(v),v)
+    def test_validate_value_2(self):
+        v = {
+            "tawfepo":"real",
+            "value":1,
+            }
+        try:
+            utils.validate_value(v)
+        except VentureException as e:
+            self.assertEqual(e.exception, 'parse')
+
+    def test_validate_expression_1(self):
+        e = ['a',{'type':'atom','value':1},['b']]
+        self.assertEqual(utils.validate_expression(e),e)
+    def test_validate_expression_2(self):
+        try:
+            utils.validate_expression(['a','b',[2]])
+        except VentureException as e:
+            self.assertEqual(e.exception, 'parse')
+            self.assertEqual(e.data['expression_index'], [2,0])
+
+    def test_validate_positive_integer_1(self):
+        self.assertEqual(utils.validate_positive_integer(1.0),1)
+    def test_sanitize_positive_integer_2(self):
+        for i in [-1,0,1.3]:
+            try:
+                utils.validate_positive_integer(i)
+            except VentureException as e:
+                self.assertEqual(e.exception, 'parse')
+
+    def test_validate_boolean_1(self):
+        self.assertEqual(utils.validate_boolean(True),True)
+    def test_validate_boolean_2(self):
+        try:
+            utils.validate_boolean(1.2)
+        except VentureException as e:
+            self.assertEqual(e.exception, 'parse')
+
+    def test_validate_arg_1(self):
+        i = {"instruction":"moo","symbol":"moo"}
+        self.assertEqual(utils.validate_arg(i,"symbol",utils.validate_symbol),"moo")
+    def test_validate_arg_2(self):
+        i = {"instruction":"moo","symbol":2}
+        try:
+            utils.validate_arg(i,"symbol",utils.validate_symbol)
+        except VentureException as e:
+            self.assertEqual(e.exception,'invalid_argument')
+            self.assertEqual(e.data['argument'],'symbol')
+    def test_validate_arg_3(self):
+        i = {"instruction":"moo","symbol":2}
+        try:
+            utils.validate_arg(i,"red",utils.validate_symbol)
+        except VentureException as e:
+            self.assertEqual(e.exception,'missing_argument')
+            self.assertEqual(e.data['argument'],'red')
+    def test_validate_arg_4(self):
+        i = {"instruction":"moo"}
+        self.assertEqual(utils.validate_arg(i,"red",utils.validate_symbol,required=False),None)
+    def test_validate_arg_5(self):
+        i = {"instruction":"moo","symbol":"moo"}
+        self.assertEqual(utils.validate_arg(i,"symbol",utils.validate_symbol,
+            modifier=lambda x: "red"),"red")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
