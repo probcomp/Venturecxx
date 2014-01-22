50c533010c56e5fabce05d8d8e2b0d14d36dcaeb
Alexey Radul <axch@mit.edu>
2014-01-22 14:50:08 -0500

    The iteration count in testTrig1 is really not a sample count at all.  Hardcode.

--- test/sp_conformance/test_trig.py
+++ test/sp_conformance/test_trig.py
@@ -3,14 +3,13 @@ from testconfig import config
 
 def testTrig1():
   "Simple test that verifies sin^2 + cos^2 = 1 as x varies"
-  N = config["num_samples"]
   ripl = config["get_ripl"]()
   ripl.assume("sq","(lambda (x) (* x x))")
   ripl.assume("x","(normal 0.0 1.0)")
   ripl.assume("a","(sq (sin x))")
   ripl.assume("b","(sq (cos x))")
   ripl.predict("(+ a b)")
-  for i in range(N):
+  for i in range(10):
     ripl.infer(1)
     assert abs(ripl.report(5) - 1) < .001
   return reportPassage("TestTrig1")
