e8993aafd55ff4cc3b2f97020ca04d130f835ba4
Alexey Radul <axch@mit.edu>
2014-01-27 18:26:17 -0500

    Rearrange the order of definition of builtins into some more logic.

--- backend/lite/builtin.py
+++ backend/lite/builtin.py
@@ -50,17 +50,12 @@ def builtInSPsList():
 
            [ "not", deterministic(lambda x: not x) ],
 
-           [ "simplex", SP(NullRequestPSP(),dstructures.SimplexOutputPSP()) ],
-
+           # Symbols are Python strings
+           [ "is_symbol", deterministic(lambda x: isinstance(x, basestring)) ],
 
            [ "lookup", SP(NullRequestPSP(),dstructures.LookupOutputPSP()) ],
            [ "contains", SP(NullRequestPSP(),dstructures.ContainsOutputPSP()) ],
            [ "size", SP(NullRequestPSP(),dstructures.SizeOutputPSP()) ],
-
-           [ "array", SP(NullRequestPSP(),dstructures.ArrayOutputPSP()) ],
-           [ "is_array", SP(NullRequestPSP(),dstructures.IsArrayOutputPSP()) ],
-           [ "dict", SP(NullRequestPSP(),dstructures.DictOutputPSP()) ],
-           [ "matrix", SP(NullRequestPSP(),dstructures.MatrixOutputPSP()) ],
 
            [ "pair", SP(NullRequestPSP(),dstructures.PairOutputPSP()) ],
            [ "list", SP(NullRequestPSP(),dstructures.ListOutputPSP()) ],
@@ -72,27 +67,40 @@ def builtInSPsList():
            [ "second", SP(NullRequestPSP(),dstructures.SecondListOutputPSP()) ],
            [ "rest", SP(NullRequestPSP(),dstructures.RestListOutputPSP()) ],
 
-           # Symbols are Python strings
-           [ "is_symbol", deterministic(lambda x: isinstance(x, basestring)) ],
+           [ "array", SP(NullRequestPSP(),dstructures.ArrayOutputPSP()) ],
+           [ "is_array", SP(NullRequestPSP(),dstructures.IsArrayOutputPSP()) ],
+           [ "dict", SP(NullRequestPSP(),dstructures.DictOutputPSP()) ],
+           [ "matrix", SP(NullRequestPSP(),dstructures.MatrixOutputPSP()) ],
+           [ "simplex", SP(NullRequestPSP(),dstructures.SimplexOutputPSP()) ],
+
+           [ "branch", SP(conditionals.BranchRequestPSP(),ESRRefOutputPSP()) ],
+           [ "biplex", SP(NullRequestPSP(),conditionals.BiplexOutputPSP()) ],
+           [ "make_csp", SP(NullRequestPSP(),csp.MakeCSPOutputPSP()) ],
+
+           [ "eval", SP(eval_sps.EvalRequestPSP(),ESRRefOutputPSP()) ],
+           [ "get_current_environment", SP(NullRequestPSP(),eval_sps.GetCurrentEnvOutputPSP()) ],
+           [ "get_empty_environment", SP(NullRequestPSP(),eval_sps.GetEmptyEnvOutputPSP()) ],
+           [ "extend_environment", SP(NullRequestPSP(),eval_sps.ExtendEnvOutputPSP()) ],
+
+           [ "mem", SP(NullRequestPSP(),msp.MakeMSPOutputPSP()) ],
+
+           [ "scope_include", SP(NullRequestPSP(),scope.ScopeIncludeOutputPSP()) ],
 
            [ "flip", SP(NullRequestPSP(),discrete.BernoulliOutputPSP()) ],
            [ "bernoulli", SP(NullRequestPSP(),discrete.BernoulliOutputPSP()) ],
            [ "categorical", SP(NullRequestPSP(),discrete.CategoricalOutputPSP()) ],
 
            [ "normal", SP(NullRequestPSP(),continuous.NormalOutputPSP()) ],
-
            [ "uniform_continuous", SP(NullRequestPSP(),continuous.UniformOutputPSP()) ],
            [ "beta", SP(NullRequestPSP(),continuous.BetaOutputPSP()) ],
-
            [ "gamma", SP(NullRequestPSP(),continuous.GammaOutputPSP()) ],
            [ "student_t", SP(NullRequestPSP(),continuous.StudentTOutputPSP()) ],
 
-           [ "branch", SP(conditionals.BranchRequestPSP(),ESRRefOutputPSP()) ],
-           [ "biplex", SP(NullRequestPSP(),conditionals.BiplexOutputPSP()) ],
+           [ "dirichlet", SP(NullRequestPSP(),discrete.DirichletOutputPSP()) ],
+           [ "symmetric_dirichlet", SP(NullRequestPSP(),discrete.SymmetricDirichletOutputPSP()) ],
 
-           [ "make_csp", SP(NullRequestPSP(),csp.MakeCSPOutputPSP()) ],
-
-           [ "mem", SP(NullRequestPSP(),msp.MakeMSPOutputPSP()) ],
+           [ "make_dir_mult", SP(NullRequestPSP(),discrete.MakerCDirMultOutputPSP()) ],
+           [ "make_uc_dir_mult", SP(NullRequestPSP(),discrete.MakerUDirMultOutputPSP()) ],
 
            [ "make_beta_bernoulli", SP(NullRequestPSP(),discrete.MakerCBetaBernoulliOutputPSP()) ],
            [ "make_uc_beta_bernoulli", SP(NullRequestPSP(),discrete.MakerUBetaBernoulliOutputPSP()) ],
@@ -100,22 +108,9 @@ def builtInSPsList():
            [ "make_sym_dir_mult", SP(NullRequestPSP(),discrete.MakerCSymDirMultOutputPSP()) ],
            [ "make_uc_sym_dir_mult", SP(NullRequestPSP(),discrete.MakerUSymDirMultOutputPSP()) ],
 
-           [ "make_dir_mult", SP(NullRequestPSP(),discrete.MakerCDirMultOutputPSP()) ],
-           [ "make_uc_dir_mult", SP(NullRequestPSP(),discrete.MakerUDirMultOutputPSP()) ],
-
-           [ "dirichlet", SP(NullRequestPSP(),discrete.DirichletOutputPSP()) ],
-           [ "symmetric_dirichlet", SP(NullRequestPSP(),discrete.SymmetricDirichletOutputPSP()) ],
-
            [ "make_crp", SP(NullRequestPSP(),crp.MakeCRPOutputPSP()) ],
 
            [ "make_lazy_hmm", SP(NullRequestPSP(),hmm.MakeUncollapsedHMMOutputPSP()) ],
-
-           [ "scope_include", SP(NullRequestPSP(),scope.ScopeIncludeOutputPSP()) ],
-
-           [ "eval", SP(eval_sps.EvalRequestPSP(),ESRRefOutputPSP()) ],
-           [ "get_current_environment", SP(NullRequestPSP(),eval_sps.GetCurrentEnvOutputPSP()) ],
-           [ "get_empty_environment", SP(NullRequestPSP(),eval_sps.GetEmptyEnvOutputPSP()) ],
-           [ "extend_environment", SP(NullRequestPSP(),eval_sps.ExtendEnvOutputPSP()) ],
   ]
 
 def builtInSPs():
