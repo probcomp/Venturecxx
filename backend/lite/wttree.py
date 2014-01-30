5092f7b1df1162eee8a46d43367e6b36539c776e
Alexey Radul <axch@mit.edu>
2014-01-30 10:57:38 -0500

    Starting to actually write weight-balanced trees.  Paying attention to the Python style guide while I'm at it.

--- backend/lite/wttree.py
+++ backend/lite/wttree.py
@@ -11,3 +11,27 @@ The actual implementation is cribbed close to verbatim from wt-trees
 in MIT Scheme, which were written by Stephen Adams and modified by
 Chris Hansen and Taylor Campbell.'''
 
+# TODO I imagine much memory can be saved by representing the
+# node structure with just a tuple and None, manely
+# data Tree k v = Maybe (k,v,Node k v,Node k v,Int)
+# instead of the below classes
+# data Tree k v = Either EmptyNode (Node k v)
+
+class EmptyNode: #pylint: disable=W0232
+  def size(self):
+    return 0
+
+class Node:
+  def __init__(self, key, value, left, right):
+    self.key = key
+    self.value = value
+    self.left = left
+    self.right = right
+    self.ct = left.size() + right.size() + 1
+
+  def size(self):
+    return self.ct
+
+# TODO Manually inline this?
+def node_weight(node):
+  return node.size() + 1
