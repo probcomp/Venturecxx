f60c56ef03ba4c5493bc61a7173e156393db7d38
Alexey Radul <axch@mit.edu>
2014-01-30 01:28:53 -0500

    Placeholder for functional tree implementation.
    I decided against sticking with red-black trees because the remove
    operation is more obvious for weight-balanced trees, and because the
    metadata they carry to maintain balance is useful for other purposes
    as well (notably size and indexing).

---
+++ backend/lite/wttree.py
@@ -0,0 +1,13 @@
+'''Weight-balanced trees.
+
+This program is based on
+
+  Stephen Adams, Implementing Sets Efficiently in a Functional
+     Language, CSTR 92-10, Department of Electronics and Computer
+     Science, University of Southampton, 1992.
+
+but only implements the operations that are useful for Venture Lite.
+The actual implementation is cribbed close to verbatim from wt-trees
+in MIT Scheme, which were written by Stephen Adams and modified by
+Chris Hansen and Taylor Campbell.'''
+
