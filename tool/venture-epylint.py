4545018dd5e2f62510644498dced06f6b3ab29a6
Alexey Radul <axch@mit.edu>
2014-01-29 23:36:19 -0500

    Work properly on flymake temporary files in weird directories.

--- tool/venture-epylint.py
+++ tool/venture-epylint.py
@@ -26,10 +26,13 @@ ignore = ",".join ( [
 mypath = os.path.dirname(sys.argv[0])
 lintfile = mypath + "/pylintrc"
 
-cmd = "pylint --output-format parseable --include-ids y --reports n --rcfile %s %s" % \
-    (lintfile, sys.argv[1])
+workdir = os.path.dirname(sys.argv[1])
+filename = os.path.basename(sys.argv[1])
 
-p = Popen ( cmd, shell = True, bufsize = -1,
+cmd = "pylint --output-format parseable --include-ids y --reports n --rcfile %s %s" % \
+    (lintfile, filename)
+
+p = Popen ( cmd, shell = True, bufsize = -1, cwd = workdir,
             stdin = PIPE, stdout = PIPE, stderr = PIPE, close_fds = True )
 pylint_re = re.compile (
     '^([^:]+):(\d+):\s*\[([WECR])([^,]+),\s*([^\]]+)\]\s*(.*)$'
