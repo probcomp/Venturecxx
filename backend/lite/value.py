bf47d6d3cab3f1f068c3f9058baf416388fc9c7e
Daniel Selsam <daniel.selsam@gmail.com>
2014-01-31 23:36:16 +0000

    Draft the full range of Venture Values.

--- backend/lite/value.py
+++ backend/lite/value.py
@@ -4,4 +4,13 @@ class VentureValue(object):
   __metaclass__ = ABCMeta
 
   def getNumber(self): raise Exception("Cannot convert %s to number" % type(self))
+  def getAtom(self): raise Exception("Cannot convert %s to atom" % type(self))
   def getBool(self): raise Exception("Cannot convert %s to bool" % type(self))
+  def getSymbol(self): raise Exception("Cannot convert %s to symbol" % type(self))
+  def getArray(self): raise Exception("Cannot convert %s to array" % type(self))
+  def getPair(self): raise Exception("Cannot convert %s to pair" % type(self))
+  def getSimplex(self): raise Exception("Cannot convert %s to simplex" % type(self))
+  def getDict(self): raise Exception("Cannot convert %s to dict" % type(self))
+  def getMatrix(self): raise Exception("Cannot convert %s to matrix" % type(self))
+  def getSP(self): raise Exception("Cannot convert %s to sp" % type(self))
+  def getEnvironment(self): raise Exception("Cannot convert %s to environment" % type(self))
--- backend/lite/values.py
+++ backend/lite/values.py
@@ -11,13 +11,16 @@ class VentureAtom(value.VentureValue):
 
 class VentureBool(value.VentureValue):
   def __init__(self,boolean): self.boolean = boolean
+  def getBool(self): return self.boolean
 
 class VentureSymbol(value.VentureValue):
   def __init__(self,symbol): self.symbol = symbol
+  def getSymbol(self): return self.symbol
 
 class VentureArray(value.VentureValue):
   def __init__(self,array): self.array = array
-    
+  def getArray(self): return self.array
+
 class VentureNil(value.VentureValue):
   def __init__(self): pass
 
@@ -25,14 +28,18 @@ class VenturePair(value.VentureValue):
   def __init__(self,first,rest):
     self.first = first
     self.rest = rest
+  def getPair(self): return (self.first,self.rest)
 
 class VentureSimplex(value.VentureValue):
   def __init__(self,simplex): self.simplex = simplex          
+  def getSimplex(self): return self.simplex
 
 class VentureDict(value.VentureValue):
   def __init__(self,d): self.dict = d
+  def getDict(self): return self.dict
 
 class VentureMatrix(value.VentureValue):
   def __init__(self,matrix): self.matrix = matrix
+  def getMatrix(self): return self.matrix
 
 ## SPs and Environments as well
