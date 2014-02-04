385f3a746f7dd9ee83f9babf0ec83a78b84955ab
Alexey Radul <axch@mit.edu>
2014-02-04 16:43:33 -0500

    Draft installing Venture from either a checkout or a tarball.

--- tool/scventure.py
+++ tool/scventure.py
@@ -3,11 +3,43 @@
 Installs Venture (including development sources) on all nodes."""
 from starcluster.clustersetup import ClusterSetup
 from starcluster.logger import log
+import os.path
+import tempfile
+import subprocess
 
-class VentureInstaller(ClusterSetup):
-  def __init__(self, tarfile, skip_cxx=False):
+class VentureInstaller(ClusterSetup): # Exceptions by default are acceptable pylint: disable=abstract-method
+  def __init__(self, tarfile=None, checkout=None, unpacked_dir=None, skip_cxx=False): # pylint: disable=super-init-not-called
+    # The example in docs didn't call super.
     self.tarfile = tarfile
+    self.checkout = checkout
     self.skip_cxx = skip_cxx
+    self.unpacked_venture_dir = unpacked_dir
+    self._check_config()
+
+  def _check_config(self):
+    if self.tarfile is not None and not os.path.isfile(self.tarfile):
+      log.warn("%s does not appear to be a file; ignoring." % self.tarfile)
+      self.tarfile = None
+    if self.checkout is not None and not os.path.isdir(self.checkout):
+      log.warn("%s does not appear to be a directory; ignoring." % self.checkout)
+      self.checkout = None
+    if self.tarfile is None and self.checkout is None:
+      # TODO What exception to raise?
+      raise Exception("No source for Venture specified.  Please indicate either a tarball or a checkout.")
+    if self.tarfile is not None and self.checkout is not None:
+      log.warn("Checkout %s and tarfile %s given as sources for Venture.  Preferring the checkout." % (self.checkout, self.tarfile))
+      self.tarfile = None
+    if self.checkout is not None and self.unpacked_venture_dir is None:
+      # The purpose of this complexity is to allow a user to install
+      # Venture from a checkout that names the repository something
+      # other than Venturecxx.
+      candidate = os.path.basename(self.checkout)
+      if candidate == "":
+        candidate = os.path.basename(os.path.dirname(self.checkout))
+      if candidate != "":
+        self.unpacked_venture_dir = candidate
+    if self.unpacked_venture_dir is None:
+      self.unpacked_venture_dir = "Venturecxx"
 
   def run(self, nodes, master, user, user_shell, volumes):
     for node in nodes:
@@ -20,6 +52,7 @@ class VentureInstaller(ClusterSetup):
       self.prepare_for_c11(node)
       self.ensure_c11(node)
     self.ensure_python_deps(node)
+    self.ensure_venture_source(node)
     self.ensure_venture(node)
 
   def ensure_basics(self, node):
@@ -45,12 +78,30 @@ class VentureInstaller(ClusterSetup):
     log.info("Installing Python dependencies on %s" % node.alias)
     node.apt_install('python-pyparsing python-flask python-requests python-numpy python-matplotlib')
 
-  def ensure_venture(self, node):
-    log.info("Installing Venture on %s" % node.alias)
-    node.ssh.put(self.tarfile, 'venture.tgz')
-    node.ssh.execute('tar --extract --gunzip --file venture.tgz')
-    node.ssh.execute('cd Venturecxx; pip install -r requirements.txt')
-    if not self.skip_cxx:
-      node.ssh.execute('cd Venturecxx; python setup.py install')
+  def ensure_venture_source(self, node):
+    if self.checkout is not None:
+      tempd = tempfile.mkdtemp()
+      try:
+        tarfile = os.path.join(tempd, "venture.tgz")
+        try:
+          subprocess.call(["tar", "-czf", tarfile, self.checkout])
+          node.ssh.put(tarfile, 'venture.tgz')
+          node.ssh.execute('tar --extract --gunzip --file venture.tgz')
+        finally:
+          if os.path.isfile(tarfile):
+            os.remove(tarfile)
+      finally:
+        os.rmdir(tempd)
+    elif self.tarfile is not None:
+      node.ssh.put(self.tarfile, 'venture.tgz')
+      node.ssh.execute('tar --extract --gunzip --file venture.tgz')
     else:
-      node.ssh.execute('cd Venturecxx; SKIP_CXX_BACKEND=true python setup.py install')
+      raise Exception("This should not happen: no Venture source found in valid config.")
+
+  def ensure_venture(self, node):
+    log.info("Building Venture on %s" % node.alias)
+    node.ssh.execute('cd %s; pip install -r requirements.txt' % self.unpacked_venture_dir)
+    if not self.skip_cxx:
+      node.ssh.execute('cd %s; python setup.py install' % self.unpacked_venture_dir)
+    else:
+      node.ssh.execute('cd %s; SKIP_CXX_BACKEND=true python setup.py install' % self.unpacked_venture_dir)
