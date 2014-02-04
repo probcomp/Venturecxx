2dd6f6fb2f2c342df680230da4eeed0d7b7c1cb6
Alexey Radul <axch@mit.edu>
2014-02-04 14:31:52 -0500

    Complete draft of plugin that uploads a Venture tarball.

--- tool/scventure.py
+++ tool/scventure.py
@@ -5,8 +5,8 @@ from starcluster.clustersetup import ClusterSetup
 from starcluster.logger import log
 
 class VentureInstaller(ClusterSetup):
-  def __init__(self):
-    pass
+  def __init__(self, tarfile):
+    self.tarfile = tarfile
 
   def run(self, nodes, master, user, user_shell, volumes):
     for node in nodes:
@@ -20,22 +20,27 @@ class VentureInstaller(ClusterSetup):
     self.ensure_venture(node)
 
   def prepare_for_c11(self, node):
-    node.ssh.execute('apt-get update')
-    node.ssh.execute('apt-get install -y libboost1.48-all-dev libgsl0-dev cmake make git python-pip python-virtualenv ccache')
+    log.info("Preparing %s for C++11" % node.alias)
+    node.apt_command('update')
+    node.apt_install('libboost1.48-all-dev libgsl0-dev cmake make git python-pip python-virtualenv ccache')
     node.ssh.execute('pip install -U distribute')
 
   def ensure_c11(self, node):
-    node.ssh.execute('apt-get install -y python-software-properties')
+    log.info("Installing C++11 on %s" % node.alias)
+    node.apt_install('python-software-properties')
     node.ssh.execute('add-apt-repository -y ppa:ubuntu-toolchain-r/test')
-    node.ssh.execute('apt-get update')
-    node.ssh.execute('apt-get install -y gcc-4.8 g++-4.8')
+    node.apt_command('update')
+    node.apt_install('gcc-4.8 g++-4.8')
     node.ssh.execute('update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50')
     node.ssh.execute('update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50')
 
   def ensure_python_deps(self, node):
-    node.ssh.execute('apt-get install -y python-pyparsing python-flask python-requests python-numpy python-matplotlib')
+    log.info("Installing Python dependencies on %s" % node.alias)
+    node.apt_install('python-pyparsing python-flask python-requests python-numpy python-matplotlib')
 
   def ensure_venture(self, node):
-    # TODO get the source from somewhere
-    node.ssh.execute('pip install -r requirements.txt')
-    node.ssh.execute('python setup.py install')
+    log.info("Installing Venture on %s" % node.alias)
+    node.ssh.put(self.tarfile, 'venture.tgz')
+    node.ssh.execute('tar --extract --gunzip --file venture.tgz')
+    node.ssh.execute('cd Venturecxx; pip install -r requirements.txt')
+    node.ssh.execute('cd Venturecxx; python setup.py install')
