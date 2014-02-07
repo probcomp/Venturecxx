3d1a27a27e9473ad21331ea29e934371eeeb199d
owainevans <owain@mit.edu>
2014-02-07 15:08:08 -0500

    Update __init__.py
    minor

--- python/lib/venturemagics/__init__.py
+++ python/lib/venturemagics/__init__.py
@@ -14,5 +14,3 @@
 # 	
 # You should have received a copy of the GNU General Public License along with Venture.  If not, see <http://www.gnu.org/licenses/>.
 # -*- coding: utf-8 -*-
-
-from venture_unit import *
