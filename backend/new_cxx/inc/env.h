8e0b34929e917879d36619862f24e284b592a1e9
Daniel Selsam <daniel.selsam@gmail.com>
2014-02-10 11:15:39 -0500

    draft header for env

---
+++ backend/new_cxx/inc/env.h
@@ -0,0 +1,22 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#include "types.h"
+#include "value.h"
+#include "values.h"
+#include "node.h"
+
+struct VentureEnvironment : VentureValue
+{
+  VentureEnvironment(shared_ptr<VentureEnvironment> outerEnv,
+		     const vector<shared_ptr<VentureSymbol> > & syms,
+		     const vector<Node*> & nodes);
+
+  void addBinding(shared_ptr<VentureSymbol> sym,Node * node);
+  Node * lookupSymbol(shared_ptr<VentureSymbol> sym);
+
+  shared_ptr<VentureEnvironment> outerEnv;
+  map<string,Node*> frame;
+};
+
+#endif
