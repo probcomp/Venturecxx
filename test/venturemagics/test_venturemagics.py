df6cb335382e456cec54721aa7688a13e5e12780
Owain Evans <owain@mit.edu>
2014-02-10 19:33:24 -0500

    test_venturemagics.py tests magics by running a notebook and an .ipy file. it now finds them by using the os.path.realpath(__file__) to find its own location and assuming that the other files are in the same location

--- test/venturemagics/test_venturemagics.py
+++ test/venturemagics/test_venturemagics.py
@@ -7,7 +7,7 @@ Test Venturemagics2.py by running it on notebook, IPython terminal, and regular
 Python interpreter. Calls a notebook tester (nb_tester.py), a notebook
 (test_venturemagics2_nb.ipynb), and a .ipy file (test_venturemagics2_ipy.ipy).
 """
-import subprocess
+import subprocess,os
 from venture.venturemagics.venturemagics import *
 
 
@@ -17,23 +17,28 @@ from venture.venturemagics.venturemagics import *
 # Script for testing notebooks taken from 
 # https://github.com/ipython/ipython/wiki/Cookbook%3a-Notebook-utilities
 
+
 def testMagicNotebook():
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    print file_dir
     notebook_tester = 'test/venturemagics/nb_tester.py'
     test_file =  'test/venturemagics/test_venturemagics_nb.ipynb'
-    
+    notebook_tester = file_dir + '/nb_tester.py'
+    test_file =  file_dir + '/test_venturemagics_nb.ipynb'
     out=subprocess.check_output(['python',notebook_tester,test_file])
     
     if 'failure' in out.lower():
         assert False, 'Notebook tester (%s) reports failure on notebook (%s)' % (
             notebook_tester, test_file)
     
-    raise SkipTest("The sequel fails in Jenkins for some reason.  Issue: https://app.asana.com/0/9277419963067/10168145986333")
+    #raise SkipTest("The sequel fails in Jenkins for some reason.  Issue: https://app.asana.com/0/9277419963067/10168145986333")
 
 
 
 ## Testing in IPython
 def testMagicIpython():
-    test_file = 'test/venturemagics/test_venturemagics_ipy.ipy'
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    test_file = file_dir + '/test_venturemagics_ipy.ipy'
     out = subprocess.check_output(['ipython',test_file])
     if 'error' in out.lower() or 'assertion' in out.lower():
         assert False, 'Error running %s in IPython' % test_file
