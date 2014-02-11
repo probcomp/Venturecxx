c6c60fc9484596a753f7b29a3d1ede8cd03418c2
Daniel Selsam <daniel.selsam@gmail.com>
2014-02-11 14:54:59 -0500

    Sketch of db

--- backend/new_cxx/inc/db.h
+++ backend/new_cxx/inc/db.h
@@ -6,19 +6,20 @@ struct LatentDB { virtual ~LatentDB() {}; };
 struct DB
 {
   bool hasValue(Node * node);
-  VentureValuePtr registerValue(Node * node);
-  void extractValue(Node * node, VentureValuePtr value);
+  VentureValuePtr getValue(Node * node);
+  void registerValue(Node * node, VentureValuePtr value);
 
-  bool hasLatentDB(Node * node);
-  void registerLatentDB(Node * node, VentureValuePtr value);
+  bool hasLatentDB(OutputNode * makerNode);
+  void hasLatentDB(OutputNode * makerNode);
+  void registerLatentDB(OutputNode * makerNode, shared_ptr<LatentDB> latentDB);
 
   Node * getESRParent(VentureSPPtr sp,FamilyID id);
   void registerSPFamily(VentureSPPtr sp,FamilyID id,Node * esrParent);
 
 private:
   map<Node*,shared_ptr<LatentDB> > latentDBs;
-  map<Node*,VentureValuePtr> values;
-  map<Node*,map<FamilyID,RootNodePtr> > spFamilyDBs;
+  map<OutputNode*,VentureValuePtr> values;
+  map<shared_ptr<SP>,map<FamilyID,RootNodePtr> > spFamilyDBs;
   
 };
 
--- backend/new_cxx/src/db.cxx
+++ backend/new_cxx/src/db.cxx
@@ -1,7 +1,47 @@
-bool DB::hasValue(Node * node) { throw 500; }
-VentureValuePtr DB::registerValue(Node * node) { throw 500; }
-void DB::extractValue(Node * node, VentureValuePtr value) { throw 500; }
-bool DB::hasLatentDB(Node * node) { throw 500; }
-void DB::registerLatentDB(Node * node, VentureValuePtr value) { throw 500; }
-Node * DB::getESRParent(VentureSPPtr sp,FamilyID id) { throw 500; }
-void DB::registerSPFamily(VentureSPPtr sp,FamilyID id,Node * esrParent) { throw 500; }
+  // map<Node*,shared_ptr<LatentDB> > latentDBs;
+  // map<Node*,VentureValuePtr> values;
+  // map<shared_ptr<SP>,map<FamilyID,RootNodePtr> > spFamilyDBs;
+
+bool DB::hasValue(Node * node) { return values.count(node); }
+
+VentureValuePtr DB::getValue(Node * node)
+{
+  assert(values.count(node));
+  return values[node];
+}
+
+void DB::registerValue(Node * node,VentureValuePtr value)
+{
+  assert(!values.count(node));
+  values[node] = value;
+}
+
+bool DB::hasLatentDB(OutputNode * makerNode)
+{
+  return latentDBs.count(makerNode);
+}
+
+void DB::getLatentDB(OutputNode * makerNode)
+{
+  assert(latentDBs.count(makerNode));
+  return latentDBs[makerNode];
+}
+
+void DB::registerLatentDB(OutputNode * makerNode, shared_ptr<LatentDB> latentDB)
+{
+  assert(!latentDBs.count(makerNode));
+  latentDBs[makerNode] = latentDB;
+}
+
+Node * DB::getESRParent(shared_ptr<VentureSP> sp,FamilyID id)
+{
+  assert(spFamilyDBs[sp].count(id));
+  return spFamilyDBs[sp][id];
+}
+
+void DB::registerSPFamily(shared_ptr<VentureSP> sp,FamilyID id,Node * esrParent)
+{
+  assert(!spFamilyDBs[sp].count(id));
+  spFamilyDBs[sp][id] = esrParent;
+}
+
