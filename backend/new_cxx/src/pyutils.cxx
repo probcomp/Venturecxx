76b40191df09300a20611fc6387a5c2ecea802c6
Daniel Selsam <daniel.selsam@gmail.com>
2014-02-11 14:55:28 -0500

    Draft of pyutils

--- backend/new_cxx/src/pyutils.cxx
+++ backend/new_cxx/src/pyutils.cxx
@@ -1,2 +1,30 @@
-VentureValuePtr parseValue(boost::python::dict d) { throw 500; }
-VentureValuePtr parseExpression(boost::python::object o) { throw 500; }
+VentureValuePtr parseValue(boost::python::dict d)
+{
+  if (d["type"] == "boolean") { return new VentureBool(boost::python::extract<bool>(d["value"])); }
+  else if (d["type"] == "number") { return new VentureNumber(boost::python::extract<double>(d["value"])); }
+  else if (d["type"] == "symbol") { return new VentureSymbol(boost::python::extract<string>(d["value"])); }
+  else if (d["type"] == "atom") { return new VentureAtom(boost::python::extract<uint32_t>(d["value"])); }
+  else { assert(false); }
+}
+
+
+VentureValuePtr parseExpression(boost::python::object o)
+{
+  boost::python::extract<boost::python::dict> getDict(o);
+  if (getDict.check()) { return parseValue(getDict()); }
+  
+  boost::python::extract<boost::python::list> getList(o);
+  assert(getList.check());
+  
+  boost::python::list l = getList();
+  
+  vector<VentureValuePtr> exp;
+  
+  boost::python::ssize_t L = boost::python::len(l);
+  
+  for(boost::python::ssize_t i=0; i<L; i++)
+  {
+    exp.push_back(parseExpression(l[i-1]));
+  }
+  return new VentureArray(exp);
+}
