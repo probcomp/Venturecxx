ab63fd3fa169c907cc70a4a5ba46c294445ef93f
Alexey Radul <axch@mit.edu>
2014-02-11 12:08:27 -0500

    Crash-test pgibbs more.

--- test/inference_language/test_pgibbs.py
+++ test/inference_language/test_pgibbs.py
@@ -2,12 +2,13 @@ import math
 import scipy.stats as stats
 from venture.test.stats import statisticalTest, reportKnownContinuous
 from venture.test.config import get_ripl, collectSamples, ignore_inference_quality
-from nose.plugins.attrib import attr
 
-# TODO N needs to be managed here more intelligently
-@statisticalTest
-@attr('slow')
 def testPGibbsBlockingMHHMM1():
+  yield checkPGibbsBlockingMHHMM1, True
+  yield checkPGibbsBlockingMHHMM1, False
+
+@statisticalTest
+def checkPGibbsBlockingMHHMM1(mutate):
   """The point of this is that it should give reasonable results in very few transitions but with a large number of particles."""
   ripl = get_ripl()
 
@@ -30,7 +31,12 @@ def testPGibbsBlockingMHHMM1():
   ripl.observe("y4",5.0)
   ripl.predict("x4",label="pid")
 
-  predictions = collectSamples(ripl,"pid",infer={"kernel":"pgibbs","transitions":10,"scope":0,"block":"ordered","particles":20})
+  if ignore_inference_quality():
+    infer = {"kernel":"pgibbs","transitions":2,"scope":0,"block":"ordered","particles":3, "with_mutation":mutate}
+  else:
+    infer = {"kernel":"pgibbs","transitions":10,"scope":0,"block":"ordered","particles":20, "with_mutation":mutate}
+
+  predictions = collectSamples(ripl,"pid",infer=infer)
   cdf = stats.norm(loc=390/89.0, scale=math.sqrt(55/89.0)).cdf
   return reportKnownContinuous(cdf, predictions, "N(4.382, 0.786)")
 
