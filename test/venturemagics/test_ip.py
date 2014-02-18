eb94478d6087ff00b02996e5d960c08b48423bb9
Owain Evans <owain@mit.edu>
2014-02-18 07:23:22 -0500

    now with start engines in test_ddip

--- test/venturemagics/test_ip.py
+++ test/venturemagics/test_ip.py
@@ -2,16 +2,16 @@ import subprocess,time
 from IPython.parallel import Client
 print 'RUNNING TEST_IP'
 no_engines = 2
-try: 
-    subprocess.Popen(['ipcluster', 'start', '--n=%i' % no_engines])
-    print 'SUBPROCESS IPCLUS START SUCCESS'
-except: assert False,"subprocess.Popen(['ipcluster', 'start', '--n=%i' % no_engines])"
-time.sleep(10)
-cli = Client()
-print 'CLI IDS:',cli.ids
-dv = cli[:]
-print dv.apply(lambda:'HELLO, IM AN ENGINE')
+# try: 
+#     subprocess.Popen(['ipcluster', 'start', '--n=%i' % no_engines])
+#     print 'SUBPROCESS IPCLUS START SUCCESS'
+# except: assert False,"subprocess.Popen(['ipcluster', 'start', '--n=%i' % no_engines])"
+# time.sleep(10)
+# cli = Client()
+# print 'CLI IDS:',cli.ids
+# dv = cli[:]
+# print dv.apply(lambda:'HELLO, IM AN ENGINE')
 
-stop=subprocess.Popen(['ipcluster', 'stop'])
-stop.wait()
-print  'SUBPROCESS IPCLUS START STOP SUCCESS'
+# stop=subprocess.Popen(['ipcluster', 'stop'])
+# stop.wait()
+# print  'SUBPROCESS IPCLUS START STOP SUCCESS'
