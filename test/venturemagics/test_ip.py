3208e19c9f9d0776c0bcc84371681bdaff0f64d2
Owain Evans <owain@mit.edu>
2014-02-18 06:57:43 -0500

    minor

--- test/venturemagics/test_ip.py
+++ test/venturemagics/test_ip.py
@@ -1,7 +1,11 @@
 import subprocess
 
+print 'RUNNING TEST_IP'
 no_engines = 2
-try: subprocess.Popen(['ipcluster', 'start', '--n=%i' % no_engines])
+try: 
+    subprocess.Popen(['ipcluster', 'start', '--n=%i' % no_engines])
+    print 'SUBPROCESS IPCLUS START SUCCESS'
 except: assert False,"subprocess.Popen(['ipcluster', 'start', '--n=%i' % no_engines])"
 stop=subprocess.Popen(['ipcluster', 'stop'])
 stop.wait()
+print  'SUBPROCESS IPCLUS START STOP SUCCESS'
