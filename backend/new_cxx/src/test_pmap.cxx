41ff58e3014f473c4e691ec98e11d0de6e929cd5
vlad <vlad@localhost.home>
2014-02-18 15:48:48 -0500

    More stringent pmap test.

--- backend/new_cxx/src/test_pmap.cxx
+++ backend/new_cxx/src/test_pmap.cxx
@@ -1,12 +1,24 @@
 #include "../inc/wttree.hpp"
 
+#include<iostream>
+
+using namespace std;
+
 using wttree::PMap;
 
 int main() {
   PMap<int, int> pmap;
   
-  for (int i = 0; i < 100; ++i)
+  size_t N = 10000000;
+  
+  for (size_t i = 0; i < N; ++i)
   {
     pmap = pmap.insert(i, i);
+  }
+  
+  for(size_t i = N; --i >0;)
+  {
+    //cout << pmap.size() << endl;
+    pmap = pmap.remove(i);
   }
 }
