9bc73c46cf18a58f93b8c0e07730fe901e89ef19
Daniel Selsam <daniel.selsam@gmail.com>
2014-02-21 23:24:56 +0000

    Never compiled sketch of the beta bernoullis

---
+++ backend/new_cxx/inc/sps/betabernoulli.h
@@ -0,0 +1,74 @@
+#ifndef SPS_BETA_BERNOULLI_H
+#define SPS_BETA_BERNOULLI_H
+
+#include "types.h"
+#include "psp.h"
+#include "args.h"
+#include "sp.h"
+
+// Collapsed SPAux
+struct BetaBernoulliSPAux : SPAux
+{
+  BetaBernoulliSPAux() {}
+};
+
+struct MakeBetaBernoulliOutputPSP : PSP
+{
+  VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
+  bool childrenCanAAA() const { return true; }
+};
+
+
+struct BetaBernoulliOutputPSP : RandomPSP
+{
+ BetaBernoulliOutputPSP(double alpha, double beta) : alpha(alpha), beta(beta) {}
+
+  VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
+  double logDensity(VentureValuePtr value,shared_ptr<Args> args) const;
+  void incorporate(VentureValuePtr value,shared_ptr<Args> args) const;
+  void unincorporate(VentureValuePtr value,shared_ptr<Args> args) const;
+
+  double logDensityOfCounts(shared_ptr<SPAux> spAux) const;
+
+private:
+  double alpha;
+  double beta;
+};
+
+// Uncollapsed SPAux
+struct UBetaBernoulliSPAux : BetaBernoulliSPAux
+{
+ UCDirMultSPAux(double p): BetaBernoulliSPAux(), p(p) {}
+  double p;
+};
+
+// Uncollapsed
+struct MakeUBetaBernoulliOutputPSP : RandomPSP
+{
+  VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
+  double logDensity(VentureValuePtr value, shared_ptr<Args> args) const;
+};
+
+struct UBetaBernoulliSP : SP
+{
+  UBetaBernoulliSP(PSP * requestPSP, PSP * outputPSP): SP(requestPSP,outputPSP) {}
+
+  bool hasAEKernel() const { return true; }
+  void AEInfer(shared_ptr<Args> args,gsl_rng * rng) const;
+};
+
+struct UBetaBernoulliOutputPSP : RandomPSP
+{
+  UBetaBernoulliOutputPSP() {}
+
+  VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
+  double logDensity(VentureValuePtr value,shared_ptr<Args> args) const;
+  void incorporate(VentureValuePtr value,shared_ptr<Args> args) const;
+  void unincorporate(VentureValuePtr value,shared_ptr<Args> args) const;
+
+};
+
+
+
+
+#endif
