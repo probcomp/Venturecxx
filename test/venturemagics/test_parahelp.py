fb8ade9114cff4cd08d1e0658194dd52bdd5074d
Owain Evans <owain@mit.edu>
2014-02-21 09:10:41 -0500

    rejig

--- test/venturemagics/test_parahelp.py
+++ test/venturemagics/test_parahelp.py
@@ -3,8 +3,8 @@ import subprocess,os,time
 file_dir = os.path.dirname(os.path.realpath(__file__))
 test_file = file_dir + '/para_test.ipy'
 
-out = subprocess.checkoutput(['ipython',test_file])
-time.sleep(50)
+out = subprocess.Popen(['ipython',test_file])
+time.sleep(60)
 print 'para_est out',out,'\n'
 stop = subprocess.Popen(['ipcluster','stop'])
 out.kill()
