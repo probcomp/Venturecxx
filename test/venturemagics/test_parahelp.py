0ff223059beb03cb2d4c1d65b7ca2729dd572fb9
Owain Evans <owain@mit.edu>
2014-02-21 08:51:58 -0500

    small

--- test/venturemagics/test_parahelp.py
+++ test/venturemagics/test_parahelp.py
@@ -3,11 +3,12 @@ import subprocess,os,time
 file_dir = os.path.dirname(os.path.realpath(__file__))
 test_file = file_dir + '/para_test.ipy'
 
-out = subprocess.Popen(['ipython',test_file])
-time.sleep(30)
+out = subprocess.checkoutput(['ipython',test_file])
+time.sleep(50)
+print 'para_est out',out,'\n'
 stop = subprocess.Popen(['ipcluster','stop'])
 out.kill()
-print 'para_test output',out
+print 'para_test done'
 
 # if 'error' in out.lower() or 'assertion' in out.lower():
 #     assert False, 'Error running %s in IPython' % test_file
