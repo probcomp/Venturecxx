51dc00ec87ac15c3a76632c66cf350b3820a71db
Daniel Selsam <daniel.selsam@gmail.com>
2014-02-22 05:57:52 +0000

    Added dirichlet

--- backend/new_cxx/inc/sps/discrete.h
+++ backend/new_cxx/inc/sps/discrete.h
@@ -16,8 +16,14 @@ struct CategoricalOutputPSP : RandomPSP
   double logDensity(VentureValuePtr value, shared_ptr<Args> args) const;
 };
 
-// TODO not implemented
+
 struct SymmetricDirichletOutputPSP : RandomPSP
+{
+  VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
+  double logDensity(VentureValuePtr value,shared_ptr<Args> args) const;
+};
+
+struct DirichletOutputPSP : RandomPSP
 {
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
   double logDensity(VentureValuePtr value,shared_ptr<Args> args) const;
