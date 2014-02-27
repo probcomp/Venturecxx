d61da35c702078b68d5877acac5c31ef6f65d274
Alexey Radul <axch@mit.edu>
2014-02-27 15:26:35 -0500

    Draft a test for rejection sampling of Gaussians that checks all configurations of parameter availability.

--- test/inference_language/test_gotchas.py
+++ test/inference_language/test_gotchas.py
@@ -17,3 +17,33 @@ def testOuterMix1():
   predictions = collectSamples(ripl,1)
   ans = [(1,.5), (2,.25), (3,.25)]
   return reportKnownDiscrete(ans, predictions)
+
+def testRejectNormal1():
+  """Rejection sampling shouldn't work if both mean and variance of a
+  normal are subject to change; shouldn't work if the mean is known
+  but the variance and the output are unknown; but still should work
+  if the mean and the output are known even if the variance is not
+  (unless the mean and the output are exactly equal).
+
+  TODO Actually, the logDensityBound of normal is well-defined as long
+  as the variance is bounded away from zero, but that seems too hard
+  to chase down."""
+  for incl_mu in [False, True]:
+    for incl_sigma in [False, True]:
+      for incl_out in [False, True]:
+        yield checkRejectNormal, incl_mu, incl_sigma, incl_out
+
+def checkRejectNormal(incl_mu, incl_sigma, incl_out):
+  # Sadly, there doesn't seem to be a pretty way to arrange the scopes
+  # and blocks such that I can easily control all samples, except by
+  # metaprogramming.
+  def maybewrap(command, doit):
+    if doit:
+      return "(scope_include (quote scaffold) 0 %s)" % command
+    else:
+      return command
+  ripl = get_ripl()
+  ripl.assume("mu", maybewrap("(normal 0 1)", incl_mu))
+  ripl.assume("sigma", maybewrap("(uniform_continuous 0 10)", incl_sigma))
+  ripl.assume("out", maybewrap("(normal mu sigma)", incl_out))
+  ripl.infer("(reject scaffold 0 1)")
