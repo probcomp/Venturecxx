44d9071e7e0064d203782e01604fbd57d868a9c9
Daniel Selsam <daniel.selsam@gmail.com>
2014-03-04 22:44:45 +0000

    Added failing test case

--- test/conformance/test_forget.py
+++ test/conformance/test_forget.py
@@ -1,12 +1,22 @@
 from venture.test.config import get_ripl, collectSamples, defaultKernel
 from nose.tools import eq_
 
-def testForgetContinuousInference():
+def testForgetContinuousInference1():
+  ripl = get_ripl()
+  for i in range(10):
+    pid = "pid%d" % i
+    print pid
+    ripl.predict("(flip)",label=pid)
+    ripl.forget(pid)
+
+def testForgetContinuousInference2():
   ripl = get_ripl()
   ripl.predict("(flip)")
-  ripl.predict("(flip)")
-  ripl.infer(10)
+#  ripl.predict("(flip)")
+#  ripl.infer(10)
   ripl.forget(1)
-  ripl.forget(2)
+#  ripl.forget(2)
   ripl.predict("(flip)")
-  ripl.infer(10)
+#  ripl.infer(10)
+
+
