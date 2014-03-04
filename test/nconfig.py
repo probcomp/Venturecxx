28714478070007d98d87f862c61518964614e920
Vlad Firoiu <vladfi2@gmail.com>
2014-03-04 15:10:30 -0500

    Test mh instead of pgibbs by default.

--- test/nconfig.py
+++ test/nconfig.py
@@ -6,5 +6,6 @@ config["num_transitions_per_sample"] = 100
 config["should_reset"] = False
 config["get_ripl"] = "cxx"
 config["global_reporting_threshold"] = 0.001
-config["infer"] = "(pgibbs default ordered 2 5)"
+#config["infer"] = "(pgibbs default ordered 2 5)"
+config["infer"] = "(mh default one 10)"
 config["ignore_inference_quality"] = False
