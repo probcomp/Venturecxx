c79fa4d7db3deed9e1d5327ad315610f82933478
Alexey Radul <axch@mit.edu>
2014-03-03 19:35:53 -0500

    Let's define the intial state argument of the hmm to be a simplex point (not a matrix).

--- test/inference_quality/micro/test_latents.py
+++ test/inference_quality/micro/test_latents.py
@@ -10,7 +10,7 @@ def testHMMSP1():
   ripl = get_ripl()
   ripl.assume("f","""
 (make_lazy_hmm
- (matrix (array 0.5 0.5))
+ (simplex 0.5 0.5)
  (matrix (array (array 0.7 0.3)
                (array 0.3 0.7)))
  (matrix (array (array 0.9 0.2)
@@ -35,13 +35,13 @@ def testHMMSP2():
   ripl.assume("f","""
 (if (flip)
 (make_lazy_hmm
- (matrix (array 0.5 0.5))
+ (simplex 0.5 0.5)
  (matrix (array (array 0.7 0.3)
                (array 0.3 0.7)))
  (matrix (array (array 0.9 0.2)
                (array 0.1 0.8))))
 (make_lazy_hmm
- (matrix (array 0.5 0.5))
+ (simplex 0.5 0.5)
  (matrix (array (array 0.7 0.3)
                (array 0.3 0.7)))
  (matrix (array (array 0.9 0.2)
