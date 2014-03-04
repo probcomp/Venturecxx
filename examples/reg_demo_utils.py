a24b3d050b669b59418e26281744630c665e1141
Owain Evans <owain@mit.edu>
2014-03-04 13:59:47 -0500

    reg demo changes

--- examples/reg_demo_utils.py
+++ examples/reg_demo_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 from scipy import stats
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
 def hexbin_plot(x,y):
     fig,ax = subplots()
@@ -52,36 +52,7 @@ pivot_model='''
 [assume y (mem (lambda (i) (y_x (x i))  ))] 
                      
 [assume n (gamma 1 1) ]
-'''
-
-pivot_model_old='''
-[assume w0 (mem (lambda (p)(normal 0 3))) ]
-[assume w1 (mem (lambda (p)(normal 0 3))) ]
-[assume w2 (mem (lambda (p)(normal 0 1))) ]
-[assume noise (mem (lambda (p) (gamma 2 1) )) ]
-[assume pivot (normal 0 5)]
-[assume p (lambda (x) (if (< x pivot) 0 1) ) ]
-
-[assume f (lambda (x w0 w1 w2) (+ w0 (* w1 x) (* w2 (* x x) ) ) ) ]
-
-[assume f2 (lambda (x)
-             ( (lambda (p) (+ (w0 p) (* (w1 p) x) (* (w2 p) (* x x)))  ) 
-               (p x)  ) ) ]
-
-[assume noise_p (lambda (fx p) (normal fx (noise p)) )] 
-
-[assume y_x (lambda (x) 
-                ( (lambda (p) (noise_p  (f x (w0 p) (w1 p) (w2 p)) p ) )
-                     (p x) ) ) ]
-
-[assume y (mem (lambda (i) (noise_p  (f (x i) (w0 (p (x i))) (w1 (p (x i))) (w2 (p (x i)) ))  (p (x i)) ) ) ) ]
-
-[assume y2 (mem (lambda (i) 
-                ( (lambda (x p)
-                    (noise_p  (f x (w0 p) (w1 p) (w2 p)) p ) )
-                     (x i) (p (x i)) 
-                     ) ) ) ]
-[assume n (gamma 1 1) ]
+[assume model_name (quote pivot)]
 '''
 
 pivot_check='''
@@ -92,25 +63,25 @@ pivot_check='''
 [observe (w2 0) 0.]
 [observe (noise 0) .01]'''
 
-def test_pivot(v):
-    v.execute_program(pivot_check)
-    v.infer(1)
-    assert v.predict('(= 0 (p (x 0)))')
-    assert .1 > (0 - v.predict('(f (x 0))'))
-    assert .5 > (0 - v.assume('y0','(y 0)') ) # y0 close to 0
-    assert .5 > (0 - v.predict('(y_x (x 0))'))
-    
-    f= np.array( [v.predict('(f %i)' % i) for i in range(5)] )
-    assert all( 0.1 > np.abs(f - np.arange(5)) )
-    [v.observe('(y %i)' % i, str(i+.01) ) for i in range(20,25)]
-    y_x20 = np.array( [v.predict('(y_x %i)' % i) for i in range(20,25)] )
-    y20 = np.array( [v.predict('(y %i)' % i) for i in range(20,25)] )
+def test_pivot():
+    v_crp=mk_c(); v_crp.execute_program(x_model_crp + pivot_model)
+    v_t=mk_l(); v_t.execute_program(x_model_t+pivot_model)
+    vs=[v_t,v_crp]
+    for v in vs:
+        v.execute_program(pivot_check)
+        v.infer(1)
+        assert v.predict('(= 0 (p (x 0)))')
+        assert .1 > (0 - v.predict('(f (x 0))'))
+        assert .5 > (0 - v.assume('y0','(y 0)') ) # y0 close to 0
+        assert .5 > (0 - v.predict('(y_x (x 0))'))
+
+        f= np.array( [v.predict('(f %i)' % i) for i in range(5)] )
+        assert all( 0.1 > np.abs(f - np.arange(5)) )
+        [v.observe('(y %i)' % i, str(i+.01) ) for i in range(20,25)]
+        y_x20 = np.array( [v.predict('(y_x %i)' % i) for i in range(20,25)] )
+        y20 = np.array( [v.predict('(y %i)' % i) for i in range(20,25)] )
     #assert all( [ 2 > (y_x20[i] - y20[i]) for y_x20,y20 ] ) 
 
-v_crp=mk_c(); v_crp.execute_program(x_model_crp+pivot_model)
-v_t=mk_l(); v_t.execute_program(x_model_t+pivot_model)
-vs=[v_t,v_crp]
-[test_pivot(v) for v in vs]
 
 quad_fourier_model='''
 [assume w0 (normal 0 3) ]
@@ -128,7 +99,7 @@ quad_fourier_model='''
 [assume y_x (lambda (x) (normal (f x) noise) ) ]
 [assume y (mem (lambda (i) (normal (f (x i) ) noise) ) )]
 [assume n (gamma 1 1)]
-'''
+[assume model_name (quote quad_fourier)]'''
 quad_fourier_checks='''
 [observe (x 0) 0.]
 [observe w0 0.]
@@ -139,6 +110,8 @@ quad_fourier_checks='''
 '''
 
 def test_quad_fourier(v):
+    v=mk_c()
+    v.execute_program(x_model_t + quad_fourier_model)
     v.execute_program(quad_fourier_checks)
     v.infer(1)
     assert .1 > abs( 0 - v.predict('(x 0)') )
@@ -149,11 +122,7 @@ def test_quad_fourier(v):
     assert all( [ .5 > (xy[0] - xy[1]) for xy in xys ] )
     assert all( [ xf[1] - xy[1] for (xf,xy) in zip(xfs,xys) ] )    
     assert .1 > ( v.predict('(y 0)') - v.predict('(y_x 0)') )
-    print xfs,xys
 
-[v.clear() for v in vs]
-[v.execute_program(x_model_t + quad_fourier_model) for v in vs]
-#[test_quad_fourier(v) for v in vs]
 
 
 
@@ -188,7 +157,9 @@ def observe_infer(vs,xys,no_transitions,withn=True):
 
 def logscores(mr,name='Model'):
     logscore = mr.get_global_logscore()
-    print '%s mean, max logscore:' % name, np.mean(logscore), np.max(logscore)
+    try: name=mr.sample('model_name')
+    except: pass
+    print '%s logscore: (mean, max) ' % name, np.mean(logscore), np.max(logscore)
     return np.mean(logscore), np.max(logscore)
 
 
@@ -198,39 +169,60 @@ def plot_cond(ripl,no_reps=50):
     n = int( np.round( ripl.sample('n') ) )  #FIXME
     xs = [ripl.sample('(x %i)' % i) for i in range(n)]
     ys = [ripl.sample('(y %i)' % i) for i in range(n)]
-    xr = np.linspace(1.5*min(xs),1.5*max(xs),100)
+    xr = np.linspace(1.5*min(xs),1.5*max(xs),20)
     f_xr = [ripl.sample('(f %f)' % x) for x in xr]
     
     # gaussian noise 1sd
-    noise = ripl.sample('noise')
+    try: model_name = ripl.sample('model_name')
+    except: model_name = 'anon model'
+    
+    noise=ripl.sample('(noise 0)') if model_name=='pivot' else ripl.sample('noise')
     f_a = [fx+noise for fx in f_xr]
     f_b = [fx-noise for fx in f_xr]
 
     # scatter for y conditional on x
     y_x = [  [ripl.sample('(y_x %f)' % x) for r in range(no_reps)] for x in xr]
-    
-    
     fig,ax = plt.subplots(1,2,figsize=(9,2),sharex=True,sharey=True)
     ax[0].scatter(xs,ys)
     ax[0].set_color_cycle(['m', 'gray','gray'])
     ax[0].plot(xr,f_xr,xr,f_a,xr,f_b)
-    ax[0].set_title('Data and inferred f with 1sd noise')
+    ax[0].set_title('Data and inferred f with 1sd noise (name= %s )' % model_name)
     
-    [ax[1].scatter(xr,[y[i] for y in y_x]) for i in range(len(y_x))]
-    ax[1].set_title('Conditionals of y given x')
+    [ ax[1].scatter(xr,[y[i] for y in y_x],s=6) for i in range(no_reps) ]
+    ax[1].set_title('Conditionals of y given x (name= %s)' % model_name)
     
     return None
 
+def if_lst_flatten(l):
+    if type(l[0])==list: return [el for subl in l for el in subl]
+    return l
 
-def test_funcs():
-    xys = generate_data(10,[0,3],[0,0,1,0,0],False)
-    vc = mk_l(); vl=mk_l(); vs = [vc,vl]
-    vc.execute_program(x_model_t+pivot_model); vl.execute_program(x_model_t+quad_fourier_model)
+def test_funcs(mripl=False):
+    xys = generate_data(n=14,xparams=[0,3],yparams=[0,0,1,0,0],sin=False) # y=x^2
+    if mripl:
+        v_piv = MRipl(2,lite=lite,verbose=False); v_fo = MRipl(2,lite=lite,verbose=False)
+        vs = [v_piv,v_fo]
+    else:
+        v_piv = mk_c(); v_fo=mk_c(); vs = [v_piv,v_fo]
+    v_piv.execute_program(x_model_t+pivot_model); v_fo.execute_program(x_model_t+quad_fourier_model)
+
     observe_infer(vs,xys,300,withn=True)
+
     [logscores(v) for v in vs]
-    [plot_cond(v) for v in vs]
 
+    if mripl:
+        [mr_map_nomagic(v,plot_cond,limit=1) for v in vs]
+    else:
+        [plot_cond(v) for v in vs]
 
+    # in-sample guess should be close to true vals after enough inference
+    [v.infer(300) for v in vs]
+    f0 = if_lst_flatten( [v.predict('(f 0)') for v in vs] )
+    f1 = if_lst_flatten( [v.predict('(f 1)') for v in vs] )
+    f1 = if_lst_flatten( [v.predict('(f -1)') for v in vs] )
+    assert all( 6 > np.abs((0 - np.array(f0) ) ))
+    assert all( 6 > np.abs((1 - np.array(f1) ) ))
+    assert all( 6 > np.abs((1 - np.array(f1) ) ))
 
 ### PLAN: different plots/scores
 #1. av logscore and best logscore.
