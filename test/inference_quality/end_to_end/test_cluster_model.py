0c99a279124f0a78c5ebe05b00e9c71ef96d3701
Alexey Radul <axch@mit.edu>
2014-03-04 22:50:33 -0500

    Style.

--- test/inference_quality/end_to_end/test_cluster_model.py
+++ test/inference_quality/end_to_end/test_cluster_model.py
@@ -15,11 +15,11 @@ def testCRPMix1a():
   ripl.assume('cv_shape', 4.0)
   ripl.assume('cv_scale', "(inv_gamma 1.0 1.0)")
         
-  ripl.assume('get_cluster_mean', "(mem (lambda (cluster) (uniform_continuous -10.0 10.0)))");
-  ripl.assume('get_cluster_variance', "(mem (lambda (cluster) (inv_gamma cv_shape cv_scale)))");
-  ripl.assume('get_cluster', "(mem (lambda (point) (cluster_crp)))");
-  ripl.assume('sample_cluster', "(lambda (cluster) (normal (get_cluster_mean cluster) (get_cluster_variance cluster)))");
-  ripl.assume('get_point', "(mem (lambda (point) (sample_cluster (get_cluster point))))");
+  ripl.assume('get_cluster_mean', "(mem (lambda (cluster) (uniform_continuous -10.0 10.0)))")
+  ripl.assume('get_cluster_variance', "(mem (lambda (cluster) (inv_gamma cv_shape cv_scale)))")
+  ripl.assume('get_cluster', "(mem (lambda (point) (cluster_crp)))")
+  ripl.assume('sample_cluster', "(lambda (cluster) (normal (get_cluster_mean cluster) (get_cluster_variance cluster)))")
+  ripl.assume('get_point', "(mem (lambda (point) (sample_cluster (get_cluster point))))")
 
   ripl.predict("(get_cluster_mean (get_cluster 1))",label="cmean")
   ripl.observe("(get_point 1)","5")
@@ -29,7 +29,7 @@ def testCRPMix1a():
 
   mus = []
 
-  for t in range(T):
+  for _ in range(T):
     ripl.infer(B)
     mus.append(ripl.report("cmean"))
 
@@ -59,7 +59,7 @@ def testCRPMix1b():
 
   mus = []
 
-  for t in range(T):
+  for _ in range(T):
     ripl.infer(B)
     mus.append(ripl.report("cmean"))
 
