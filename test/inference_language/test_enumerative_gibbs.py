04e52d7f1ba1e6d3a30c33a7fa08c774c719677f
Daniel Selsam <daniel.selsam@gmail.com>
2014-03-06 04:26:15 +0000

    Tweaks to test_enumerative_gibbs

--- test/inference_language/test_enumerative_gibbs.py
+++ test/inference_language/test_enumerative_gibbs.py
@@ -6,17 +6,15 @@ from testconfig import config
 @statisticalTest
 def testEnumerativeGibbsBasic1():
   """Basic sanity test"""
-  if config["get_ripl"] != "lite": raise SkipTest("This test is not supported by CXX yet")
   ripl = get_ripl()
   ripl.predict("(bernoulli)",label="pid")
 
-  predictions = collectSamples(ripl,"pid",None,{"kernel":"gibbs"})
+  predictions = collectSamples(ripl,"pid",infer={"kernel":"gibbs"})
   ans = [(True,.5),(False,.5)]
   return reportKnownDiscrete(ans, predictions)
 
 def testEnumerativeGibbsGotcha():
   """Enumeration should not break on things that look like they're in the support but aren't"""
-  raise SkipTest("Issue: https://app.asana.com/0/9277419963067/10464755327019")
   ripl = get_ripl()
   ripl.predict("(bernoulli 1)")
   ripl.predict("(bernoulli 0)")
@@ -28,7 +26,6 @@ def testEnumerativeGibbsXOR1():
   """Tests that an XOR chain mixes with enumerative gibbs.
      Note that with RESET=True, this will seem to mix with MH.
      The next test accounts for that."""
-  if config["get_ripl"] != "lite": raise SkipTest("This test is not supported by CXX yet")
   ripl = get_ripl()
 
   ripl.assume("x","(scope_include 0 0 (bernoulli 0.001))",label="pid")
@@ -42,7 +39,6 @@ def testEnumerativeGibbsXOR1():
 @statisticalTest
 def testEnumerativeGibbsXOR2():
   """Tests that an XOR chain mixes with enumerative gibbs."""
-  if config["get_ripl"] != "lite": raise SkipTest("This test is not supported by CXX yet")
   ripl = get_ripl()
 
   ripl.assume("x","(scope_include 0 0 (bernoulli 0.0015))",label="pid")
@@ -56,16 +52,15 @@ def testEnumerativeGibbsXOR2():
 @statisticalTest
 def testEnumerativeGibbsXOR3():
   """A regression catching a mysterious math domain error."""
-  if config["get_ripl"] != "lite": raise SkipTest("This test is not supported by CXX yet")
   ripl = get_ripl()
 
   ripl.assume("x","(scope_include 0 0 (bernoulli 0.0015))",label="pid")
   ripl.assume("y","(scope_include 0 0 (bernoulli 0.0005))")
-  ripl.assume("noisy_true","(lambda (pred noise) (flip (if pred 1.0 noise)))")
+  ripl.assume("noisy_true","(lambda (pred noise) (scope_include 0 0 (flip (if pred 1.0 noise))))")
   # This predict is the different between this test and
   # testEnumerativeGibbsXOR2, and currently causes a mystery math
   # domain error.
-  raise SkipTest("Issue https://app.asana.com/0/9277419963067/10249544822516")
+
   ripl.predict("(noisy_true (= (+ x y) 1) .000001)")
   ripl.observe("(noisy_true (= (+ x y) 1) .000001)","true")
   predictions = collectSamples(ripl,"pid",None,{"kernel":"gibbs","scope":0,"block":0})
