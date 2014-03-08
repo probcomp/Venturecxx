dac1825e6484a45353ee9939d8922691e95390af
Daniel Selsam <daniel.selsam@gmail.com>
2014-03-07 22:55:50 -0500

    Copied (not ideal) some basic tests and made them use slice

--- test/inference_language/test_slice.py
+++ test/inference_language/test_slice.py
@@ -15,6 +15,8 @@ def testSliceBasic1():
   cdf = stats.norm(loc=10, scale=1).cdf
   return reportKnownContinuous(cdf, predictions, "N(10,1.0))")
 
+## TODO these tests are repeats from test_basic_stats
+## better to have a test_basic_continuous file that iterates over kernels
 @statisticalTest
 def testSliceNormalWithObserve1():
   if config["get_ripl"] != "lite": raise SkipTest("Slice only implemented in lite")
@@ -28,3 +30,68 @@ def testSliceNormalWithObserve1():
   predictions = collectSamples(ripl,"pid",infer_merge={"kernel":"slice"})
   cdf = stats.norm(loc=12, scale=math.sqrt(0.5)).cdf
   return reportKnownContinuous(cdf, predictions, "N(12,sqrt(1.5))")
+
+@statisticalTest
+def testSliceNormalWithObserve2a():
+  "Checks the posterior distribution on a Gaussian given an unlikely observation.  The difference between this and 1 is an extra predict, which apparently has a deleterious effect on mixing."
+  ripl = get_ripl()
+  ripl.assume("a", "(normal 10.0 1.0)")
+  ripl.observe("(normal a 1.0)", 14.0)
+  # Posterior for a is normal with mean 12, precision 2
+  ripl.predict("(normal a 1.0)")
+
+  predictions = collectSamples(ripl,1,infer_merge={"kernel":"slice"})
+  cdf = stats.norm(loc=12, scale=math.sqrt(0.5)).cdf
+  return reportKnownContinuous(cdf, predictions, "N(12,sqrt(0.5))")
+
+@statisticalTest
+def testSliceNormalWithObserve2b():
+  "Checks the posterior distribution on a Gaussian given an unlikely observation"
+  ripl = get_ripl()
+  ripl.assume("a", "(normal 10.0 1.0)")
+  ripl.observe("(normal a 1.0)", 14.0)
+  # Posterior for a is normal with mean 12, precision 2
+  ripl.predict("(normal a 1.0)")
+
+  predictions = collectSamples(ripl,3,infer_merge={"kernel":"slice"})
+  cdf = stats.norm(loc=12, scale=math.sqrt(1.5)).cdf
+  return reportKnownContinuous(cdf, predictions, "N(12,sqrt(1.5))")
+
+@statisticalTest
+def testSliceStudentT1():
+  "Simple program involving simulating from a student_t"
+  ripl = get_ripl()
+  ripl.assume("a", "(student_t 1.0)")
+  ripl.observe("(normal a 1.0)", 3.0)
+  predictions = collectSamples(ripl,1,infer_merge={"kernel":"slice"})
+
+  # Posterior of a is proprtional to
+  def postprop(a):
+    return stats.t(1).pdf(a) * stats.norm(loc=3).pdf(a)
+  import scipy.integrate as integrate
+  (normalize,_) = integrate.quad(postprop, -10, 10)
+  def posterior(a): return postprop(a) / normalize
+  (meana,_) = integrate.quad(lambda x: x * posterior(x), -10, 10)
+  (meanasq,_) = integrate.quad(lambda x: x * x * posterior(x), -10, 10)
+  vara = meanasq - meana * meana
+  return reportKnownMeanVariance(meana, vara, predictions)
+
+@statisticalTest
+def testSliceStudentT2():
+  "Simple program involving simulating from a student_t"
+  ripl = get_ripl()
+  ripl.assume("a", "(student_t 1.0)")
+  ripl.observe("(normal a 1.0)", 3.0)
+  ripl.predict("(normal a 1.0)")
+  predictions = collectSamples(ripl,3,infer="mixes_slowly",infer_merge={"kernel":"slice"})
+
+  # Posterior of a is proprtional to
+  def postprop(a):
+    return stats.t(1).pdf(a) * stats.norm(loc=3).pdf(a)
+  import scipy.integrate as integrate
+  (normalize,_) = integrate.quad(postprop, -10, 10)
+  def posterior(a): return postprop(a) / normalize
+  (meana,_) = integrate.quad(lambda x: x * posterior(x), -10, 10)
+  (meanasq,_) = integrate.quad(lambda x: x * x * posterior(x), -10, 10)
+  vara = meanasq - meana * meana
+  return reportKnownMeanVariance(meana, vara + 1.0, predictions)
