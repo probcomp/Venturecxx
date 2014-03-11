129e49a660ef336174000899f32386a7478e979e
Alexey Radul <axch@mit.edu>
2014-03-10 23:17:29 -0400

    Teach the inference instruction parser about slice sampling.

--- python/lib/ripl/utils.py
+++ python/lib/ripl/utils.py
@@ -194,6 +194,9 @@ def expToDict(exp):
   elif tag == "func-mh":
     assert len(exp) == 4
     return {"kernel":"mh","scope":exp[1],"block":exp[2],"transitions":exp[3],"with_mutation":False}
+  elif tag == "slice":
+    assert len(exp) == 4
+    return {"kernel":"slice","scope":exp[1],"block":exp[2],"transitions":exp[3],"with_mutation":True}
   elif tag == "pgibbs":
     assert len(exp) == 5
     return {"kernel":"pgibbs","scope":exp[1],"block":exp[2],"particles":exp[3],"transitions":exp[4],"with_mutation":True}
