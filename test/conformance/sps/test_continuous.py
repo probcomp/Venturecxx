eb67801a353effe56a1e6fe7904c41a229ed913a
Alexey Radul <axch@mit.edu>
2014-03-16 15:15:33 -0400

    Smoke test MVGaussian a little more.

--- test/conformance/sps/test_continuous.py
+++ test/conformance/sps/test_continuous.py
@@ -1,5 +1,19 @@
 from nose.tools import eq_
-from venture.test.config import get_ripl
+import scipy.stats as stats
+from venture.test.stats import statisticalTest, reportKnownContinuous
+from venture.test.config import get_ripl, collectSamples
 
 def testMVGaussSmoke():
-  eq_(get_ripl().predict("(is_array (multivariate_normal (array 1 2) (matrix (list (list 3 4) (list 5 6)))))"), True)
+  eq_(get_ripl().predict("(is_array (multivariate_normal (array 1 2) (matrix (list (list 3 4) (list 4 6)))))"), True)
+
+@statisticalTest
+def testMVGaussPrior():
+  """Confirm that projecting a multivariate Gaussian to one dimension
+  results in a univariate Gaussian."""
+  ripl = get_ripl()
+  ripl.assume("vec", "(multivariate_normal (array 1 2) (matrix (list (list 1 2) (list 2 1))))")
+  ripl.predict("(lookup vec 0)")
+
+  predictions = collectSamples(ripl, 2)
+  cdf = stats.norm(loc=1, scale=1).cdf
+  return reportKnownContinuous(cdf, predictions, "N(1,1)")
