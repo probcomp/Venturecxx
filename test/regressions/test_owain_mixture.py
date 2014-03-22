b6353528c09d1bc40cfc54c0ec52f21faaa5d9e8
owainevans <owain@mit.edu>
2014-03-22 17:22:26 -0400

    Update test_owain_mixture.py

--- test/regressions/test_owain_mixture.py
+++ test/regressions/test_owain_mixture.py
@@ -21,7 +21,36 @@ def testOwainMixture():
     ripl.observe('(y %i)' %i, '%f' % 2.0)
 
   for i in range(10,20):
-    ripl.observe('(x %i)' %i, '%f' % 3.0)
-    ripl.predict('(y %i)' %i)    
+    ripl.observe('(x %i)'%i,'%f' % 3)
+    ripl.infer(10)
+    [ripl.predict('(y %i)'%i) for r in range(20) ]  
+
+  ripl.infer(100)
+  
+  
+  ## without memoizing y's
+
+  ripl = get_ripl()
+  ripl.assume("alpha","(uniform_continuous .01 1)")
+  ripl.assume("crp","(make_crp alpha)")
+  ripl.assume("z","(mem (lambda (i) (crp) ) )")
+  ripl.assume("mu","(mem (lambda (z) (normal 0 5) ) )")
+  ripl.assume("sig","(mem (lambda (z) (uniform_continuous .1 8) ) )")
+
+  ripl.assume("x","(mem (lambda (i) (normal (mu (z i)) (sig (z i))))  )")
+
+  ripl.assume("w","(lambda (z) 1)")
+  ripl.assume("f","(lambda (z x) (* (w z) x) )")
+
+  ripl.assume("y","(lambda (i) (normal (f (z i) (x i)) .1) )")
+
+  for i in range(10):
+    ripl.observe('(x %i)' %i, '%f' % 1.0)
+    ripl.observe('(y %i)' %i, '%f' % 2.0)
+
+  for i in range(10,20):
+    ripl.observe('(x %i)'%i,'%f' % 3)
+    ripl.infer(10)
+    [ripl.predict('(y %i)'%i) for r in range(20) ]  
 
   ripl.infer(100)
