4bd728310a6320e7b4b9dbf4478a4d560b0b808e
Alexey Radul <axch@mit.edu>
2014-04-04 13:14:13 -0400

    Making sure methods are overridden.

--- backend/lite/scope.py
+++ backend/lite/scope.py
@@ -1,9 +1,14 @@
+from utils import override
 from psp import DeterministicPSP
 
 class ScopeIncludeOutputPSP(DeterministicPSP):
+  @override(DeterministicPSP)
   def simulate(self,args): return args.operandValues[2]
+  @override(DeterministicPSP)
   def gradientOfSimulate(self, _args, _value, direction): return [0, 0, direction]
+  @override(DeterministicPSP)
   def canAbsorb(self, _trace, appNode, parentNode): return parentNode != appNode.operandNodes[2]
   
+  @override(DeterministicPSP)
   def description(self,name):
     return "%s :: <SP <scope> <block> <object> -> <object>>\n  Returns its third argument unchanged at runtime, but tags the subexpression creating the object as being within the given scope and block." % name
