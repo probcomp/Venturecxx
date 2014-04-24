43b3abe132312e9963b1d06668817a59eb6a4e06
Alexey Radul <axch@mit.edu>
2014-04-24 14:20:41 -0400

    It seems that scipy.stats was intended here.

--- backend/lite/continuous.py
+++ backend/lite/continuous.py
@@ -64,7 +64,7 @@ class InverseWishartPSP(RandomPSP):
     if (dof <= 81+n) and (dof == np.round(dof)):
         x = np.random.randn(dof,n)
     else:
-        x = np.diag(np.sqrt(stats.chi2.rvs(dof-(np.arange(n)))))
+        x = np.diag(np.sqrt(scipy.stats.chi2.rvs(dof-(np.arange(n)))))
         x[np.triu_indices_from(x,1)] = np.random.randn(n*(n-1)/2)
     R = np.linalg.qr(x,'r')
     T = scipy.linalg.solve_triangular(R.T,chol.T).T
