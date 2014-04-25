414f0226f91185b91b4e140940a7ea12c1469b2a
Alexey Radul <axch@mit.edu>
2014-04-25 16:23:23 -0400

    Take over the old names.

--- test/conformance/sps/test_types.py
+++ test/conformance/sps/test_types.py
@@ -5,33 +5,33 @@ from venture.lite.psp import NullRequestPSP
 from nose import SkipTest
 from nose.tools import eq_
 
-def testTypes2():
+def testTypes():
   for (name,sp) in builtInSPsList():
     if isinstance(sp.requestPSP, NullRequestPSP):
-      yield propTypeCorrect2, name, sp
+      yield propTypeCorrect, name, sp
 
-def propTypeCorrect2(_name, sp):
+def propTypeCorrect(_name, sp):
   type_ = sp.venture_type()
-  checkTypedProperty(helpPropTypeCorrect2, fully_uncurried_sp_type(type_), sp, type_)
+  checkTypedProperty(helpPropTypeCorrect, fully_uncurried_sp_type(type_), sp, type_)
 
-def helpPropTypeCorrect2(args_lists, sp, type_):
+def helpPropTypeCorrect(args_lists, sp, type_):
   if len(args_lists) == 0:
     pass # OK
   else:
     args = r.BogusArgs(args_lists[0], sp.constructSPAux())
     answer = carefully(sp.outputPSP.simulate, args)
     assert answer in type_.return_type
-    helpPropTypeCorrect2(args_lists[1:], answer, type_.return_type)
+    helpPropTypeCorrect(args_lists[1:], answer, type_.return_type)
 
-def testRandomMark2():
+def testRandomMark():
   for (name,sp) in builtInSPsList():
     if isinstance(sp.requestPSP, NullRequestPSP):
-      yield propRandomAnnotated2, name, sp
+      yield propRandomAnnotated, name, sp
 
-def propRandomAnnotated2(name, sp):
-  checkTypedProperty(helpPropRandomAnnotated2, sp.venture_type().args_types, name, sp)
+def propRandomAnnotated(name, sp):
+  checkTypedProperty(helpPropRandomAnnotated, sp.venture_type().args_types, name, sp)
 
-def helpPropRandomAnnotated2(args_list, name, sp):
+def helpPropRandomAnnotated(args_list, name, sp):
   args = r.BogusArgs(args_list, sp.constructSPAux())
   answer = carefully(sp.outputPSP.simulate, args)
   if not sp.outputPSP.isRandom():
