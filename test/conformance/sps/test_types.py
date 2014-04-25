ce885132dd36db83bbbc4f7d0810dbb64555b8b6
Alexey Radul <axch@mit.edu>
2014-04-25 16:16:21 -0400

    Flush old world order.
    The new world order is better because it reports the random arguments
    that caused each test failure.

--- test/conformance/sps/test_types.py
+++ test/conformance/sps/test_types.py
@@ -5,56 +5,6 @@ from venture.lite.exception import VentureValueError
 from venture.lite.sp import SPType
 from nose import SkipTest
 from nose.tools import eq_
-
-def testTypes():
-  for (name,sp) in builtInSPsList():
-    if isinstance(sp.requestPSP, NullRequestPSP):
-      yield propTypeCorrect, name, sp
-
-def testRandomMark():
-  for (name,sp) in builtInSPsList():
-    if isinstance(sp.requestPSP, NullRequestPSP):
-      yield propRandomAnnotated, name, sp
-
-def appropriate_arguments(sp, type_, budget=20):
-  for _ in range(budget):
-    args = r.random_args_for_sp(sp, type_)
-    if args is None: continue
-    try:
-      answer = sp.outputPSP.simulate(args)
-      yield args, answer
-    except ValueError: continue
-    except VentureValueError: continue
-
-def propTypeCorrect(name, sp):
-  type_ = sp.venture_type()
-  app_ct = 0
-  for (_, answer) in appropriate_arguments(sp, type_):
-    app_ct += 1
-    helpPropTypeCorrect(answer, type_.return_type)
-  if app_ct == 0:
-    raise SkipTest("Could not find appropriate args for %s" % name)
-
-def helpPropTypeCorrect(answer, type_):
-  assert answer in type_
-  if isinstance(type_, SPType):
-    for (_, ans2) in appropriate_arguments(answer, type_, budget=1):
-      helpPropTypeCorrect(ans2, type_.return_type)
-
-def propRandomAnnotated(name, sp):
-  app_ct = 0
-  for (args, answer) in appropriate_arguments(sp, sp.venture_type()):
-    app_ct += 1
-    if not sp.outputPSP.isRandom():
-      for _ in range(5):
-        eq_(answer,sp.outputPSP.simulate(args))
-    else:
-      raise SkipTest("%s claims to be random" % name)
-  if app_ct == 0:
-    raise SkipTest("Could not find appropriate args for %s" % name)
-
-######################################################################
-
 from venture.lite.value import VentureType
 
 class ArgumentsNotAppropriate(Exception):
