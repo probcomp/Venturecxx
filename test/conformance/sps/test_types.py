bebc116d3343b97a50dc72067655ee7f6484ce21
Alexey Radul <axch@mit.edu>
2014-04-25 16:24:53 -0400

    Better naming convention.  Functions named "prop" are properties that obey the interface expected by checkTypedProperty.

--- test/conformance/sps/test_types.py
+++ test/conformance/sps/test_types.py
@@ -8,30 +8,30 @@ from nose.tools import eq_
 def testTypes():
   for (name,sp) in builtInSPsList():
     if isinstance(sp.requestPSP, NullRequestPSP):
-      yield propTypeCorrect, name, sp
+      yield checkTypeCorrect, name, sp
 
-def propTypeCorrect(_name, sp):
+def checkTypeCorrect(_name, sp):
   type_ = sp.venture_type()
-  checkTypedProperty(helpPropTypeCorrect, fully_uncurried_sp_type(type_), sp, type_)
+  checkTypedProperty(propTypeCorrect, fully_uncurried_sp_type(type_), sp, type_)
 
-def helpPropTypeCorrect(args_lists, sp, type_):
+def propTypeCorrect(args_lists, sp, type_):
   if len(args_lists) == 0:
     pass # OK
   else:
     args = r.BogusArgs(args_lists[0], sp.constructSPAux())
     answer = carefully(sp.outputPSP.simulate, args)
     assert answer in type_.return_type
-    helpPropTypeCorrect(args_lists[1:], answer, type_.return_type)
+    propTypeCorrect(args_lists[1:], answer, type_.return_type)
 
 def testRandomMark():
   for (name,sp) in builtInSPsList():
     if isinstance(sp.requestPSP, NullRequestPSP):
-      yield propRandomAnnotated, name, sp
+      yield checkRandomAnnotated, name, sp
 
-def propRandomAnnotated(name, sp):
-  checkTypedProperty(helpPropRandomAnnotated, sp.venture_type().args_types, name, sp)
+def checkRandomAnnotated(name, sp):
+  checkTypedProperty(propRandomAnnotated, sp.venture_type().args_types, name, sp)
 
-def helpPropRandomAnnotated(args_list, name, sp):
+def propRandomAnnotated(args_list, name, sp):
   args = r.BogusArgs(args_list, sp.constructSPAux())
   answer = carefully(sp.outputPSP.simulate, args)
   if not sp.outputPSP.isRandom():
