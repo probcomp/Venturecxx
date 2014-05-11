b744c16a36499877048636a4af6e461aee8629a0
dlovell <dlovell@gmail.com>
2014-05-11 13:01:51 -0400

    use Simulator

--- examples/test_simulator.py
+++ examples/test_simulator.py
@@ -6,6 +6,7 @@
 import time
 import math
 #
+from simulator import Simulator
 from venture.venturemagics.ip_parallel import MRipl
 
 
@@ -51,11 +52,17 @@ xs = [step_i * velocity for step_i in range(N_steps)]
 X_L_rels = [X_L - x for x in xs]
 ds = map(convert_X_L_rel_to_d, X_L_rels)
 thetas = map(convert_X_L_rel_to_theta, X_L_rels)
-distance_observation_data = [('get_noisy_distance_observation', d) for d in ds]
-angle_observation_data = [('get_noisy_angle_observation', theta) for theta in thetas]
+distance_observation_data = [
+        ('(get_noisy_distance_observation %s)' % i, d)
+        for i, d in enumerate(ds)
+        ]
+angle_observation_data = [
+        ('(get_noisy_angle_observation %s)' % i, theta)
+        for i, theta in enumerate(thetas)
+        ]
 #
-observe_list = zip(distance_observation_data, angle_observation_data)
-sample_list = ['(repeat (lambda()(get_state %s)) 16)' % i for i range(N_steps)]
+observe_strs_list = zip(distance_observation_data, angle_observation_data)
+sample_strs_list = [['(repeat (lambda()(get_state %s)) 16)' % i] for i in range(N_steps)]
 
 # In[ ]:
 
@@ -201,80 +208,21 @@ program_assumes = """
 
 # In[ ]:
 
-v=MRipl(N_mripls, backend=backend)
+program = program_utils + program_constants + program_assumes
+print 'read program'
+simulator = Simulator(program, observe_strs_list, sample_strs_list,
+        N_mripls, backend, N_infer)
+print 'initialized simulator'
 
+samples_list = []
+import time
+for step_i in range(N_steps):
+    start = time.time()
+    samples_i = simulator.step()
+    stop = time.time()
+    delta = stop - start
+    print 'step_i=%s took %s seconds' % (step_i, delta)
+    samples_list.append(samples_i)
+    pass
 
-# In[ ]:
-
-constants_out = v.execute_program(program_constants);
-assumes_out = v.execute_program(program_assumes)
-
-
-# In[ ]:
-
-deadreckon_heading_out = v.snapshot(exp_list=exp_list_heading, plot=True);
-
-
-# In[ ]:
-
-deadreckon_x_out = v.snapshot(exp_list=exp_list_x, plot=True);
-
-
-# In[ ]:
-
-deadreckon_y_out = v.snapshot(exp_list=exp_list_y, plot=True);
-
-
-# In[ ]:
-
-angle_observes_out = observe_data(v, angle_observation_data)
-distance_observes_out = observe_data(v, distance_observation_data)
-start = time.time()
-infer_out = v.infer(N_infer)
-first_infer_duration = time.time() - start
-
-
-# In[ ]:
-
-post_observe_x_out = v.snapshot(exp_list=exp_list_x, plot=True)
-
-
-# In[ ]:
-
-post_observe_y_out = v.snapshot(exp_list=exp_list_y, plot=True)
-
-
-# In[ ]:
-
-new_x = N_steps * velocity
-new_X_L_rel = X_L - new_x
-new_d = convert_X_L_rel_to_d(new_X_L_rel)
-new_distance_observation_datum = ('get_noisy_distance_observation', new_d)
-new_theta = convert_X_L_rel_to_theta(new_X_L_rel)
-new_angle_observation_datum = ('get_noisy_angle_observation', new_theta)
-#
-observe_datum(v, (N_steps, new_distance_observation_datum))
-observe_datum(v, (N_steps, new_angle_observation_datum))
-start = time.time()
-new_infer_out = v.infer(N_infer)
-new_infer_duration = time.time() - start
-
-
-# In[ ]:
-
-print N_steps
-print first_infer_duration
-print new_infer_duration
-
-
-# In[ ]:
-
-nth_state = 2
-x = post_observe_x_out['values']['(get_x (get_state %s))' % nth_state]
-y = post_observe_y_out['values']['(get_y (get_state %s))' % nth_state]
-scatter(x, y)
-title('\n'.join([
-    'Particles for state %s' % nth_state,
-    '"Ground truth" is (%s, %s)' % (xs[nth_state], 0),
-]))
 
