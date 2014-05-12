734c4def33533e1edf131dc489f778fa8b2cda52
Daniel Selsam <daniel.selsam@gmail.com>
2014-05-12 10:36:58 -0400

    Refactored particle filter test.

--- test/smc/test_particle_filter.py
+++ test/smc/test_particle_filter.py
@@ -6,6 +6,32 @@ from venture.test.stats import statisticalTest, reportKnownContinuous, reportKno
 from venture.test.config import get_ripl, collectSamples, defaultKernel
 
 
+def testIncorporateDoesNotCrash():
+  """A sanity test for stack handling of incorporate"""
+  if config["get_ripl"] != "lite": raise SkipTest("Clone only implemented in lite")
+  ripl = get_ripl()
+  P = 60
+  ripl.assume("f","""
+(mem (lambda (i)
+  (if (eq i 0)
+    (bernoulli 0.5)
+    (if (f (- i 1))
+      (bernoulli 0.7)
+      (bernoulli 0.3)))))
+""")
+
+  ripl.assume("g","""
+(mem (lambda (i)
+  (if (f i)
+    (bernoulli 0.8)
+    (bernoulli 0.1))))
+""")
+
+  ripl.infer("(resample %d)" % P)
+  ripl.observe("(g 1)",False)
+  ripl.infer("(incorporate)") # not necessary, just testing that it doesn't crash
+
+
 @statisticalTest
 def testBasicParticleFilter():
   """A sanity test for particle filtering"""
@@ -38,7 +64,6 @@ def testBasicParticleFilter():
   ripl.observe("(g 4)",False)
   ripl.infer("(resample %d)" % P)
   ripl.observe("(g 5)",False)
-  ripl.infer("(incorporate)") # not necessary, just testing that it doesn't crash
   ripl.infer("(resample 1)")
   ripl.predict("(g 6)")
   
