221430564bb22bf8edc9b60089c829e2010e831f
Daniel Selsam <daniel.selsam@gmail.com>
2014-05-13 12:16:14 -0400

    Implemented scopeExclude in lite

--- backend/lite/scope.py
+++ backend/lite/scope.py
@@ -16,3 +16,19 @@ class ScopeIncludeOutputPSP(DeterministicPSP):
 def isScopeIncludeOutputPSP(thing):
   return isinstance(thing, ScopeIncludeOutputPSP) or \
     (isinstance(thing, TypedPSP) and isScopeIncludeOutputPSP(thing.psp))
+
+class ScopeExcludeOutputPSP(DeterministicPSP):
+  @override(DeterministicPSP)
+  def simulate(self,args): return args.operandValues[1]
+  @override(DeterministicPSP)
+  def gradientOfSimulate(self, _args, _value, direction): return [0, direction]
+  @override(DeterministicPSP)
+  def canAbsorb(self, _trace, appNode, parentNode): return parentNode != appNode.operandNodes[1]
+  
+  @override(DeterministicPSP)
+  def description(self,name):
+    return "%s returns its third argument unchanged at runtime, but tags the subexpression creating the object as being within the given scope and block." % name
+
+def isScopeExcludeOutputPSP(thing):
+  return isinstance(thing, ScopeExcludeOutputPSP) or \
+    (isinstance(thing, TypedPSP) and isScopeExcludeOutputPSP(thing.psp))
