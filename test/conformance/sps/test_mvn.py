5164b67f62827da8d339bd5a3accd956fde2b9ad
Alexey Radul <axch@mit.edu>
2014-05-16 17:59:27 -0400

    Consistent interface to observing vectors (which are the outputs of mvnormals) in Lite and Puma (with test).

--- test/conformance/sps/test_mvn.py
+++ test/conformance/sps/test_mvn.py
@@ -91,7 +91,7 @@ def testMVN3():
   ripl.assume("x","(multivariate_normal mu sigma)")
   ripl.assume("y","(multivariate_normal x sigma)")
   v = [{"type": "real", "value": 2}, {"type": "real", "value": 2}]
-  ripl.observe("y",{"type":"list","value":v})
+  ripl.observe("y",{"type":"vector","value":v})
   ripl.predict("(lookup x 0)",label="pid")
 
   predictions = collectSamples(ripl,"pid")
