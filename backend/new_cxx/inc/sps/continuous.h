f492c381923e7afdb8f9246301604bd7bbace6c3
dlovell <dlovell@gmail.com>
2014-05-16 08:48:32 -0400

    remove extraneous methods from PosePSP

--- backend/new_cxx/inc/sps/continuous.h
+++ backend/new_cxx/inc/sps/continuous.h
@@ -26,10 +26,6 @@ struct PosePSP : RandomPSP
 {
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
   double logDensity(VentureValuePtr value, shared_ptr<Args> args) const;
-
-  bool isContinuous() const { return true; }
-  double getSupportLowerBound() const { return 0; }
-
 };
 
 struct GammaPSP : RandomPSP
