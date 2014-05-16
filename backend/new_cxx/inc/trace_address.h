2a01bd6d512d6f8b0888486e4599938b9549ecf4
Alexey Radul <axch@mit.edu>
2014-05-16 12:42:26 -0400

    Draft the type signature for addresses.

--- backend/new_cxx/inc/trace_address.h
+++ backend/new_cxx/inc/trace_address.h
@@ -1,4 +1,17 @@
 #ifndef TRACE_ADDRESS_H
 #define TRACE_ADDRESS_H
 
+#include "types.h"
+
+class Step { virtual ~Step(); };
+class OperatorStep : Step {};
+class OperandStep : Step { int index; };
+class RequesterStep : Step {}; // From the output node to the request node
+class ESRStep : Step { FamilyID index; }
+
+struct TraceAddress
+{
+  vector<Step> path;
+};
+
 #endif
