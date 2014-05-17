65ed7faeac29a6d06d9820c831da3c59db49c74e
Alexey Radul <axch@mit.edu>
2014-05-17 10:37:24 -0400

    Draft templated copying of sets of pointers.

--- backend/new_cxx/src/stop_and_copy.cxx
+++ backend/new_cxx/src/stop_and_copy.cxx
@@ -1,9 +1,24 @@
+#include <boost/foreach.hpp>
 #include "concrete_trace.h"
+
+// Deep-copying concrete traces by analogy with the stop-and-copy
+// garbage collection algorithm.
 
 shared_ptr<ConcreteTrace> ConcreteTrace::stop_and_copy()
 {
   ForwardingMap forward = map<void*, void*>();
   return this->copy_help(forward);
+}
+
+template <typename T>
+set<T*> copy_set(set<T*> s, ForwardingMap forward)
+{
+  set<T*> answer = set<T*>();
+  BOOST_FOREACH(T* obj, s)
+  {
+    answer.insert(obj->copy_help(forward));
+  }
+  return answer;
 }
 
 shared_ptr<ConcreteTrace> ConcreteTrace::copy_help(ForwardingMap forward)
@@ -15,6 +30,5 @@ shared_ptr<ConcreteTrace> ConcreteTrace::copy_help(ForwardingMap forward)
   answer->arbitraryErgodicKernels = copy_set(this->arbitraryErgodicKernels, forward);
   answer->unpropagatedObservations = copy_map(this->unpropagatedObservations, forward);
   // ...
-  return answer
+  return answer;
 }
-
