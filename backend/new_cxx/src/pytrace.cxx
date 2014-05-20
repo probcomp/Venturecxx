5f75de21ab21d1ca5c3066bf153a27c4ce909084
Alexey Radul <axch@mit.edu>
2014-05-20 19:28:40 -0400

    Teach Puma to understand cycle kernels (so that continuous cycles work).

--- backend/new_cxx/src/pytrace.cxx
+++ backend/new_cxx/src/pytrace.cxx
@@ -123,9 +123,12 @@ struct Inferer
   BlockID block;
   shared_ptr<ScaffoldIndexer> scaffoldIndexer;
   size_t transitions;
+  bool cycle; // TODO Turn this into an enum for mixtures
+  vector<shared_ptr<Inferer> > subkernels;
   
   Inferer(shared_ptr<ConcreteTrace> trace, boost::python::dict params) : trace(trace)
   {
+    cycle = false;
     string kernel = boost::python::extract<string>(params["kernel"]);
     if (kernel == "mh")
     {
@@ -150,45 +153,74 @@ struct Inferer
     {
       gKernel = shared_ptr<GKernel>(new SliceGKernel);
     }
+    else if (kernel == "cycle")
+    {
+      cycle = true;
+      boost::python::list subs = boost::python::extract<boost::python::list>(params["subkernels"]);
+      boost::python::ssize_t len = boost::python::len(subs);
+      subkernels = vector<shared_ptr<Inferer> >(len);
+
+      for (boost::python::ssize_t i = 0; i < len; ++i)
+      {
+        subkernels[i] = shared_ptr<Inferer>(new Inferer(trace, boost::python::extract<boost::python::dict>(subs[i])));
+      }
+    }
     else
     {
       cout << "\n***Kernel '" << kernel << "' not supported. Using MH instead.***" << endl;
       gKernel = shared_ptr<GKernel>(new MHGKernel);
     }
     
-    scope = fromPython(params["scope"]);
-    block = fromPython(params["block"]);
+    if (!(kernel == "cycle")) {
+      scope = fromPython(params["scope"]);
+      block = fromPython(params["block"]);
 
-    if (block->hasSymbol() && block->getSymbol() == "ordered_range")
-    {
-      VentureValuePtr minBlock = fromPython(params["min_block"]);
-      VentureValuePtr maxBlock = fromPython(params["max_block"]);
-      scaffoldIndexer = shared_ptr<ScaffoldIndexer>(new ScaffoldIndexer(scope,block,minBlock,maxBlock));
+      if (block->hasSymbol() && block->getSymbol() == "ordered_range")
+      {
+        VentureValuePtr minBlock = fromPython(params["min_block"]);
+        VentureValuePtr maxBlock = fromPython(params["max_block"]);
+        scaffoldIndexer = shared_ptr<ScaffoldIndexer>(new ScaffoldIndexer(scope,block,minBlock,maxBlock));
+      }
+      else
+      {
+        scaffoldIndexer = shared_ptr<ScaffoldIndexer>(new ScaffoldIndexer(scope,block));
+      }
     }
-    else
-    {
-      scaffoldIndexer = shared_ptr<ScaffoldIndexer>(new ScaffoldIndexer(scope,block));
-    }
-    
     transitions = boost::python::extract<size_t>(params["transitions"]);
   }
   
   void infer()
   {
     if (trace->numUnconstrainedChoices() == 0) { return; }
-    
     for (size_t i = 0; i < transitions; ++i)
     {
-      mixMH(trace.get(), scaffoldIndexer, gKernel);
+      if (cycle) { inferCycle(); }
+      else { inferPrimitive(); inferAEKernels(); }
+    }
+  }
 
-      for (set<Node*>::iterator iter = trace->arbitraryErgodicKernels.begin();
-        iter != trace->arbitraryErgodicKernels.end();
-        ++iter)
-      {
-        OutputNode * node = dynamic_cast<OutputNode*>(*iter);
-        assert(node);
-        trace->getMadeSP(node)->AEInfer(trace->getMadeSPAux(node),trace->getArgs(node),trace->getRNG());
-      }
+  void inferCycle()
+  {
+    for (int i = 0; i < subkernels.size(); i++)
+    {
+      subkernels[i]->infer();
+    }
+  }
+
+  void inferPrimitive()
+  {
+    mixMH(trace.get(), scaffoldIndexer, gKernel);
+  }
+
+  void inferAEKernels()
+  {
+    for (set<Node*>::iterator iter = trace->arbitraryErgodicKernels.begin();
+      iter != trace->arbitraryErgodicKernels.end();
+      ++iter)
+    {
+      OutputNode * node = dynamic_cast<OutputNode*>(*iter);
+      assert(node);
+      trace->getMadeSP(node)->AEInfer(trace->getMadeSPAux(node),trace->getArgs(node),trace->getRNG());
     }
   }
 };
