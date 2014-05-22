c5414a2e5c252e7b085837cdae460f186839b6d9
dlovell <dlovell@gmail.com>
2014-05-22 18:50:04 -0400

    implement bugfixes, restructing
    time to move on to mripl

--- examples/just_gps.py
+++ examples/just_gps.py
@@ -1,16 +1,11 @@
+import functools
+#
 import numpy
+import pylab
+pylab.ion()
+pylab.show()
+#
 from venture.shortcuts import make_puma_church_prime_ripl
-
-
-vehicle_a = 0.299541
-vehicle_b = 0.0500507
-vehicle_h = 0
-vehicle_L = 0.257717
-
-
-epsilon = .1 * 2 ** -2
-N_predicts = 100
-N_infer = 100
 
 
 def _convert_real(val):
@@ -21,58 +16,88 @@ def _convert(val):
     is_list = isinstance(val, (list, tuple))
     val = _convert_list(val) if is_list else _convert_real(val)
     return val
+get_predict = lambda ripl, x: ripl.predict('(get_pose %s)' % x)
 
 
-program = """
+vehicle_a = 0.299541
+vehicle_b = 0.0500507
+vehicle_h = 0
+vehicle_L = 0.257717
+#
+constant_velocity = .1
+constant_steering = 0
+gps_heading = .1
+gen_gps = lambda t: _convert((constant_velocity * t, 0, gps_heading))
+#
+N_infer = 100
+N_steps = 10
+predict_which = 0
 
-[assume controls (list (list 0 0)
-                       (list .1 .0)
-                       (list .1 .0)
-                       (list .1 .0)
-                       (list .1 .0)
-                       (list .1 0))]
 
-[assume get_control (lambda (t) (lookup controls t))]
+program_constants = """
 
 [assume vehicle_params (list %s %s %s %s)]
 
-[assume get_pose (lambda (t)
-  (if (= t 0) (list 0 0 0)
+[assume constant_control (list %s %s)]
+
+""" % (
+        vehicle_a, vehicle_b, vehicle_h, vehicle_L,
+        constant_velocity, constant_steering,
+#        fractional_xy_error_std,
+#        fractional_heading_error_std,
+#        additive_xy_error_std,
+#        additive_heading_error_std,
+        )
+
+program_assumes = """
+
+[assume fractional_xy_error_std (gamma 1.0 100.0)]
+
+[assume fractional_heading_error_std (gamma 1.0 100.0)]
+
+[assume additive_xy_error_std (gamma 1.0 1.0)]
+
+[assume additive_heading_error_std (gamma 1.0 100.0)]
+
+[assume get_pose (mem (lambda (t)
+  (if (= t 0) (list (uniform_continuous -1 1)
+                    (uniform_continuous -1 1)
+                    (uniform_continuous -1 1))
               (simulate_motion 1
                                (get_pose (- t 1))
-                               (get_control (- t 1))
-                               vehicle_params)))]
+                               constant_control
+                               vehicle_params
+                               fractional_xy_error_std
+                               fractional_heading_error_std
+                               additive_xy_error_std
+                               additive_heading_error_std
+                               ))))]
 
-""" % (vehicle_a, vehicle_b, vehicle_h, vehicle_L)
+"""
+
+program = program_constants + program_assumes
 
 
+# ripl with NO observes
 ripl0 = make_puma_church_prime_ripl()
 ripl0.execute_program(program)
 ripl0.infer(N_infer)
-get_predict = lambda x: ripl0.predict('(get_pose %s)' % x)
-predicts0 = numpy.array(map(get_predict, [4] * N_predicts))
+get_predict0 = functools.partial(get_predict, ripl0)
+predicts0 = numpy.array(map(get_predict0, range(N_steps)))
 
+# ripl with observes
 ripl1 = make_puma_church_prime_ripl()
 ripl1.execute_program(program)
-heading = .01
-ripl1.observe('(simulate_gps (get_pose 1))', _convert((0, 0, heading)))
-ripl1.observe('(simulate_gps (get_pose 2))', _convert((.1+epsilon, 0, heading)))
-ripl1.observe('(simulate_gps (get_pose 3))', _convert((.2+epsilon, 0, heading)))
-ripl1.observe('(simulate_gps (get_pose 4))', _convert((.3+epsilon, 0, heading)))
-ripl1.observe('(simulate_gps (get_pose 5))', _convert((.4+epsilon, 0, heading)))
+for t in range(1, N_steps):
+    ripl1.observe('(simulate_gps (get_pose %s) .1 .1)' % t, gen_gps(t))
+    pass
 ripl1.infer(N_infer)
 ripl1.infer('(mh default one %s)' % N_infer)
-get_predict = lambda x: ripl1.predict('(get_pose %s)' % x)
-predicts1 = numpy.array(map(get_predict, [4] * N_predicts))
+ripl1.infer('(pgibbs default ordered 3 %s)' % N_infer)
+get_predict1 = functools.partial(get_predict, ripl1)
+predicts1 = numpy.array(map(get_predict1, range(N_steps)))
 
-print 'predicts0.mean(axis=0):'
-print predicts0.mean(axis=0)
 
-print 'predicts1.mean(axis=0):'
-print predicts1.mean(axis=0)
-
-import pylab
-pylab.ion()
-pylab.show()
-pylab.hist(predicts0[:, 0])
-pylab.hist(predicts1[:, 0])
+pylab.figure()
+pylab.plot(predicts0[:, 0], predicts0[:, 1], 'r-x')
+pylab.plot(predicts1[:, 0], predicts1[:, 1], 'g-x')
