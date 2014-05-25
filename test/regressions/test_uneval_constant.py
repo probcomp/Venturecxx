7097d99939869afaa338ae6150a4d28fc12f4c63
Alexey Radul <axch@mit.edu>
2014-05-25 15:59:55 -0400

    Oops, forgot that Lite has no freeze.

--- test/regressions/test_uneval_constant.py
+++ test/regressions/test_uneval_constant.py
@@ -1,3 +1,5 @@
+from testconfig import config
+from nose import SkipTest
 from venture.test.config import get_ripl
 
 def testUnevalConstantAndForget():
@@ -14,6 +16,8 @@ copying of which would be invalid."""
 def testUnevalConstantAndFreeze():
   """Check that unevaling a constant node does not produce a trace the
 copying of which would be invalid."""
+  if config["get_ripl"] != "puma":
+    raise SkipTest("freeze only implemented in Puma")
   ripl = get_ripl()
   ripl.assume("foo", "(if (flip) 0 1)")
   ripl.infer(10)
@@ -25,6 +29,8 @@ copying of which would be invalid."""
 def testUnevalConstantAndFreezeWithObservations():
   """Check that unevaling a constant node does not produce an invalid
 trace (even when transitions are rejected)."""
+  if config["get_ripl"] != "puma":
+    raise SkipTest("freeze only implemented in Puma")
   ripl = get_ripl()
   ripl.assume("foo", "(if (flip) 0 1)")
   ripl.observe("(normal foo 0.1)", 1)
