569403f042a3da81e171dced92e9866f5e099685
Alexey Radul <axch@mit.edu>
2014-05-25 11:25:40 -0400

    Right.  Flush copy-pasta artifacts.

--- test/performance/asymptotics/test_particle_filtering.py
+++ test/performance/asymptotics/test_particle_filtering.py
@@ -7,40 +7,12 @@ from nose.plugins.attrib import attr
 # it's metaprogrammed because freeze is only known to work on
 # directives.
 
-def loadHMMParticleAsymptoticProgram1(M, num_particles):
-  """Easiest possible HMM asymptotic test for particle filtering"""
-  ripl = get_ripl()
-  ripl.infer("(resample %s)" % num_particles)
-
-  ripl.assume("f","""
-(mem (lambda (i)
-  (if (eq i 0)
-    (scope_include (quote states) 0 (normal 0.0 1.0))
-    (scope_include (quote states) i (normal (f (- i 1)) 1.0)))))
-""")
-  ripl.assume("g","""
-(mem (lambda (i)
-  (normal (f i) 1.0)))
-""")
-
-  previousValue = 0.0
-  for m in range(M):
-    newValue = scipy.stats.norm.rvs(previousValue,1.0)
-    ripl.observe("(g %d)" % m,"%d" % newValue)
-    previousValue = newValue
-
-  return ripl
-
-
-# O(N) forwards
-# O(N log N) to infer
 @attr('slow')
 def testHMMParticleAsymptotics1():
   num_particles = 5
   def particulate(num_steps):
     ripl = get_ripl()
     ripl.infer("(resample %s)" % num_particles)
-#    ripl = loadHMMParticleAsymptoticProgram1(num_steps, 5)
 
     def do_filter():
       for m in range(num_steps):
