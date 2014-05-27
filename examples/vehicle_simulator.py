8a41d3a89f9605791e8773c75b3e9013a6636f58
dlovell <dlovell@gmail.com>
2014-05-27 02:17:40 -0400

    generate observes

--- examples/vehicle_simulator.py
+++ examples/vehicle_simulator.py
@@ -12,12 +12,17 @@ def make_frame_dts(frame):
     first_dt = frame.index[0]
     return numpy.append([first_dt], numpy.diff(list(frame.index)))
 
+def insert_dts(frame):
+    frame = frame.copy()
+    dts = make_frame_dts(frame)
+    frame['dt'] = dts
+    return frame
+
 laser_sensor = 3
 def postprocess_sensor_frame(frame):
     frame = frame.copy()
     frame = frame[frame.Sensor!=laser_sensor]
-    dts = make_frame_dts(frame)
-    frame['dt'] = dts
+    frame = insert_dts(frame)
     return frame
 
 def read_frame(filename, dirname='', index_col=None, colname_map=None,
@@ -44,6 +49,91 @@ def read_frames(dirname):
     laser_frame = read_frame(dirname=dirname, **laser_frame_config)
     sensor_frame = read_frame(dirname=dirname, **sensor_frame_config)
     return gps_frame, control_frame, laser_frame, sensor_frame
+
+
+def my_xs(frame, t):
+    eps = 1E-4
+    xs = frame.truncate(before=t-eps, after=t+eps).irow(0)
+    return xs
+def _convert(val):
+    def _convert_real(val):
+        return {"type":"real","value":val}
+    def _convert_list(val):
+        return {"type":"vector","value":map(_convert, val)}
+    is_list = isinstance(val, (list, tuple))
+    return _convert_list(val) if is_list else _convert_real(val)
+def generate_observes_for_gps_at_t(t, i, gps_frame):
+    xs = my_xs(gps_frame, t)
+    observe_str = vp.simulate_gps_str % i
+    observe_val = _convert((xs.x, xs.y, xs.heading))
+    observes = [(observe_str, observe_val), ]
+    return observes
+def generate_observes_for_control_at_t(t, i, control_frame):
+    xs = my_xs(control_frame, t)
+    observes = [
+            (vp.get_control_str % (i, 0), xs.Velocity),
+            (vp.get_control_str % (i, 1), xs.Steering),
+            ]
+    return observes
+def modify_observes_for_control_at_i(i, control_observes):
+    if len(control_observes) != 0:
+        velocity = control_observes[0][1]
+        steering = control_observes[0][1]
+        control_observes = [
+                (vp.get_control_str % (i, 0), velocity),
+                (vp.get_control_str % (i, 1), steering),
+                ]
+    return control_observes
+def generate_observes_for_dt_at_t(t, i, sensor_frame):
+    xs = my_xs(sensor_frame, t)
+    dt = xs.dt
+    observes = [(vp.sample_dt_str % i, dt), ]
+    return observes
+def get_i(sensor_frame, t):
+    # first i is never really observed, we only know the dt
+    return sensor_frame.index.get_loc(t) + 1
+sensor_to_key = {1:'gps', 2:'control', 3:'laser'}
+def generate_observes_for_t(t, sensor_frame, last_control_observes,
+        control_frame, gps_frame, **kwargs):
+    sensor_xs = my_xs(sensor_frame, t)
+    dt = sensor_xs.dt
+    which_sensor = sensor_to_key[sensor_xs.Sensor]
+    i = get_i(sensor_frame, t)
+    if dt == t:
+        # first sensor, if not control, don't add a control
+        # observe a dt, but not a control
+        # in addition, we always observe a dt for index 0
+        # or perhaps, we always
+        pass
+    # not a special case
+    control_observes, sensor_observes = [], []
+    if which_sensor == 'control':
+        control_observes = generate_observes_for_control_at_t(t, i,
+                control_frame)
+        sensor_observes = []
+        pass
+    else:
+        control_observes = modify_observes_for_control_at_i(i, last_control_observes)
+        if which_sensor == 'gps':
+            sensor_observes = generate_observes_for_gps_at_t(t, i, gps_frame)
+            pass
+        else:
+            assert False, 'uknown observe: %s' % which_sensor
+            pass
+        pass
+    dt_observes = generate_observes_for_dt_at_t(t, i, sensor_frame)
+    observes = dt_observes + control_observes + sensor_observes
+    return observes, control_observes
+def generate_all_observes(sensor_frame, control_frame, gps_frame):
+    control_observes = []
+    all_observes = []
+    for i, t in enumerate(list(sensor_frame.index)):
+        observes, control_observes = generate_observes_for_t(
+                t, sensor_frame, control_observes, control_frame, gps_frame)
+        all_observes.append(observes)
+        pass
+    return all_observes
+
 
 
 def create_gps_observes(gps_frame):
