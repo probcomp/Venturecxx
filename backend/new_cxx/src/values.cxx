61a68b3d438b30a9e93c763529cded36d098421d
strin <stl501@gmail.com>
2014-05-29 14:19:30 -0700

    relax symmetric matrix check.

--- backend/new_cxx/src/values.cxx
+++ backend/new_cxx/src/values.cxx
@@ -21,10 +21,10 @@ VentureSymmetricMatrix::VentureSymmetricMatrix(const Eigen::MatrixXd & m)
   {
     for(size_t j = i+1; j < m.cols(); j++) 
     {
-      if(fabs(m(i,j)-m(j,i)) > 1e-8*fabs(m(i,j)))
+      if(fabs(m(i,j)-m(j,i)) > 1e-6*fabs(m(i,j)))
       {
         cout << this->toString() << endl;
-        throw "failed to initiate a symmetric matrix from an asymmetric one.";
+        cout << "warning, failed to initiate a symmetric matrix from an asymmetric one." << endl;
       }
     }
   }
