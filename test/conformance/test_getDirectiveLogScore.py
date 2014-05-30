2ad485bfbb88bac1dffcf70fad6e4b1aa8480948
Alexey Radul <axch@mit.edu>
2014-05-30 14:24:20 -0400

    Right.  Use supported interface instead of hacking through to trace methods that only Lite has.

--- test/conformance/test_getDirectiveLogScore.py
+++ test/conformance/test_getDirectiveLogScore.py
@@ -12,7 +12,7 @@ def testgetDirectiveLogScore():
     ripl.execute_program(src)
     trace = ripl.sivm.core_sivm.engine.getDistinguishedTrace()
     logscore_lite = trace.getDirectiveLogScore(1)
-    val = trace.groundValueAt(trace.scopes['dummy'][0].pop()).number
+    val = ripl.report(1)
     logscore_true = -0.5*np.log(2*np.pi) - 0.5*val*val
     
     assert_equal(logscore_lite, logscore_true)
