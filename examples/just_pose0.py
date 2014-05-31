4b300e77902ef52ed2aff57448c7be2f91a56607
dlovell <dlovell@gmail.com>
2014-05-31 15:12:08 -0400

    add just_pose0.py

---
+++ examples/just_pose0.py
@@ -0,0 +1,42 @@
+from collections import Counter
+#
+import numpy
+#
+from venture.shortcuts import make_puma_church_prime_ripl
+
+
+program = """
+[assume pose_0
+  (scope_include (quote 0) 0
+    (list (uniform_continuous -100 100)
+          (uniform_continuous -100 100)
+          (uniform_continuous -3.14 3.14)
+          ))]
+"""
+
+
+def get_ripl():
+    ripl = make_puma_church_prime_ripl()
+    ripl.execute_program(program)
+    ripl.observe('(normal (lookup pose_0 0) 0.1)', -.01)
+    ripl.observe('(normal (lookup pose_0 1) 0.1)', 1.563)
+    ripl.observe('(normal (lookup pose_0 2) 0.1)', -.000112)
+    ripl.infer('(incorporate)')
+    out = ripl.infer('(mh default all 100)')
+    #out = ripl.infer('(mh latents all 100)')
+    return ripl
+
+def sample_new_pose(ripl):
+    out = ripl.infer('(mh default one 100)')
+    return ripl.predict('pose_0')
+
+def sample_new_poses(ripl, N):
+    return [sample_new_pose(ripl) for _i in range(N)]
+
+def count_poses(poses):
+    return Counter(map(tuple, poses))
+
+ripl = get_ripl()
+poses = sample_new_poses(ripl, 100)
+counter = count_poses(poses)
+print '\n'.join(map(str, counter.iteritems()))
