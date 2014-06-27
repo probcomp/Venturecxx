9ca25d9fdeb603d70bc4fdd81a1e24eb341ac406
Alexey Radul <axch@mit.edu>
2014-06-26 23:09:38 -0400

    Right.  The hack I had suggested for not depending on predicting directive ids here doesn't work with Puma.  Produce a different hack.

--- test/conformance/test_getDirectiveLogScore.py
+++ test/conformance/test_getDirectiveLogScore.py
@@ -4,12 +4,13 @@ import numpy as np
 
 def testgetDirectiveLogScore():
     ripl = get_ripl()
-    ripl.assume('x', '(scope_include (quote dummy) 0 (normal 0 1))',)
+    ripl.assume('x', '(scope_include (quote dummy) 0 (normal 0 1))', label="x")
     trace = ripl.sivm.core_sivm.engine.getDistinguishedTrace()
-    # can't give string directive id for a trace; find number of last directive
-    directive_id = max(trace.families)
-    logscore_lite = trace.getDirectiveLogScore(directive_id)
+    # can't give string directive id for a trace; find number of
+    # directive by label
+    directive_id = ripl.sivm.label_dict["x"]
+    logscore_venture = trace.getDirectiveLogScore(directive_id)
     val = ripl.report(directive_id)
     logscore_true = -0.5*np.log(2*np.pi) - 0.5*val*val
 
-    assert_equal(logscore_lite, logscore_true)
+    assert_equal(logscore_venture, logscore_true)
