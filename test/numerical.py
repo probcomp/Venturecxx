cce5c72dccb61936753a72724625c0cfe5a03dee
Alexey Radul <axch@mit.edu>
2014-06-29 14:18:39 -0400

    Implementing three steps of Richardson extrapolation seems to be good enough for checking the gradient of the log density of normal and uniform_continuous.

--- test/numerical.py
+++ test/numerical.py
@@ -1,6 +1,13 @@
 def derivative(f, x):
   return lambda(h): (f(x+h) - f(x-h)) / (2*h)
 
+def richardson_step(f, degree):
+  return lambda(h): (2**degree * f(h/2) - f(h)) / (2**degree - 1)
+
 def richardson(f):
-  # TODO Actually implement Richardson extrapolation
-  return f(0.01)
+  # TODO Memoize the incoming function (possibly with an explicit
+  # stream) to save compute on repeated evaluations at the same h
+
+  # Could also implement the "stop when at machine precision" rule,
+  # instead of always taking exactly three steps.
+  return richardson_step(richardson_step(richardson_step(f, 2), 4), 6)(0.1)
