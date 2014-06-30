ca2a27d0a83384fba234aa0c78ef8c670dd67d13
Alexey Radul <axch@mit.edu>
2014-06-30 12:00:57 -0400

    Break overlong lines.

--- backend/new_cxx/src/value.cxx
+++ backend/new_cxx/src/value.cxx
@@ -13,31 +13,95 @@ void cannotConvertType(const VentureValue * obj, string target)
 
 bool VentureValue::hasDouble() const { return false; }
 
-double VentureValue::getDouble() const { cannotConvertType(this,"double"); assert(false); throw "no return"; }
+double VentureValue::getDouble() const
+{
+  cannotConvertType(this,"double"); assert(false); throw "no return";
+}
+
 bool VentureValue::hasInt() const { return false; }
-long VentureValue::getInt() const { cannotConvertType(this,"int"); assert(false); throw "no return"; }
-int VentureValue::getAtom() const { cannotConvertType(this,"atom"); assert(false); throw "no return"; }
+long VentureValue::getInt() const
+{
+  cannotConvertType(this,"int"); assert(false); throw "no return";
+}
+
+int VentureValue::getAtom() const
+{
+  cannotConvertType(this,"atom"); assert(false); throw "no return";
+}
+
 bool VentureValue::isBool() const { return false; }
-bool VentureValue::getBool() const { cannotConvertType(this,"bool"); assert(false); throw "no return"; }
+bool VentureValue::getBool() const
+{
+  cannotConvertType(this,"bool"); assert(false); throw "no return";
+}
+
 bool VentureValue::hasSymbol() const { return false; }
-const string& VentureValue::getSymbol() const { cannotConvertType(this,"symbol"); assert(false); throw "no return"; }
-vector<VentureValuePtr> VentureValue::getArray() const { cannotConvertType(this,"array"); assert(false); throw "no return"; }
+const string& VentureValue::getSymbol() const
+{
+  cannotConvertType(this,"symbol"); assert(false); throw "no return";
+}
 
-const VentureValuePtr& VentureValue::getFirst() const { cannotConvertType(this,"pair"); assert(false); throw "no return"; }
-const VentureValuePtr& VentureValue::getRest() const { cannotConvertType(this,"pair"); assert(false); throw "no return"; }
+vector<VentureValuePtr> VentureValue::getArray() const
+{
+  cannotConvertType(this,"array"); assert(false); throw "no return";
+}
+
+
+const VentureValuePtr& VentureValue::getFirst() const
+{
+  cannotConvertType(this,"pair"); assert(false); throw "no return";
+}
+
+const VentureValuePtr& VentureValue::getRest() const
+{
+  cannotConvertType(this,"pair"); assert(false); throw "no return";
+}
+
   
-const Simplex& VentureValue::getSimplex() const { cannotConvertType(this,"simplex"); assert(false); throw "no return"; }
-const MapVVPtrVVPtr& VentureValue::getDictionary() const { cannotConvertType(this,"dictionary"); assert(false); throw "no return"; }
-VectorXd VentureValue::getVector() const { cannotConvertType(this,"vector"); assert(false); throw "no return"; }
+const Simplex& VentureValue::getSimplex() const
+{
+  cannotConvertType(this,"simplex"); assert(false); throw "no return";
+}
 
-MatrixXd VentureValue::getMatrix() const { cannotConvertType(this,"matrix"); assert(false); throw "no return"; }
+const MapVVPtrVVPtr& VentureValue::getDictionary() const
+{
+  cannotConvertType(this,"dictionary"); assert(false); throw "no return";
+}
 
-const vector<ESR>& VentureValue::getESRs() const { cannotConvertType(this,"requests"); assert(false); throw "no return"; }
-const vector<shared_ptr<LSR> >& VentureValue::getLSRs() const { cannotConvertType(this,"requests"); assert(false); throw "no return"; }
+VectorXd VentureValue::getVector() const
+{
+  cannotConvertType(this,"vector"); assert(false); throw "no return";
+}
 
-Node * VentureValue::getNode() const { cannotConvertType(this,"node") ; assert(false); throw "no return"; }
 
-shared_ptr<SPAux> VentureValue::getSPAux() const { cannotConvertType(this,"sprecord") ; assert(false); throw "no return"; }
+MatrixXd VentureValue::getMatrix() const
+{
+  cannotConvertType(this,"matrix"); assert(false); throw "no return";
+}
+
+
+const vector<ESR>& VentureValue::getESRs() const
+{
+  cannotConvertType(this,"requests"); assert(false); throw "no return";
+}
+
+const vector<shared_ptr<LSR> >& VentureValue::getLSRs() const
+{
+  cannotConvertType(this,"requests"); assert(false); throw "no return";
+}
+
+
+Node * VentureValue::getNode() const
+{
+  cannotConvertType(this,"node") ; assert(false); throw "no return";
+}
+
+
+shared_ptr<SPAux> VentureValue::getSPAux() const
+{
+  cannotConvertType(this,"sprecord") ; assert(false); throw "no return";
+}
+
 
 VentureValuePtr VentureValue::lookup(VentureValuePtr index) const { assert(false); }
 bool VentureValue::contains(VentureValuePtr index) const { assert(false); }
@@ -54,7 +118,11 @@ boost::python::dict VentureValue::toPython(Trace * trace) const
 }
 
 
-size_t VentureValue::hash() const { assert(false); assert(false); throw "no return"; }
+size_t VentureValue::hash() const
+{
+  assert(false); assert(false); throw "no return";
+}
+
 
 
 struct VentureSymbol;
