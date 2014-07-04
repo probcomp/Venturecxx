66b2dead5c75ab43f8b09de4236844d37f1545ec
David Wadden <dave.wadden@gmail.com>
2014-07-04 19:41:04 -0400

    Progress on multiclass logistic; renaming files, etc

---
+++ examples/ml-models/run_models.py
@@ -0,0 +1,79 @@
+'''
+Illustrates sequential Bayesian updating in Venture, for linear regression
+model in 2 dimensions
+'''
+
+from __future__ import division
+import re
+from venture.shortcuts import (make_puma_church_prime_ripl,
+                               make_lite_church_prime_ripl)
+import pandas as pd
+
+def make_data():
+  w = matrix([0.5, -0.25]).T
+  sigma_2 = 0.25
+  X = matrix(np.random.uniform(-1, 1, size = [20,2]))
+  y = matrix(np.random.normal(X * w, np.sqrt(sigma_2)))
+  return {'X' : X, 'y' : y, 'w' : w, 'sigma_2' : sigma_2}
+
+def load_model(infile):
+  with open(infile) as f:
+    lines = [x.strip() for x in f.readlines()]
+  pass_next = False
+  res = []
+  for counter, line in enumerate(lines):
+    pass_this = pass_next
+    pass_next = False
+    if pass_this:
+      continue
+    if re.match('^;', line) or not line:
+      continue
+    if 'assume D' in line:
+      D = int(line.split(' ')[2][:-1])
+    if line == '...':
+      pass_next = True
+      # get the previous line, and loop
+      nextline = lines[counter+1]
+      fmt_string = nextline.replace('D', '{i}')
+      for i in range(2, D + 1):
+        res.append(fmt_string.format(i = i))
+    elif '...' in line:
+      symbol = line.split()[1]
+      replacement = ' '.join([symbol + '_' + str(i) for i in range(2, D + 1)])
+      newline = line.replace('...', replacement).replace(' ' + symbol + '_D', '')
+      res.append(newline)
+    else:
+      res.append(line)
+  res = '\n'.join(res)
+  return res
+
+def build_ripl(infile = 'multiclass-logistic.vnt'):
+  model = load_model(infile)
+  r = make_lite_church_prime_ripl()
+  r.load_prelude()
+  _ = r.execute_program(model)
+  return r
+
+def simulate(infile = 'regression2.vnt'):
+  r = build_ripl(infile)
+  _ = r.forget('sigma_2')
+  _ = r.assume('sigma_2', 1, label = 'sigma_2')
+  _ = r.forget('w')
+  _ = r.assume('w', '(vector 3 -2)', label = 'w')
+  _ = r.assume('mem_unif', '(mem (lambda (i) (uniform_continuous -1 1)))')
+  _ = r.assume('simulate', '(lambda (i) (list (mem_unif i) (y (vector 1 (mem_unif i)))))')
+  res = {'x' : [], 'y' : []}
+  for i in range(20):
+    thisone = r.sample('(simulate {0})'.format(i))
+    res['x'].append(thisone[0])
+    res['y'].append(thisone[1])
+  return pd.DataFrame(res)
+
+def runme():
+  r = build_ripl()
+  _ = r.sample('sigma_2')
+  data = simulate()
+  for i, row in data[:5].iterrows():
+    r.observe('(y (vector 1 {0}))'.format(row['x']), row['y'])
+    data = r.infer('(cycle ((rejection default all) (peek w) (peek sigma_2)) 1)')
+
--- examples/ml-models/sequential_bayes_gaussian.py
+++ examples/ml-models/sequential_bayes_gaussian.py
@@ -1,48 +1,65 @@
-from venture.venturemagics.ip_parallel import MRipl
+from venture.shortcuts import (make_puma_church_prime_ripl,
+                               make_lite_church_prime_ripl)
 import numpy as np, scipy as sp, pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 from os import path
+import os
 
 def runme(outdir):
   techniques = {'hmc' : '(hmc default all 0.05 5 1)',
                 'nesterov' : '(nesterov default all 0.1 5 1)',
                 'mh' : '(mh default all 1)',
                 'rejection' : '(rejection default all 1)'}
-  ripls = {technique : MRipl(256, 'lite', False) for technique in techniques}
+  ripls = {technique : make_lite_church_prime_ripl() for technique in techniques}
+  for ripl in ripls.values(): ripl.infer('(resample 200)')
 
   for name, ripl in ripls.items():
     ripl.assume('mu', '(normal 0 10)')
     ripl.assume('x', '(lambda () (normal mu 1))')
 
   count = 0
-  n = 10
+  n = 20
+
+  plt.rc('text', usetex = True)
 
   data = np.random.normal(6, 1, n)
-  for datum in data:
+  for ix, datum in enumerate(data):
+    these_data = data[:(ix+1)]
     res = {}
     # get the results
     for name, ripl in ripls.items():
       _ = ripl.observe('(x)', datum)
       technique = techniques[name]
       if name == 'rejection':
-        this_res = ripl.infer('(cycle ({0} (peek mu)) 1)'.format(technique))
-        res[name] = np.array([x['mu'] for x in this_res]).ravel()
+        this_res = ripl.infer('(cycle ({0} (peek-all mu)) 1)'.format(technique))
+        res[name] = np.array(this_res['mu']).ravel()
       else:
-        this_res = ripl.infer('(cycle ({0} (peek mu)) 5)'.format(technique))
-        res[name] = np.array([x['mu'] for x in this_res]).T
+        this_res = ripl.infer('(cycle ({0} (peek-all mu)) 5)'.format(technique))
+        res[name] = np.array(this_res['mu'])
     # make the plots
-    for i in range(n):
+    for i in range(5):
+      label = 'Data point {0}; Iteration {1}'.format(ix + 1, i + 1)
       print count
       fig, ax = plt.subplots(1)
-      sns.distplot(res['rejection'], ax = ax, label = 'rejection', hist = False)
-      for technique in ['mh', 'hmc', 'nesterov']:
-        sns.distplot(res[technique][i], ax = ax, label = technique, hist = False)
-      ax.set_xlim([-5,10])
-      ax.set_ylim([0,2])
+      sns.kdeplot(res['rejection'], ax = ax, label = 'rejection')
+      for technique in ['mh', 'hmc']:
+        sns.kdeplot(res[technique][i], ax = ax, label = technique)
+      nester = res['nesterov'][i]
+      this_bw = 'scott' if count < 10 else 0.05
+      sns.kdeplot(res['nesterov'][i], ax = ax, label = 'nesterov', bw = this_bw)
+      sns.rugplot(these_data, ax = ax, height = 0.1, color = 'black')
+      ax.set_xlim([-0,10])
+      ax.set_ylim([0,1.5])
+      ax.set_xlabel('\mu')
+      ax.set_title(label)
       fig.savefig(path.join(outdir, 'iter_{0}'.format(count)))
       plt.close(fig)
       count += 1
 
 outdir = path.join(path.dirname(path.realpath(__file__)), 'gaussian-evolution')
 runme(outdir)
+video_name = path.join(outdir, 'inference_comparison.mp4')
+template_str = path.join(outdir, 'iter_%d.png')
+scale_correction = '"scale=trunc(iw/2)*2:trunc(ih/2)*2"'
+os.system('avconv -y -r 5 -i {0} -vf {1} {2}'.format(template_str, scale_correction, video_name))
