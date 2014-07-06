91995ca8f3264b784402604a9626639b28122099
Alexey Radul <axch@mit.edu>
2014-07-06 19:17:52 -0400

    Draft a smoke test for peek-all.

--- test/inference_language/test_peek.py
+++ test/inference_language/test_peek.py
@@ -16,3 +16,22 @@ def testPeekSmoke2():
   predictions = ripl.infer("(cycle ((peek (normal 0 1) x)) %s)" % default_num_samples())['x']
   cdf = stats.norm(loc=0.0, scale=1.0).cdf
   return reportKnownContinuous(cdf, predictions, "N(0,1)")
+
+@statisticalTest
+def testPeekAllSmoke1():
+  ripl = get_ripl()
+  ripl.infer("(resample 2)")
+  ripl.assume("x", "(normal 0 1)")
+  predictionss = ripl.infer("(cycle ((mh default one 1) (peek-all x)) %s)" % default_num_samples())['x']
+  assert len(predictionss) == 2
+  cdf = stats.norm(loc=0.0, scale=1.0).cdf
+  return reportKnownContinuous(cdf, predictionss[0], "N(0,1)")
+
+@statisticalTest
+def testPeekAllSmoke2():
+  ripl = get_ripl()
+  ripl.infer("(resample 2)")
+  predictionss = ripl.infer("(cycle ((peek-all (normal 0 1) x)) %s)" % default_num_samples())['x']
+  assert len(predictionss) == 2
+  cdf = stats.norm(loc=0.0, scale=1.0).cdf
+  return reportKnownContinuous(cdf, predictionss[0], "N(0,1)")
