6ff6912ee90d6d9120d64409337644cf0916889f
Alexey Radul <axch@mit.edu>
2014-07-06 20:41:53 -0400

    A smoke test for plotf.  Duly fails.

--- test/inference_language/test_peek.py
+++ test/inference_language/test_peek.py
@@ -37,3 +37,12 @@ def testPeekAllSmoke2():
     assert len(item) == 2
   cdf = stats.norm(loc=0.0, scale=1.0).cdf
   return reportKnownContinuous(cdf, [i[0] for i in predictionss], "N(0,1)")
+
+def testPlotfSmoke():
+  # This is the example from examples/normal_plot.vnt
+  ripl = get_ripl()
+  ripl.infer("(resample 10)")
+  ripl.assume("x", "(normal 0 1)")
+  ripl.assume("y", "(normal x 1)")
+  ripl.assume("abs", "(lambda (x) (if (< x 0) (- 0 x) x))")
+  ripl.infer("(cycle ((mh default all 1) (plotf (ltsr lctl pc0r h0 h1 p0s2 p0d1ds) x y (abs (- y x)))) 100)")
