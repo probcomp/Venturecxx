96a646945a1ab1c4832914d0302ba896c8cb1045
vladfi1 <vladfi2@gmail.com>
2014-07-08 14:53:59 -0400

    Some more runtime error tests.

--- test/exception/test_runtime.py
+++ test/exception/test_runtime.py
@@ -1,7 +1,6 @@
-from nose.tools import eq_, raises
+from nose.tools import raises
 from venture.test.config import get_ripl, backend
 from testconfig import config
-from nose import SkipTest
 from venture.lite.exception import VentureError
 
 @backend('lite')
@@ -9,3 +8,31 @@ from venture.lite.exception import VentureError
 def testSymbolNotFound():
   ripl = get_ripl()
   ripl.predict('a')
+
+@backend('lite')
+@raises(VentureError)
+def testDoubleAssume():
+  ripl = get_ripl()
+  ripl.assume('a', 1)
+  ripl.assume('a', 1)
+
+@backend('lite')
+@raises(VentureError)
+def testNoSPRef():
+  ripl = get_ripl()
+  ripl.predict('(1 + 1)')
+
+@backend('lite')
+@raises(VentureError)
+def testLambda():
+  ripl = get_ripl()
+  ripl.assume('err', '(lambda () a)')
+  ripl.predict('(err)')
+
+@backend('lite')
+@raises(VentureError)
+def testLargeStack():
+  ripl = get_ripl()
+  ripl.assume('f', '(lambda (i) (if (= i 0) a (f (- i 1))))')
+  ripl.predict('(f 20)')
+
