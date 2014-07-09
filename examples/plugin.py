234c00ce9376847be3125ef9f6c82deef733b98d
Alexey Radul <axch@mit.edu>
2014-07-09 00:08:34 -0400

    Draw blinking red dots.

--- examples/plugin.py
+++ examples/plugin.py
@@ -6,19 +6,25 @@ from venture.lite.builtin import typed_nr
 
 import pygame
 pygame.init()
-windowSurfaceObj = pygame.display.set_mode((640, 400))
+window = pygame.display.set_mode((640, 400))
 pygame.display.update()
 
 class DrawFramePSP(psp.RandomPSP):
   "In the inference language, this will be usable as a raw symbol (no parameters)"
-  def __init__(self): pass
+  def __init__(self, window):
+    self.window = window
   def canAbsorb(self, _trace, _appNode, _parentNode): return False
   def simulate(self, args):
     inferrer = args.operandValues[0]
-    print 9
+    import pygame
+    import random
+    self.window.fill(pygame.Color(255, 255, 255))
+    for _ in range(10):
+      pygame.draw.circle(self.window, pygame.Color(255, 0, 0), (int(random.uniform(30, 610)), int(random.uniform(30, 370))), 20, 0)
+    pygame.display.update()
     return inferrer
 
-draw_sp = typed_nr(DrawFramePSP(), [v.ForeignBlobType()], v.ForeignBlobType())
+draw_sp = typed_nr(DrawFramePSP(window), [v.ForeignBlobType()], v.ForeignBlobType())
 
 class QuitPSP(psp.RandomPSP):
   "In the inference language, this will be usable as a raw symbol (no parameters)"
