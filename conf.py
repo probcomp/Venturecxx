836b306f9ac34ebd05f152df52c8434fa986a9f8
Alexey Radul <axch@mit.edu>
2014-07-15 14:51:03 -0400

    Do not show the source yet, because our code is not yet public.

--- conf.py
+++ conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
