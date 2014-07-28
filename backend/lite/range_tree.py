06868e80aed47cae08e51a111b5865ab35f20db7
Alexey Radul <axch@mit.edu>
2014-07-28 17:41:31 -0400

    Whitespace.

--- backend/lite/range_tree.py
+++ backend/lite/range_tree.py
@@ -6,9 +6,9 @@ class Node(object):
   def __init__(self, vals):
     if len(vals) == 0:
       raise ValueError("Can't instantiate Node with empty values.")
-    
+
     self.leaf = len(vals) == 1
-    
+
     if self.leaf:
       self.total = vals[0]
     else:
@@ -16,7 +16,7 @@ class Node(object):
       self.left = Node(vals[:self.mid])
       self.right = Node(vals[self.mid:])
       self.total = self.left.total + self.right.total
-  
+
   def __getitem__(self, index):
     if self.leaf:
       return self.total
@@ -24,7 +24,7 @@ class Node(object):
       return self.left[index]
     else:
       return self.right[index - self.mid]
-  
+
   def delta(self, index, d):
     self.total += d
     if not self.leaf:
@@ -32,26 +32,26 @@ class Node(object):
         self.left.delta(index, d)
       else:
         self.right.delta(index - self.mid, d)
-  
+
   def increment(self, index):
     self.delta(index, 1)
-  
+
   def decrement(self, index):
     self.delta(index, -1)
-  
+
   def __setitem__(self, index, value):
     self.delta(self, index, value - self[index])
-  
+
   def __iter__(self):
     if self.leaf:
       yield self.total
     else:
       for leaf in chain(self.left, self.right):
         yield leaf
-  
+
   def leaves(self):
     return list(self)
-  
+
   def __len__(self):
     if self.leaf:
       return 1
@@ -60,12 +60,12 @@ class Node(object):
 
 def sample(*nodes):
   n = nodes[0]
-  
+
   if n.leaf: return 0
-  
+
   total = sum(node.total for node in nodes)
   left = sum(node.left.total for node in nodes)
-  
+
   if npr.random() * total < left:
     return sample(*[node.left for node in nodes])
   else:
