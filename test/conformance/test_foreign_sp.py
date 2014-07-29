76ef63eb3291bef6d8a5d707e445b8a9d8db0ad6
Anthony Lu <lua@mit.edu>
2014-07-29 18:55:37 -0400

    Annotate the test with ignoresConfiguredInferenceProgram.

--- test/conformance/test_foreign_sp.py
+++ test/conformance/test_foreign_sp.py
@@ -1,6 +1,7 @@
-from venture.test.config import get_ripl
+from venture.test.config import get_ripl, ignoresConfiguredInferenceProgram
 from venture.lite import builtin
 
+@ignoresConfiguredInferenceProgram
 def test_foreign_aaa():
     builtins = builtin.builtInSPs()
     ripl = get_ripl()
@@ -15,6 +16,7 @@ def test_foreign_aaa():
     ripl.infer(10)
     assert ripl.sample("f")["counts"] == [1]
 
+@ignoresConfiguredInferenceProgram
 def test_foreign_aaa_resampled():
     builtins = builtin.builtInSPs()
     ripl = get_ripl()
@@ -30,6 +32,7 @@ def test_foreign_aaa_resampled():
     ripl.infer(10)
     assert ripl.sample("f")["counts"] == [1]
 
+@ignoresConfiguredInferenceProgram
 def test_foreign_aaa_uc():
     builtins = builtin.builtInSPs()
     ripl = get_ripl()
