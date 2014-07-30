43d15142e97fd2cf7b24af15766b23d870f9f563
Alexey Radul <axch@mit.edu>
2014-07-30 17:43:09 -0400

    I don't think I am going to count incorporate as a meaningful inference primitive.

--- test/conformance/test_get_global_logscore.py
+++ test/conformance/test_get_global_logscore.py
@@ -1,8 +1,10 @@
-from venture.test.config import get_ripl
 from nose import SkipTest
 from nose.tools import assert_almost_equal
 import numpy as np
 
+from venture.test.config import get_ripl, on_inf_prim
+
+@on_inf_prim("none")
 def test_global_logscore():
     ripl = get_ripl()
     for _ in range(100):
@@ -12,6 +14,7 @@ def test_global_logscore():
     logscore_true = -100*np.log(2)
     assert_almost_equal(logscore, logscore_true)
 
+@on_inf_prim("none")
 def test_global_logscore_coupled():
     raise SkipTest("Exchangeable coupling breaks get_global_logscore. Issue: https://app.asana.com/0/11127829865276/14115439427385")
     ripl = get_ripl()
