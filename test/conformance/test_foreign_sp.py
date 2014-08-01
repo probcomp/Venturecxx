d5d5a124c2c58422378c7dcb1349b62352818044
Anthony Lu <lua@mit.edu>
2014-08-01 16:12:18 -0400

    Add a statistical test of foreign HMM SP.

--- test/conformance/test_foreign_sp.py
+++ test/conformance/test_foreign_sp.py
@@ -1,4 +1,5 @@
-from venture.test.config import get_ripl, defaultInfer, skipWhenInParallel
+from venture.test.config import get_ripl, defaultInfer, skipWhenInParallel, collectSamples
+from venture.test.stats import statisticalTest, reportKnownDiscrete
 from venture.lite import builtin
 
 import numpy as np
@@ -61,3 +62,32 @@ def test_foreign_latents():
 
     ripl.infer(defaultInfer())
     assert ripl.sample("f")[0] == [np.matrix([[1]]), np.matrix([[1]])]
+
+@statisticalTest
+def test_foreign_latents_infer():
+    "Same as test.inference_quality.micro.test_latents.testHMMSP1"
+
+    builtins = builtin.builtInSPs()
+    ripl = get_ripl()
+    ripl.bind_foreign_sp("test_lazy_hmm", builtins["make_lazy_hmm"])
+
+    ripl.assume("f", """
+(test_lazy_hmm
+ (simplex 0.5 0.5)
+ (matrix (array (array 0.7 0.3)
+                (array 0.3 0.7)))
+ (matrix (array (array 0.9 0.2)
+                (array 0.1 0.8))))
+""")
+    ripl.observe("(f 1)","atom<0>")
+    ripl.observe("(f 2)","atom<0>")
+    ripl.observe("(f 3)","atom<1>")
+    ripl.observe("(f 4)","atom<0>")
+    ripl.observe("(f 5)","atom<0>")
+    ripl.predict("(f 6)",label="pid")
+    ripl.predict("(f 7)")
+    ripl.predict("(f 8)")
+
+    predictions = collectSamples(ripl,"pid")
+    ans = [(0,0.6528), (1,0.3472)]
+    return reportKnownDiscrete(ans, predictions)
