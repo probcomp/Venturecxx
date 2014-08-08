2c8116141f32a5774459a1e40ae92633cec45cf9
Alexey Radul <axch@mit.edu>
2014-08-08 18:37:52 -0400

    Hm.  I might have a problem.

--- backend/hs/TraceView.hs
+++ backend/hs/TraceView.hs
@@ -441,6 +441,13 @@ exec' (Infer prog) = do
   (t, e) <- lift get
   let t' = extend_trace_view t e
   let inf_exp = App prog [Datum $ ReifiedTraceView t]
+  -- Can exec' ever be called inside a regen' without an intervening
+  -- extend?  I think so.  If that is the case, then it may happen
+  -- that some nodes in the current view are not yet regenerated.  If
+  -- that, in turn, is the case, then the enclosed eval' of the
+  -- inf_exp may uncover new dependencies on such unregenerated
+  -- values, in which case it will be necessary to regenerate them.
+  -- TODO: Make that happen.
   ((addr, d), t'') <- mapMonad lift $ coroutineRunRegenEffect (eval' inf_exp e) mempty t'
   let ReifiedTraceView t''' = fromJust "eval returned empty node" $ valueOf $ fromJust "eval returned invalid address" $ lookupNode addr t''
   -- TODO What do I do with the density coming up from the bottom, if any?
