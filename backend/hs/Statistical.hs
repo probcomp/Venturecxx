dcc6c0858c87447b2466a14390af681a9d5bb7c7
Alexey Radul <axch@mit.edu>
2014-08-11 17:44:55 -0400

    Draft the knownDiscrete test.

--- backend/hs/Statistical.hs
+++ backend/hs/Statistical.hs
@@ -3,6 +3,9 @@
 
 module Statistical where
 
+import qualified Data.Map as M
+import Data.List
+
 import Data.Random.Distribution
 import Data.Random.Distribution.ChiSquare
 
@@ -20,3 +23,27 @@ fisherMethod :: [PValue] -> PValue
 fisherMethod pvals | any (== 0) pvals = 0
                    | otherwise = sf (ChiSquare (2 * fromIntegral (length pvals))) chisq where
                    chisq = -2 * (sum $ map log pvals)
+
+chi2test :: [Int] -> [Double] -> (Double, PValue)
+chi2test observed expected = (stat, pval) where
+    stat = sum $ map cmp $ zip observed expected
+    cmp (obs, exp) = (fromIntegral obs - exp) ** 2 / exp
+    pval = sf (ChiSquare (fromIntegral (length expected) - 1)) stat
+
+counts :: (Ord a, Eq a) => [a] -> [Int]
+counts = map length . group . sort
+
+normalize :: (Eq a, Ord a) => [(a, Double)] -> M.Map a Double
+normalize items = M.fromList items' where
+    total = sum $ map snd items
+    items' = map scale items
+    scale (i, rate) = (i, rate / total)
+
+knownDiscrete :: (Ord a, Eq a) => [(a, Double)] -> [a] -> Result String
+knownDiscrete expected_rates observed = Result (pval, msg) where
+    total = length observed
+    expected_cts = normalize expected_rates
+    cts = map count $ M.keys expected_cts
+    count item = length $ filter (== item) observed
+    (chisq, pval) = chi2test cts $ map (* fromIntegral total) $ M.elems expected_cts
+    msg = "discrete distribution didn't line up" -- TODO: more information
