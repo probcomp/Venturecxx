70c16d03f9a7c6c70ba94f775fbd8603591290de
Alexey Radul <axch@mit.edu>
2014-08-11 17:03:38 -0400

    Turns out I needed to actually do something to make cabal recognize test failures.
    Silly Haskell community, not integrating their tools.  Good thing the
    tests actually pass.

--- backend/hs/VentureTest.hs
+++ backend/hs/VentureTest.hs
@@ -1,15 +1,21 @@
+import System.Exit
 import Test.HUnit
 
 import Language
 import Venture
 import Engine
 
-main = runTestTT $ test
-  [ venture_main 1 [Predict 1] >>= (@?= [1])
-  , venture_main 1 [Predict $ App v_id [1]] >>= (@?= [1])
-  , venture_main 1 [Predict $ v_let1 "id" v_id (App (Var "id") [1])] >>= (@?= [1])
-  -- K combinator
-  , venture_main 1 [Predict $ App (App v_k [1]) [2]] >>= (@?= [1])
-  ]
+main = do
+  Counts { failures = f, errors = e } <- runTestTT $ test
+   ([ venture_main 1 [Predict 1] >>= (@?= [1])
+    , venture_main 1 [Predict $ App v_id [1]] >>= (@?= [1])
+    , venture_main 1 [Predict $ v_let1 "id" v_id (App (Var "id") [1])] >>= (@?= [1])
+    -- K combinator
+    , venture_main 1 [Predict $ App (App v_k [1]) [2]] >>= (@?= [1])
+    ])
+  if f + e > 0 then
+      exitWith $ ExitFailure $ f + e
+  else
+      exitSuccess
     where v_id = (Lam ["x"] (Var "x"))
           v_k = (Lam ["x"] (Lam ["y"] (Var "x")))
