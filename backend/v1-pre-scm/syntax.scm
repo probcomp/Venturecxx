6f3e7f30f645969e420855e75973088ef280653a
Alexey Radul <axch@mit.edu>
2014-08-22 16:34:44 -0400

    Draft an interpreter for the v1-pre language, with intent to make traces pluggable, by copying most of rmvm and translating into my current mental model.
    Note: Depends on my pattern case library.  We can make it a git
    submodule, but until we do
    git clone git@github.com:axch/pattern-case.git

---
+++ backend/v1-pre-scm/syntax.scm
@@ -0,0 +1,42 @@
+(declare (usual-integrations))
+
+(define ((tagged-list? tag) thing)
+  (and (pair? thing)
+       (eq? (car thing) tag)))
+
+(define (constant? exp)
+  (and (not (symbol? exp))
+       (or (not (pair? exp))
+           (eq? (car exp) 'quote))))
+(define (constant-value exp)
+  (if (pair? exp)
+      (cadr exp)
+      exp))
+(define-algebraic-matcher constant constant? constant-value)
+
+(define begin-form? (tagged-list? 'begin))
+;; Wins with list of expressions
+(define-algebraic-matcher begin-form begin-form? cdr)
+
+(define definition? (tagged-list? 'define))
+;; Wins with variable and expression
+(define-algebraic-matcher definition definition? cadr caddr)
+
+(define (var? thing) (symbol? thing))
+(define-algebraic-matcher var var? id-project)
+
+(define if-form? (tagged-list? 'if))
+(define-algebraic-matcher if-form if-form? cadr caddr cadddr)
+
+(define lambda-form? (tagged-list? 'lambda))
+(define-algebraic-matcher lambda-form lambda-form? cadr caddr)
+
+(define extend-form? (tagged-list? 'ext))
+(define-algebraic-matcher extend-form extend-form? cadr)
+
+(define-integrable (nullary-magic tag)
+  ;; TODO Festoon this with all the needful integrable declarations
+  (lambda (thing win lose)
+    (if (and (pair? thing) (null? (cdr thing)) (eq? tag (car thing)))
+        (win)
+        (lose))))
--- backend/v1-pre-scm/v1-pre.scm
+++ backend/v1-pre-scm/v1-pre.scm
@@ -1,4 +1,6 @@
 (declare (usual-integrations))
+(declare (integrate-external "syntax"))
+(declare (integrate-external "pattern-case/pattern-case"))
 
 (define-structure (evaluation-context
                    (safe-accessors #t)
@@ -14,4 +16,124 @@
 (define-structure (rdb (safe-accessors #t))
   )
 
+;; A standard lexical environment structure; holds addresses into
+;; traces rather than values.
+(define-structure (env-frame (safe-accessors #t))
+  parent
+  symbols
+  addresses) ; Parallel lists mapping symbols to addresses
 
+;; Compound procedures
+(define-structure (compound (safe-accessors #t))
+  formals
+  body
+  env
+  trace
+  read-traces)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (eval exp env trace addr read-traces)
+  (trace-search-one trace addr (lambda (v) v)
+   (lambda ()
+     (let ((answer (do-eval exp env trace addr read-traces)))
+       ;; And maybe metadata depending on what kind of trace it is.
+       ;; The metadata is presumably dependent on the structure of
+       ;; the thunk, so this abstraction will not endure
+       (record! trace exp env addr read-traces answer)
+       answer))))
+
+(define (do-eval exp env trace addr read-traces)
+  (case* exp
+    ((constant val) val)
+    ((var x)
+     (env-search env x
+      (lambda (addr)
+        (traces-lookup (cons trace read-traces) addr))
+      (lambda ()
+        ((access eval system-global-environment) x user-initial-environment))))
+    ((lambda-form formals body)
+     ;; Do I need to close over the maker address?
+     (make-compound formals body env trace read-traces))
+    ;; Interesting new operation: evaluate in a subtrace.  Adding a
+    ;; new trace frame like this hides the nested evaluation from
+    ;; appearing in the current trace, and can shield the current
+    ;; trace from reflective changes made to the nexted one if the
+    ;; trace modification policy enforces that.
+    ((extend-form subform)
+     (let ((subtrace (trace-extend trace))
+           (addr* (extend-address addr 'ext)))
+       ;; Could add trace to read-traces here instead of stashing
+       ;; it in the parent pointer
+       (eval subform env subtrace addr* read-traces)))
+    ;; Permit reflection on the evaluation context
+    ((nullary-magic 'get-current-environment) env)
+    ((nullary-magic 'get-current-trace) trace)
+    ((nullary-magic 'get-current-read-traces) read-traces)
+    ((definition x subexp)
+     (let ((addr* (extend-address addr 'defn)))
+       (eval subexp env trace addr* read-traces)
+       (env-bind! env x addr*)))
+    ((if-form p c a)
+     (if (eval p env trace (extend-address addr 'if-p) read-traces)
+         (eval c env trace (extend-address addr 'if-c) read-traces)
+         (eval a env trace (extend-address addr 'if-a) read-traces)))
+    ((begin-form forms)
+     (let ()
+       (define result #f)
+       (for-each
+        (lambda (s i)
+          (let ((addr* (extend-address addr `(begin ,i))))
+            (set! result (eval s env trace addr* read-traces))))
+        forms
+        (iota (length forms)))
+       result))
+    (else ;; Application
+     (let ((subaddrs (map (lambda (e i)
+                            (let ((addr* (extend-address addr `(app-sub ,i))))
+                              (eval e env trace addr* read-traces)
+                              addr*))
+                          (subforms exp)
+                          (iota (length (subforms exp))))))
+       (apply (car subaddrs) (cdr subaddrs) addr trace read-traces)))))
+
+(define (apply oper opands addr cur-trace read-traces)
+  (let ((oper (trace-lookup cur-trace oper)))
+    (cond ((primitive? oper)
+           ((primitive-apply oper) opands))
+          ((compound? oper)
+           (let ((formals (compound-formals oper))
+                 (body (compound-body oper))
+                 (env (compound-env oper))
+                 (trace (compound-trace oper))
+                 (read-traces (compound-read-traces oper)))
+             (let ((env* (extend-env env formals opands))
+                   (trace* cur-trace)
+                   (addr* (extend-address addr 'app))
+                   ;; This way, a compound procedure does not carry
+                   ;; write permission to the trace in which it was
+                   ;; created
+                   (read-traces* (cons trace read-traces)))
+               (eval body env* trace* addr* read-traces*))))
+          ((procedure? oper) ; An MIT Scheme procedure
+           (let ((arguments (map (lambda (o)
+                                   (traces-lookup (cons cur-trace read-traces) o))
+                                 opands)))
+             ((access apply system-global-environment) oper arguments))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (env-search env symbol win lose)
+  (if (env-frame? env)
+      (let loop ((ss (env-frame-symbols env))
+                 (as (env-frame-addresses env)))
+        (cond ((null? ss)
+               (env-search (env-frame-parent env) symbol win lose))
+              ((eq? (car ss) symbol)
+               (win (car as)))
+              (else (loop (cdr ss) (cdr as)))))
+      (lose)))
+(define extend-env make-env-frame)
+(define (env-bind! env sym addr)
+  (set-env-frame-symbols! env (cons sym (env-frame-symbols env)))
+  (set-env-frame-addresses! env (cons addr (env-frame-addresses env))))
