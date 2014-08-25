cfc730f23a9b92754b0683b4f4826faf6fb01f0f
Alexey Radul <axch@mit.edu>
2014-08-25 15:55:44 -0400

    Turn the examples from the rmvm source into test cases.

--- backend/v1-pre-scm/test/load.scm
+++ backend/v1-pre-scm/test/load.scm
@@ -25,6 +25,28 @@
       assert-proc
       run-tests-and-exit)))
 
+(define map-defn
+  '(define map
+     (lambda (f lst)
+       (if (pair? lst)
+           (cons (f (car lst)) (map f (cdr lst)))
+           lst))))
+
 (define-each-check
-  (equal? (top-eval 1) 1))
+  (equal? (top-eval 1) 1)
+  (equal? (top-eval '((lambda () 1))) 1)
+  (equal? (top-eval '((lambda (x) 1) 2)) 1)
+  (equal? (top-eval '((lambda (x) x) 2)) 2)
+  (equal? (top-eval '((lambda (x) (ext x)) 3)) 3)
+  (equal? (top-eval '((ext (lambda (x) (ext x))) 4)) 4)
+  (equal? (top-eval '(+ 3 2)) 5)
+  (equal? (top-eval '(((lambda (x) (lambda (y) (+ x y))) 3) 4)) 7)
+  (equal? (top-eval '(((lambda (x) (ext (lambda (y) (+ x y)))) 3) 4)) 7)
+  (equal? (top-eval '(begin (+ 2 3) (* 2 3))) 6)
+  (equal? (top-eval
+           `(begin
+              ,map-defn
+              (map (lambda (x) (+ x 1)) (list 1 2 3))))
+          '(2 3 4))
+  )
 
