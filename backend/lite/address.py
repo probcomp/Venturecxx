840af3ba3d1aba8ee9c81f64acef92903a76a2d0
Vlad Firoiu <vladfi2@gmail.com>
2014-08-26 16:02:43 -0400

    Reverse list order.

--- backend/lite/address.py
+++ backend/lite/address.py
@@ -9,8 +9,8 @@ class EmptyList(object):
   def __repr__(self):
     return str(list(self))
   
-  def prepend(self, first):
-    return List(first, self)
+  def append(self, last):
+    return List(last, self)
 
   def map(self, f):
     return self
@@ -19,18 +19,16 @@ emptyList = EmptyList()
 
 class List(object):
   
-  def __init__(self, first, rest=emptyList):
-    assert(first is not None)
-    assert(rest is not None)
-    self.first = first
+  def __init__(self, last, rest=emptyList):
+    self.last = last
     self.rest = rest
   
   # FIXME: quadratic runtime :(
   # even python3's "yield from" doesn't work
   def __iter__(self):
-    yield self.first
     for i in self.rest:
       yield i
+    yield self.last
   
   def __len__(self):
     return 1 + len(self.rest)
@@ -38,11 +36,11 @@ class List(object):
   def __repr__(self):
     return str(list(self))
   
-  def prepend(self, first):
-    return List(first, self)
+  def append(self, last):
+    return List(last, self)
 
   def map(self, f):
-    return self.rest.map(f).prepend(f(self.first))
+    return self.rest.map(f).append(f(self.last))
 
 class Address(List):
   """Maintains a call stack."""
@@ -51,11 +49,11 @@ class Address(List):
   
   def request(self, index):
     """Make a new stack frame."""
-    return self.prepend(index)
+    return self.append(index)
   
   def extend(self, index):
     """Extend the current stack frame."""
-    return Address(self.first.prepend(index), self.rest)
+    return Address(self.last.append(index), self.rest)
   
   def asList(self):
     """Converts to nested lists."""
