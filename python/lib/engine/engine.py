68463e5ab7e31628f361337e36f5bf34bdf5d786
vladfi1 <vladfi2@gmail.com>
2014-08-28 19:52:56 -0400

    Use dill instead of pickle in order to serialize lambdas.

--- python/lib/engine/engine.py
+++ python/lib/engine/engine.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License along with Venture.  If not, see <http://www.gnu.org/licenses/>.
 import random
-import pickle
+import dill as pickle
 import time
 
 from venture.exception import VentureException
