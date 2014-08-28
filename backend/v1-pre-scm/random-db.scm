0c7e5103316c142f0b21c7e753e40f190b458548
Alexey Radul <axch@mit.edu>
2014-08-28 16:25:16 -0400

    Right.  Better return the answer if I want to do statistics on it.

--- backend/v1-pre-scm/random-db.scm
+++ backend/v1-pre-scm/random-db.scm
@@ -239,7 +239,7 @@
                      (enforce-constraints t)
                      (map (lambda (i) (mcmc-step t))
                           '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)))))
-     (pp answer)))
+     answer))
 
 ;;; On observations: VKM argues that requiring observe to accept only
 ;;; a constant (!) (wrt current trace) procedure that has a density as
