4ab4d6d828e78d849b949a8110baea2e22b78aab
David Wadden <dave.wadden@gmail.com>
2014-08-29 19:16:58 -0400

    test_peek.py now passes.

--- test/inference_language/test_peek.py
+++ test/inference_language/test_peek.py
@@ -1,14 +1,23 @@
 import scipy.stats as stats
+from nose.tools import eq_
 
 from venture.test.stats import statisticalTest, reportKnownContinuous
 from venture.test.config import get_ripl, default_num_samples, on_inf_prim
+
+def extract_from_frame(infer_result, names):
+  '''Extract trace for desired variable(s) from InferResult'''
+  return infer_result.dataset()[names]
+
+def extract_from_panel(infer_result, names):
+  '''Extract trace for desired variable(s) from InferResult panel'''
+  return infer_result.panel().loc[:,:,names]
 
 @statisticalTest
 @on_inf_prim("peek") # Technically also MH, but not testing it
 def testPeekSmoke1():
   ripl = get_ripl()
   ripl.assume("x", "(normal 0 1)")
-  predictions = ripl.infer("(cycle ((mh default one 1) (peek x)) %s)" % default_num_samples())['x']
+  predictions = extract_from_frame(ripl.infer("(cycle ((mh default one 1) (peek x)) %s)" % default_num_samples()), 'x')
   cdf = stats.norm(loc=0.0, scale=1.0).cdf
   return reportKnownContinuous(cdf, predictions, "N(0,1)")
 
@@ -16,7 +25,13 @@ def testPeekSmoke1():
 @on_inf_prim("peek")
 def testPeekSmoke2():
   ripl = get_ripl()
-  predictions = ripl.infer("(cycle ((peek (normal 0 1) x)) %s)" % default_num_samples())['x']
+  predictions = extract_from_frame(ripl.infer("(cycle ((peek (normal 0 1))) %s)" % default_num_samples()), '(normal 0.0 1.0)')
+  cdf = stats.norm(loc=0.0, scale=1.0).cdf
+  return reportKnownContinuous(cdf, predictions, "N(0,1)")
+
+def testPeekSmoke3():
+  ripl = get_ripl()
+  predictions = extract_from_frame(ripl.infer("(cycle ((peek (pair (normal 0 1) label))) %s)" % default_num_samples()), 'label')
   cdf = stats.norm(loc=0.0, scale=1.0).cdf
   return reportKnownContinuous(cdf, predictions, "N(0,1)")
 
@@ -26,22 +41,20 @@ def testPeekAllSmoke1():
   ripl = get_ripl()
   ripl.infer("(resample 2)")
   ripl.assume("x", "(normal 0 1)")
-  predictionss = ripl.infer("(cycle ((mh default one 1) (peek x)) %s)" % default_num_samples())['x']
-  for item in predictionss:
-    assert len(item) == 2
+  predictionss = extract_from_panel(ripl.infer("(cycle ((mh default one 1) (peek x)) %s)" % default_num_samples()), 'x')
+  eq_(predictionss.shape, (2,2))
   cdf = stats.norm(loc=0.0, scale=1.0).cdf
-  return reportKnownContinuous(cdf, [i[0] for i in predictionss], "N(0,1)")
+  return reportKnownContinuous(cdf, predictionss.values.ravel(), "N(0,1)")
 
 @statisticalTest
 @on_inf_prim("peek") # Technically also resample, but not testing it
 def testPeekAllSmoke2():
   ripl = get_ripl()
   ripl.infer("(resample 2)")
-  predictionss = ripl.infer("(cycle ((peek (normal 0 1) x)) %s)" % default_num_samples())['x']
-  for item in predictionss:
-    assert len(item) == 2
+  predictionss = extract_from_panel(ripl.infer("(cycle ((peek (pair (normal 0 1) x))) %s)" % default_num_samples()), 'x')
+  eq_(predictionss.shape, (2,2))
   cdf = stats.norm(loc=0.0, scale=1.0).cdf
-  return reportKnownContinuous(cdf, [i[0] for i in predictionss], "N(0,1)")
+  return reportKnownContinuous(cdf, predictionss.values.ravel(), "N(0,1)")
 
 @statisticalTest
 @on_inf_prim("plotf") # Technically also resample and MH, but not testing them
@@ -52,10 +65,10 @@ def testPlotfSmoke():
   ripl.assume("x", "(normal 0 1)")
   ripl.assume("y", "(normal x 1)")
   ripl.assume("abs", "(lambda (x) (if (< x 0) (- 0 x) x))")
-  out = ripl.infer("(cycle ((mh default all 1) (plotf (ltsr lctl pc0r h0 h1 p0s2 p0d1ds) x y (abs (- y x)))) 3)")
+  out = ripl.infer("(cycle ((mh default all 1) (plotf (ltsr lctl pc0r h0 h1 p0s2 p0d1ds) x y (abs (- y x)) (pair (abs x) abs_x))) 3)")
   cdf = stats.norm(loc=0.0, scale=2.0).cdf
   result = out.dataset()
-  for k in ["x", "y", "sweeps", "time (s)", "log score", "particle", "(abs (sub y x))"]:
+  for k in ["x", "y", "sweep count", "time (s)", "log score", "particle id", "(abs (sub y x))", "abs_x"]:
     assert k in result
     assert len(result[k]) == 30
   # Check that the dataset can be extracted again
