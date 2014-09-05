876e52c7f7552bf09154d564c92982849958f2ae
Yutian Chen <yutian.chen@eng.cam.ac.uk>
2014-09-05 23:26:19 +0100

    Remove unrelevant bits.

--- subsampled_mh_test/jointdplr_avg_acc.m
+++
@@ -1,9 +0,0 @@
-function acc = jointdplr_avg_acc(Ypred, Ytst)
-
-Ytst = Ytst(:);
-assert(size(Ypred, 1) == length(Ytst));
-
-acc = mean(bsxfun(@eq, bsxfun(@rdivide, cumsum(Ypred, 2), 1 : size(Ypred, 2)) > 0.5, Ytst));
-% acc = mean(bsxfun(@rdivide, cumsum(bsxfun(@eq, Ypred, Ytst), 2)), 1:size(Ypred,2));
-
-end
