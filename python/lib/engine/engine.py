26cc848ed2dbd4b6cd332d7473300397fa7b41db
David Wadden <dave.wadden@gmail.com>
2014-09-26 20:12:10 -0400

    Multiprocessing seems to use cPickle rather than pickle (based on the error message when the Pipe breaks).

--- python/lib/engine/engine.py
+++ python/lib/engine/engine.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License along with Venture.  If not, see <http://www.gnu.org/licenses/>.
 import random
 import dill
-import pickle
+import cPickle as pickle
 import time
 
 from venture.exception import VentureException
