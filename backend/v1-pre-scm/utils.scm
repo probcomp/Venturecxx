8e5d870c951adeff10cc5a5d181254ae93275ba6
Alexey Radul <axch@mit.edu>
2014-09-29 17:09:44 -0400

    Hack using adaptive plot to compare empirical data to analytic CDFs.

--- backend/v1-pre-scm/utils.scm
+++ backend/v1-pre-scm/utils.scm
@@ -33,5 +33,36 @@
            (win (car vs)))
           (else (loop (cdr ks) (cdr vs))))))
 
+(define *binwidth* 0.2)
+
 (define (histogram-test-data data)
-  (gnuplot-histogram-alist (map (lambda (x) (cons x 0.2)) data) "test data" 0.2))
+  (gnuplot-histogram-alist (map (lambda (x) (cons x *binwidth*)) data) "test data" *binwidth*))
+
+(define (compare-data-to-cdf samples analytic . adverbs)
+  (let* ((samples (sort samples <))
+         (n (length samples))
+         (xlow (scheme-apply min samples))
+         (xhigh (scheme-apply max samples))
+         (padding (* 0.1 (- xhigh xlow)))
+         (cdf-plot (plot-stop-drawing!
+                    (scheme-apply replot (new-plot analytic (- xlow padding) (+ xhigh padding)) 'invisibly adverbs)))
+         (empirical (append-map
+                     (lambda (x i)
+                       (list (cons x (/ i n)) (cons x (/ (+ i 1) n))))
+                     samples (iota n))))
+    (call-with-temporary-file-pathname
+     (lambda (empirical-pathname)
+       (call-with-temporary-file-pathname
+        (lambda (analytic-pathname)
+          (gnuplot-write-alist empirical empirical-pathname)
+          (gnuplot-write-alist (plot-relevant-points-alist cdf-plot) analytic-pathname)
+          (let ((command (string-append
+                          "gnuplot -p -e \'"
+                          "set style data lines; "
+                          "set key noautotitles; "
+                          "plot \"" (->namestring empirical-pathname) "\" title \"empirical\""
+                          ", \"" (->namestring analytic-pathname) "\" title \"analytic\""
+                          "'")))
+            (display command)
+            (newline)
+            (run-shell-command command))))))))
