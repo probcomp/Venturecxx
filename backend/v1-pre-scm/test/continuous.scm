b2f4d04cd3f755c5e371124f4fd4462bddec9088
Alexey Radul <axch@mit.edu>
2014-10-06 15:32:16 -0400

    Don't actually want to see K-S debug info now.

--- backend/v1-pre-scm/test/continuous.scm
+++ backend/v1-pre-scm/test/continuous.scm
@@ -1,7 +1,6 @@
 (define-test (forward-normal-dist)
   (let ()
     (define samples (collect-samples `(begin ,gaussian-defn (normal 0 1))))
-    (pp (list 'program-samples (sort samples <)))
     (check (> (k-s-test samples (lambda (x) (gaussian-cdf x 0 1))) *p-value-tolerance*))))
 
 (define-test (forward-2-normal-dist)
@@ -11,7 +10,6 @@
        `(begin
           ,gaussian-defn
           (normal (normal 0 1) 1))))
-    (pp (list 'program-samples (sort samples <)))
     (check (> (k-s-test samples (lambda (x) (gaussian-cdf x 0 (sqrt 2)))) *p-value-tolerance*))))
 
 (define-test (unrestricted-infer-normal-dist)
@@ -27,11 +25,9 @@
             (assume y (normal mu 1))
             (infer (mcmc 10))
             (predict mu)))))
-    (pp (list 'program-samples (sort samples <)))
     (check (> (k-s-test samples (lambda (x) (gaussian-cdf x 0 1))) *p-value-tolerance*))))
 
 (define-test (observed-normal-dist)
   (let ()
     (define samples (collect-samples (gaussian-example 20)))
-    (pp (list 'program-samples (sort samples <)))
     (check (> (k-s-test samples (lambda (x) (gaussian-cdf x 1 (/ 1 (sqrt 2))))) *p-value-tolerance*))))
