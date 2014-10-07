f47bd35858046cff1004868823633cd8ab08a09f
Erik Sjoberg <sjobeek@gmail.com>
2014-10-07 22:27:23 +0900

    modified directory for bokeh data files

--- script/container_init.sh
+++ script/container_init.sh
@@ -1,10 +1,12 @@
 screen -S x11vnc -d -m x11vnc -forever -create
 
 mkdir /root/bokeh_data
-screen -S bokeh -d -m bokeh-server --ip=0.0.0.0 --ws-port=5007 -D=/root/bokeh_data
+cd /root/bokeh_data
+screen -S bokeh -d -m bokeh-server --ip=0.0.0.0 --ws-port=5007 -D=/root/bokeh_data/
 
 cd /root/Venturecxx/examples
-
 screen -S ipy_nb -d -m ipython notebook
+
+cd /root/Venturecxx
 
 /bin/bash
