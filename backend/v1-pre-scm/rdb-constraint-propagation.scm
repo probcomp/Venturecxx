b32cbd8bb33c3a9c17ff6668d55cac1bfaa377dc
Alexey Radul <axch@mit.edu>
2014-10-23 23:30:01 -0400

    I figure some of the motivating notes ought to live here.

--- backend/v1-pre-scm/rdb-constraint-propagation.scm
+++ backend/v1-pre-scm/rdb-constraint-propagation.scm
@@ -9,6 +9,65 @@
 ;;; I now think there may be a runtime solution to the same problem
 ;;; that's more general.
 
+;;; This program is motivated in part by the following set of
+;;; observations (though it does not fully implement them):
+
+#|
+Determining constancy and determinism can be a static analysis:
+- A (higher-order) procedure is deterministic if everything leaving
+  via positive positions is a deterministic function of whatever comes
+  in via negative positions
+  - This can be viewed as taint analysis from the source of randomness
+  - Or as type inference about the presence or absence of the randomness monad
+    - Assume automatic return on if branches where needed --
+      conservative analysis for finding procedures that are always
+      deterministic
+- This can be checked by a constancy analysis: Mark as constant
+  everything coming in through negative positions, propagate constancy
+  through the body, check constancy of everything leaving through
+  positive positions.
+  - Actually, the reverse version will work better: solve the system
+    of equations assuming procedures are deterministic unless proven
+    otherwise.
+- Constancy propagates through constancy-preserving syntax, and
+  through applications of deterministic procedures to constant
+  arguments (or constant and deterministic, if the arguments are
+  higher-order).
+- Constancy begins at source code literals and at variable references
+  that leave the current trace.
+- Mutation may break constancy if the replacement expression is
+  random.
+  - Mutation changes the notion of flow to include stuff flowing into
+    the object being mutated.
+  - I almost certainly don't want to mess with the timing analysis of
+    trying to recover the constancy of a sometime-random variable
+    being set to a constant value.  Just don't do that.
+- Containers: Of course, I could always choose to differentiate
+  between random containers and containers (some of) whose contents
+  are random; then accessors become polymorphic in the randomness of
+  that which is accessed.
+  - This is of course the place where the distinction between actually
+    constant and merely constant-body compound procedures comes from.
+- The safe thing for foreign procedures is to assume they are random
+  unless marked otherwise, but the convenient thing with procedures
+  pulled from Scheme is to assume they are deterministic unless marked
+  random (can mark them deterministic inside scheme->venture).
+- This whole analysis is not part of "the interpreter", though it does
+  depend on the syntax of the language, so can use additional metadata
+  tags to mark some procedures deterministic.
+- Of course, all the above only works for single-threaded execution.
+- Note: This is constancy and determinism with respect to the
+  manipulations that inference on the present trace might perform, not
+  absolute constancy over all runs of the program.  That is why things
+  that come in from external traces are presumed to be constant.
+|#
+
+;;; Note, however, that the "true" interface to observations should be
+;;; much looser than "only stuff this program can backpropagate to an
+;;; assessable random choice" -- any tail-assessable thing is
+;;; sematically observable, though the Markov chain may end up not
+;;; being ergodic if base measures are not fixed enough.
+
 (define (rdb-backpropagate-constraints! trace)
   (define (foldee addr value accum)
     (receive (addr* value*)
