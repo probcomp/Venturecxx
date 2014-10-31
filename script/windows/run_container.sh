e3813f290e732f4230eab365d27d1874bef695ff
Erik Sjoberg <sjobeek@gmail.com>
2014-10-31 20:39:15 +0900

    Added helpful message to indicate local IP-address of notebook server

--- script/windows/run_container.sh
+++ script/windows/run_container.sh
@@ -1,9 +1,13 @@
 boot2docker up
+boot2docker ip
 boot2docker ssh "
 cd $(pwd)
 if [ ${PWD##*/} = 'windows' ]; then
     cd ../..
 fi
+
+echo 'Visit http://192.168.59.103:8888/ in your browser to see example notebooks'
+
 docker run -ti -p 8888:8888 -p 5900:5900 -v \$(pwd):/root/Venturecxx venture
 
 echo 'Press enter to continue...'
