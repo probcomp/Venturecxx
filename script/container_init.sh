5f69f099ffb0526060ddbf89a85e40811a709948
Erik Sjoberg <sjobeek@gmail.com>
2014-10-31 22:21:29 +0900

    replacing bokeh_demos content of script with default contents

--- script/container_init.sh
+++ script/container_init.sh
@@ -1,12 +1,7 @@
 screen -S x11vnc -d -m x11vnc -forever -create
 
-mkdir /root/bokeh_data
-cd /root/bokeh_data
-screen -S bokeh -d -m bokeh-server --ip=0.0.0.0 --ws-port=5007 -D=/root/bokeh_data/
-
 cd /root/Venturecxx/examples
-screen -S ipy_nb -d -m ipython notebook
 
-cd /root/Venturecxx
+screen -S ipython_nb -d -m ipython notebook
 
-/bin/bash
+/bin/bash 
