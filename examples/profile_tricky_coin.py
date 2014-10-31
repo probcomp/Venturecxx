623a5288b23641c9ff2416098af7ae5c54420e2c
vladfi1 <vladfi2@gmail.com>
2014-10-30 22:08:33 -0400

    Updated example with (e)gibbs.

--- examples/profile_tricky_coin.py
+++ examples/profile_tricky_coin.py
@@ -4,7 +4,7 @@ from venture.exception import underline
 ripl = s.make_lite_church_prime_ripl()
 
 ripl.execute_program("""
-  [assume tricky (flip 0.5)]
+  [assume tricky (scope_include (quote tricky) 0 (flip 0.5))]
   [assume weight (if tricky (uniform_continuous 0 1) 0.5)]
   [assume coin (lambda () (flip weight))]
   [observe (coin) true]
@@ -13,13 +13,17 @@ ripl.execute_program("""
 """)
 
 ripl.profiler_enable()
-ripl.infer('(mh default one 100)')
+ripl.infer('(mh default one 10)')
+ripl.infer('(gibbs tricky one 1)')
 
 def printAddr((did, index)):
   exp = ripl.sivm._get_exp(did)
-  text, indeces = ripl.humanReadable(exp, did, index)
+  text, indyces = ripl.humanReadable(exp, did, index)
   print text
-  print underline(indeces)
+  print underline(indyces)
 
-map(printAddr, data.principle[0])
+data = ripl.profile_data()
+
+
+map(lambda addrs: map(printAddr, addrs), data.principal)
 
