68c3a978711fc574e94e4306d5f3772bbd7d2893
Erik Sjoberg <sjobeek@gmail.com>
2014-11-13 23:05:00 +0900

    de-duplicated windows and linux run_docker_container scripts

--- script/windows/3_run_container.sh
+++ script/windows/3_run_container.sh
@@ -8,7 +8,7 @@ fi
 
 echo 'Visit http://192.168.59.103:8888/ in your browser to see example notebooks'
 
-docker run -ti -p 8888:8888 -p 5900:5900 -p 9001:9001 -v \$(pwd):/root/Venturecxx venture 
+source script/run_docker_container
 
 echo 'Press enter to continue...'
 read -n1 -s
