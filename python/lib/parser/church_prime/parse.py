ccc6fb447ae5f5c5599cd5bdf8107ebeeb164d6f
David Wadden <dave.wadden@gmail.com>
2014-12-07 21:33:20 -0500

    Fixing bug in parser

--- python/lib/parser/church_prime/parse.py
+++ python/lib/parser/church_prime/parse.py
@@ -1,17 +1,17 @@
 # Copyright (c) 2014, MIT Probabilistic Computing Project.
-# 
+#
 # This file is part of Venture.
-# 	
+#
 # Venture is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 	
+#
 # Venture is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 	
+#
 # You should have received a copy of the GNU General Public License
 # along with Venture.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -485,8 +485,8 @@ class ChurchPrimeParser(object):
         'labeled_define': [('symbol', unparse_symbol), ('expression', unparse_expression)],
         'assume': [('symbol', unparse_symbol), ('expression', unparse_expression)],
         'labeled_assume': [('symbol', unparse_symbol), ('expression', unparse_expression)],
-        'observe': [('symbol', unparse_symbol), ('value', unparse_value)],
-        'labeled_observe': [('symbol', unparse_symbol), ('value', unparse_value)],
+        'observe': [('expression', unparse_expression), ('value', unparse_value)],
+        'labeled_observe': [('expression', unparse_expression), ('value', unparse_value)],
         'predict': [('expression', unparse_expression)],
         'labeled_predict': [('expression', unparse_expression)],
         'configure': [('options', unparse_json)],
