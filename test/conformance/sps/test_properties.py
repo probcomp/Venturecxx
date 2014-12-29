70ef25024f5d6fb63ff0d61c8a16fa8f1c86cabf
Alexey Radul <axch@mit.edu>
2014-12-29 18:29:32 -0500

    Right.  The lack of automatic reuse of Python SPs is really annoying.

--- test/conformance/sps/test_properties.py
+++ test/conformance/sps/test_properties.py
@@ -208,6 +208,7 @@ def checkRiplAgreesWithDeterministicSimulate(name, sp):
     "is_probability", # Not implemented
     "is_boolean", # Not implemented
     "is_procedure", # Not implemented
+    "print", # Not implemented
   ]:
     raise SkipTest("%s in Puma not implemented compatibly with Lite" % name)
   checkTypedProperty(propRiplAgreesWithDeterministicSimulate, fully_uncurried_sp_type(sp.venture_type()), name, sp)
