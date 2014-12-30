8ff90361902c78e876d2767ac230e0efad7c374c
Alexey Radul <axch@mit.edu>
2014-12-30 04:22:56 -0500

    Whitespace.

--- backend/new_cxx/inc/sps/deterministic.h
+++ backend/new_cxx/inc/sps/deterministic.h
@@ -5,132 +5,132 @@
 #include "args.h"
 
 struct AddOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct SubOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct MulOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct DivOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct IntDivOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct IntModOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct EqOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct GtOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct GteOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct LtOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct LteOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct SinOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct CosOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct TanOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct HypotOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct ExpOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct LogOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct PowOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct SqrtOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct NotOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct IsNumberOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct IsIntegerOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct IsBoolOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct IsSymbolOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct ToAtomOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
 struct IsAtomOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
@@ -140,7 +140,7 @@ struct ProbabilityOutputPSP : PSP
 };
 
 struct IsProbabilityOutputPSP : PSP
-{ 
+{
   VentureValuePtr simulate(shared_ptr<Args> args, gsl_rng * rng) const;
 };
 
