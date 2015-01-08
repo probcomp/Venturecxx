3a770bd30843135ce516c44cfcea36df4b87e038
Erik Sjoberg <sjobeek@gmail.com>
2015-01-07 19:04:55 -0800

    Basic trace and scatter distributions working

--- examples/interactive_demos/bokeh_trace.py
+++ examples/interactive_demos/bokeh_trace.py
@@ -47,6 +47,8 @@ def setup_datasource(venture_mripl, parameters):
     # Index for plotting purposes
     pred_index_list = [0]
     param_dsource.add(data=pred_index_list, name='pred_index')
+    zero_list = [0]  # dynamically expanding list of zeros...  hack for 1d scatter
+    param_dsource.add(data=zero_list, name='zero_list')
 
     # Flatten prediction dataframe into column datasource
     # One column for each parameter for each Ripl (one column = one lines)
@@ -65,6 +67,8 @@ def append_update_to_datasource(venture_mripl, datasource, parameters):
     
     last_pred_index = datasource.data['pred_index'][-1]
     datasource.data['pred_index'].append(last_pred_index + 1)
+    last_zero_item = datasource.data['zero_list'][-1]
+    datasource.data['zero_list'].append(last_zero_item + 0.001)
     
     for idx, prediction_set in pred_df.iterrows():
         for parameter, value in prediction_set.iteritems():
@@ -72,36 +76,51 @@ def append_update_to_datasource(venture_mripl, datasource, parameters):
     
     return pred_df  # Latest predicted values
 
-def setup_dsource(venture_ripl):
-    # Create datasource class to record parameters 
-    param_dsource = ColumnDataSource()
-    x_val_list = [0]
-    inter_list = [venture_ripl.predict('w0')]
-    slope_list = [venture_ripl.predict('w1')]
-    noise_list = [venture_ripl.predict('noise')]
-    param_dsource.add(data=x_val_list, name='x_val')
-    param_dsource.add(data=inter_list, name='intercept')
-    param_dsource.add(data=slope_list, name='slope') 
-    param_dsource.add(data=noise_list, name='noise') 
-
-    return param_dsource
-
-
-
-def trace_grid(param_dsource):
-    """Return bokeh grid of vertical traces for given ripl"""
-
+def single_trace_figure(parameter, param_line_set, xrange_ref=None, yrange_ref=None):
+    return
     
-        
-    # Initialize plot itself
-    fig = bk.figure(plot_width=150, plot_height=150,
-                    title="Dist",
-                    x_range=[-20,20], y_range=[-10,10],
-                    tools=['pan','wheel_zoom', 'box_zoom','previewsave','resize','reset'])
+def trace_grid(param_dsource, parameters, scatter=False, trace_alpha=0.6, scatter_alpha=0.2):
+    """Return bokeh grid of vertical traces for given ripl"""
+    from collections import OrderedDict
+    # Grab lists of parameters to plot
+    param_trace_set = OrderedDict()
+    xrange_set = OrderedDict()
+    tracefig_set = OrderedDict()
+    distfig_set = OrderedDict()
+    #Shared y-axis given to each trace plot
+    yrange_shared = DataRange1d(sources=[param_dsource.columns()])
+    
+    for param in parameters:
+        #Full set of traces for this parameter (= number of MRipls)
+        param_trace_set[param] = sorted([key for key in param_dsource.data.keys() 
+                                             if key.startswith(param)])
+    
+        # Shared datarange to link x-axes between trace and distribution plots
+        xrange_set[param] = DataRange1d(sources=[param_dsource.columns()])
 
-    fig.scatter('intercept',
-                [0 for _ in range(len(param_dsource.data['intercept']))],
-                 source=param_dsource, color="green", marker='o', alpha=0.1)
+        # Create vertical trace plots
+        
+        tracefig_set[param] = bk.figure(plot_width=150, plot_height=300,
+                                        x_range=xrange_set[param], y_range=yrange_shared,
+                                        title=param,
+                                        tools=['pan','wheel_zoom', 'box_zoom',
+                                        'previewsave','resize','reset','box_select'])
+        #Draw lines for each trace:
+        for this_trace_label in param_trace_set[param]: 
+            tracefig_set[param].line(this_trace_label,'pred_index', source=param_dsource,
+                                     color="blue", alpha=trace_alpha)
+
+        if scatter:
+            # Create scatter/distribution plots
+            distfig_set[param] = bk.figure(plot_width=150, plot_height=150,
+                                            x_range=xrange_set[param], y_range=(-0.19, 1.19),
+                                            tools=['pan','wheel_zoom', 'box_zoom',
+                                                   'previewsave','resize','reset','box_select'])
+            # Draw points for each trace
+            for this_trace_label in param_trace_set[param]: 
+                distfig_set[param].scatter(this_trace_label,'zero_list', source=param_dsource,
+                                               color="blue", marker='o', alpha=scatter_alpha)
+
 
     #Gradient from white --30--> grey --10--> blue
     colors_40 =   ["#FFFFFF","#FBFBFB","#F7F7F7","#F4F4F4","#F0F0F0","#EDEDED","#E9E9E9",
@@ -112,19 +131,11 @@ def trace_grid(param_dsource):
                    "#5B5BB8","#4E4EC2","#4141CC","#3434D6","#2727E0","#1A1AEA","#0D0DF4","#0000FF"]
     r_colors_40 = colors_40[::-1]
 
-    
-
-    # Plot traces of parameters
-    fig2 = bk.figure(plot_width=150, plot_height=400,
-                    title="Trace",
-                    tools=['pan','wheel_zoom', 'box_zoom',
-                    'previewsave','resize','reset'])
-    fig2.line('intercept','x_val', source=param_dsource, color="green")
-    fig2.line('slope','x_val', source=param_dsource, color="blue")
-    fig2.line('noise','x_val', source=param_dsource, color="red")
-
-
-    gd = bk.GridPlot(children=[[fig2], [fig]])
+    if scatter:
+        gd = bk.GridPlot(children=[[tracefig_set['w0'], tracefig_set['w1'], tracefig_set['noise']],
+                                   [ distfig_set['w0'],  distfig_set['w1'],  distfig_set['noise']]])
+    else:
+        gd = bk.GridPlot(children=[[tracefig_set['w0'], tracefig_set['w1'], tracefig_set['noise']]])
 
     return gd
 
@@ -132,6 +143,9 @@ def display_plots(grid):
     """Load JS code into notebook and display"""
     load_notebook(force=True)
     bk.publish_display_data({'text/html':bk.notebook_div(grid)})
+
+
+
 
 def interactive_trace(ven_ripl,
                       inference_program=["(mh default one 1)",
