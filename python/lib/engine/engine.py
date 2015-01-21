03a161d2c6b5665842bf530e55011dc6302e9e8b
Alexey Radul <axch@mit.edu>
2015-01-21 14:47:51 -0500

    Draft do macro for Venture.

--- python/lib/engine/engine.py
+++ python/lib/engine/engine.py
@@ -536,24 +536,38 @@ def _inference_prelude():
 def _compute_inference_prelude():
   ans = []
   for (name, form) in [
-        ["cycle", """(lambda (ks iter)
+      ["cycle", """(lambda (ks iter)
   (iterate (sequence ks) iter))"""],
       # Repeatedly apply the given action, discarding the values
-        ["iterate", """(lambda (f iter)
+      ["iterate", """(lambda (f iter)
   (if (<= iter 1)
       f
       (lambda (t) (f (rest ((iterate f (- iter 1)) t))))))"""],
       # Apply the given list of actions in sequence, discarding the values.
       # This is Haskell's sequence_
-        ["sequence", """(lambda (ks)
+      ["sequence", """(lambda (ks)
   (if (is_pair ks)
       (lambda (t) ((sequence (rest ks)) (rest ((first ks) t))))
       (lambda (t) (pair nil t))))"""],
-        ["begin", "sequence"],
-        ["mixture", """(lambda (weights kernels transitions)
+      ["begin", "sequence"],
+      ["mixture", """(lambda (weights kernels transitions)
   (iterate (lambda (t) ((categorical weights kernels) t)) transitions))"""],
       # pass :: State a ()  pass = return ()
-        ["pass", "(lambda (t) (pair nil t))"]]:
+      ["pass", "(lambda (t) (pair nil t))"],
+      # bind :: State a b -> (b -> State a c) -> State a c
+      ["bind", """(lambda (act next)
+  (lambda (t)
+    (let ((res (act t)))
+      ((next (first res)) (second res)))))"""],
+      # bind_ :: State a b -> State a c -> State a c
+      # drop the value of type b but perform both actions
+      ["bind_", """(lambda (act next)
+  (lambda (t)
+    (let ((res (act t)))
+      (next (second res)))))"""],
+      # return :: b -> State a b
+      ["return", """(lambda (val) (lambda (t) (pair val t)))"""]
+  ]:
     from venture.parser.church_prime_parser import ChurchPrimeParser
     from venture.sivm.macro import desugar_expression
     from venture.sivm.core_sivm import _modify_expression
