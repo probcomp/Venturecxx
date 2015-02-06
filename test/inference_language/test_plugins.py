a11ee709fce2a8d67d5bb0a0db7499b0a73b6c8d
Alexey Radul <axch@mit.edu>
2015-02-06 15:41:09 -0500

    Flush tests of obsolete constructs that will be broken by changing the return value of ripl.infer.

--- test/inference_language/test_plugins.py
+++ test/inference_language/test_plugins.py
@@ -31,16 +31,3 @@ def test_timer4():
   ripl = get_ripl()
   ripl.infer('(call_back timer_start)')
   ripl.infer('(call_back timer_time)')
-
-def test_dataset_to_file():
-  ripl = get_ripl()
-  ripl.assume('x', '(normal 0 1)')
-  infer = '''(begin
-                (resample 2)
-                (cycle ((mh default one 1) (peek x)) 5)
-                (call_back dataset_to_file (quote test_dataset)))'''
-  res = ripl.infer(infer)
-  assert path.exists('test_dataset.txt')
-  ds = pd.read_table('test_dataset.txt')
-  assert_array_almost_equal(res.dataset(), ds, decimal = 3)
-  os.remove('test_dataset.txt')
