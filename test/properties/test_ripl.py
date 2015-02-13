d4caf68e46c549e4c4e3a9fe7e901087f1ba5bc4
Alexey Radul <axch@mit.edu>
2015-02-13 15:28:09 -0500

    Shatter the test_properties file into several, generally characterizing the tests by testee.

---
+++ test/properties/test_ripl.py
@@ -0,0 +1,148 @@
+from nose import SkipTest
+from testconfig import config
+
+from venture.test.config import get_ripl, on_inf_prim, gen_on_inf_prim
+from venture.test.randomized import * # Importing many things, which are closely related to what this is trying to do pylint: disable=wildcard-import, unused-wildcard-import
+from venture.lite.psp import NullRequestPSP
+from venture.lite.sp import VentureSPRecord
+from venture.lite.value import AnyType, VentureValue
+import venture.value.dicts as v
+
+from test_sps import relevantSPs
+
+@on_inf_prim("none")
+def testExpressionFor():
+  checkTypedProperty(propExpressionWorks, AnyType())
+
+def propExpressionWorks(value):
+  expr = value.expressionFor()
+  result = carefully(eval_in_ripl, expr)
+  assert value.equal(result)
+
+@on_inf_prim("none")
+def testRiplRoundTripThroughStack():
+  checkTypedProperty(propRiplRoundTripThroughStack, AnyType())
+
+def propRiplRoundTripThroughStack(value):
+  expr = v.quote(value.asStackDict())
+  result = carefully(eval_in_ripl, expr)
+  assert value.equal(result)
+
+def eval_in_ripl(expr):
+  ripl = get_ripl()
+  ripl.predict(expr, label="thing")
+  return VentureValue.fromStackDict(ripl.report("thing", type=True))
+
+@gen_on_inf_prim("none")
+def testRiplSimulate():
+  for (name,sp) in relevantSPs():
+    if name in ["scope_include", # Because scope_include is
+                                 # misannotated as to the true
+                                 # permissible types of scopes and
+                                 # blocks
+                "get_current_environment", # Because BogusArgs gives a bogus environment
+                "extend_environment", # Because BogusArgs gives a bogus environment
+              ]:
+      continue
+    if config["get_ripl"] != "lite" and name in [
+        ## Expected failures
+        "dict", # Because keys and values must be the same length
+        "matrix", # Because rows must be the same length
+        "lookup", # Because the key must be an integer for sequence lookups
+        "get_empty_environment", # Environments can't be rendered to stack dicts
+    ]:
+      continue
+    if not sp.outputPSP.isRandom():
+      yield checkRiplAgreesWithDeterministicSimulate, name, sp
+
+def checkRiplAgreesWithDeterministicSimulate(name, sp):
+  if config["get_ripl"] != "lite" and name in [
+    ## Incompatibilities with Puma
+    "min", # Not implemented
+    "real", # Not implemented
+    "atom_eq", # Not implemented
+    "arange", # Not implemented
+    "linspace", # Not implemented
+    "diag_matrix", # Not implemented
+    "ravel", # Not implemented
+    "matrix_mul", # Not implemented
+    "repeat", # Not implemented
+    "vector_dot", # Not implemented
+    "zip", # Not implemented
+    "is_procedure", # Not implemented
+    "print", # Not implemented
+  ]:
+    raise SkipTest("%s in Puma not implemented compatibly with Lite" % name)
+  checkTypedProperty(propRiplAgreesWithDeterministicSimulate, fully_uncurried_sp_type(sp.venture_type()), name, sp)
+
+def propRiplAgreesWithDeterministicSimulate(args_lists, name, sp):
+  """Check that the given SP produces the same answer directly and
+through a ripl (applied fully uncurried)."""
+  args = BogusArgs(args_lists[0], sp.constructSPAux())
+  answer = carefully(sp.outputPSP.simulate, args)
+  if isinstance(answer, VentureSPRecord):
+    if isinstance(answer.sp.requestPSP, NullRequestPSP):
+      if not answer.sp.outputPSP.isRandom():
+        args2 = BogusArgs(args_lists[1], answer.spAux)
+        ans2 = carefully(answer.sp.outputPSP.simulate, args2)
+        inner = [v.symbol(name)] + [val.expressionFor() for val in args_lists[0]]
+        expr = [inner] + [val.expressionFor() for val in args_lists[1]]
+        assert ans2.equal(carefully(eval_in_ripl, expr))
+      else:
+        raise SkipTest("Putatively deterministic sp %s returned a random SP" % name)
+    else:
+      raise SkipTest("Putatively deterministic sp %s returned a requesting SP" % name)
+  else:
+    expr = [v.symbol(name)] + [val.expressionFor() for val in args_lists[0]]
+    assert answer.equal(carefully(eval_in_ripl, expr))
+
+def eval_foreign_sp(name, sp, expr):
+  ripl = get_ripl()
+  ripl.bind_foreign_sp(name, sp)
+  ripl.predict(expr, label="thing")
+  return VentureValue.fromStackDict(ripl.report("thing", type=True))
+
+@gen_on_inf_prim("none")
+def testForeignInterfaceSimulate():
+  for (name,sp) in relevantSPs():
+    if name in ["scope_include", # Because scope_include is
+                                 # misannotated as to the true
+                                 # permissible types of scopes and
+                                 # blocks
+                "get_current_environment", # Because BogusArgs gives a bogus environment
+                "extend_environment", # Because BogusArgs gives a bogus environment
+              ]:
+      continue
+    if config["get_ripl"] != "lite" and name in [
+        ## Expected failures
+        "dict", # Because keys and values must be the same length
+        "matrix", # Because rows must be the same length
+        "get_empty_environment", # Environments can't be rendered to stack dicts
+    ]:
+      continue
+    if not sp.outputPSP.isRandom():
+      yield checkForeignInterfaceAgreesWithDeterministicSimulate, name, sp
+
+def checkForeignInterfaceAgreesWithDeterministicSimulate(name, sp):
+  checkTypedProperty(propForeignInterfaceAgreesWithDeterministicSimulate, fully_uncurried_sp_type(sp.venture_type()), name, sp)
+
+def propForeignInterfaceAgreesWithDeterministicSimulate(args_lists, name, sp):
+  """Check that the given SP produces the same answer directly and
+through the foreign function interface (applied fully uncurried)."""
+  args = BogusArgs(args_lists[0], sp.constructSPAux())
+  answer = carefully(sp.outputPSP.simulate, args)
+  if isinstance(answer, VentureSPRecord):
+    if isinstance(answer.sp.requestPSP, NullRequestPSP):
+      if not answer.sp.outputPSP.isRandom():
+        args2 = BogusArgs(args_lists[1], answer.spAux)
+        ans2 = carefully(answer.sp.outputPSP.simulate, args2)
+        inner = [v.symbol("test_sp")] + [val.expressionFor() for val in args_lists[0]]
+        expr = [inner] + [val.expressionFor() for val in args_lists[1]]
+        assert ans2.equal(carefully(eval_foreign_sp, "test_sp", sp, expr))
+      else:
+        raise SkipTest("Putatively deterministic sp %s returned a random SP" % name)
+    else:
+      raise SkipTest("Putatively deterministic sp %s returned a requesting SP" % name)
+  else:
+    expr = [v.symbol("test_sp")] + [val.expressionFor() for val in args_lists[0]]
+    assert answer.equal(carefully(eval_foreign_sp, "test_sp", sp, expr))
---
+++ test/properties/test_sps.py
@@ -0,0 +1,172 @@
+# Testing the (Python) SP objects standalone
+
+import math
+from testconfig import config
+from nose.tools import eq_, assert_almost_equal
+
+from venture.test.config import gen_in_backend
+from venture.test.randomized import * # Importing many things, which are closely related to what this is trying to do pylint: disable=wildcard-import, unused-wildcard-import
+from venture.lite.builtin import builtInSPsList
+from venture.lite.psp import NullRequestPSP
+from venture.lite.sp import VentureSPRecord
+from venture.lite.utils import FixedRandomness
+
+blacklist = ['make_csp', 'apply_function', 'make_gp']
+
+# Select particular SPs to test thus:
+# nosetests --tc=relevant:'["foo", "bar", "baz"]'
+def relevantSPs():
+  for (name,sp) in builtInSPsList:
+    if isinstance(sp.requestPSP, NullRequestPSP):
+      if "relevant" not in config or config["relevant"] is None or name in config["relevant"]:
+        if name not in blacklist: # Placeholder for selecting SPs to do or not do
+          yield name, sp
+
+@gen_in_backend("none")
+def testTypes():
+  for (name,sp) in relevantSPs():
+    yield checkTypeCorrect, name, sp
+
+def checkTypeCorrect(_name, sp):
+  type_ = sp.venture_type()
+  checkTypedProperty(propTypeCorrect, fully_uncurried_sp_type(type_), sp, type_)
+
+def propTypeCorrect(args_lists, sp, type_):
+  """Check that the successive return values of the given SP (when
+applied fully uncurried) match the expected types."""
+  if len(args_lists) == 0:
+    pass # OK
+  else:
+    if isinstance(sp, VentureSPRecord):
+      sp, aux = sp.sp, sp.spAux
+    else:
+      aux = carefully(sp.constructSPAux)
+    args = BogusArgs(args_lists[0], aux)
+    answer = carefully(sp.outputPSP.simulate, args)
+    assert answer in type_.return_type
+    propTypeCorrect(args_lists[1:], answer, type_.return_type)
+
+@gen_in_backend("none")
+def testDeterministic():
+  for (name,sp) in relevantSPs():
+    if not sp.outputPSP.isRandom():
+      yield checkDeterministic, name, sp
+
+def checkDeterministic(name, sp):
+  checkTypedProperty(propDeterministic, fully_uncurried_sp_type(sp.venture_type()), name, sp)
+
+def propDeterministic(args_lists, name, sp):
+  """Check that the given SP returns the same answer every time (applied
+fully uncurried)."""
+  args = BogusArgs(args_lists[0], sp.constructSPAux())
+  answer = carefully(sp.outputPSP.simulate, args)
+  if isinstance(answer, VentureSPRecord):
+    if isinstance(answer.sp.requestPSP, NullRequestPSP):
+      if not answer.sp.outputPSP.isRandom():
+        args2 = BogusArgs(args_lists[1], answer.spAux)
+        ans2 = carefully(answer.sp.outputPSP.simulate, args2)
+        for _ in range(5):
+          new_ans = carefully(sp.outputPSP.simulate, args)
+          new_ans2 = carefully(new_ans.sp.outputPSP.simulate, args2)
+          eq_(ans2, new_ans2)
+      else:
+        raise SkipTest("Putatively deterministic sp %s returned a random SP" % name)
+    else:
+      raise SkipTest("Putatively deterministic sp %s returned a requesting SP" % name)
+  else:
+    for _ in range(5):
+      assert_almost_equal(answer, carefully(sp.outputPSP.simulate, args), places = 10)
+
+@gen_in_backend("none")
+def testRandom():
+  for (name,sp) in relevantSPs():
+    if sp.outputPSP.isRandom():
+      if not name in ["make_uc_dir_mult", "categorical", "make_uc_sym_dir_mult",
+                      "log_bernoulli", "log_flip",  # Because the default distribution does a bad job of picking arguments at which log_bernoulli's output actually varies.
+                      "noisy_id" # Because it intentionally pretends to be random even though it's not.
+      ]:
+        yield checkRandom, name, sp
+
+def checkRandom(_name, sp):
+  # I take the name because I want it to appear in the nose arg list
+
+  # Generate five approprite input/output pairs for the sp
+  args_type = fully_uncurried_sp_type(sp.venture_type())
+  def f(args_lists): return evaluate_fully_uncurried(sp, args_lists)
+  answers = [findAppropriateArguments(f, args_type, 30) for _ in range(5)]
+
+  # Check that it returns different results on repeat applications to
+  # at least one of the inputs.
+  for answer in answers:
+    if answer is None: continue # Appropriate input was not found; skip
+    [args, ans, _] = answer
+    for _ in range(10):
+      ans2 = evaluate_fully_uncurried(sp, args)
+      if not ans2 == ans:
+        return True # Output differed on some input: pass
+
+  assert False, "SP deterministically gave i/o pairs %s" % answers
+
+def evaluate_fully_uncurried(sp, args_lists):
+  if isinstance(sp, VentureSPRecord):
+    sp, aux = sp.sp, sp.spAux
+  else:
+    aux = carefully(sp.constructSPAux)
+  args = BogusArgs(args_lists[0], aux)
+  answer = carefully(sp.outputPSP.simulate, args)
+  if len(args_lists) == 1:
+    return answer
+  else:
+    return evaluate_fully_uncurried(answer, args_lists[1:])
+
+@gen_in_backend("none")
+def testLogDensityDeterministic():
+  for (name,sp) in relevantSPs():
+    if name not in ["dict", "multivariate_normal", "wishart", "inv_wishart", "categorical"]: # TODO
+      yield checkLogDensityDeterministic, name, sp
+
+def checkLogDensityDeterministic(_name, sp):
+  checkTypedProperty(propLogDensityDeterministic, (fully_uncurried_sp_type(sp.venture_type()), final_return_type(sp.venture_type())), sp)
+
+def propLogDensityDeterministic(rnd, sp):
+  (args_lists, value) = rnd
+  if not len(args_lists) == 1:
+    raise SkipTest("TODO: Write the code for measuring log density of curried SPs")
+  answer = carefully(sp.outputPSP.logDensity, value, BogusArgs(args_lists[0], sp.constructSPAux()))
+  if math.isnan(answer):
+    raise ArgumentsNotAppropriate("Log density turned out to be NaN")
+  for _ in range(5):
+    eq_(answer, carefully(sp.outputPSP.logDensity, value, BogusArgs(args_lists[0], sp.constructSPAux())))
+
+@gen_in_backend("none")
+def testFixingRandomness():
+  for (name,sp) in relevantSPs():
+    yield checkFixingRandomness, name, sp
+
+def checkFixingRandomness(name, sp):
+  checkTypedProperty(propDeterministicWhenFixed, fully_uncurried_sp_type(sp.venture_type()), name, sp)
+
+def propDeterministicWhenFixed(args_lists, name, sp):
+  # TODO Abstract out the similarities between this and propDeterministic
+  args = BogusArgs(args_lists[0], sp.constructSPAux())
+  randomness = FixedRandomness()
+  with randomness:
+    answer = carefully(sp.outputPSP.simulate, args)
+  if isinstance(answer, VentureSPRecord):
+    if isinstance(answer.sp.requestPSP, NullRequestPSP):
+      args2 = BogusArgs(args_lists[1], answer.spAux)
+      randomness2 = FixedRandomness()
+      with randomness2:
+        ans2 = carefully(answer.sp.outputPSP.simulate, args2)
+      for _ in range(5):
+        with randomness:
+          new_ans = carefully(sp.outputPSP.simulate, args)
+        with randomness2:
+          new_ans2 = carefully(new_ans.sp.outputPSP.simulate, args2)
+        eq_(ans2, new_ans2)
+    else:
+      raise SkipTest("SP %s returned a requesting SP" % name)
+  else:
+    for _ in range(5):
+      with randomness:
+        eq_(answer, carefully(sp.outputPSP.simulate, args))
