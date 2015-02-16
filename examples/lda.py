ce4fc68d245d58752cb26aabe9c79be5f3c65987
Alexey Radul <axch@mit.edu>
2015-02-16 16:53:39 -0500

    Line breaks.

--- examples/lda.py
+++ examples/lda.py
@@ -22,9 +22,12 @@ class LDA(VentureUnit):
         self.assume("vocab", self.parameters['vocab'])
         self.assume("alpha_document_topic", "(gamma 1.0 1.0)")
         self.assume("alpha_topic_word", "1.0")
-        self.assume("get_document_topic_sampler", "(mem (lambda (doc) (make_sym_dir_mult alpha_document_topic topics)))")
-        self.assume("get_topic_word_sampler", "(mem (lambda (topic) (make_sym_dir_mult alpha_topic_word vocab)))")
-        self.assume("get_word", "(mem (lambda (doc pos) ((get_topic_word_sampler ((get_document_topic_sampler doc))))))")
+        self.assume("get_document_topic_sampler",
+                    "(mem (lambda (doc) (make_sym_dir_mult alpha_document_topic topics)))")
+        self.assume("get_topic_word_sampler",
+                    "(mem (lambda (topic) (make_sym_dir_mult alpha_topic_word vocab)))")
+        self.assume("get_word",
+                    "(mem (lambda (doc pos) ((get_topic_word_sampler ((get_document_topic_sampler doc))))))")
         return
 
 
