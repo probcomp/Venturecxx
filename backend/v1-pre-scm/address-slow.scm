f6af4abd9c65047eb83b9d683ca7f1394f1064f6
Alexey Radul <axch@mit.edu>
2015-02-19 13:28:20 -0500

    Report an address's creation date in addition to its assigned hash number.

--- backend/v1-pre-scm/address-slow.scm
+++ backend/v1-pre-scm/address-slow.scm
@@ -1,6 +1,9 @@
 (declare (usual-integrations))
 
-(define-structure (address (constructor %make-address))
+(define-structure (address
+                   (constructor %make-address)
+                   (print-procedure
+                    (simple-unparser-method 'A (lambda (address) (list (address-index address))))))
   index)
 
 (define-integrable (address<? a1 a2)
