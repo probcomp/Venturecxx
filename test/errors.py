503c2c9bf0e74628fb869d91e233d00b8514b17f
Alexey Radul <axch@mit.edu>
2015-03-26 16:10:03 -0400

    Move Venture-specific error testing functions to their own helper module.

---
+++ test/errors.py
@@ -0,0 +1,39 @@
+from nose.tools import assert_raises
+from StringIO import StringIO
+import sys
+import re
+
+from venture.exception import VentureException
+
+ansi_escape = re.compile(r'\x1b[^m]*m')
+
+def assert_annotation_succeeds(f, *args, **kwargs):
+  with assert_raises(VentureException) as cm:
+    f(*args, **kwargs)
+  assert "stack_trace" in cm.exception.data
+
+def assert_error_message_contains(text, f, *args, **kwargs):
+  text = text.strip()
+  with assert_raises(VentureException) as cm:
+    f(*args, **kwargs)
+  message = ansi_escape.sub('', str(cm.exception))
+  if text in message:
+    pass # OK
+  else:
+    print "Did not find pattern"
+    print text
+    print "in"
+    import traceback
+    print traceback.format_exc()
+    print cm.exception
+    assert text in message
+
+def assert_print_output_contains(text, f, *args, **kwargs):
+  text = text.strip()
+  old_stderr = sys.stderr
+  result = StringIO()
+  sys.stderr = result
+  f(*args, **kwargs)
+  sys.stderr = old_stderr
+  ans = result.getvalue()
+  assert text in ansi_escape.sub('', ans)
--- test/stack/functionality/sivm_exception_annotation_test.py
+++ test/stack/functionality/sivm_exception_annotation_test.py
@@ -1,58 +1,23 @@
-from nose.tools import assert_raises, eq_
-from StringIO import StringIO
-import sys
+from nose.tools import eq_
 import time
-import re
 
 from venture.test.config import get_ripl, broken_in, on_inf_prim
-from venture.exception import VentureException
+import venture.test.errors as err
 import venture.value.dicts as v
 
-ansi_escape = re.compile(r'\x1b[^m]*m')
-
-def assert_annotation_succeeds(f, *args, **kwargs):
-  with assert_raises(VentureException) as cm:
-    f(*args, **kwargs)
-  assert "stack_trace" in cm.exception.data
-
-def assert_error_message_contains(text, f, *args, **kwargs):
-  text = text.strip()
-  with assert_raises(VentureException) as cm:
-    f(*args, **kwargs)
-  message = ansi_escape.sub('', str(cm.exception))
-  if text in message:
-    pass # OK
-  else:
-    print "Did not find pattern"
-    print text
-    print "in"
-    import traceback
-    print traceback.format_exc()
-    print cm.exception
-    assert text in message
-
-def assert_print_output_contains(text, f, *args, **kwargs):
-  text = text.strip()
-  old_stderr = sys.stderr
-  result = StringIO()
-  sys.stderr = result
-  f(*args, **kwargs)
-  sys.stderr = old_stderr
-  ans = result.getvalue()
-  assert text in ansi_escape.sub('', ans)
 
 @broken_in("puma", "Puma does not report error addresses")
 @on_inf_prim("none")
 def testBasicAnnotation():
   sivm = get_ripl().sivm
   expr = v.app(v.sym("add"), v.num(1), v.sym("foo"))
-  assert_annotation_succeeds(sivm.assume, v.sym("x"), expr)
+  err.assert_annotation_succeeds(sivm.assume, v.sym("x"), expr)
 
 @broken_in("puma", "Puma does not report error addresses")
 @on_inf_prim("none")
 def testBasicAnnotation2():
   ripl = get_ripl()
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 (add 1 foo)
        ^^^
 """,
@@ -65,7 +30,7 @@ def testAnnotateErrorTriggeredByInference():
   ripl.assume("control", "(flip)")
   ripl.force("control", True)
   ripl.predict("(if control 1 badness)")
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 (if control 1 badness)
 ^^^^^^^^^^^^^^^^^^^^^^
 (if control 1 badness)
@@ -77,12 +42,12 @@ def testAnnotateErrorTriggeredByInference():
 @on_inf_prim("none")
 def testAnnotateProgrammaticAssume():
   ripl = get_ripl()
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((assume x (add 1 foo)) model)
  ^^^^^^^^^^^^^^^^^^^^^^
 """,
   ripl.infer, "(assume x (+ 1 foo))")
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 (add 1.0 foo)
          ^^^
 """,
@@ -97,7 +62,7 @@ def testAnnotateErrorTriggeredByInferenceOverProgrammaticAssume():
   ripl.infer("(force control true)")
   ripl.infer("(predict (if control 1 badness))")
   # TODO Solve the double macroexpansion problem
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((biplex control (make_csp (quote ()) (quote 1.0)) (make_csp (quote ()) (quote badness))))
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ((biplex control (make_csp (quote ()) (quote 1.0)) (make_csp (quote ()) (quote badness))))
@@ -114,14 +79,14 @@ def testAnnotateDefinedProgrammaticAssume():
   # themselves produces this; which does point to the culprit.
   # However, the top stack frame is somewhat misleading as to when the
   # problem was identified.  I might be able to live with that.
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((action) model)
  ^^^^^^^^
 (lambda () (assume x (add 1 foo)))
            ^^^^^^^^^^^^^^^^^^^^^^
 """,
   ripl.infer, "(action)")
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 (add 1.0 foo)
          ^^^
 """,
@@ -131,7 +96,7 @@ def testAnnotateDefinedProgrammaticAssume():
 @on_inf_prim("none")
 def testAnnotateInferenceProgramError():
   ripl = get_ripl()
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((observe (normal 0 1) (add 1 foo)) model)
                               ^^^
 """,
@@ -141,7 +106,7 @@ def testAnnotateInferenceProgramError():
 @on_inf_prim("none")
 def testAnnotateInferenceProgramErrorInDefinition():
   ripl = get_ripl(persistent_inference_trace=True)
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 (observe (normal 0 1) (add 1 foo))
                              ^^^
 """,
@@ -152,7 +117,7 @@ def testAnnotateInferenceProgramErrorInDefinition():
 def testAnnotateDefinedInferenceProgramError():
   ripl = get_ripl(persistent_inference_trace=True)
   ripl.define("badness", "(lambda () (observe (normal 0 1) (+ 1 foo)))")
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((badness) model)
  ^^^^^^^^^
 (lambda () (observe (normal 0 1) (add 1 foo)))
@@ -169,14 +134,14 @@ def testAnnotateDefinedQuasiquotedProgrammaticAssume():
   # themselves produces this; which does point to the culprit.
   # However, the top stack frame is somewhat misleading as to when the
   # problem was identified.  I might be able to live with that.
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((action (quote foo)) model)
  ^^^^^^^^^^^^^^^^^^^^
 (lambda (name) (assume x (add 1 (unquote name))))
                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 """,
   ripl.infer, "(action 'foo)")
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 (add 1.0 foo)
          ^^^
 """,
@@ -201,7 +166,7 @@ def testAnnotateInferenceErrorInDo():
   expression = """\
 (do (assume x (normal 0 1))
     (observe x (+ 1 badness)))"""
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((do (assume x (normal 0 1)) (observe x (add 1 badness))) model)
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ((do (assume x (normal 0 1)) (observe x (add 1 badness))) model)
@@ -217,7 +182,7 @@ def testAnnotateInferenceErrorInDefinedDo():
 (do (assume x (normal 0 1))
     (y <- (sample x))
     (observe x (+ 1 badness)))""")
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 (action model)
 ^^^^^^^^^^^^^^
 (do (assume x (normal 0 1)) (y <- (sample x)) (observe x (add 1 badness)))
@@ -232,7 +197,7 @@ def testAnnotateInferenceErrorInQuasiquote():
   expression = """\
 (lambda (t) (pair (lookup `(,(+ 1 badness) 5) 0) t))
 """
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((lambda (t) (pair (lookup (quasiquote ((unquote (add 1 badness)) 5)) 0) t)) model)
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ((lambda (t) (pair (lookup (quasiquote ((unquote (add 1 badness)) 5)) 0) t)) model)
@@ -247,7 +212,7 @@ def testAnnotateInferenceErrorInImplicitQuasiquote():
   expression = """\
 (assume x (normal ,(+ 1 badness) 1))
 """
-  assert_error_message_contains("""\
+  err.assert_error_message_contains("""\
 ((assume x (normal (unquote (add 1 badness)) 1)) model)
                                    ^^^^^^^
 """,
@@ -264,7 +229,7 @@ def testLoopErrorAnnotationSmoke():
     ripl.infer(expression)
     time.sleep(0.01) # Give it time to start
     ripl.stop_continuous_inference() # Join the other thread to make sure it errored
-  assert_print_output_contains("""\
+  err.assert_print_output_contains("""\
 ((do (make_csp (quote (t)) (quote (pair (add 1 badness) t)))) model)
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ((do (make_csp (quote (t)) (quote (pair (add 1 badness) t)))) model)
