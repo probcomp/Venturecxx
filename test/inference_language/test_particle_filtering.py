3405a548bbde84ceaaf3aa2f2c7fc18db377eb0c
Alexey Radul <axch@mit.edu>
2015-03-26 10:09:13 -0400

    We should have a particle filtering smoke test.

---
+++ test/inference_language/test_particle_filtering.py
@@ -0,0 +1,30 @@
+import scipy.stats
+
+from venture.test.config import get_ripl, broken_in, on_inf_prim
+
+# TODO Rewrite this to use mem like one normally would.  Right now
+# it's metaprogrammed because freeze is only known to work on
+# directives.
+
+@broken_in('lite', "freeze is only implemented in Puma")
+@on_inf_prim("resample")
+def testHMMParticleSmoke():
+  num_particles = 3
+  num_steps = 10
+
+  ripl = get_ripl()
+  ripl.infer("(resample %s)" % num_particles)
+
+  for m in range(num_steps):
+    newValue = scipy.stats.norm.rvs(0,1.0)
+    if m == 0:
+      ripl.assume("state_0", "(normal 0.0 1.0)", label="st0")
+    else:
+      ripl.assume("state_%d" % m, "(normal state_%d 1.0)" % (m-1), label="st%s" % m)
+    ripl.observe("(normal state_%d 1.0)" % m, newValue, label="obs%d" % m)
+    if m > 0:
+      ripl.freeze("st%s" % (m-1))
+      ripl.forget("obs%d" % (m-1))
+    if m > 1:
+      ripl.forget("st%s" % (m-2))
+    ripl.infer("(resample %s)" % num_particles)
