1f59e389969e8077475e635c22ed0c27d1d147be
Alexey Radul <axch@mit.edu>
2015-04-22 18:36:07 -0400

    Write an actual benchmark to try out.

--- backend/hs/Benchmark.hs
+++ backend/hs/Benchmark.hs
@@ -1,3 +1,18 @@
+import Control.Monad.State
+import System.Environment (getArgs)
+
+import Trace
+import Venture
+import qualified Inference as I
+
+answer :: Int -> IO (Value Double)
+answer steps = evalStateT (do
+  assume "x" (app (var "normal") [0, 1])
+  assume "y" $ app (var "normal") [var "x", 1]
+  observe (var "y") 2
+  replicateM_ steps (resimulation_mh I.default_one)
+  sampleM (var "x")) initial
 
 main :: IO ()
-main = putStrLn "Boo!"
+main = do [steps] <- liftM (fmap read) getArgs
+          answer steps >>= (putStrLn . show)
