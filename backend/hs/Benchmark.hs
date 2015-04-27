6f76569d10eb0664e830c9501cb66d32c2ca6dd2
Alexey Radul <axch@mit.edu>
2015-04-27 18:14:42 -0400

    Enhance the benchmark: report the running trace, add a version that resets the trace mid-run a few times, and accept the benchmark to run from the command line.

--- backend/hs/Benchmark.hs
+++ backend/hs/Benchmark.hs
@@ -3,6 +3,7 @@
 import Control.Monad.State.Strict
 import System.Environment (getArgs)
 
+import Utils (Pretty(..))
 import Trace
 import Venture
 import qualified Inference as I
@@ -19,9 +20,21 @@ answer2 :: Int -> IO (Value Double)
 answer2 steps = evalStateT (do
   assume "coin" (app (var "make-cbeta-bernoulli") [1, 1])
   assume "result" (app (var "coin") [])
-  replicateM_ steps (resimulation_mh I.default_one)
+  mapM_ infer [0..steps-1]
   sampleM (var "result")) initial
+    where infer step = do resimulation_mh I.default_one
+                          if step `mod` 499 == 0 then do
+                              model <- gets _trace
+                              lift $ putStrLn $ show $ pp model
+                          else
+                              return ()
+
+answer3 :: Int -> IO [(Value Double)]
+answer3 steps = mapM (\_ -> answer2 $ steps `div` 10) [1..10]
 
 main :: IO ()
-main = do [steps] <- liftM (fmap read) getArgs
-          answer2 steps >>= (putStrLn . show)
+main = do [index, steps] <- liftM (fmap read) getArgs
+          let act = [ answer steps >>= (putStrLn . show)
+                    , answer2 steps >>= (putStrLn . show)
+                    , answer3 steps >>= (putStrLn . show)] !! index
+          act
