da57dedbffd1a499a9c098b537767d51868fb21e
David Wadden <dave.wadden@gmail.com>
2015-05-11 23:43:26 -0400

    Inference and simple reporting now work

--- examples/blog/poisson_ball.py
+++ examples/blog/poisson_ball.py
@@ -1,23 +1,15 @@
-import scipy
-import numpy as np
-import pandas as pd
-from os.path import join, exists
-import os
-from shutil import rmtree
-from matplotlib import pyplot as plt
-
-from venture.lite.discrete import DiscretePSP
-from venture.lite import value as v
-from venture.lite.builtin import typed_nr
 import venture.ripl.utils as u
 
 def __venture_start__(ripl):
-  ripl.bind_callback('print_this', print_this)
+  ripl.bind_callback('data_dump', data_dump)
 
-def print_this(_, ds):
+def data_dump(_, ds, file_name):
+  ds = u.strip_types(ds)[0].asPandas()
+  from IPython.core.debugger import Pdb; Pdb('Linux').set_trace()
+
+def chisquare_vs_blog(_, ds):
   ds = u.strip_types(ds)[0].asPandas()
   counts = ds.n_balls.value_counts()
   max = counts.index.max()
+  from IPython.core.debugger import Pdb; Pdb('Linux').set_trace()
   print counts[range(0, int(max + 1))].fillna(0)
-  
-  
