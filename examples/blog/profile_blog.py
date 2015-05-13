765127ffa7ae91f0df8f49c13925e7bc4b8c4deb
David Wadden <dave.wadden@gmail.com>
2015-05-13 02:56:13 -0400

    Refactor blog profiling code to create quality profiler

--- examples/blog/profile_blog.py
+++ examples/blog/profile_blog.py
@@ -7,14 +7,15 @@ from time import time
 from functools import partial
 from pandas import DataFrame
 from multiprocessing import Pool
+from sys import argv
 
-def profile_one(inference_flag, n):
+def run_one(inference_flag, out, n_iter, n_obs):
   method_flag = inference_flag.split('.')[-1]
-  blog_file = 'poisson-ball-blog/poisson-ball-{0}.blog'.format(n)
-  result_file = 'blog-results/poisson-ball-results-{0}-{1}.json'.format(method_flag, n)
+  blog_file = 'poisson-ball-blog/poisson-ball-{0}.blog'.format(n_obs)
+  result_file = out + '/' + 'poisson-ball-results-{0}-{1}-{2}.json'.format(method_flag, n_iter, n_obs)
   cmd = ['blog',
          blog_file,
-         '-n', '100000',
+         '-n', n_iter,
          '-s', inference_flag,
          '-o', result_file]
   start = time()
@@ -22,18 +23,30 @@ def profile_one(inference_flag, n):
   elapsed = time() - start
   return elapsed
 
-def profile(inference_flag):
-  profile_partial = partial(profile_one, inference_flag)
+def profile_time(inference_flag):
+  profile_partial = partial(run_one, inference_flag, 'blog-profile-time', '100000')
   workers = Pool(10)
   times = workers.map(profile_partial, range(1,11))
   ds = DataFrame(dict(n_obs_pairs = range(1,11),
                       times= times))
   outname = inference_flag.split('.')[-1]
-  ds.to_csv('blog-results/' + outname + '.txt', index = False, sep = '\t')
+  ds.to_csv('blog-profile-time/' + outname + '.txt', index = False, sep = '\t')
 
+def profile_times():
+  profile_time('blog.sample.LWSampler')
+  profile_time('blog.sample.MHSampler')
+
+def profile_quality():
+  # profile_partial = partial(run_one, 'blog-profile-quality',
+  #                           'blog.sample.LWSampler', n_obs = 5)
+  # workers = Pool(10)
+  # times = workers.map(lambda n_iter: profile_partial(n_iter = n_iter),
+                       
+  
 def main():
-  profile('blog.sample.LWSampler')
-  profile('blog.sample.MHSampler')
+  dispatch = {'time' : profile_times,
+              'quality' : profile_quality}
+  dispatch[argv[1]]()
 
 if __name__ == '__main__':
   main()
