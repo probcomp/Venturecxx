54e6eb0e62229d99d72b112e39cd78c1718d1e25
Alexey Radul <axch@mit.edu>
2015-05-14 13:35:16 -0400

    Reuse primitive SP binding abstraction.

--- backend/new_cxx/src/sps/lite.cxx
+++ backend/new_cxx/src/sps/lite.cxx
@@ -260,10 +260,6 @@ boost::python::dict ForeignLiteSP::toPython(Trace * trace, shared_ptr<SPAux> aux
 
 void PyTrace::bindPrimitiveSP(const string& sym, boost::python::object sp)
 {
-  VentureValuePtr spRecord(new VentureSPRecord(new ForeignLiteSP(sp)));
-  ConstantNode * node = trace->createConstantNode(spRecord);
-  processMadeSP(trace.get(), node, false, false, shared_ptr<DB>(new DB()));
-  assert(dynamic_pointer_cast<VentureSPRef>(trace->getValue(node)));
+  Node* node = trace->bindPrimitiveSP(sym, new ForeignLiteSP(sp));
   trace->boundForeignSPNodes.insert(shared_ptr<Node>(node));
-  trace->globalEnvironment->addBinding(sym, node);
 }
