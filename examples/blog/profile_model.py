df937dd524ea458ccfdc29468a4247cff2c52a0e
David Wadden <dave.wadden@gmail.com>
2015-05-15 18:21:35 -0400

    Code and plots of inference quality in Venture

--- examples/blog/profile_model.py
+++ examples/blog/profile_model.py
@@ -74,29 +74,50 @@ def profile_quality_venture_loop(set_fun_name, inference_fun_name):
 def compare_quality():
   'Compare quality of likelihood weighting in Venture and Blog'
   n_iters = [1000,1668,2782,4641,7742,12915,21544,35938,59948,100000]
-  divergences = pd.DataFrame(map(compute_divergences, n_iters))
-  divergences.index = n_iters
-  fig, ax = plt.subplots(1)
-  divergences.plot(ax = ax, logx = True,
-                   title = 'Inference quality in poisson-ball model: BLOG and Venture')
-  ax.set_xlabel('# iterations (log scale)')
-  ax.set_ylabel('KL divergence')
-  fig.savefig('profile-quality/poisson-ball-kl-divergence.png')
-                   
+  pairs  = map(compute_divergences, n_iters)
+  kls = pd.DataFrame([pair[0] for pair in pairs], index = n_iters)
+  ts = pd.DataFrame([pair[1] for pair in pairs], index = n_iters)
+  fig, axs = plt.subplots(2,2,figsize = (12,8))
+  lw_idx = ['prefix_k-lw', 'set_as_list-lw']
+  mh_idx = ['prefix_k-mh', 'set_as_list-mh']
+  fig = kl_vs_iter(kls[lw_idx + ['blog']], fig, axs[0,0])
+  fig = kl_vs_iter(kls[mh_idx], fig, axs[1,0])
+  fig = kl_vs_time(kls[lw_idx], ts[lw_idx], fig, axs[0,1])
+  fig = kl_vs_time(kls[mh_idx], ts[mh_idx], fig, axs[1,1])
+  fig.suptitle('Inference quality in poisson-ball model: BLOG and Venture')
+  fig.savefig('profile-quality/poisson-ball-inference-quality.png')
+  
+def kl_vs_iter(kls, fig, ax):
+  kls.plot(ax = ax, logx = True)
+  if ax.is_last_row(): ax.set_xlabel('# iterations (log scale)')
+  if ax.is_first_col(): ax.set_ylabel('KL divergence')
+  return fig
+
+def kl_vs_time(kls, ts, fig, ax):
+  for label in ts:
+    ax.plot(ts[label], kls[label], label = label)
+  ax.set_xscale('log')
+  ax.legend()
+  if ax.is_last_row(): ax.set_xlabel('Wall time (log seconds)')
+  if ax.is_first_col(): ax.set_ylabel('KL divergence')
+  return fig
+  
 def compute_divergences(n_iter):
   'Compute divergences between Venture, Blog, and ground truth after n_iter samples'
   exact_posterior = get_exact_posterior()
-  venture_posterior = get_venture_posterior(n_iter)
-  blog_posterior = get_blog_posterior(n_iter)
-  return {'exact_venture' : kl(exact_posterior, venture_posterior),
-          'exact_blog'    : kl(exact_posterior, blog_posterior),
-          'venture_blog'  : kl(venture_posterior, blog_posterior)}
+  ps, ts = get_venture_posteriors(n_iter)
+  ps['blog'] = get_blog_posterior(n_iter)
+  kls = {key : kl(exact_posterior, value) for key, value in ps.iteritems()}
+  return kls, ts
 
 def kl(ps, qs):
-  def enough_supported(xs, ix): return xs[ix].sum() / xs.sum() > 0.99
   # For KL to work, support of distributions must be identical
   # Index out any entries for which only 1 distribution has support
   # Check that such entries constitute less than 1% of the support
+  def drop_no_support(xs): return xs.replace(0, np.nan).dropna()
+  def enough_supported(xs, ix): return xs[ix].sum() / xs.sum() > 0.99
+  ps = drop_no_support(ps)
+  qs = drop_no_support(qs)
   ix = ps.index.intersection(qs.index)
   if enough_supported(ps, ix) and enough_supported(qs, ix):
     return stats.entropy(ps[ix], qs[ix])
@@ -107,14 +128,26 @@ def get_exact_posterior():
   return pd.read_csv('profile-quality/poisson-ball-exact-posterior-5.csv',
                      index_col = 'n_balls')['p']
 
-def get_venture_posterior(n_iter):
-  ds = pd.read_csv('profile-quality/poisson-ball-results-venture.csv',
-                   nrows = n_iter)
+def get_venture_posteriors(n_iter):
+  path_prefix = 'profile-quality/poisson-ball-results-venture-'
+  xs = map(partial(get_venture_posterior, n_iter),
+           [path_prefix + x for x in ["prefix_k-lw-5.csv",
+                                      "prefix_k-mh-5.csv",
+                                      "set_as_list-lw-5.csv",
+                                      "set_as_list-mh-5.csv"]])
+  ts = dict([(x[0], x[2]) for x in xs])
+  ps = dict([(x[0], x[1]) for x in xs])
+  return ps, ts 
+
+def get_venture_posterior(n_iter, f_name):
+  ds = pd.read_csv(f_name, nrows = n_iter)
+  name = '-'.join(f_name.split('-')[-3:-1])
   ds['n_balls'] = ds['n_balls'].apply(int)
   ds['particle weight'] = ds['particle log weight'].apply(np.exp)
   ps = ds.groupby('n_balls').agg({'particle weight' : sum})['particle weight']
-  ps.name = 'venture'
-  return ps
+  ps.name = name
+  elapsed_t = max(ds['time (s)']) - min(ds['time (s)'])
+  return name, ps, elapsed_t
 
 def get_blog_posterior(n_iter):
   f_name = 'profile-quality/poisson-ball-results-LWSampler-{0}-5.json'.format(n_iter)
@@ -135,4 +168,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-  
