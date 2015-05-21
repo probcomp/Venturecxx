9e818822624e746f347e3d106673c932a9c9416f
David Wadden <dave.wadden@gmail.com>
2015-05-21 02:06:00 -0400

    Renaming

--- examples/blog/compare-scaffolds/run-tests.sh
+++ examples/blog/compare-scaffolds/run-tests.sh
@@ -1,21 +1,21 @@
-profile_unknown_size () {
-    outfile="sets-unknown-size.txt"
+profile_with_filter () {
+    outfile="sets-with-filter.txt"
     if [ -e $outfile ]
     then
         rm $outfile
     fi
-    for set_model in unknown_n unknown_n_predict unknown_n_thunk
+    for set_model in filter_n filter_n_predict filter_n_thunk
     do
         echo "Running tests for $set_model" >> $outfile
-        venture lite -P -L plugins.py -f sets_unknown_size.vnt \
-                -e "[infer (run_test $set_model)]" >> $outfile
+        venture lite -P -L plugins.py -f compare_scaffolds.vnt \
+                -e "[infer (test_sets_with_filter $set_model)]" >> $outfile
     done
 }
 
 main () {
-    if [ "$1" == "profile_unknown_size" ]
+    if [ "$1" == "profile_with_filter" ]
     then
-        profile_unknown_size
+        profile_with_filter
     fi
 }
 
