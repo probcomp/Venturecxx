b24a1a02e017baf7918f9dece9f88775fee5e6f9
Alexey Radul <axch@mit.edu>
2015-06-03 17:10:27 -0400

    Qualified import.

--- test/analytics/test_analytics.py
+++ test/analytics/test_analytics.py
@@ -25,14 +25,10 @@ from nose.tools import eq_, assert_almost_equal
 from nose.plugins.attrib import attr
 
 from venture.venturemagics.ip_parallel import MRipl
-from venture.unit import *
+import venture.unit as unit
 from venture.test.stats import statisticalTest, reportKnownContinuous
 from venture.test.config import get_ripl,get_mripl,ignore_inference_quality,default_num_samples,gen_in_backend, gen_on_inf_prim
 import venture.value.dicts as v
-
-
-
-
 
 ## Functions used by tests
 def betaModel(ripl):
@@ -70,7 +66,7 @@ def _testLoadModel(riplThunk):
     vBackend = v.backend if isinstance(v,MRipl) else v.backend()
 
     v,assumes,observes,queryExps = betaModel(v)
-    model = Analytics(v,queryExps=queryExps)
+    model = unit.Analytics(v,queryExps=queryExps)
 
     def attributesMatch():
         eq_( model.backend,vBackend )
@@ -80,14 +76,14 @@ def _testLoadModel(riplThunk):
     attributesMatch() # assumes extracted from ripl_mripl
 
     v.clear()   # now assumes given as kwarg
-    model = Analytics(v,assumes=assumes,observes=observes,queryExps=queryExps)
+    model = unit.Analytics(v,assumes=assumes,observes=observes,queryExps=queryExps)
     attributesMatch()
 
 def _testHistory(riplThunk):
     v=riplThunk()
     v,assumes,observes,queryExps = betaModel(v)
     samples = 5
-    model = Analytics(v,queryExps=queryExps)
+    model = unit.Analytics(v,queryExps=queryExps)
     history,_ = model.runFromConditional(samples,runs=1)
     eq_(history.data,observes)
     assert all( [sym in history.nameToSeries for sym,_ in assumes] )
@@ -112,7 +108,7 @@ def _testRuns(riplThunk):
     v,_,_,queryExps,_ = normalModel( riplThunk() )
     samples = default_num_samples()
     no_runs = 4
-    model = Analytics(v,queryExps=queryExps)
+    model = unit.Analytics(v,queryExps=queryExps)
 
     history,_ = model.runFromConditional(samples,runs=no_runs)
     eq_( len(history.nameToSeries['x']), no_runs)
@@ -139,7 +135,7 @@ def _testInfer(riplThunk,conditional_prior,inferProg):
     else:
         samples = int(math.sqrt(default_num_samples()))
         runs = int(math.sqrt(default_num_samples()))
-    model = Analytics(v)
+    model = unit.Analytics(v)
     infer_kwargs = dict(runs=runs,infer=inferProg,simpleInfer=True)
 
     if conditional_prior == 'conditional':
@@ -174,7 +170,7 @@ def _testSampleFromJoint(riplThunk,useMRipl):
 
     v,_,_,queryExps,xPriorCdf = normalModel( riplThunk() )
     samples = default_num_samples()
-    model = Analytics(v,queryExps=queryExps)
+    model = unit.Analytics(v,queryExps=queryExps)
     history = model.sampleFromJoint(samples, useMRipl=useMRipl)
     xSamples = nameToFirstValues(history,'x')
     return reportKnownContinuous(xPriorCdf,xSamples)
@@ -193,7 +189,7 @@ def _testRunFromJoint1(riplThunk,inferProg):
     if riplThunk.func_name in 'get_ripl':
         raise SkipTest('Same bug as samplefromjoint with identical ripl seeds')
     v,_,_,queryExps,xPriorCdf = normalModel( riplThunk() )
-    model = Analytics(v,queryExps=queryExps)
+    model = unit.Analytics(v,queryExps=queryExps)
     # variation across runs
     history = model.runFromJoint(1, runs=default_num_samples(), infer=inferProg)
     return reportKnownContinuous(xPriorCdf,snapshot_t(history,'x',0))
@@ -201,7 +197,7 @@ def _testRunFromJoint1(riplThunk,inferProg):
 @statisticalTest
 def _testRunFromJoint2(riplThunk,inferProg):
     v,_,_,queryExps,xPriorCdf = normalModel( riplThunk() )
-    model = Analytics(v,queryExps=queryExps)
+    model = unit.Analytics(v,queryExps=queryExps)
     if ignore_inference_quality():
         step_size = 1
     else:
@@ -235,7 +231,7 @@ def _testCompareSampleDicts(sameDistribution):
         dicts = [ dict(x=np.random.normal(0,1,40)) for _ in range(2) ]
     else:
         dicts = [ dict(x=np.random.normal(0,i,40)) for i in range(1,3) ]
-    cReport = compareSampleDicts(dicts,('',''),plot=False)
+    cReport = unit.compareSampleDicts(dicts,('',''),plot=False)
     assert hasattr(cReport,'reportString')
     assert hasattr(cReport,'labels')
     return cReport.statsDict['x']['KSSameContinuous'] #test result object
@@ -256,7 +252,7 @@ def _testCompareSnapshots(riplThunk):
     else:
         samples = 2 * int(math.sqrt(2 * default_num_samples()))
         runs = int(math.sqrt(2 * default_num_samples()))
-    model = Analytics(v,queryExps=queryExps)
+    model = unit.Analytics(v,queryExps=queryExps)
     history,_ = model.runFromConditional(samples,runs=runs)
     # two final snapshots should be very similar in distribution
     report = history.compareSnapshots(probes = (-2,-1))
@@ -275,7 +271,7 @@ def _testForce(riplThunk):
         v.assume('x%i'%i,'(normal 0 100)')
     for _ in range(4):
         v.observe('(normal (+ x0 x1) 30)',100.)
-    model = Analytics(v)
+    model = unit.Analytics(v)
     samples = default_num_samples()
     inferProg = '(mh default one 1)'
     fdict = dict( [('x%i'%i,0) for i in range(5)] )
@@ -287,7 +283,7 @@ def _testAtomType(conditional_prior):
     # make sure Analytics isn't confusing atoms with numbers
     v = get_ripl()
     v.observe('(categorical (simplex 0.5 0.5))', 'atom<1>')
-    model = Analytics(v)
+    model = unit.Analytics(v)
     samples = 1
     if conditional_prior == 'conditional':
         history, _ = model.runFromConditional(samples)
@@ -303,15 +299,15 @@ def testAtomType():
 
 ## FIXME clean-up and add this test of Geweke (if Geweke is being used)
 def _testGewekeTest():
-    params = generateMRiplParams(no_ripls=(2,3), backends=('puma','lite'),
-                                 modes=(True,))  ## ONLY LOCAL
+    params = unit.generateMRiplParams(no_ripls=(2,3), backends=('puma','lite'),
+                                      modes=(True,))  ## ONLY LOCAL
     results = []
 
     for (no_ripls, backend, mode) in params:
         v=MRipl(no_ripls,backend=backend,local_mode=mode)
         v.assume('mu','(normal 0 30)')
         v.observe('(normal mu 200)','0')
-        model = Analytics(v)
+        model = unit.Analytics(v)
         fwd,inf,_=model.gewekeTest(50,plot=False,useMRipl=True)
         muSamples= [h.nameToSeries['mu'][0].values for h in [fwd,inf] ]
 
