a46b854781292e2962560130d97ba7dc6bc00133
Alexey Radul <axch@mit.edu>
2015-06-10 15:29:15 -0400

    Build the initial environment.

--- backend/untraced/trace.py
+++ backend/untraced/trace.py
@@ -2,6 +2,8 @@ from ..lite import types as t
 from ..lite import value as vv
 from ..lite import env
 from ..lite import address as addr
+from ..lite.sp import VentureSPRecord
+from ..lite import builtin
 
 import evaluator
 
@@ -10,6 +12,11 @@ class Trace(object):
   def __init__(self):
     self.results = {}
     self.env = env.VentureEnvironment()
+    for name, val in builtin.builtInValues().iteritems():
+      self.bindPrimitiveName(name, val)
+    for name, sp in builtin.builtInSPs().iteritems():
+      self.bindPrimitiveSP(name, sp)
+    self.env = env.VentureEnvironment(self.env) # New frame so users can shadow globals
 
   def extractRaw(self, id):
     return self.results[id]
@@ -21,3 +28,11 @@ class Trace(object):
     assert id not in self.results
     py_exp = t.ExpressionType().asPython(vv.VentureValue.fromStackDict(exp))
     self.results[id] = evaluator.eval(addr.Address(addr.List(id)), py_exp, self.env)
+
+  def bindPrimitiveName(self, name, val):
+    self.env.addBinding(name, val)
+
+  def bindPrimitiveSP(self, name, sp):
+    # TODO Mess with SPRecords and SPRefs properly
+    spVal = VentureSPRecord(sp)
+    self.env.addBinding(name, spVal)
