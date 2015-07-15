6f1aef0530f67b276abdfb349b3ea0cdbe02c293
Alexey Radul <axch@mit.edu>
2015-07-15 02:35:06 -0400

    Minimally brand the reference manual as referring to VentureScript.

--- refman/conf.py
+++ refman/conf.py
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Venture'
+project = u'VentureScript'
 copyright = u'2014, MIT Probabilistic Computing Project'
 
 # The version info for the project you're documenting, acts as replacement for
