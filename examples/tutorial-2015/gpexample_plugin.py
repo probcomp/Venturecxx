021f480a375a9f10ebcaa261149f4d11a69de7c4
Ben Zinberg <bzinberg@mit.edu>
2015-07-18 19:35:03 -0400

    made code non-fake, except for the GP visualization command which needs to be ported

---
+++ examples/tutorial-2015/gpexample_plugin.py
@@ -0,0 +1,119 @@
+import sys
+sys.path.append('.')
+
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+# Let's see what we need here
+# from models.covFunctions import *
+# from models.tools import array
+
+from venture import shortcuts
+import venture.lite.types as t
+import venture.lite.sp as sp
+from venture.lite.function import VentureFunction
+from venture.lite.builtin import deterministic_typed
+import gpmem
+import pickle
+import collections
+from numpy.random import random as rand
+import scipy.spatial.distance as spdist
+
+@np.vectorize
+def regexmpl_f_noiseless(x):
+    return 0.3 + 0.4*x + 0.5*np.sin(2.7*x) + (1.1/(1+x**2))
+
+
+# Covariance functions
+squaredExponentialType = sp.SPType([t.NumberType(), t.NumberType()], t.NumberType())
+def squared_exponential(sf, l):
+    def f(x1, x2):
+        A = spdist.cdist([[x1/l]],[[x2/l]],'sqeuclidean')
+        ans = sf * np.exp(-0.5*A)[0,0]
+        return ans
+    return f
+
+whitenoiseType = sp.SPType([t.NumberType()], t.NumberType())
+def whitenoise(s):
+    def f(x1, x2):
+        tol = 1.e-9  # Tolerance for declaring two vectors "equal"
+        M = spdist.cdist(x1, x2, 'sqeuclidean')
+        A = s * (M < tol)
+        return A
+    return f
+
+def __venture_start__(ripl, *args):
+
+    # External SPs
+    argmaxSP = deterministic_typed(np.argmax, [t.HomogeneousArrayType(t.NumberType())], t.NumberType())
+    absSP = deterministic_typed(abs, [t.NumberType()], t.NumberType())
+    make_se_SP = deterministic_typed(lambda sf, l:
+                VentureFunction(squared_exponential(sf, l),
+                    name="SE", parameter=[sf,l], sp_type=squaredExponentialType),
+            [t.NumberType(), t.NumberType()], t.AnyType("VentureFunction"))
+    make_whitenoise_SP = deterministic_typed(lambda s:
+                VentureFunction(whitenoise(s),
+                    name="WN",parameter=[s], sp_type=whitenoiseType),
+            [t.NumberType()], t.AnyType("VentureFunction"))
+    make_const_func_SP = deterministic_typed(lambda c:
+            VentureFunction(lambda x: c,
+                sp_type = sp.SPType([], t.NumberType())),
+            [t.NumberType()], t.AnyType("VentureFunction"))
+
+    add_funcs_SP = deterministic_typed(lambda f1, f2: VentureFunction(lambda x: f1(x) + f2(x)),
+        [t.AnyType("VentureFunction"), t.AnyType("VentureFunction")],
+        t.AnyType("VentureFunction"))
+
+    ripl.bind_foreign_sp('gpmem', gpmem.gpmemSP)
+    ripl.bind_foreign_inference_sp('argmax_of_array', argmaxSP)
+    ripl.bind_foreign_sp('abs', absSP)
+    ripl.bind_foreign_sp('make_squaredexp', make_se_SP)
+    ripl.bind_foreign_sp('add_funcs', add_funcs_SP)
+    ripl.bind_foreign_sp('make_whitenoise', make_whitenoise_SP)
+    ripl.bind_foreign_sp('make_const_func', make_const_func_SP)
+
+
+    # Gpmem example
+    def make_audited_expensive_function(name):
+        def expensive_f(x):
+            expensive_f.count += 1 # A tracker for how many times I am called
+            ans = (0.2 + np.exp(-0.1*abs(x-2))) * np.cos(0.4*x)
+            print "[PROBE %s] Probe #%d: %s(%f) = %f" % (expensive_f.name, expensive_f.count, expensive_f.name, x, ans)
+            return ans
+        expensive_f.count = 0
+        expensive_f.name = name
+        audited_sp = deterministic_typed(expensive_f, [t.NumberType()], t.NumberType())
+        return sp.VentureSPRecord(audited_sp)
+
+    ripl.bind_foreign_sp('make_audited_expensive_function', deterministic_typed(
+        make_audited_expensive_function, [t.StringType()], sp.SPType([t.NumberType()], t.NumberType())))
+
+    # Regression example
+    @np.vectorize
+    def f_noisy(x):
+        p_outlier = 0.1
+        stdev = (1.0 if rand() < p_outlier else 0.1)
+        return np.random.normal(regexmpl_f_noiseless(x), stdev)
+
+    # Generate and save a data set
+    # print "Generating regression example data set"
+    n = 100
+    regexempl_data_xs = np.random.normal(0,1,n)
+    regexempl_data_ys = f_noisy(regexempl_data_xs)
+
+    ## The probe function
+    def f_restr(x):
+        matches = np.argwhere(np.abs(regexempl_data_xs - x) < 1e-6)
+        if matches.size == 0:
+            raise Exception('Illegal query')
+        else:
+            assert matches.size == 1
+            i = matches[0,0]
+            return regexempl_data_ys[i]
+    f_restr_sp = deterministic_typed(f_restr, [t.NumberType()], t.NumberType())
+    ripl.bind_foreign_sp('get_regexmpl_lookuper', deterministic_typed(
+        lambda: sp.VentureSPRecord(f_restr_sp), [], sp.SPType([t.NumberType()], t.NumberType())))
+
+    get_regexempl_data_xs_SP = deterministic_typed(lambda: regexempl_data_xs, [], t.HomogeneousArrayType(t.NumberType()))
+    ripl.bind_foreign_sp('get_regexempl_data_xs', get_regexempl_data_xs_SP)
+        
---
+++ examples/tutorial-2015/gpmem.py
@@ -0,0 +1,102 @@
+from venture.lite.psp import PSP, DeterministicPSP, TypedPSP, RandomPSP
+from venture.lite.sp import SP, VentureSPRecord, SPType
+from venture.lite.lkernel import SimulationAAALKernel
+from venture.lite.env import VentureEnvironment
+from venture.lite.request import Request,ESR
+from venture.lite.address import emptyAddress
+from venture.lite.builtin import no_request, typed_nr, deterministic_typed, typed_func_psp
+from venture.lite.function import VentureFunction
+import venture.lite.types as t
+from venture.lite.gp import GPOutputPSP, GPSP, GPSPAux
+import collections
+
+class MakeGPMSPOutputPSP(DeterministicPSP):
+    """"
+    This class (except its simulate method) is based on DeterministicAAAMakerPSP.
+    """
+    def __init__(self):
+        # We need to keep the aux ourselves, because proposals to the arguments
+        # of this function will cause f_compute and f_emu to be re-created,
+        # with initially blank auxs that we need to replace with our stored
+        # aux.
+        self.shared_aux = GPSPAux(collections.OrderedDict())
+
+    def simulate(self, args):
+        f_node = args.operandNodes[0]
+        prior_covariance_function = args.operandValues()[1]
+        f_compute = VentureSPRecord(
+                SP(GPMComputerRequestPSP(f_node), GPMComputerOutputPSP()))
+        # Prior mean is fixed to zero, because the current GP implementation
+        # assumes this
+        f_emu = VentureSPRecord(GPSP(zero_function, prior_covariance_function))
+        f_emu.spAux = self.shared_aux
+        f_compute.spAux = self.shared_aux
+        # TODO ways to get_Xseen and get_Yseen? maybe this belongs in the
+        # inference side
+        return t.pythonListToVentureList([f_compute, f_emu])
+
+    def childrenCanAAA(self): return True
+    def getAAALKernel(self): return GPMDeterministicMakerAAALKernel(self)
+
+class GPMDeterministicMakerAAALKernel(SimulationAAALKernel):
+    """
+    This is based on DeterministicMakerAAALKernel.
+    """
+    def __init__(self,makerPSP): self.makerPSP = makerPSP
+    def simulate(self, _trace, args):
+        ## We ain't assuming that the output is an SPRecord.
+        ## In fact, it should be a list of two SPRecords.
+        ## And their auxs are already attached.
+        # spRecord = self.makerPSP.simulate(args)
+        # spRecord.spAux = args.madeSPAux
+        # return spRecord
+
+        ## So the new version is:
+        return self.makerPSP.simulate(args)
+    def weight(self, _trace, newValue, _args):
+        # Using newValue.spAux here because args.madeSPAux is liable to be
+        # None when detaching. This has something to do with when the Args
+        # object is constructed relative to other things that happen
+        # during detach/regen. TODO: fix it so that this code is less
+        # fragile.
+
+        (f_compute, f_emu) = newValue.asPythonList()
+        answer = f_emu.sp.outputPSP.logDensityOfCounts(self.makerPSP.shared_aux)
+        # print "gpmem LKernel weight = %s" % answer
+        return answer
+
+gpmemSP = no_request(MakeGPMSPOutputPSP())
+
+zero_function = VentureFunction(lambda x: 0, [t.AnyType()], t.NumberType())
+
+# TODO treat this as not necessarily random?
+class GPMComputerRequestPSP(DeterministicPSP):
+    def __init__(self, f_node):
+        self.f_node = f_node
+
+    def simulate(self, args):
+        id = str(args.operandValues())
+        exp = ["gpmemmedSP"] + [["quote",val] for val in args.operandValues()]
+        env = VentureEnvironment(None,["gpmemmedSP"],[self.f_node])
+        return Request([ESR(id,exp,emptyAddress,env)])
+
+# TODO Perhaps this could subclass ESRRefOutputPSP to correctly handle
+# back-propagation?
+class GPMComputerOutputPSP(DeterministicPSP):
+    def simulate(self,args):
+        assert len(args.esrNodes()) ==  1
+        return args.esrValues()[0]
+
+    def incorporate(self, value, args):
+        # TODO maybe best to just call on someone else's incorporate method
+        # instead? idk
+        x = args.operandValues()[0].getNumber()
+        y = value.getNumber()
+        args.spaux().samples[x] = y
+
+    def unincorporate(self, value, args):
+        x = args.operandValues()[0].getNumber()
+        samples = args.spaux().samples
+        if x in samples:
+            del samples[x]
+
