824a2086313eb0597f00a38f5c79b83bc75a55e1
Alexey Radul <axch@mit.edu>
2015-07-20 16:33:07 -0400

    Right.  Use the programmatic api to be syntax agnostic.

--- test/config.py
+++ test/config.py
@@ -75,6 +75,7 @@ from StringIO import StringIO
 
 import venture.shortcuts as s
 import venture.venturemagics.ip_parallel as ip_parallel
+import venture.value.dicts as v
 
 def yes_like(thing):
   if isinstance(thing, str):
@@ -195,7 +196,7 @@ def _collectData(iid,ripl,address,num_samples=None,infer=None):
     predictions.append(ripl.report(address))
     if iid:
       ripl.sivm.core_sivm.engine.reinit_inference_problem()
-      ripl.infer("(incorporate)")
+      ripl.infer(v.app(v.sym("incorporate")))
   return predictions
 
 disable_default_infer = False
