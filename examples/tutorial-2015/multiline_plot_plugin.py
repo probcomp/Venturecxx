6aaf7b9d77bf65e13b4efdd14ed27f43b342175a
Alexey Radul <axch@mit.edu>
2015-07-20 23:34:46 -0400

    Move the plugins closer to where they are actually used, for better flow.

---
+++ examples/tutorial-2015/multiline_plot_plugin.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as clr
+
+import venture.lite.types as t
+from venture.lite.builtin import deterministic_typed
+
+def plot_lines(xs, yss, data_xs, data_ys, ymin, ymax, huemin, huemax, linewidth):
+    hues = np.linspace(huemin, huemax, len(yss))
+    fig, ax = plt.subplots(1)
+    for (ys, hue) in zip(yss, hues):
+        ax.plot(xs, ys, color=clr.hsv_to_rgb(np.array([hue, 1, 1]).reshape((1,1,3))).reshape(3), linewidth=linewidth)
+    ax.scatter(data_xs, data_ys, color='k')
+    ax.set_ylim(ymin, ymax)
+    plt.show()
+    # plt.savefig('plotlines_fig.png')
+
+def __venture_start__(ripl):
+    plot_lines_sp = deterministic_typed(plot_lines,
+            [t.HomogeneousArrayType(t.NumberType()), # xs
+                t.HomogeneousArrayType(t.HomogeneousArrayType(t.NumberType())), # yss
+                t.HomogeneousArrayType(t.NumberType()), # data_xs
+                t.HomogeneousArrayType(t.NumberType()), # data_ys
+                t.NumberType(), # ymin
+                t.NumberType(), # ymax
+                t.NumberType(), # huemin
+                t.NumberType(), # huemax
+                t.NumberType(), # linewidth
+                ],
+            t.NilType())
+    ripl.bind_foreign_inference_sp('plot_lines', plot_lines_sp)
