cf42e7df410f9e86519740981c7fc44b3506466c
Alexey Radul <axch@mit.edu>
2015-07-28 18:57:11 -0400

    Segregate the SP construction helpers into their own module.

--- backend/lite/builtin.py
+++ backend/lite/builtin.py
@@ -20,7 +20,7 @@ import numpy as np
 from numbers import Number
 
 from sp import SP, SPType
-from psp import NullRequestPSP, ESRRefOutputPSP, DeterministicPSP, TypedPSP, DispatchingPSP
+from psp import TypedPSP, DispatchingPSP
 
 import discrete
 import dirichlet
@@ -40,7 +40,9 @@ import value as v
 import types as t
 import env
 from utils import careful_exp, raise_
-from exception import VentureBuiltinSPMethodError, VentureValueError
+from exception import VentureValueError
+
+from sp_help import *
 
 # The types in the types module are generated programmatically, so
 # pylint doesn't find out about them.
@@ -48,84 +50,6 @@ from exception import VentureBuiltinSPMethodError, VentureValueError
 
 def builtInValues():
   return { "true" : v.VentureBool(True), "false" : v.VentureBool(False), "nil" : v.VentureNil() }
-
-def no_request(output): return SP(NullRequestPSP(), output)
-
-def esr_output(request): return SP(request, ESRRefOutputPSP())
-
-def typed_nr(output, args_types, return_type, **kwargs):
-  return no_request(TypedPSP(output, SPType(args_types, return_type, **kwargs)))
-
-class FunctionPSP(DeterministicPSP):
-  def __init__(self, f, descr=None, sim_grad=None):
-    self.f = f
-    self.descr = descr
-    self.sim_grad = sim_grad
-    if self.descr is None:
-      self.descr = "deterministic %s"
-  def simulate(self,args):
-    return self.f(args)
-  def gradientOfSimulate(self, args, _value, direction):
-    # Don't need the value if the function is deterministic, because
-    # it consumes no randomness.
-    if self.sim_grad:
-      return self.sim_grad(args, direction)
-    else:
-      raise VentureBuiltinSPMethodError("Cannot compute simulation gradient of '%s'" % self.description("<unknown name>"))
-  def description(self,name):
-    if '%s' in self.descr:
-      return self.descr % name
-    else:
-      return self.descr
-
-def typed_func_psp(f, args_types, return_type, descr=None, sim_grad=None, **kwargs):
-  return TypedPSP(FunctionPSP(f, descr, sim_grad), SPType(args_types, return_type, **kwargs))
-
-def typed_func(*args, **kwargs):
-  return no_request(typed_func_psp(*args, **kwargs))
-
-# TODO This should actually be named to distinguish it from the
-# previous version, which accepts the whole args object (where this
-# one splats the operand values).
-def deterministic_psp(f, descr=None, sim_grad=None):
-  def new_grad(args, direction):
-    return sim_grad(args.operandValues(), direction)
-  return FunctionPSP(lambda args: f(*args.operandValues()), descr, sim_grad=(new_grad if sim_grad else None))
-
-def deterministic_typed_psp(f, args_types, return_type, descr=None, sim_grad=None, **kwargs):
-  return TypedPSP(deterministic_psp(f, descr, sim_grad), SPType(args_types, return_type, **kwargs))
-
-def deterministic(f, descr=None, sim_grad=None):
-  return no_request(deterministic_psp(f, descr, sim_grad))
-
-def deterministic_typed(f, args_types, return_type, descr=None, sim_grad=None, **kwargs):
-  return typed_nr(deterministic_psp(f, descr, sim_grad), args_types, return_type, **kwargs)
-
-def binaryNum(f, sim_grad=None, descr=None):
-  return deterministic_typed(f, [t.NumberType(), t.NumberType()], t.NumberType(), sim_grad=sim_grad, descr=descr)
-
-def binaryNumS(output):
-  return typed_nr(output, [t.NumberType(), t.NumberType()], t.NumberType())
-
-def unaryNum(f, sim_grad=None, descr=None):
-  return deterministic_typed(f, [t.NumberType()], t.NumberType(), sim_grad=sim_grad, descr=descr)
-
-def unaryNumS(f):
-  return typed_nr(f, [t.NumberType()], t.NumberType())
-
-def naryNum(f, sim_grad=None, descr=None):
-  return deterministic_typed(f, [t.NumberType()], t.NumberType(), variadic=True, sim_grad=sim_grad, descr=descr)
-
-def zero_gradient(args, _direction):
-  return [0 for _ in args]
-
-def binaryPred(f, descr=None):
-  return deterministic_typed(f, [t.AnyType(), t.AnyType()], t.BoolType(), sim_grad=zero_gradient, descr=descr)
-
-def type_test(tp):
-  return deterministic_typed(lambda thing: thing in tp, [t.AnyType()], t.BoolType(),
-                             sim_grad = zero_gradient,
-                             descr="%s returns true iff its argument is a " + tp.name())
 
 def grad_times(args, direction):
   assert len(args) == 2, "Gradient only available for binary multiply"
