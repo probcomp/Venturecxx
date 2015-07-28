f7abbafce999c23900cdc8ae577bd85f67206fd6
Anthony Lu <lua@mit.edu>
2015-07-28 12:57:30 -0400

    Standardize on p*(p-1)//2.
    // is integer division, which is what we want.

--- backend/lite/continuous.py
+++ backend/lite/continuous.py
@@ -119,7 +119,7 @@ class InverseWishartPSP(RandomPSP):
       x = np.random.normal(size=(dof, p))
     else:
       x = np.diag(np.sqrt(np.random.chisquare(dof - np.arange(p), size=p)))
-      x[np.triu_indices_from(x,1)] = np.random.normal(size=(p*(p-1)/2))
+      x[np.triu_indices_from(x,1)] = np.random.normal(size=(p*(p-1)//2))
     R = np.linalg.qr(x, 'r')
     T = scipy.linalg.solve_triangular(R.T, chol.T).T
     return np.matrix(np.dot(T,T.T))
@@ -176,7 +176,7 @@ class WishartPSP(RandomPSP):
       A = np.random.normal(size=(p, dof))
     else:
       A = np.diag(np.sqrt(np.random.chisquare(dof - np.arange(p), size=p)))
-      A[np.tri(n,k=-1,dtype=bool)] = np.random.normal(size=(p*(p-1)/2.))
+      A[np.tri(n,k=-1,dtype=bool)] = np.random.normal(size=(p*(p-1)//2))
     X = np.dot(chol, A)
     return np.matrix(np.dot(X, X.T))
 
