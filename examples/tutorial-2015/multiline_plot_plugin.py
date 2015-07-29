eddfa4280c201abeff8332729522478ec43272d8
Anthony Lu <lua@mit.edu>
2015-07-29 19:04:31 -0400

    Introduce array_from_dataset so I can use accumulate_dataset.

--- examples/tutorial-2015/multiline_plot_plugin.py
+++ examples/tutorial-2015/multiline_plot_plugin.py
@@ -2,8 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as clr
 
+import pandas as pd
+
 import venture.lite.types as t
 from venture.lite.builtin import deterministic_typed
+
+def array_from_dataset(d):
+    [ind_name] = d.ind_names
+    yss = d.data[ind_name]
+    return map(t.VentureValue.fromStackDict, yss)
 
 def plot_lines(xs, yss, data_xs, data_ys, ymin, ymax, huemin, huemax, linewidth):
     hues = np.linspace(huemin, huemax, len(yss))
@@ -16,6 +23,10 @@ def plot_lines(xs, yss, data_xs, data_ys, ymin, ymax, huemin, huemax, linewidth)
     # plt.savefig('plotlines_fig.png')
 
 def __venture_start__(ripl):
+    array_from_dataset_sp = deterministic_typed(array_from_dataset,
+            [t.ForeignBlobType()],
+            t.ArrayType())
+    ripl.bind_foreign_inference_sp('array_from_dataset', array_from_dataset_sp)
     plot_lines_sp = deterministic_typed(plot_lines,
             [t.HomogeneousArrayType(t.NumberType()), # xs
                 t.HomogeneousArrayType(t.HomogeneousArrayType(t.NumberType())), # yss
