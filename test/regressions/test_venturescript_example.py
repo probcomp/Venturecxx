1d1fabd6a65a0e18be76fe15ae4728c1d76baa88
Alexey Radul <axch@mit.edu>
2015-07-31 14:34:05 -0400

    VentureScript requires semicolons now (wasn't caught b/c was only being run under inference quality mode).

--- test/regressions/test_venturescript_example.py
+++ test/regressions/test_venturescript_example.py
@@ -37,16 +37,16 @@ def testVentureScriptAbstractExample():
   r.set_mode("venture_script")
   r.bind_foreign_sp("uniform_continuous", typed_nr(EnumerableUniformOutputPSP(), [t.NumberType(), t.NumberType()], t.NumberType()))
   r.execute_program("""
-infer resample(2)
-assume is_funny = tag(quote(fun), 0, flip(0.3))
-assume funny_mean = tag(quote(mean), 0, uniform_continuous(-10,10))
-assume mean = if (is_funny) { funny_mean } else { 0 }
-assume trial = proc() { normal(mean, 1) }
-observe trial() = 8
-observe trial() = 8
-observe trial() = 8
-observe trial() = 8
-infer emap(default, all, 1, false)
+infer resample(2);
+assume is_funny = tag(quote(fun), 0, flip(0.3));
+assume funny_mean = tag(quote(mean), 0, uniform_continuous(-10,10));
+assume mean = if (is_funny) { funny_mean } else { 0 };
+assume trial = proc() { normal(mean, 1) };
+observe trial() = 8;
+observe trial() = 8;
+observe trial() = 8;
+observe trial() = 8;
+infer emap(default, all, 1, false);
 """)
   eq_(True, r.sample("is_funny"))
   assert_almost_equal(8, r.sample("funny_mean"))
