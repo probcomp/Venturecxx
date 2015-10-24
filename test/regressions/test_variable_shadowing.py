c08920c8392739e785bd3423f5c6adae9eff60ca
Eli Sennesh <eligottlieb@gmail.com>
2015-10-23 20:56:21 -0400

    trace functionality: "seal" the environment after the inference prelude
    Add a method to all Trace implementations that "seals" the environment by
    replacing the current global environment with a child environment.  Apply this
    method to seal the environment after the inference prelude is loaded, ensuring
    that users can shadow prelude-level bindings but never redefine them.
    
    Testing Done: test/regressions/test_variable_shadowing.py
    Bug Number: 167 (https://github.com/probcomp/Venturecxx/issues/167)
    
    Signed-off-by: Eli Sennesh <eligottlieb@gmail.com>

---
+++ test/regressions/test_variable_shadowing.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2015 MIT Probabilistic Computing Project.
+#
+# This file is part of Venture.
+#
+# Venture is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Venture is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Venture.  If not, see <http://www.gnu.org/licenses/>.
+
+from nose.tools import eq_
+
+from venture.test.config import get_ripl, broken_in
+
+def testVariableShadowing():
+  """Prior to the patch in which we implemented Trace.sealEnvironment(), we were
+  able to destructively redefine symbols from the inference prelude, and things
+  exploded.  Now we should see a shadowing behavior when we try similar
+  definitions.
+"""
+  ripl = get_ripl()
+  ripl.execute_program('''
+[define repeat 3]
+[infer (accumulate_dataset 10 (do (collect repeat)))]
+''')
+  eq_(ripl.evaluate('repeat'), ripl.evaluate('3.0'))
