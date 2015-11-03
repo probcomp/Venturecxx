0abf18ff2b3021be80d579eee07a2f2eddfd5858
Alexey Radul <axch@mit.edu>
2015-11-03 14:07:15 -0500

    Pre-build grammars to get a clean reading on the version in check_sdist
    (also explain the steps).

--- script/jenkins/check_sdist.sh
+++ script/jenkins/check_sdist.sh
@@ -2,13 +2,25 @@
 
 set -ex
 
+# Build the grammar files if needed, for clean output when detecting
+# the version.
+python setup.py --version
+
+# Compute the name of the distribution file.
 version=`python setup.py --version`
 dist_name="Venture-CXX-$version.tar.gz"
+
+# Build the distribution.
 python setup.py sdist
 
+# Install it in a fresh virtualenv
 venv_dir=`mktemp -dt "jenkins-sdist-install.XXXXXXXXXX"`
 virtualenv $venv_dir
 . $venv_dir/bin/activate
 pip install "dist/$dist_name"
+
+# Test the result
 verifyinstall.sh
+
+# Clean up
 /bin/rm -fr $venv_dir
