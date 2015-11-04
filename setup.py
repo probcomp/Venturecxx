e105d3156dc93141f3d265ed9098d996c57a4272
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-11-04 23:23:53 +0000

    Proper band-aid^WSETUPTOOLS LEG AMPUTATION for sdist version mangling.

--- setup.py
+++ setup.py
@@ -18,8 +18,10 @@
 
 try:
     from setuptools import setup, Extension
+    from setuptools.command.sdist import sdist
 except ImportError:
     from distutils.core import setup, Extension
+    from distutils.command.sdist import sdist
 
 import os
 import sys
@@ -286,33 +288,22 @@ tests_require = [
     'pexpect',
 ]
 
-# XXX It appears that setuptools doesn't like developers to create
-# distributions of local versions (those indicated by the '+'
-# character) of their packages.  Specifically, I have observed that
-# python setup.py sdist would, before adding this section,
-# occasionally change the '+' to a hypen:
-#   $ ls dist/
-#   Venture-CXX-0.4.1.post183+gb0f4204.tar.gz
-#   Venture-CXX-0.4.1.post184+g0abf18f.tar.gz
-#   Venture-CXX-0.4.1.post185+g8e252ec.tar.gz
-#   Venture-CXX-0.4.1.post189-g8d55da8.tar.gz
-#   Venture-CXX-0.4.1.post190-ga198fc4.tar.gz
-# I suspect that this is due to detecting version modifiers (the "a8"
-# and "c4") in the local version string.  Since such substrings cannot
-# be prevented from appearing in git commit numbers, I chose to
-# suppress the local component.  (And alternative would have been to
-# try to confuse setuptools' regex by adding extra characters, perhaps
-# at the end).
-
-pos = version.find('+')
-if pos > -1:
-    public_version = version[:pos]
-else:
-    public_version = version
+# XXX For inexplicable reasons, during sdist.run, setuptools quietly
+# modifies self.distribution.metadata.version to replace plus signs by
+# hyphens -- even where they are explicitly allowed by PEP 440.
+# distutils does not do this -- only setuptools.
+class local_sdist(sdist):
+    # This is not really a subcommand -- it's actually a predicate to
+    # determine whether to run a subcommand.  So modifying anything in
+    # it is a little evil.  But it'll do.
+    def fixidioticegginfomess(self):
+        self.distribution.metadata.version = version
+        return False
+    sub_commands = [('sdist_fixidioticegginfomess', fixidioticegginfomess)]
 
 setup (
     name = 'Venture-CXX',
-    version = public_version,
+    version = version,
     author = 'MIT.PCP',
     url = 'TBA',
     long_description = 'TBA.',
@@ -332,5 +323,8 @@ setup (
     },
     package_data = {'':['*.vnt']},
     ext_modules = ext_modules,
-    scripts = ['script/venture', 'script/vendoc']
+    scripts = ['script/venture', 'script/vendoc'],
+    cmdclass={
+        'sdist': local_sdist,
+    },
 )
