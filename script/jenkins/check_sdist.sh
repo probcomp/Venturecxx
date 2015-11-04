8d55da8073183f0827699a7bc94ff99d055fde62
Alexey Radul <axch@mit.edu>
2015-11-04 09:20:42 -0500

    Rewrite check_sdist to install the declared test dependencies before testing.

--- script/jenkins/check_sdist.sh
+++ script/jenkins/check_sdist.sh
@@ -6,9 +6,8 @@ set -ex
 # the version.
 python setup.py --version
 
-# Compute the name of the distribution file.
+# Compute the version that will be built.
 version=`python setup.py --version`
-dist_name="Venture-CXX-$version.tar.gz"
 
 # Build the distribution.
 python setup.py sdist
@@ -17,10 +16,20 @@ python setup.py sdist
 venv_dir=`mktemp -dt "jenkins-sdist-install.XXXXXXXXXX"`
 virtualenv $venv_dir
 . $venv_dir/bin/activate
-pip install "dist/$dist_name"
+pip install --find-links dist/ "Venture-CXX==$version"
 
-# Test the result
-./verifyinstall.sh
+# Smoke test the result without testing-only dependencies (copied from
+# verifyinstall.sh)
+# TODO This should be a separate script, and should exercise all the
+# major functions.
+venture lite --abstract-syntax -e '[infer (bind (collect (normal 0 1)) printf)]'
+
+# Install the test dependencies.
+pip install --find-links dist/ "Venture-CXX[tests]==$version"
+
+# Test more thoroughly (copied from verifyinstall.sh).
+# TODO Should this be the crash test suite?
+nosetests -c unattended.cfg
 
 # Clean up
 /bin/rm -fr $venv_dir
