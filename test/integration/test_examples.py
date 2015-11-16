853a7ea3b14abe7ffed8e4b281766d546ad93649
Alexey Radul <axch@mit.edu>
2015-11-16 17:54:34 -0500

    hmc-demo no longer depends on VentureUnit.

--- test/integration/test_examples.py
+++ test/integration/test_examples.py
@@ -46,7 +46,7 @@ def checkExample(example):
 @gen_in_backend("none")
 @gen_needs_backend("lite")
 def testExamples():
-  for ex in ["crp-2d-demo.py", "venture-unit/hmc-demo.py"]:
+  for ex in ["crp-2d-demo.py", "hmc-demo.py"]:
     yield checkExample, ex
 
 def checkVentureExample(command):
