0e8ca4d0ac69e4660a0b1ae2f5ec2d6f99a5d492
Alexey Radul <axch@mit.edu>
2015-11-18 17:23:48 -0500

    Draft testing completeness of Debian dependency list in a Docker container.

---
+++ script/jenkins/venture-in-docker.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -eu
+
+# Compute the version that will be built (tail skips warnings setup.py emits).
+version=`python setup.py --version | tail -1`
+dist_file="dist/venture-$version.tar.gz"
+
+# Save the version in the sdist, b/c git describe will not be available.
+echo $version > VERSION
+
+# Build the distribution.
+python setup.py sdist
+
+# Prepare a Docker container
+
+# - Clear out any stale venture distributions
+rm -rf script/jenkins/debian-test-docker/dist/
+
+# - Make sure there is a venture distribution directory
+mkdir -p script/jenkins/debian-test-docker/dist/
+
+# - Put the distribution there
+cp "$dist_file" script/jenkins/debian-test-docker/dist/
+
+# - Make the distribution test scripts available to the container
+cp script/jenkins/check_built_sdist.sh script/jenkins/debian-test-docker/
+mkdir -p script/jenkins/debian-test-docker/tool/
+cp tool/check_capabilities.sh script/jenkins/debian-test-docker/tool/
+
+# - Actually build the container
+docker build -t "venture-debian-test" script/jenkins/debian-test-docker
+
+# Run the acceptance testing in it
+docker run -t "venture-debian-test" ./check_built_sdist.sh dist/ \
+    "${version%+*}" # Version without the +foo suffix
