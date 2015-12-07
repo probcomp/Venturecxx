4833af7e5c97289126fe223b50340a45ae747fb3
Alexey Radul <axch@mit.edu>
2015-12-07 16:51:07 -0500

    Flush now-unused fromPython entry point.

--- backend/new_cxx/inc/pyutils.h
+++ backend/new_cxx/inc/pyutils.h
@@ -25,7 +25,6 @@
 
 #include "types.h"
 
-VentureValuePtr fromPython(boost::python::object o);
 VentureValuePtr parseValueO(boost::python::object o); // Requires it to be a dict
 VentureValuePtr parseValue(boost::python::dict d);
 VentureValuePtr parseExpression(boost::python::object o);
