d4859e519eafe0b473b2e4461538ca8d67aaef40
Alexey Radul <axch@mit.edu>
2015-12-06 19:23:38 -0500

    The notion of references used here is now native (except that it hasn't been ported to Puma yet).

--- test/inference_quality/end_to_end/test_inc_eval.py
+++ test/inference_quality/end_to_end/test_inc_eval.py
@@ -21,6 +21,7 @@ from nose.plugins.attrib import attr
 
 from venture.test.stats import statisticalTest, reportKnownContinuous
 from venture.test.config import get_ripl, collectSamples, on_inf_prim
+from venture.test.config import broken_in
 
 ### Expressions
 
@@ -32,11 +33,6 @@ from venture.test.config import get_ripl, collectSamples, on_inf_prim
 ##             := [ref("op_name"), ... refs of arguments]
 
 
-## References
-def loadReferences(ripl):
-  ripl.assume("make_ref","(lambda (x) (lambda () x))")
-  ripl.assume("deref","(lambda (x) (x))")
-
 ## Environments
 ## { sym => ref }
 def loadEnvironments(ripl):
@@ -46,8 +42,8 @@ def loadEnvironments(ripl):
     (dict
       (array (quote bernoulli) (quote normal) (quote add)
              (quote mul) (quote branch))
-      (array (make_ref bernoulli) (make_ref normal) (make_ref add)
-             (make_ref mul) (make_ref branch)))))
+      (array (ref bernoulli) (ref normal) (ref add)
+             (ref mul) (ref branch)))))
 """)
 
   ripl.assume("extend_env","""
@@ -69,7 +65,8 @@ def loadIncrementalEvaluator(ripl):
 
   ripl.assume("incremental_venture_apply","""
 (lambda (op args)
-  (eval (pair op (map_list deref args)) (get_empty_environment)))""")
+  (eval (pair op (map_list (lambda (x) (deref x)) args))
+        (get_empty_environment)))""")
 
   ripl.assume("incremental_apply","""
   (lambda (operator operands)
@@ -86,19 +83,18 @@ def loadIncrementalEvaluator(ripl):
       (if (not (is_pair expr))
           expr
       (if (= (deref (lookup expr 0)) (quote lambda))
-          (pair (make_ref env) (rest expr))
+          (pair (ref env) (rest expr))
           ((lambda (operator operands)
              (if (is_pair operator)
                  (incremental_apply operator operands)
                  (incremental_venture_apply operator operands)))
            (incremental_eval (deref (lookup expr 0)) env)
-           (map_list (lambda (x) (make_ref (incremental_eval (deref x) env)))
+           (map_list (lambda (x) (ref (incremental_eval (deref x) env)))
                      (rest expr)))))))
 """)
 
 
 def loadAll(ripl):
-  loadReferences(ripl)
   loadEnvironments(ripl)
   loadIncrementalEvaluator(ripl)
   ripl.assume("map_list","""
@@ -119,6 +115,7 @@ def extractValue(d):
 
 
 @on_inf_prim("none")
+@broken_in("puma", "Need to port records to Puma for references to work.  Issue #224")
 def testIncrementalEvaluator1a():
   "Extremely basic test"
   ripl = get_ripl()
@@ -129,30 +126,27 @@ def testIncrementalEvaluator1a():
   eq_(ripl.report("pid"),1)
 
 @on_inf_prim("none")
+@broken_in("puma", "Need to port records to Puma for references to work.  Issue #224")
 def testIncrementalEvaluator1b():
   "Extremely basic test"
   ripl = get_ripl()
   loadAll(ripl)
-  ripl.assume("expr","(list (make_ref add) (make_ref 1) (make_ref 1))")
+  ripl.assume("expr","(list (ref add) (ref 1) (ref 1))")
   ripl.assume("env","(incremental_initial_environment)")
   ripl.predict("(incremental_eval expr env)",label="pid")
   eq_(ripl.report("pid"),2)
 
 @statisticalTest
+@broken_in("puma", "Need to port records to Puma for references to work.  Issue #224")
 def testIncrementalEvaluator1c():
   "Incremental version of micro/test_basic_stats.py:testBernoulli1"
   ripl = get_ripl()
   loadAll(ripl)
   ripl.assume("expr","""
-(list (make_ref branch)
-      (make_ref (list (make_ref bernoulli)
-                      (make_ref 0.3)))
-      (make_ref (list (make_ref normal)
-                      (make_ref 0.0)
-                      (make_ref 1.0)))
-      (make_ref (list (make_ref normal)
-                      (make_ref 10.0)
-                      (make_ref 1.0))))
+(list (ref branch)
+      (ref (list (ref bernoulli) (ref 0.3)))
+      (ref (list (ref normal) (ref 0.0) (ref 1.0)))
+      (ref (list (ref normal) (ref 10.0) (ref 1.0))))
 """)
   ripl.assume("env","(incremental_initial_environment)")
   ripl.predict("(incremental_eval expr env)",label="pid")
@@ -164,6 +158,7 @@ def testIncrementalEvaluator1c():
 
 @attr('slow')
 @on_inf_prim("mh")
+@broken_in("puma", "Need to port records to Puma for references to work.  Issue #224")
 def testIncrementalEvaluator2():
   """Difficult test. We make sure that it stumbles on the solution
 in a reasonable amount of time."""
@@ -181,8 +176,8 @@ in a reasonable amount of time."""
       (genLeaf)
       (if (flip 0.8)
           (genVar x)
-          (list (make_ref (genBinaryOp))
-                (make_ref (genExpr x)) (make_ref (genExpr x))))))
+          (list (ref (genBinaryOp))
+                (ref (genExpr x)) (ref (genExpr x))))))
 """)
 
   ripl.assume("noise","(gamma 5 .2)")
@@ -194,7 +189,7 @@ in a reasonable amount of time."""
     (incremental_eval expr
                       (extend_env (incremental_initial_environment)
                                   (list (quote x))
-                                  (list (make_ref y))))))
+                                  (list (ref y))))))
 """)
   ripl.assume("g","(lambda (z) (normal (f z) noise))")
 
