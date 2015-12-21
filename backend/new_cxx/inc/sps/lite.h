ddb92b54c006fd549abd260c929d068239f0d4b1
Alexey Radul <axch@mit.edu>
2015-12-21 14:31:41 -0500

    Whitespace.

--- backend/new_cxx/inc/sps/lite.h
+++ backend/new_cxx/inc/sps/lite.h
@@ -45,7 +45,8 @@ struct ForeignLitePSP : PSP
   void unincorporate(VentureValuePtr value,shared_ptr<Args> args) const;
 
   bool isRandom() const;
-  bool canAbsorb(ConcreteTrace * trace,ApplicationNode * appNode,Node * parentNode) const;
+  bool canAbsorb(ConcreteTrace * trace, ApplicationNode * appNode,
+                 Node * parentNode) const;
 
   bool childrenCanAAA() const;
   shared_ptr<LKernel> const getAAALKernel();
@@ -61,16 +62,20 @@ struct ForeignLitePSP : PSP
 struct ForeignLiteSPAux : SPAux
 {
   ForeignLiteSPAux(boost::python::object aux): aux(aux) {}
-  SPAux* copy_help(ForwardingMap* m) const { return new ForeignLiteSPAux(aux.attr("copy")()); }
+  SPAux* copy_help(ForwardingMap* m) const {
+    return new ForeignLiteSPAux(aux.attr("copy")()); }
   boost::python::object aux;
 };
 
 struct ForeignLiteLKernel : LKernel
 {
   ForeignLiteLKernel(boost::python::object lkernel): lkernel(lkernel) {}
-  VentureValuePtr forwardSimulate(Trace * trace,VentureValuePtr oldValue,shared_ptr<Args> args,gsl_rng * rng);
-  double forwardWeight(Trace * trace,VentureValuePtr newValue,VentureValuePtr oldValue,shared_ptr<Args> args);
-  double reverseWeight(Trace * trace,VentureValuePtr oldValue,shared_ptr<Args> args);
+  VentureValuePtr forwardSimulate(Trace * trace, VentureValuePtr oldValue,
+                                  shared_ptr<Args> args,gsl_rng * rng);
+  double forwardWeight(Trace * trace, VentureValuePtr newValue,
+                       VentureValuePtr oldValue,shared_ptr<Args> args);
+  double reverseWeight(Trace * trace, VentureValuePtr oldValue,
+                       shared_ptr<Args> args);
 
   boost::python::object lkernel;
 };
@@ -91,24 +96,32 @@ struct ForeignLiteLatentDB : LatentDB
 
 struct ForeignLiteRequest : VentureRequest
 {
-  ForeignLiteRequest(const vector<ESR> & esrs, const vector<shared_ptr<LSR> > & lsrs): VentureRequest(esrs, lsrs) {}
-  ForeignLiteRequest(const vector<shared_ptr<LSR> > & lsrs): VentureRequest(lsrs) {}
+  ForeignLiteRequest(const vector<ESR> & esrs,
+                     const vector<shared_ptr<LSR> > & lsrs):
+    VentureRequest(esrs, lsrs) {}
+  ForeignLiteRequest(const vector<shared_ptr<LSR> > & lsrs):
+    VentureRequest(lsrs) {}
   boost::python::dict toPython(Trace * trace) const;
 };
 
 struct ForeignLiteSP : SP
 {
   // TODO: requestPSP (needs requests to be stackable)
-  ForeignLiteSP(boost::python::object sp): SP(new ForeignLitePSP(sp.attr("requestPSP")),
-                                              new ForeignLitePSP(sp.attr("outputPSP"))),
-                                           sp(sp) {}
+  ForeignLiteSP(boost::python::object sp):
+    SP(new ForeignLitePSP(sp.attr("requestPSP")),
+       new ForeignLitePSP(sp.attr("outputPSP"))),
+    sp(sp) {}
 
   shared_ptr<LatentDB> constructLatentDB() const;
-  double simulateLatents(shared_ptr<SPAux> spaux,shared_ptr<LSR> lsr,bool shouldRestore,shared_ptr<LatentDB> latentDB,gsl_rng * rng) const;
-  double detachLatents(shared_ptr<SPAux> spaux,shared_ptr<LSR> lsr,shared_ptr<LatentDB> latentDB) const;
+  double simulateLatents(shared_ptr<SPAux> spaux, shared_ptr<LSR> lsr,
+                         bool shouldRestore, shared_ptr<LatentDB> latentDB,
+                         gsl_rng * rng) const;
+  double detachLatents(shared_ptr<SPAux> spaux, shared_ptr<LSR> lsr,
+                       shared_ptr<LatentDB> latentDB) const;
 
   bool hasAEKernel() const;
-  void AEInfer(shared_ptr<SPAux> spAux, shared_ptr<Args> args, gsl_rng * rng) const;
+  void AEInfer(shared_ptr<SPAux> spAux, shared_ptr<Args> args,
+               gsl_rng * rng) const;
 
   boost::python::dict toPython(Trace * trace, shared_ptr<SPAux> spAux) const;
 
