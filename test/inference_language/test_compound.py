690fae21ddd89bfcac3f78f364754b1d942b876e
Alexey Radul <axch@mit.edu>
2016-02-22 15:28:04 -0500

    Style: Spacing.

--- test/inference_language/test_compound.py
+++ test/inference_language/test_compound.py
@@ -37,24 +37,21 @@ def init_ripl(venChurch=None):
     ripl.assume("x", 1)
     return ripl
 
-
-# simple smoke tests
+# Simple smoke test
 
 @broken_in("puma", "Does not have refs: Issue #224.")
 def test_compound_assume_smoke():
-
     smoke_prog ="""
-
     [assume a_ref (ref (uniform_discrete 2 3))]
     [assume b_ref (ref (uniform_discrete 3 4))]
-    (assume_values (a b) (list  a_ref b_ref))
+    (assume_values (a b) (list a_ref b_ref))
 
     [assume l (list a_ref b_ref)]
-    (assume_values (c  d ) l)
+    (assume_values (c d) l)
 
-    (assume_values ( u )  (list (ref (uniform_discrete 20 21))))
-
+    (assume_values (u) (list (ref (uniform_discrete 20 21))))
     """
+
     ripl = get_ripl()
     ripl.execute_program(smoke_prog)
 
@@ -66,20 +63,17 @@ def test_compound_assume_smoke():
 
     assert ripl.sample("u") == 20, "compound assume does not work for a one-element-compound"
 
+# Testing observations
 
-# testing observations
 @broken_in("puma", "Does not have refs: Issue #224.")
 def test_compound_assume_observations():
-
     obs_prog ="""
-
     [assume a_ref (ref (normal 0 1))]
     [assume b_ref (ref (normal 0 1))]
-    [assume l (list  a_ref b_ref)]
+    [assume l (list a_ref b_ref)]
     (assume_values (a b) l)
-
-
     """
+
     ripl = get_ripl()
 
     ripl.execute_program(obs_prog)
@@ -88,8 +82,8 @@ def test_compound_assume_observations():
 
     assert ripl.sample("(deref a_ref)") == 1, "simple a_ref is not observed"
     assert ripl.sample("a") == 1, "first element compund is not observed"
-    assert ripl.sample("(deref (first  l ) )") == 1, "first compound list item is not observed"
-    assert ripl.sample("(deref (second l ) )") != 1, "confused second and first compound element"
+    assert ripl.sample("(deref (first  l))") == 1, "first compound list item is not observed"
+    assert ripl.sample("(deref (second l))") != 1, "confused second and first compound element"
     assert ripl.sample("(deref b_ref)") != 1, "confused second and first compound element"
     assert ripl.sample("b") != 1, "second element compound is confused with first"
 
@@ -97,25 +91,23 @@ def test_compound_assume_observations():
 
     assert ripl.sample("b") == 2, "second element compound is not observed"
     assert ripl.sample("(deref a_ref)") == 1, "second observation made the first incorrect"
-    assert ripl.sample("(deref (first  l) )") == 1, "second observation made the first incorrect"
-    assert ripl.sample("(deref (second l ) )") == 2, "deref of second list item is not observed"
+    assert ripl.sample("(deref (first  l))") == 1, "second observation made the first incorrect"
+    assert ripl.sample("(deref (second l))") == 2, "deref of second list item is not observed"
     assert ripl.sample("(deref b_ref)") == 2, "deref of second ref is not observed"
 
+# Testing inference
 
-# testing inference
 @broken_in("puma", "Does neither support assume_values nor GPs yet")
 @in_backend("lite")
 def test_compound_assume_inf_happening():
     inf_test_prog ="""
-
-    [assume a_ref (tag (quote a_scope ) 0 (ref (normal 0 10)))]
-    [assume b_ref (tag (quote b_scope ) 0 (ref (normal -10 10)))]
-    [assume l (list  a_ref b_ref)]
+    [assume a_ref (tag (quote a_scope) 0 (ref (normal 0 10)))]
+    [assume b_ref (tag (quote b_scope) 0 (ref (normal -10 10)))]
+    [assume l (list a_ref b_ref)]
     (assume_values (a b) l)
 
-    [assume obs_1 (lambda ( )  (normal a 1))]
-    [assume obs_2 (lambda ( )  (normal b 1))]
-
+    [assume obs_1 (lambda () (normal a 1))]
+    [assume obs_2 (lambda () (normal b 1))]
     """
 
     ripl = init_ripl(venChurch=True)
@@ -127,34 +119,29 @@ def test_compound_assume_inf_happening():
     for _ in range(20):
         ripl.observe("(obs_1)", np.random.normal(5, 0.1))
 
-    ripl.infer("(mh (quote a_scope ) 0 100)")
-
+    ripl.infer("(mh (quote a_scope) 0 100)")
 
     assert ripl.sample("b") == previous_value, "inferred to wrong part of the compound"
 
     for _ in range(20):
         ripl.observe("(obs_2)", np.random.normal(-15, 0.1))
 
-    ripl.infer("(mh (quote b_scope ) 0 100)")
-
+    ripl.infer("(mh (quote b_scope) 0 100)")
 
     assert ripl.sample("b") != previous_value, "inferred for second part didn't work"
-
 
 @broken_in("puma", "Does neither support assume_values nor GPs yet")
 @statisticalTest
 @in_backend("lite")
 def test_compound_assume_inf_first_element():
     inf_test_prog ="""
-
-    [assume a_ref (tag (quote a_scope ) 0 (ref (normal 0 10)))]
-    [assume b_ref (tag (quote b_scope ) 0 (ref (normal -10 10)))]
-    [assume l (list  a_ref b_ref)]
+    [assume a_ref (tag (quote a_scope) 0 (ref (normal 0 10)))]
+    [assume b_ref (tag (quote b_scope) 0 (ref (normal -10 10)))]
+    [assume l (list a_ref b_ref)]
     (assume_values (a b) l)
 
-    [assume obs_1 (lambda ( )  (normal a 1))]
-    [assume obs_2 (lambda ( )  (normal b 1))]
-
+    [assume obs_1 (lambda () (normal a 1))]
+    [assume obs_2 (lambda () (normal b 1))]
     """
 
     ripl = init_ripl(venChurch=True)
@@ -164,7 +151,7 @@ def test_compound_assume_inf_first_element():
     for _ in range(20):
         ripl.observe("(obs_1)", np.random.normal(5, 1))
 
-    ripl.infer("(mh (quote a_scope ) 0 100)")
+    ripl.infer("(mh (quote a_scope) 0 100)")
 
     # just read test/config.py - this should use collectSamples
     post_samples = [ripl.sample("(obs_1)") for _ in range(30)]
@@ -176,15 +163,13 @@ def test_compound_assume_inf_first_element():
 @in_backend("lite")
 def test_compound_assume_inf_second_element():
     inf_test_prog ="""
-
-    [assume a_ref (tag (quote a_scope ) 0 (ref (normal 0 10)))]
-    [assume b_ref (tag (quote b_scope ) 0 (ref (normal -10 10)))]
-    [assume l (list  a_ref b_ref)]
+    [assume a_ref (tag (quote a_scope) 0 (ref (normal 0 10)))]
+    [assume b_ref (tag (quote b_scope) 0 (ref (normal -10 10)))]
+    [assume l (list a_ref b_ref)]
     (assume_values (a b) l)
 
-    [assume obs_1 (lambda ( )  (normal a 1))]
-    [assume obs_2 (lambda ( )  (normal b 1))]
-
+    [assume obs_1 (lambda () (normal a 1))]
+    [assume obs_2 (lambda () (normal b 1))]
     """
 
     ripl = init_ripl(venChurch=True)
@@ -194,12 +179,12 @@ def test_compound_assume_inf_second_element():
     for _ in range(20):
         ripl.observe("(obs_1)", np.random.normal(5, 0.1))
 
-    ripl.infer("(mh (quote a_scope ) 0 100)")
+    ripl.infer("(mh (quote a_scope) 0 100)")
 
     for _ in range(20):
         ripl.observe("(obs_2)", np.random.normal(-15, 0.1))
 
-    ripl.infer("(mh (quote b_scope ) 0 100)")
+    ripl.infer("(mh (quote b_scope) 0 100)")
 
     # just read test/config.py - this should use collectSamples
     post_samples = [ripl.sample("(obs_2)") for _ in range(30)]
