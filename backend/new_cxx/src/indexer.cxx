bfd159bd4729da7a9a6e5766f5b595f49ba16214
Alexey Radul <axch@mit.edu>
2016-04-04 17:09:43 -0400

    Break lines; outdent else clauses.

--- backend/new_cxx/src/indexer.cxx
+++ backend/new_cxx/src/indexer.cxx
@@ -22,49 +22,68 @@
 #include "concrete_trace.h"
 
 // TODO add assert from lite
-ScaffoldIndexer::ScaffoldIndexer(ScopeID scope, BlockID block): scope(scope), block(block) {}
-ScaffoldIndexer::ScaffoldIndexer(ScopeID scope, BlockID block, BlockID minBlock, BlockID maxBlock): scope(scope), block(block), minBlock(minBlock), maxBlock(maxBlock) {}
+ScaffoldIndexer::ScaffoldIndexer(ScopeID scope, BlockID block):
+  scope(scope), block(block) {}
 
-boost::shared_ptr<Scaffold> ScaffoldIndexer::sampleIndex(ConcreteTrace * trace) const
+ScaffoldIndexer::ScaffoldIndexer(ScopeID scope, BlockID block,
+                                 BlockID minBlock, BlockID maxBlock):
+  scope(scope), block(block), minBlock(minBlock), maxBlock(maxBlock) {}
+
+boost::shared_ptr<Scaffold> ScaffoldIndexer::sampleIndex(
+    ConcreteTrace * trace) const
 {
   if (block->hasSymbol() && block->getSymbol() == "one")
-    {
-      BlockID actualBlock = trace->sampleBlock(scope);
-      vector<set<Node*> > setsOfPNodes;
-      setsOfPNodes.push_back(trace->getNodesInBlock(scope, actualBlock));
-      boost::shared_ptr<Scaffold> scaffold = constructScaffold(trace, setsOfPNodes, false);
-      //cout << scaffold->showSizes() << endl;
-      return scaffold;
-    }
+  {
+    BlockID actualBlock = trace->sampleBlock(scope);
+    vector<set<Node*> > setsOfPNodes;
+    setsOfPNodes.push_back(trace->getNodesInBlock(scope, actualBlock));
+    boost::shared_ptr<Scaffold> scaffold =
+      constructScaffold(trace, setsOfPNodes, false);
+    //cout << scaffold->showSizes() << endl;
+    return scaffold;
+  }
   else if (block->hasSymbol() && block->getSymbol() == "all")
-    {
-      vector<set<Node*> > setsOfPNodes;
-      setsOfPNodes.push_back(trace->getAllNodesInScope(scope));
-      return constructScaffold(trace, setsOfPNodes, false);
-    }
+  {
+    vector<set<Node*> > setsOfPNodes;
+    setsOfPNodes.push_back(trace->getAllNodesInScope(scope));
+    return constructScaffold(trace, setsOfPNodes, false);
+  }
   else if (block->hasSymbol() && block->getSymbol() == "ordered")
-    {
-      return constructScaffold(trace, trace->getOrderedSetsInScope(scope), false);
-    }
+  {
+    return constructScaffold(trace, trace->getOrderedSetsInScope(scope),
+                             false);
+  }
   else if (block->hasSymbol() && block->getSymbol() == "ordered_range")
-    {
-      return constructScaffold(trace, trace->getOrderedSetsInScopeAndRange(scope, minBlock, maxBlock), false);
-    }
+  {
+    return constructScaffold(
+        trace, trace->getOrderedSetsInScopeAndRange(scope, minBlock, maxBlock),
+        false);
+  }
   else
-    {
-      vector<set<Node*> > setsOfPNodes(1, trace->getNodesInBlock(scope, block));
-      return constructScaffold(trace, setsOfPNodes, false);
-    }
+  {
+    vector<set<Node*> > setsOfPNodes(1, trace->getNodesInBlock(scope, block));
+    return constructScaffold(trace, setsOfPNodes, false);
+  }
 }
 
 
-double ScaffoldIndexer::logDensityOfIndex(Trace * trace, boost::shared_ptr<Scaffold> scaffold) const
+double ScaffoldIndexer::logDensityOfIndex(
+    Trace * trace, boost::shared_ptr<Scaffold> scaffold) const
 {
-  if (dynamic_pointer_cast<VentureSymbol>(block) && block->getSymbol() == "one")
+  if (dynamic_pointer_cast<VentureSymbol>(block) &&
+      block->getSymbol() == "one")
   {
     return trace->logDensityOfBlock(scope);
   }
-  else if (dynamic_pointer_cast<VentureSymbol>(block) && block->getSymbol() == "all") { return 0; }
-  else if (dynamic_pointer_cast<VentureSymbol>(block) && block->getSymbol() == "ordered") { return 0; }
+  else if (dynamic_pointer_cast<VentureSymbol>(block) &&
+           block->getSymbol() == "all")
+  {
+    return 0;
+  }
+  else if (dynamic_pointer_cast<VentureSymbol>(block) &&
+           block->getSymbol() == "ordered")
+  {
+    return 0;
+  }
   else { return 0; }
 }
