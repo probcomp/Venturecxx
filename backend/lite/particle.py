0a2737fcf7fc0ac2c37db30253a16fe8a7e665ce
Alexey Radul <axch@mit.edu>
2016-04-05 08:08:35 -0400

    Break long lines.

--- backend/lite/particle.py
+++ backend/lite/particle.py
@@ -50,7 +50,8 @@ class Particle(Trace):
     self.discardedAAAMakerNodes = particle.discardedAAAMakerNodes
 
     # (2) Maps to things that change outside of particle methods
-    self.madeSPAuxs = { node : spaux.copy() for node, spaux in particle.madeSPAuxs.iteritems() }
+    self.madeSPAuxs = { node : spaux.copy()
+                        for node, spaux in particle.madeSPAuxs.iteritems() }
 
   def initFromTrace(self, trace):
     self.base = trace
@@ -64,7 +65,8 @@ class Particle(Trace):
     self.madeSPs = PMap() # PMap Node SP
 
     self.scopes = PMap()  # PMap scopeid (PMap blockid (PSet Node))
-    self.esrParents = PMap() # PMap Node [Node] # mutable list ok b/c only touched by one particle
+    # mutable list ok here b/c only touched by one particle
+    self.esrParents = PMap() # PMap Node [Node]
     self.numRequests = PMap() # PMap Node Int
     self.regenCounts = PMap() # PMap Node int
     self.newMadeSPFamilies = PMap() # PMap Node (PMap id Node)
@@ -92,9 +94,15 @@ class Particle(Trace):
   def registerRandomChoiceInScope(self, scope, block, node):
     assert block is not None
     (scope, block) = self._normalizeEvaluatedScopeAndBlock(scope, block)
-    if not scope in self.scopes: self.scopes = self.scopes.insert(scope, PMap())
-    if not block in self.scopes.lookup(scope): self.scopes = self.scopes.adjust(scope, lambda blocks: blocks.insert(block, PSet()))
-    self.scopes = self.scopes.adjust(scope, lambda blocks: blocks.adjust(block, lambda pnodes: pnodes.insert(node)))
+    if not scope in self.scopes:
+      self.scopes = self.scopes.insert(scope, PMap())
+    if not block in self.scopes.lookup(scope):
+      def ins_b(blocks):
+        return blocks.insert(block, PSet())
+      self.scopes = self.scopes.adjust(scope, ins_b)
+    def ins_n(blocks):
+      return blocks.adjust(block, lambda pnodes: pnodes.insert(node))
+    self.scopes = self.scopes.adjust(scope, ins_n)
 
   def unregisterRandomChoiceInScope(self, scope, block, node): assert False
 
@@ -131,7 +139,8 @@ class Particle(Trace):
 
   def appendEsrParentAt(self, node, parent):
     assert not self.base.esrParentsAt(node)
-    if not node in self.esrParents: self.esrParents = self.esrParents.insert(node, [])
+    if not node in self.esrParents:
+      self.esrParents = self.esrParents.insert(node, [])
     self.esrParents.lookup(node).append(parent)
 
   def regenCountAt(self, scaffold, node):
@@ -140,11 +149,14 @@ class Particle(Trace):
     else: return self.base.regenCountAt(scaffold, node)
 
   def incRegenCountAt(self, scaffold, node):
-    if not node in self.regenCounts: self.regenCounts = self.regenCounts.insert(node, 0)
+    if not node in self.regenCounts:
+      self.regenCounts = self.regenCounts.insert(node, 0)
     self.regenCounts = self.regenCounts.adjust(node, lambda rc: rc + 1)
 
   def incRequestsAt(self, node):
-    if not node in self.numRequests: self.numRequests = self.numRequests.insert(node, self.base.numRequestsAt(node))
+    if not node in self.numRequests:
+      base_num_requests = self.base.numRequestsAt(node)
+      self.numRequests = self.numRequests.insert(node, base_num_requests)
     self.numRequests = self.numRequests.adjust(node, lambda nr: nr + 1)
 
   def childrenAt(self, node):
@@ -154,14 +166,20 @@ class Particle(Trace):
       return self.base.childrenAt(node)
 
   def addChildAt(self, node, child):
-    if not node in self.newChildren: self.newChildren = self.newChildren.insert(node, PSet())
-    self.newChildren = self.newChildren.adjust(node, lambda children: children.insert(child))
+    if not node in self.newChildren:
+      self.newChildren = self.newChildren.insert(node, PSet())
+    def ins(children):
+      return children.insert(child)
+    self.newChildren = self.newChildren.adjust(node, ins)
 
   def discardAAAMadeSPAuxAt(self, node):
     self.discardedAAAMakerNodes = self.discardedAAAMakerNodes.insert(node)
 
   def getAAAMadeSPAuxAt(self, node):
-    return None if node in self.discardedAAAMakerNodes else self.base.getAAAMadeSPAuxAt(node)
+    if node in self.discardedAAAMakerNodes:
+      return None
+    else:
+      return self.base.getAAAMadeSPAuxAt(node)
 
 ### SPFamilies
 
@@ -181,12 +199,16 @@ class Particle(Trace):
 
   def registerFamilyAt(self, node, esrId, esrParent):
     makerNode = self.spRefAt(node).makerNode
-    if not makerNode in self.newMadeSPFamilies: self.newMadeSPFamilies = self.newMadeSPFamilies.insert(makerNode, PMap())
-    self.newMadeSPFamilies = self.newMadeSPFamilies.adjust(makerNode, lambda ids: ids.insert(esrId, esrParent))
+    if not makerNode in self.newMadeSPFamilies:
+      self.newMadeSPFamilies = self.newMadeSPFamilies.insert(makerNode, PMap())
+    def ins(ids):
+      return ids.insert(esrId, esrParent)
+    self.newMadeSPFamilies = self.newMadeSPFamilies.adjust(makerNode, ins)
 
 
   def madeSPFamilyAt(self, node, esrId):
-    if node in self.newMadeSPFamilies and esrId in self.newMadeSPFamilies.lookup(node):
+    if node in self.newMadeSPFamilies and \
+       esrId in self.newMadeSPFamilies.lookup(node):
       return self.newMadeSPFamilies.lookup(node).lookup(esrId)
     else:
       return self.base.madeSPFamilyAt(node, esrId)
@@ -211,7 +233,8 @@ class Particle(Trace):
 
 ### Miscellaneous bookkeeping
   def numBlocksInScope(self, scope):
-    if scope != "default": return len(self.scopes.lookup(scope)) + self.base.numBlocksInScope(scope)
+    if scope != "default":
+      return len(self.scopes.lookup(scope)) + self.base.numBlocksInScope(scope)
     actualUnconstrainedChoices = self.base.rcs.copy()
     for node in self.rcs: actualUnconstrainedChoices.add(node)
     for node in self.ccs: actualUnconstrainedChoices.remove(node)
@@ -219,16 +242,19 @@ class Particle(Trace):
 
 ### Commit
   def commit(self):
-    # note that we do not call registerRandomChoice() because it in turn calls registerRandomChoiceInScope()
+    # note that we do not call registerRandomChoice() because it in
+    # turn calls registerRandomChoiceInScope()
     for node in self.rcs: self.base.rcs.add(node)
 
     # this iteration includes "default"
     for (scope, blocks) in self.scopes.iteritems():
       for (block, pnodes) in blocks.iteritems():
         for pnode in pnodes:
-          self.base.registerRandomChoiceInScope(scope, block, pnode, unboxed=True)
+          self.base.registerRandomChoiceInScope(scope, block, pnode,
+                                                unboxed=True)
 
-    # note that we do not call registerConstrainedChoice() because it in turn calls unregisterRandomChoice()
+    # note that we do not call registerConstrainedChoice() because it
+    # in turn calls unregisterRandomChoice()
     for node in self.ccs: self.base.registerConstrainedChoice(node)
 
     for node in self.aes: self.base.registerAEKernel(node)
@@ -241,12 +267,17 @@ class Particle(Trace):
 
 
 
-    for (node, esrParents) in self.esrParents.iteritems(): self.base.setEsrParentsAt(node, esrParents)
-    for (node, numRequests) in self.numRequests.iteritems(): self.base.setNumRequestsAt(node, numRequests)
-    for (node, newMadeSPFamilies) in self.newMadeSPFamilies.iteritems(): self.base.addNewMadeSPFamilies(node, newMadeSPFamilies)
-    for (node, newChildren) in self.newChildren.iteritems(): self.base.addNewChildren(node, newChildren)
+    for (node, esrParents) in self.esrParents.iteritems():
+      self.base.setEsrParentsAt(node, esrParents)
+    for (node, numRequests) in self.numRequests.iteritems():
+      self.base.setNumRequestsAt(node, numRequests)
+    for (node, newMadeSPFamilies) in self.newMadeSPFamilies.iteritems():
+      self.base.addNewMadeSPFamilies(node, newMadeSPFamilies)
+    for (node, newChildren) in self.newChildren.iteritems():
+      self.base.addNewChildren(node, newChildren)
 
-    for (node, spaux) in self.madeSPAuxs.iteritems(): self.base.setMadeSPAuxAt(node, spaux)
+    for (node, spaux) in self.madeSPAuxs.iteritems():
+      self.base.setMadeSPAuxAt(node, spaux)
 
   # untested
   def transferRegenCounts(self, scaffold):
@@ -256,13 +287,29 @@ class Particle(Trace):
 
 
 ################### Methods that should never be called on particles
-  def registerAAAMadeSPAuxAt(self, node, aux): raise Exception("Should not be called on a particle")
-  def unregisterFamilyAt(self, node, esrId): raise Exception("Should not be called on a particle")
-  def popEsrParentAt(self, node): raise Exception("Should not be called on a particle")
-  def removeChildAt(self, node, child): raise Exception("Should not be called on a particle")
-  def decRequestsAt(self, node): raise Exception("Should not be called on a particle")
-  def unregisterAEKernel(self, node): raise Exception("Should not be called on a particle")
-  def unregisterConstrainedChoice(self, node): raise Exception("Should not be called on a particle")
-  def decRegenCountAt(self, scaffold, node): raise Exception("Should never be called on a particle")
-  def numRequestsAt(self, node): raise Exception("Should not be called on a particle")
+  def registerAAAMadeSPAuxAt(self, node, aux):
+    raise Exception("Should not be called on a particle")
 
+  def unregisterFamilyAt(self, node, esrId):
+    raise Exception("Should not be called on a particle")
+
+  def popEsrParentAt(self, node):
+    raise Exception("Should not be called on a particle")
+
+  def removeChildAt(self, node, child):
+    raise Exception("Should not be called on a particle")
+
+  def decRequestsAt(self, node):
+    raise Exception("Should not be called on a particle")
+
+  def unregisterAEKernel(self, node):
+    raise Exception("Should not be called on a particle")
+
+  def unregisterConstrainedChoice(self, node):
+    raise Exception("Should not be called on a particle")
+
+  def decRegenCountAt(self, scaffold, node):
+    raise Exception("Should never be called on a particle")
+
+  def numRequestsAt(self, node):
+    raise Exception("Should not be called on a particle")
