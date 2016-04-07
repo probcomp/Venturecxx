eca3d01cde77e94f4b4c9c9a44ccaee36b2ca84a
Anthony Lu <lua@mit.edu>
2016-04-07 18:25:42 -0400

    Add a mem re-entrancy guard.
    This guards against programs like
    
      assume f = mem(proc (x) {
        if (flip()) { 1 }
        else { 1 + f(x) }
      }
    
    where a memoized procedure calls itself with the same arguments,
    producing inconsistent results that overwrite each other in the mem table.
    
    The existing Lite and Puma backends guard against this by checking if a
    mem table entry appeared during evaluation of the procedure call. This
    detects the consistency bug, but only after the recursion starts
    unwinding.
    
    Here, we instead populate the mem table entry with a sentinel value, and
    check for the sentinel on entry to the procedure (rather than checking
    on exit). This leads to similar results, but fails slightly faster
    because the recursion doesn't happen in the first place.

--- backend/mite/sps/mem.py
+++ backend/mite/sps/mem.py
@@ -1,8 +1,14 @@
 from venture.lite.env import VentureEnvironment
+from venture.lite.exception import VentureError
 
 from venture.mite.sp import RequestReferenceSP
 from venture.mite.sp import SimulationSP
 from venture.mite.sp_registry import registerBuiltinSP
+
+# mem re-entrancy sentinel, guaranteed to be a unique object
+# used to indicate that a mem table entry is now being computed.
+
+sentinel = object()
 
 class MemoizedSP(RequestReferenceSP):
   def __init__(self, spNode):
@@ -14,10 +20,20 @@ class MemoizedSP(RequestReferenceSP):
     operands = tuple(args.operandValues())
     ref = self.memTable.get(operands)
     if ref is None:
-      exp = ["sp"] + [["quote", operand] for operand in operands]
-      env = VentureEnvironment(None, ["sp"], [self.spNode])
-      ref = args.newRequest(exp, env)
+      self.memTable[operands] = sentinel
+      try:
+        exp = ["sp"] + [["quote", operand] for operand in operands]
+        env = VentureEnvironment(None, ["sp"], [self.spNode])
+        ref = args.newRequest(exp, env)
+      finally:
+        # Remove the sentinel, even if the evaluation threw an error.
+        assert self.memTable[operands] is sentinel
+        del self.memTable[operands]
       self.memTable[operands] = ref
+    elif ref is sentinel:
+      # This can only happen if a recursive memoized function
+      # called itself with the same arguments.
+      raise VentureError("Recursive mem argument loop detected.")
     else:
       args.shareRequest(ref)
     return ref
