cc5b8e6909ce0a4be9110d5c7ff748148ed93b5f
Alexey Radul <axch@mit.edu>
2016-04-18 16:32:01 -0400

    Coding line goes at the top of the file.

--- python/lib/sivm/venture_sivm.py
+++ python/lib/sivm/venture_sivm.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Copyright (c) 2013, 2014, 2015, 2016 MIT Probabilistic Computing Project.
 #
 # This file is part of Venture.
@@ -14,9 +16,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Venture.  If not, see <http://www.gnu.org/licenses/>.
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import copy
 import cStringIO as StringIO
