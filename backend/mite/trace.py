c815e88b8142867394252ee642d4e7b425b82ba3
Anthony Lu <lua@mit.edu>
2016-04-26 00:52:40 -0400

    Make evalFamily and friends into methods on EvalContext

--- backend/mite/trace.py
+++ backend/mite/trace.py
@@ -9,9 +9,7 @@ from venture.lite.trace import Trace as LiteTrace
 
 from venture.mite.builtin import builtInSPs
 from venture.mite.builtin import builtInValues
-from venture.mite.evaluator import evalFamily, unevalFamily, processMadeSP
-from venture.mite.evaluator import constrain, unconstrain
-from venture.mite.evaluator import restore
+from venture.mite.evaluator import EvalContext, RestoreContext
 from venture.mite.sp import Args
 
 class Trace(LiteTrace):
@@ -28,7 +26,7 @@ class Trace(LiteTrace):
 
   def bindPrimitiveSP(self, name, sp):
     spNode = self.createConstantNode(None, sp)
-    processMadeSP(self, spNode)
+    EvalContext(self).processMadeSP(spNode)
     self.globalEnv.addBinding(name, spNode)
 
   ## Internal interface to eval/regen
@@ -41,8 +39,8 @@ class Trace(LiteTrace):
       self.addChildAt(operandNode, outputNode)
     return outputNode
 
-  def argsAt(self, node):
-    return Args(self, node)
+  def argsAt(self, node, context=None):
+    return Args(self, node, context=context)
 
   def madeSPAt(self, node):
     # SPs and SPRecords are the same now.
@@ -61,16 +59,15 @@ class Trace(LiteTrace):
 
   def eval(self, id, exp):
     assert id not in self.families
-    (w, self.families[id]) = evalFamily(
-      EvalContext(self), Address(List(id)),
-      self.unboxExpression(exp), self.globalEnv)
+    (w, self.families[id]) = EvalContext(self).evalFamily(
+      Address(List(id)), self.unboxExpression(exp), self.globalEnv)
     # forward evaluation should not produce a weight
     # (is this always true?)
     assert w == 0
 
   def uneval(self, id):
     assert id in self.families
-    w = unevalFamily(EvalContext(self), self.families[id])
+    w = EvalContext(self).unevalFamily(self.families[id])
     assert w == 0
     del self.families[id]
 
@@ -83,7 +80,7 @@ class Trace(LiteTrace):
       # scaffold.lkernels[appNode] = DeterministicLKernel(self.pspAt(appNode), val)
       # xiWeight = regenAndAttach(self, scaffold, False, OmegaDB(), {})
       node.observe(val)
-      weight += constrain(EvalContext(self), node, node.observedValue)
+      weight += EvalContext(self).constrain(node, node.observedValue)
     self.unpropagatedObservations.clear()
     if not math.isinf(weight) and not math.isnan(weight):
       return weight
@@ -99,13 +96,13 @@ class Trace(LiteTrace):
     for node, val in self.unpropagatedObservations.iteritems():
       # appNode = self.getConstrainableNode(node)
       node.observe(val)
-      constrain(EvalContext(self), node, node.observedValue)
+      EvalContext(self).constrain(node, node.observedValue)
     self.unpropagatedObservations.clear()
 
   def unobserve(self, id):
     node = self.families[id]
     if node.isObservation:
-      weight = unconstrain(EvalContext(self), node)
+      weight = EvalContext(self).unconstrain(node)
       node.isObservation = False
     else:
       assert node in self.unpropagatedObservations
@@ -131,8 +128,8 @@ class Trace(LiteTrace):
       for id in reversed(sorted(self.families)):
         node = self.families[id]
         if node.isObservation:
-          weight += unconstrain(RestoreContext(self, rhoDB), self.families[id])
-        unevalFamily(RestoreContext(self, rhoDB), node)
+          weight += RestoreContext(self, rhoDB).unconstrain(self.families[id])
+        RestoreContext(self, rhoDB).unevalFamily(node)
       return weight, rhoDB
     print 'detach', scaffold
     return 0, None
@@ -144,9 +141,9 @@ class Trace(LiteTrace):
       rhoDB = OmegaDB()
       for id in sorted(self.families):
         node = self.families[id]
-        restore(EvalContext(self), node)
+        EvalContext(self).restore(node)
         if node.isObservation:
-          weight += constrain(RestoreContext(self, rhoDB), node, node.observedValue)
+          weight += RestoreContext(self, rhoDB).constrain(node, node.observedValue)
       return weight
     print 'regen', scaffold
     return 0
@@ -156,9 +153,9 @@ class Trace(LiteTrace):
       weight = 0
       for id in sorted(self.families):
         node = self.families[id]
-        restore(RestoreContext(self, rhoDB), node)
+        RestoreContext(self, rhoDB).restore(node)
         if node.isObservation:
-          weight += constrain(RestoreContext(self, rhoDB), node, node.observedValue)
+          weight += RestoreContext(self, rhoDB).constrain(node, node.observedValue)
       return weight
     print 'restore', scaffold, rhoDB
     return 0
@@ -183,112 +180,14 @@ class Trace(LiteTrace):
   def unevalAndExtract(self, id, db):
     # leaves trace in an inconsistent state. use restore afterward
     assert id in self.families
-    unevalFamily(RestoreContext(self, db), self.families[id])
+    RestoreContext(self, db).unevalFamily(self.families[id])
 
   def restore(self, id, db):
     assert id in self.families
-    restore(RestoreContext(self, db), self.families[id])
+    RestoreContext(self, db).restore(self.families[id])
 
   def evalAndRestore(self, id, exp, db):
     assert id not in self.families
-    (w, self.families[id]) = evalFamily(
-      RestoreContext(self, db), Address(List(id)),
-      self.unboxExpression(exp), self.globalEnv)
+    (w, self.families[id]) = RestoreContext(self, db).evalFamily(
+      Address(List(id)), self.unboxExpression(exp), self.globalEnv)
     assert w == 0
-
-class EvalContext(object):
-  def __init__(self, trace):
-    self.trace = trace
-
-  def __getattr__(self, name):
-    # proxy the trace interface to eval/regen
-    if name in [
-        'createConstantNode', 'createLookupNode', 'createApplicationNodes',
-        'spAt', 'argsAt', 'valueAt', 'setValueAt',
-        'madeSPRecordAt', 'setMadeSPRecordAt',
-        'parentsAt', 'childrenAt',
-        'disconnectLookup', 'reconnectLookup']:
-      return getattr(self.trace, name)
-    else:
-      return object.__getattribute__(self, name)
-
-  def argsAt(self, node):
-    return Args(self.trace, node, context=self)
-
-  def applyCall(self, sp, args):
-    return sp.apply(args)
-
-  def unapplyCall(self, sp, args):
-    return sp.unapply(args)
-
-  ## Request interface to SPs
-
-  def newRequest(self, requester, raddr, exp, env):
-    if hasattr(raddr, 'address'):
-      raddr = raddr.address.last
-    address = requester.address.request(List((
-      self.trace.spRefAt(requester).makerNode.address.last, raddr)))
-    # TODO where to put w?
-    (w, requested) = evalFamily(self, address, exp, env)
-    assert w == 0
-    assert not self.trace.containsSPFamilyAt(requester, raddr), \
-      "Tried to make new request at existing address."
-    self.trace.registerFamilyAt(requester, raddr, requested)
-    self.trace.incRequestsAt(requested)
-    return raddr
-
-  def incRequest(self, requester, raddr):
-    requested = self.trace.spFamilyAt(requester, raddr)
-    self.trace.incRequestsAt(requested)
-    return raddr
-
-  def decRequest(self, requester, raddr):
-    requested = self.trace.spFamilyAt(requester, raddr)
-    self.trace.decRequestsAt(requested)
-    if self.trace.numRequestsAt(requested) == 0:
-      self.trace.unregisterFamilyAt(requester, raddr)
-      # TODO where to put w?
-      w = unevalFamily(self, requested)
-      assert w == 0
-
-  def hasRequest(self, requester, raddr):
-    return self.trace.containsSPFamilyAt(requester, raddr)
-
-  def constrainRequest(self, requester, raddr, value):
-    requested = self.trace.spFamilyAt(requester, raddr)
-    return constrain(self, requested, value, child=requester)
-
-  def unconstrainRequest(self, requester, raddr):
-    requested = self.trace.spFamilyAt(requester, raddr)
-    return unconstrain(self, requested, child=requester)
-
-  def requestedValue(self, requester, raddr):
-    requested = self.trace.spFamilyAt(requester, raddr)
-    return self.trace.valueAt(requested)
-
-  def setState(self, _node, _value):
-    pass
-
-  def getState(self, _node):
-    assert False, "Cannot restore outside a regeneration context"
-
-from collections import Iterable
-def normalize(address):
-  if isinstance(address, Iterable):
-    return tuple(normalize(part) for part in address)
-  else:
-    return address
-
-class RestoreContext(EvalContext):
-  def __init__(self, trace, omegaDB):
-    super(RestoreContext, self).__init__(trace)
-    self.omegaDB = omegaDB
-
-  def applyCall(self, sp, args):
-    return sp.restore(args)
-
-  def setState(self, node, value):
-    self.omegaDB.extractValue(normalize(node.address.last), value)
-
-  def getState(self, node):
-    return self.omegaDB.getValue(normalize(node.address.last))
