835692c152b3f16e45262c4aa752520b35f803cf
Anthony Lu <lua@mit.edu>
2016-04-29 11:43:51 -0400

    WIP: buggy dpmem constrain

--- backend/mite/sps/dpmem.py
+++ backend/mite/sps/dpmem.py
@@ -1,7 +1,8 @@
+import math
 import random
 
 from venture.lite.env import VentureEnvironment
-from venture.lite.utils import simulateCategorical
+from venture.lite.utils import simulateCategorical, logaddexp
 
 from venture.mite.sp import RequestReferenceSP
 from venture.mite.sp import SimulationSP
@@ -57,6 +58,61 @@ class DPSP(RequestReferenceSP):
     self.counts[raddr] += 1
     self.request_map[args.node] = raddr
     return args.requestedValue(raddr)
+
+  def constrain(self, value, args):
+    raddr = self.request_map.pop(args.node)
+    self.counts[raddr] -= 1
+    args.decRequest(raddr)
+
+    weights = []
+    tables = []
+    for table, count in self.counts.items():
+      if args.hasRequest(raddr) and args.requestedValue(table) == value:
+        weights.append(count)
+        tables.append(table)
+    newTable = getNextTable(self.counts)
+    exp = ["sp"]
+    env = VentureEnvironment(None, ["sp"], [self.baseSpNode])
+    args.newRequest(newTable, exp, env)
+    newWeight = args.constrain(newTable, value)
+    weights.append(self.alpha * math.exp(newWeight))
+    tables.append(newTable)
+
+    raddr = simulateCategorical(weights, tables)
+    if raddr == newTable:
+      self.counts[raddr] = 0
+    else:
+      args.decRequest(newTable)
+      args.incRequest(raddr)
+    self.counts[raddr] += 1
+    self.request_map[args.node] = raddr
+
+    return logaddexp(weights) - math.log(self.alpha + len(self.counts))
+
+  def unconstrain(self, args):
+    value = args.outputValue()
+    raddr = self.request_map.pop(args.node)
+    self.counts[raddr] -= 1
+    if self.counts[raddr] == 0:
+      newWeight = args.unconstrain(raddr)
+    else:
+      newTable = getNextTable(self.counts)
+      exp = ["sp"]
+      env = VentureEnvironment(None, ["sp"], [self.baseSpNode])
+      args.newRequest(newTable, exp, env)
+      args.constrain(newTable, value)
+      newWeight = args.unconstrain(newTable)
+      args.decRequest(newTable)
+    args.decRequest(raddr)
+
+    weights = []
+    for table, count in self.counts.items():
+      if args.hasRequest(raddr) and args.requestedValue(table) == value:
+        weights.append(count)
+    weights.append(newWeight)
+    weight = logaddexp(weights) - math.log(self.alpha + len(self.counts))
+
+    return weight, self.apply(args)
 
 class DPMemSP(SimulationSP):
   def simulate(self, args):
