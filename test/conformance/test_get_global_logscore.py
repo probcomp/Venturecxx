5eb05668e320e1c4c0b0cd7658f1c781ec43cb9c
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2016-05-24 20:28:52 +0000

    Test log_likelihood_at, log_joint_at, in easy closed-form case.

--- test/conformance/test_get_global_logscore.py
+++ test/conformance/test_get_global_logscore.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Venture.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import division
+
 import math
 
 from nose.tools import assert_almost_equal
@@ -74,3 +76,105 @@ def setup_likelihood_free():
     ripl.bind_foreign_sp('test1', tester1)
     ripl.bind_foreign_sp('test2', tester2)
     return ripl
+
+def test_loglikelihood_logjoint():
+    exp = math.exp
+    lgamma = math.lgamma
+    log = math.log
+    log1p = math.log1p
+
+    # Exponential: p(alpha) = e^{-alpha}
+    alpha = -log(1/2)
+    logp_alpha = -alpha
+    assert_almost_equal(logp_alpha, -0.6931472)
+
+    # Beta: p(theta | alpha) = Beta(theta; alpha, alpha)
+    #   = theta^{alpha - 1} (1 - theta)^{alpha - 1} / Beta(alpha, alpha)
+    #   = (theta*(1 - theta))^{alpha - 1} Gamma(2*alpha) / Gamma(alpha)^2
+    theta = 3/4
+    logp_theta_given_alpha = (alpha - 1)*(log(theta) + log1p(-theta)) \
+        + lgamma(2*alpha) - 2*lgamma(alpha)
+    assert_almost_equal(logp_theta_given_alpha, -0.1436)
+
+    # Bernoulli: p(x_i | theta) = theta delta_xi + (1 - theta) delta_{1 - xi}
+    x_0 = 0
+    x_1 = 1
+    logp_x_given_theta = log(theta) + log1p(-theta)
+    assert_almost_equal(logp_x_given_theta, log(3/16))
+
+    def check(expression, value):
+        assert_almost_equal(ripl.infer(expression)[0], value)
+
+    # Unconditional model:
+    #
+    #   alpha ~ Expon(1)
+    #   theta ~ Beta(alpha, alpha)
+    #   X_i ~ Bernoulli(theta)
+
+    ripl = get_ripl()
+    ripl.assume('alpha', '(tag (quote alpha) 0 (expon 1))')
+    ripl.force('alpha', alpha)
+    ripl.assume('theta', '(tag (quote theta) 0 (beta alpha alpha))')
+    ripl.force('theta', theta)
+    ripl.observe('(bernoulli theta)', x_0)
+    ripl.observe('(bernoulli theta)', x_1)
+
+    # p(theta | alpha)
+    check('(log_likelihood_at (quote alpha) 0)', logp_theta_given_alpha)
+
+    # p(x | theta)
+    check('(log_likelihood_at (quote theta) 0)', logp_x_given_theta)
+
+    # p(x | theta, alpha) = p(x | theta)
+    check('(log_likelihood_at default all)', logp_x_given_theta)
+
+    # p(theta, alpha) = p(theta | alpha) p(alpha)
+    check('(log_joint_at (quote alpha) 0)',
+          logp_theta_given_alpha + logp_alpha)
+
+    # p(x, theta | alpha) = p(x | theta) p(theta | alpha)
+    check('(log_joint_at (quote theta) 0)',
+          logp_x_given_theta + logp_theta_given_alpha)
+
+    # p(x, theta, alpha) = p(x | theta) p(theta | alpha) p(alpha)
+    check('(log_joint_at default all)',
+          logp_x_given_theta + logp_theta_given_alpha + logp_alpha)
+
+    # Conditional model:
+    #
+    #   alpha ~ Expon(1)
+    #   theta ~ Exactly(0.5), if alpha > 2; else Beta(alpha, alpha)
+    #   X_i ~ Bernoulli(theta)
+
+    ripl = get_ripl()
+    ripl.assume('alpha', '(tag (quote alpha) 0 (expon 1))')
+    ripl.force('alpha', alpha)
+    ripl.assume('theta', '''
+        (tag (quote theta) 0
+          (if (> alpha 2) (exactly 0.5) (beta alpha alpha)))
+    ''')
+    ripl.force('theta', theta)
+    ripl.observe('(bernoulli theta)', x_0)
+    ripl.observe('(bernoulli theta)', x_1)
+
+    # p(x | theta)      XXX ?
+    check('(log_likelihood_at (quote alpha) 0)', logp_x_given_theta)
+
+    # p(x | theta)      XXX ?
+    check('(log_likelihood_at (quote theta) 0)', logp_x_given_theta)
+
+    # p(x | theta)      XXX ?
+    check('(log_likelihood_at default all)', logp_x_given_theta)
+
+    # likelihood * p(alpha) = p(x | theta) p(alpha)
+    check('(log_joint_at (quote alpha) 0)', logp_x_given_theta + logp_alpha)
+
+    # likelihood * p(theta | alpha) = p(x | theta) p(theta | alpha)
+    #   = p(x, theta | alpha)
+    check('(log_joint_at (quote theta) 0)',
+          logp_x_given_theta + logp_theta_given_alpha)
+
+    # likelihood * p(alpha) = p(x | theta) p(theta | alpha) p(alpha)
+    #   = p(x, theta, alpha)
+    check('(log_joint_at default all)',
+          logp_x_given_theta + logp_theta_given_alpha + logp_alpha)
