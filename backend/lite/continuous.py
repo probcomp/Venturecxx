a1cddee0225075da430191012412fefc40ea9924
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2016-06-17 17:24:44 +0000

    Fix off-by-one in comment.

--- backend/lite/continuous.py
+++ backend/lite/continuous.py
@@ -592,9 +592,9 @@ class BetaOutputPSP(RandomPSP):
       # This will never happen unless the PRNG is broken.
       #
       # XXX Is numpy's PRNG broken?  Unclear: many alleged `uniform
-      # [0, 1]' samplers actually give probability 2^-64 or 2^-52 of
+      # [0, 1]' samplers actually give probability 2^-64 or 2^-53 of
       # yielding zero, e.g. by drawing an integer in {0, 1, 2, ...,
-      # 2^52 - 1} and dividing by 2^52.  This is not what numpy's PRNG
+      # 2^53 - 1} and dividing by 2^53.  This is not what numpy's PRNG
       # does but it's not too far off either.
 
       assert 0 < u
