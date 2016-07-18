2893d52db5c718d786094dbbccc39d0826e807fa
Anthony Lu <lua@mit.edu>
2016-07-18 14:12:40 -0400

    Whoops, remove dead imports from mite.builtin.

--- backend/mite/builtin.py
+++ backend/mite/builtin.py
@@ -9,10 +9,6 @@ from venture.mite.sp_registry import builtInSPs
 import venture.mite.traces
 import venture.mite.dep_graph
 import venture.mite.sps.lite_sp
-import venture.mite.sps.mem
-import venture.mite.sps.dpmem
-import venture.mite.sps.suff_stat_cat
-import venture.mite.sps.trick_coin
 
 def builtInValues():
   return { "true" : v.VentureBool(True), "false" : v.VentureBool(False),
