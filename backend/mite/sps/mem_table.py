ba5ba6a40c81f94c366c591ff49a1c4055dd9561
Anthony Lu <lua@mit.edu>
2016-07-26 15:32:50 -0400

    Add mem table reversible operations

--- backend/mite/sps/mem_table.py
+++ backend/mite/sps/mem_table.py
@@ -18,9 +18,21 @@ class MemTable(object):
 
   def assoc(self, key, value):
     self.value_map[key] = value
+    self.num_requests[key] = 0
 
   def incr(self, key):
-    pass
+    self.num_requests[key] += 1
+
+  def decr(self, key):
+    self.num_requests[key] -= 1
+
+  def count(self, key):
+    return self.num_requests[key]
+
+  def dissoc(self, key):
+    assert self.num_requests[key] == 0
+    del self.num_requests[key]
+    del self.value_map[key]
 
   def copy(self):
     # TODO
@@ -31,4 +43,7 @@ register_trace_type("mem_table", MemTable, {
   "mem_lookup": trace_action("lookup", [t.Object], t.Object),
   "mem_assoc": trace_action("assoc", [t.Object, t.Object], t.Nil),
   "mem_incr": trace_action("incr", [t.Object], t.Nil),
+  "mem_decr": trace_action("decr", [t.Object], t.Nil),
+  "mem_count": trace_action("count", [t.Object], t.Int),
+  "mem_dissoc": trace_action("dissoc", [t.Object], t.Nil),
 })
