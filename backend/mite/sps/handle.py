1898df88a763479a712bdcefa009f991471de121
Anthony Lu <lua@mit.edu>
2016-07-26 15:14:54 -0400

    Add bindings for free_request and restore_request.

--- backend/mite/sps/handle.py
+++ backend/mite/sps/handle.py
@@ -7,4 +7,6 @@ from venture.mite.state import register_trace_type, trace_action
 register_trace_type("_handle", TraceHandle, {
   "new_request": trace_action("new_request", [t.Object, t.Exp, EnvironmentType()], t.Blob),
   "value_at": trace_action("value_at", [t.Blob], t.Object),
+  "free_request": trace_action("free_request", [t.Object], t.Blob),
+  "restore_request": trace_action("restore_request", [t.Object], t.Blob),
 })
