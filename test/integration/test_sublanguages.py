fb5f4c0341367e14f98612ae91ad46d2e390030c
Alexey Radul <axch@mit.edu>
2016-08-03 14:46:41 -0400

    Test the subscanner facility too.  It's a little brittle and kooky, but ok.

--- test/integration/test_sublanguages.py
+++ test/integration/test_sublanguages.py
@@ -19,6 +19,7 @@ from nose.tools import eq_
 
 from venture.test.config import get_ripl
 from venture.parser import ast
+from venture.parser.venture_script import subscanner
 import venture.value.dicts as e
 
 def mk_my_scanner():
@@ -33,6 +34,25 @@ def testSubscannerSmoke():
     r = get_ripl()
     r.set_mode("venture_script")
     r.register_language("troll", mk_my_scanner)
+    eq_(10, r.evaluate("1 + @{troll 42}"))
+    eq_(10, r.evaluate("1 + @{troll 27 }"))
+    eq_(10, r.evaluate("1 + @{troll 4 3 3}"))
+    eq_(18, r.evaluate("@{troll 4 3 3} + @{troll 7}"))
+
+class MyReadingScanner(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+    def read(self):
+        char = self.stream.read(1)
+        while char in [" ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            char = self.stream.read(1)
+        return (ast.Located([0, 0], e.number(9)),)
+
+def testReadingSubscannerSmoke():
+    r = get_ripl()
+    r.set_mode("venture_script")
+    r.register_language("troll", lambda : subscanner.Scanner(MyReadingScanner))
     eq_(10, r.evaluate("1 + @{troll 42}"))
     eq_(10, r.evaluate("1 + @{troll 27 }"))
     eq_(10, r.evaluate("1 + @{troll 4 3 3}"))
