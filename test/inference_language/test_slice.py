ddc6f3c41feb57eb96c946519af0c66ef3a3a56d
Alexey Radul <axch@mit.edu>
2016-08-06 12:19:56 -0400

    Determinize statistical tests in test_rejection, test_slice, test_subsampledmh, test_pymem.

--- test/inference_language/test_slice.py
+++ test/inference_language/test_slice.py
@@ -54,22 +54,22 @@ def myCollectSamples(ripl, method):
                         infer=inferCommand(method))
 
 @statisticalTest
-def checkSliceBasic1(slice_method):
+def checkSliceBasic1(slice_method, seed):
   # Basic sanity test for slice
   if (config["get_ripl"] != "lite") and (slice_method == 'slice_doubling'):
     raise SkipTest("Slice sampling with doubling only implemented in Lite.")
-  ripl = get_ripl()
+  ripl = get_ripl(seed=seed)
   ripl.assume("a", "(normal 10.0 1.0)",label="pid")
   predictions = myCollectSamples(ripl, slice_method)
   return reportKnownGaussian(10, 1, predictions)
 
 @statisticalTest
-def checkSliceNormalWithObserve1(slice_method):
+def checkSliceNormalWithObserve1(slice_method, seed):
   # Checks the posterior distribution on a Gaussian given an unlikely
   # observation
   if (config["get_ripl"] != "lite") and (slice_method == 'slice_doubling'):
     raise SkipTest("Slice sampling with doubling only implemented in Lite.")
-  ripl = get_ripl()
+  ripl = get_ripl(seed=seed)
   ripl.assume("a", "(normal 10.0 1.0)",label="pid")
   ripl.observe("(normal a 1.0)", 14.0)
   # Posterior for a is normal with mean 12, precision 2
@@ -79,13 +79,13 @@ def checkSliceNormalWithObserve1(slice_method):
   return reportKnownGaussian(12, math.sqrt(0.5), predictions)
 
 @statisticalTest
-def checkSliceNormalWithObserve2a(slice_method):
+def checkSliceNormalWithObserve2a(slice_method, seed):
   # Checks the posterior distribution on a Gaussian given an unlikely
   # observation.  The difference between this and 1 is an extra
   # predict, which apparently has a deleterious effect on mixing.
   if (config["get_ripl"] != "lite") and (slice_method == 'slice_doubling'):
     raise SkipTest("Slice sampling with doubling only implemented in Lite.")
-  ripl = get_ripl()
+  ripl = get_ripl(seed=seed)
   ripl.assume("a", "(normal 10.0 1.0)", label="pid")
   ripl.observe("(normal a 1.0)", 14.0)
   # Posterior for a is normal with mean 12, precision 2
@@ -95,12 +95,12 @@ def checkSliceNormalWithObserve2a(slice_method):
   return reportKnownGaussian(12, math.sqrt(0.5), predictions)
 
 @statisticalTest
-def checkSliceNormalWithObserve2b(slice_method):
+def checkSliceNormalWithObserve2b(slice_method, seed):
   # Checks the posterior distribution on a Gaussian given an unlikely
   # observation
   if (config["get_ripl"] != "lite") and (slice_method == 'slice_doubling'):
     raise SkipTest("Slice sampling with doubling only implemented in Lite.")
-  ripl = get_ripl()
+  ripl = get_ripl(seed=seed)
   ripl.assume("a", "(normal 10.0 1.0)")
   ripl.observe("(normal a 1.0)", 14.0)
   # Posterior for a is normal with mean 12, precision 2
@@ -110,11 +110,11 @@ def checkSliceNormalWithObserve2b(slice_method):
   return reportKnownGaussian(12, math.sqrt(1.5), predictions)
 
 @statisticalTest
-def checkSliceStudentT1(slice_method):
+def checkSliceStudentT1(slice_method, seed):
   # Simple program involving simulating from a student_t
   if (config["get_ripl"] != "lite") and (slice_method == 'slice_doubling'):
     raise SkipTest("Slice sampling with doubling only implemented in Lite.")
-  ripl = get_ripl()
+  ripl = get_ripl(seed=seed)
   ripl.assume("a", "(student_t 1.0)", label="pid")
   ripl.observe("(normal a 1.0)", 3.0)
   predictions = myCollectSamples(ripl, slice_method)
@@ -133,11 +133,11 @@ def checkSliceStudentT1(slice_method):
   return reportKnownMean(meana, predictions, variance=vara)
 
 @statisticalTest
-def checkSliceStudentT2(slice_method):
+def checkSliceStudentT2(slice_method, seed):
   # Simple program involving simulating from a student_t
   if (config["get_ripl"] != "lite") and (slice_method == 'slice_doubling'):
     raise SkipTest("Slice sampling with doubling only implemented in Lite.")
-  ripl = get_ripl()
+  ripl = get_ripl(seed=seed)
   ripl.assume("a", "(student_t 1.0)")
   ripl.observe("(normal a 1.0)", 3.0)
   ripl.predict("(normal a 1.0)", label="pid")
@@ -157,11 +157,11 @@ def checkSliceStudentT2(slice_method):
   return reportKnownMean(meana, predictions, variance=vara + 1.0)
 
 @statisticalTest
-def checkSliceL(slice_method):
+def checkSliceL(slice_method, seed):
   # Checks slice sampling on an L-shaped distribution.
   if (config["get_ripl"] != "lite") and (slice_method == 'slice_doubling'):
     raise SkipTest("Slice sampling with doubling only implemented in Lite.")
-  ripl = get_ripl()
+  ripl = get_ripl(seed=seed)
   ripl.assume("a", "(uniform_continuous 0.0 1.0)")
   ripl.assume("b", "(< a 0.2)")
   ripl.observe("(flip (biplex b 0.8 0.2))", True)
