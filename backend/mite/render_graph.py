6a5cc1db2a6d70ccaa6e5eee0d4924d6d7577e49
Alexey Radul <axch@mit.edu>
2016-08-11 21:19:16 -0400

    Draft a graph renderer.

---
+++ backend/mite/render_graph.py
@@ -0,0 +1,25 @@
+from graphviz import Digraph
+
+from venture.mite.render import _jsonable_address
+
+def digraph(trace, scaffold):
+  dot = Digraph(name="A scaffold")
+  for addr in scaffold.kernels.keys():
+    name = _jsonable_address(addr)
+    def kernel_type(addr):
+      ker = scaffold.kernels[addr]
+      if isinstance(ker, dict) and 'type' in ker:
+        return ker['type']
+      else:
+        return None
+    if kernel_type(addr) == 'proposal' or kernel_type(addr) == 'propagate_lookup':
+      color = 'yellow'
+    elif kernel_type(addr) == 'constrained':
+      color = 'blue'
+    dot.node(name, label=name, fillcolor=color, style="filled")
+  for addr in scaffold.kernels.keys():
+    for child in trace.nodes[addr].children:
+      if child in scaffold.kernels:
+        dot.edge(_jsonable_address(addr),
+                 _jsonable_address(child))
+  return dot
