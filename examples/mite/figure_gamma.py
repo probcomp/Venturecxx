981d88a6159724147b67b34ce04e8d945692e357
Alexey Radul <axch@mit.edu>
2016-08-14 17:49:13 -0400

    Draft code to generate a histogram and pdf curve for the gamma sampler.
    Caveat: the pdf is evaluated in Python rather than VS, because I
    didn't want to put in a log_gamma function.  And I can't imagine
    making a mistake in transcription.

---
+++ examples/mite/figure_gamma.py
@@ -0,0 +1,51 @@
+import math
+import os
+import pickle
+import sys
+import time
+
+import matplotlib.pyplot as plt
+import scipy.special as scipy
+
+import venture.shortcuts as vs
+
+vnts_file = os.path.join(os.path.dirname(__file__), 'figure_gamma.vnts')
+
+def prep():
+  r = vs.Mite().make_ripl()
+  r.set_mode("venture_script")
+  r.execute_program_from_file(vnts_file)
+  return r
+
+def gamma_samples(r, shape, ct):
+  return [r.evaluate("simulate_std_gamma(%s)" % (shape,)) for _ in range(ct)]
+
+def gamma_assess(r, x, shape):
+  return r.evaluate("assess_std_gamma(%s, %s)" % (x, shape))
+
+def gamma_assess2(x, shape):
+  ln = (shape - 1) * math.log(x) - x - scipy.gammaln(shape)
+  return math.exp(ln)
+
+def main():
+  r = prep()
+  shape = 2
+  n = 1000
+  nbins = math.floor(math.sqrt(n))
+  samples = gamma_samples(r, shape, n)
+  plt.figure()
+  (_counts, bin_edges, _) = plt.hist(samples, bins=nbins)
+  points = []
+  assessments = []
+  def do(place, width):
+    points.append(place)
+    assessments.append(width * n * gamma_assess2(place, shape))
+  for (low, high) in zip(bin_edges, bin_edges[1:]):
+    mid = (low+high) / 2
+    do(mid, high - low)
+  plt.plot(points, assessments)
+  plt.show()
+
+
+if __name__ == '__main__':
+  main()
