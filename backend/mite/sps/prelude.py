c5a5c6e3acdd73b5f56c621c947a98d33c517a89
Alexey Radul <axch@mit.edu>
2016-11-17 14:32:48 -0500

    Make function names for the prelude interpretation, in case we want other kinds of preludes.

--- backend/mite/sps/prelude.py
+++ backend/mite/sps/prelude.py
@@ -9,16 +9,22 @@ import venture.lite.value as v
 from venture.mite.sp_registry import registerBuiltinSP
 import venture.mite
 
-with open(venture.mite.__path__[0] + '/prelude.vnt') as f:
-  prelude_source = f.read()
+def each_defn(fname):
+  with open(fname) as f:
+    source = f.read()
+  parser = ChurchPrimeParser.instance()
+  instructions = parser.parse_instructions(source)
+  for instr in instructions:
+    assert instr['instruction'] == 'define'
+    name = t.Symbol.asPython(v.VentureValue.fromStackDict(instr['symbol']))
+    expr_dict = _modify_expression(desugar_expression(instr['expression']))
+    expr = t.Exp.asPython(v.VentureValue.fromStackDict(expr_dict))
+    yield (name, expr)
 
-parser = ChurchPrimeParser.instance()
-instructions = parser.parse_instructions(prelude_source)
-for instr in instructions:
-  assert instr['instruction'] == 'define'
-  name = t.Symbol.asPython(v.VentureValue.fromStackDict(instr['symbol']))
-  expr_dict = _modify_expression(desugar_expression(instr['expression']))
-  expr = t.Exp.asPython(v.VentureValue.fromStackDict(expr_dict))
-  assert e.isLambda(expr)
-  (params, body) = e.destructLambda(expr)
-  registerBuiltinSP(name, (params, body))
+def first_prelude():
+  for (name, expr) in each_defn(venture.mite.__path__[0] + '/prelude.vnt'):
+    assert e.isLambda(expr)
+    (params, body) = e.destructLambda(expr)
+    registerBuiltinSP(name, (params, body))
+
+first_prelude()
