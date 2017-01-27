d76ac959bd1e6a4a716c0759597b3d29e2733144
Alexey Radul <axch@mit.edu>
2017-01-27 15:17:56 -0500

    Actually draft (and type-annotate) the basic regeneration recursion (ignoring traces).

--- backend/knight/regen.py
+++ backend/knight/regen.py
@@ -1,6 +1,8 @@
+from typing import Callable
 from typing import List
 from typing import NamedTuple
 from typing import Tuple
+from typing import Union
 
 import venture.lite.value as vv 
 from venture.lite.env import VentureEnvironment
@@ -18,13 +20,28 @@ class Trace(object):
   def subexpr_subtrace(self, index):
     # type: (int) -> Trace
     pass
+  def application_subtrace(self):
+    # type: () -> Trace
+    pass
+
+Datum = NamedTuple('Datum', [('datum', vv.VentureValue)])
+Request = NamedTuple('Request', [('exp', Exp), ('env', VentureEnvironment), ('trace', Trace)])
+
+RegenResult = Union[Datum, Request]
+
+class SP(vv.VentureValue):
+  def regenerator(self):
+    # type: () -> Callable[[List[vv.VentureValue], Trace], Tuple[float, RegenResult]]
+    raise NotImplementedError
 
 def regen(exp, env, trace):
   # type: (Exp, VentureEnvironment, Trace) -> Tuple[float, vv.VentureValue]
   if isinstance(exp, App):
-    (w, inter) = regen_list(exp.subs, env, trace)
-    ans = sum(inter)
-    return (w, ans)
+    (subw, subvals) = regen_list(exp.subs, env, trace)
+    oper = subvals[0]
+    assert isinstance(oper, SP)
+    (appw, val) = r_apply(oper, subvals[1:], trace.application_subtrace())
+    return (subw + appw, val)
   if isinstance(exp, Lit):
     return (0, exp.val)
 
@@ -38,6 +55,15 @@ def regen_list(exps, env, trace):
     w += dw
     anss.append(ans)
   return (w, anss)
-  
+
+def r_apply(oper, args, trace):
+  # type: (SP, List[vv.VentureValue], Trace) -> Tuple[float, vv.VentureValue]
+  reg = oper.regenerator()
+  (appw, res) = reg(args, trace)
+  if isinstance(res, Datum):
+    return (appw, res.datum)
+  elif isinstance(res, Request):
+    (recurw, val) = regen(res.exp, res.env, res.trace)
+    return (appw + recurw, val)
 
 print regen(App([Lit(vv.VentureNumber(1)), Lit(vv.VentureNumber(2))]), VentureEnvironment(), Trace())
