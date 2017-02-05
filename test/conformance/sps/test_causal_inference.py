70870b20f048c6828a235bc0b6e561acbfa150ce
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2017-02-05 15:34:14 -0500

    Tests for CMI based causal inference.
    These tests indicate that causal inference is working at the moment.
    
    At the moment, this only contains two stochastic tests with synthetic
    data.
    
    The precomputed CMI values need to be moved from the examples directory.

--- test/conformance/sps/test_causal_inference.py
+++ test/conformance/sps/test_causal_inference.py
@@ -14,7 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Venture.  If not, see <http://www.gnu.org/licenses/>.
-
 from nose import SkipTest
 from nose.tools import eq_
 import numpy as np
@@ -29,6 +28,7 @@ from venture.test.stats import statisticalTest
 from venture.test.stats import stochasticTest
 import venture.lite.value as v
 import venture.lite.types as t
+from venture.lite.sp import SPType
 from venture.lite.psp import RandomPSP
 from venture.lite.sp_help import typed_nr
 from venture.lite.sp_help import deterministic_typed
@@ -36,7 +36,9 @@ import numpy as np
 
 from pgmpy.models import BayesianModel
 
-from venture.lite.causal_inference import *
+from venture.lite.causal_inference import MakerBDBPopulationOutputPSP 
+from venture.lite.causal_inference import dag_to_dot_notation
+from venture.lite.causal_inference import get_cmi_queries
 
 def prep_ripl(ripl):
     ripl.set_mode("venture_script")
@@ -51,11 +53,19 @@ def prep_ripl(ripl):
                 min_req_args=2)
             )
     ripl.bind_foreign_sp("make_observation_function", typed_nr(MakerBDBPopulationOutputPSP(),
-        [t.StringType()], SPType([], t.BoolType())))
+        [
+            t.StringType(),
+            t.StringType(),
+            t.StringType(),
+            t.ArrayUnboxedType(t.StringType()),
+            t.ArrayUnboxedType(t.StringType()),
+        ],
+        SPType([], t.BoolType())))
     ripl.bind_foreign_sp("dag_to_dot_notation",
             deterministic_typed(dag_to_dot_notation,
                 [ 
-                t.ArrayUnboxedType(t.ArrayUnboxedType(t.BoolType()))
+                t.ArrayUnboxedType(t.ArrayUnboxedType(t.BoolType())),
+                t.ArrayUnboxedType(t.StringType())
                 ],
                 t.StringType(),
                 min_req_args=1)
@@ -71,6 +81,7 @@ def prep_ripl(ripl):
             )
     ripl.execute_program_from_file("examples/causal-inference/causal_inference.vnts") 
 
+# TODO: Find out if this is this REALLY broken in puma.
 @broken_in('puma', "Puma does not define the gaussian process builtins")
 @on_inf_prim('none')
 def test_model_crash():
@@ -86,8 +97,8 @@ def test_create_obervation_function_crash():
     prep_ripl(ripl)
     ripl.execute_program("""
         assume observation_function =
-            make_observation_function("precomputed_v_structure", "[ ]",
-            "non_existing.bdb");""")
+            make_observation_function("non_existing_population_name",
+            "precomputed_v_structure", "non_existing.bdb", [ ], ["x", "y", "z"]);""")
 
 @broken_in('puma', "Puma does not define the gaussian process builtins")
 @on_inf_prim('none')
@@ -96,22 +107,152 @@ def test_obervations_crash():
     ripl.assume("number_nodes", 3)
     prep_ripl(ripl)
     ripl.execute_program("""
-        assume observation_function =
-            make_observation_function("precomputed_v_structure", "[ ]",
-            "non_existing.bdb");""")
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_v_structure", "non_existing.bdb", [], ["x", "y", "z"]);""")
     ripl.observe("observation_function(DAG)", True)
 
 
 @broken_in('puma', "Puma does not define the gaussian process builtins")
 @on_inf_prim('none')
-def test_inference_crash():
+def test_inference_fixed_cmi_crash():
     ripl = get_ripl()
     ripl.assume("number_nodes", 3)
     prep_ripl(ripl)
     ripl.execute_program("""
-        assume observation_function =
-            make_observation_function("precomputed_v_structure", "[ ]",
-            "non_existing.bdb");""")
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_v_structure", "non_existing.bdb", [], ["x", "y", "z"]);""")
+    ripl.observe("observation_function(DAG)", True)
+    ripl.infer("mh(default, all, 1)")
+
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_inference_fixed_cmi_independent_mh_2_nodes():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 2)
+    prep_ripl(ripl)
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_independent_nodes",
+            "non_existing.bdb", [], ["x", "y"]);""")
+    ripl.observe("observation_function(DAG)", True)
+    ripl.infer("mh(default, all, 150)")
+    sample = ripl.sample("""dag_to_dot_notation(DAG, ["x", "y"])""")
+    assert sample == ""
+
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_inference_fixed_cmi_simple_link_mh_2_nodes():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 2)
+    prep_ripl(ripl)
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_simple_link_2_nodes",
+            "non_existing.bdb", [], ["x", "y"]);""")
+    ripl.observe("observation_function(DAG)", True)
+    ripl.infer("mh(default, all, 150)")
+    sample = ripl.sample("""dag_to_dot_notation(DAG, ["x", "y"])""")
+    assert (sample == "(x)-->(y), ") or (sample == "(y)-->(x), ")
+#    
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_inference_fixed_cmi_simple_link_mh_3_nodes():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 3)
+    prep_ripl(ripl)
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_simple_link",
+            "non_existing.bdb", [], ["x", "y", "z"]);""")
+    ripl.observe("observation_function(DAG)", True)
+    ripl.infer("mh(default, all, 150)")
+    sample = ripl.sample("""dag_to_dot_notation(DAG, ["x", "y", "z"])""")
+    assert (sample == "(x)-->(z), ") or (sample == "(z)-->(x), ")
+   
+
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_inference_fixed_cmi_v_struct_mh():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 3)
+    prep_ripl(ripl)
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_v_structure",
+            "non_existing.bdb", [], ["x", "y", "z"]);""")
+    ripl.observe("observation_function(DAG)", True)
+    ripl.infer("mh(default, all, 50)")
+    sample = ripl.sample("""dag_to_dot_notation(DAG, ["x", "y", "z"])""")
+    assert (sample == "(x)-->(z), (y)-->(z), ") or (sample == "(y)-->(z), (x)-->(z), ")
+
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_cmi_crash():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 3)
+    prep_ripl(ripl)
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_v_structure",
+            "non_existing.bdb", [], ["x", "y", "z"]);""")
+    ripl.observe("observation_function(DAG)", True)
+    list_of_queries = ripl.infer("""return(get_cmi_queries(["x", "y", "z"], "causal_population"))""")
+
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_cmi_correct_length():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 3)
+    prep_ripl(ripl)
+    list_of_queries = ripl.infer("""return(get_cmi_queries(["x", "y", "z"], "causal_population"))""")
+    # checking that we have total of 6 mi queries + 6 x "DROP TABLE IF EXISTS"
+    assert len(list_of_queries) == 12 
+
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_obs_function_with_cmi_crash():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 3)
+    prep_ripl(ripl)
+    ripl.execute_program("""
+        define list_of_cmi_queries = get_cmi_queries(["x", "y", "z"], "causal_population");
+        define list_of_nodes = ["x", "y", "z"];
+        """)
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+            "non_existing_population_name",
+            "precomputed_v_structure",
+            "non_existing.bdb", 
+            ${list_of_cmi_queries},
+            ${list_of_nodes});
+        """)
+    ripl.observe("observation_function(DAG)", True)
+
+@broken_in('puma', "Puma does not define the gaussian process builtins")
+@on_inf_prim('none')
+def test_causal_inference_crash():
+    ripl = get_ripl()
+    ripl.assume("number_nodes", 2)
+    prep_ripl(ripl)
+    ripl.execute_program("""
+        define list_of_nodes = ["x", "y"];
+        define list_of_cmi_queries = get_cmi_queries(list_of_nodes, "commonp")
+        """)
+    #FIXME re-run data-generator and fix naming of generator and population
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+                                      "commonp",
+                                      "commonm",
+                                      "examples/causal-inference/common_faithful.bdb", 
+                                      ${list_of_cmi_queries},
+                                      ${list_of_nodes});""")
     ripl.observe("observation_function(DAG)", True)
     ripl.infer("mh(default, all, 1)")
 
@@ -122,13 +263,27 @@ def test_inference_independent_mh_2_nodes():
     ripl.assume("number_nodes", 2)
     prep_ripl(ripl)
     ripl.execute_program("""
-        assume observation_function =
-            make_observation_function("precomputed_independent_nodes", "[ ]",
-            "non_existing.bdb");""")
+        define list_of_nodes = ["x", "y"];
+        define list_of_cmi_queries = get_cmi_queries(list_of_nodes, "commonp")
+        """)
+    #FIXME re-run data-generator and fix naming of generator and population
+    ripl.execute_program("""
+        assume observation_function = make_observation_function(
+                                      "commonp",
+                                      "commonm",
+                                      "examples/causal-inference/common_faithful.bdb", 
+                                      ${list_of_cmi_queries},
+                                      ${list_of_nodes});""")
     ripl.observe("observation_function(DAG)", True)
     ripl.infer("mh(default, all, 100)")
-    sample = ripl.sample("dag_to_dot_notation(DAG)")
-    assert sample == ""
+
+    n_samples = 10
+    all_samples = [] 
+    for _ in range(n_samples):
+        ripl.infer("mh(default, all, 1)")
+        all_samples.append(ripl.sample("""dag_to_dot_notation(DAG, ["x", "y"])"""))
+    all_samples = np.array(all_samples)
+    assert (float(sum(all_samples == ""))/n_samples) > 0.7 
 
 @broken_in('puma', "Puma does not define the gaussian process builtins")
 @on_inf_prim('none')
@@ -137,66 +292,25 @@ def test_inference_simple_link_mh_2_nodes():
     ripl.assume("number_nodes", 2)
     prep_ripl(ripl)
     ripl.execute_program("""
-        assume observation_function =
-            make_observation_function("precomputed_simple_link_2_nodes", "[ ]",
-            "non_existing.bdb");""")
-    ripl.observe("observation_function(DAG)", True)
-    ripl.infer("mh(default, all, 100)")
-    sample = ripl.sample("dag_to_dot_notation(DAG)")
-    assert (sample == "(0)-->(1), ") or (sample == "(1)-->(0), ")
-#    
-@broken_in('puma', "Puma does not define the gaussian process builtins")
-@on_inf_prim('none')
-def test_inference_simple_link_mh_3_nodes():
-    ripl = get_ripl()
-    ripl.assume("number_nodes", 3)
-    prep_ripl(ripl)
+        define list_of_nodes = ["x", "z"];
+        define list_of_cmi_queries = get_cmi_queries(list_of_nodes, "commonp")
+        """)
+    #FIXME re-run data-generator and fix naming of generator and population
     ripl.execute_program("""
-        assume observation_function =
-            make_observation_function("precomputed_simple_link", "[ ]",
-            "non_existing.bdb");""")
+        assume observation_function = make_observation_function(
+                                      "commonp",
+                                      "commonm",
+                                      "examples/causal-inference/common_faithful.bdb", 
+                                      ${list_of_cmi_queries},
+                                      ${list_of_nodes});""")
     ripl.observe("observation_function(DAG)", True)
-    ripl.infer("mh(default, all, 50)")
-    sample = ripl.sample("dag_to_dot_notation(DAG)")
-    assert (sample == "(0)-->(2), ") or (sample == "(2)-->(0), ")
-   
+    ripl.infer("mh(default, all, 10)")
 
-@broken_in('puma', "Puma does not define the gaussian process builtins")
-@on_inf_prim('none')
-def test_inference_v_struct_mh():
-    ripl = get_ripl()
-    ripl.assume("number_nodes", 3)
-    prep_ripl(ripl)
-    ripl.execute_program("""
-        assume observation_function =
-            make_observation_function("precomputed_v_structure", "[ ]",
-            "non_existing.bdb");""")
-    ripl.observe("observation_function(DAG)", True)
-    ripl.infer("mh(default, all, 50)")
-    sample = ripl.sample("dag_to_dot_notation(DAG)")
-    assert (sample == "(0)-->(2), (1)-->(2), ") or (sample == "(1)-->(2), (2)-->(2), ")
-
-@broken_in('puma', "Puma does not define the gaussian process builtins")
-@on_inf_prim('none')
-def test_cmi_crash():
-    ripl = get_ripl()
-    ripl.assume("number_nodes", 3)
-    prep_ripl(ripl)
-    ripl.execute_program("""
-        assume observation_function =
-            make_observation_function("precomputed_v_structure", "[ ]",
-            "non_existing.bdb");""")
-    ripl.observe("observation_function(DAG)", True)
-    list_of_queries = ripl.infer("""return(get_cmi_queries(["node_0", "node_1", "node_2"], "causal_population"))""")
-
-@broken_in('puma', "Puma does not define the gaussian process builtins")
-@on_inf_prim('none')
-def test_cmi_correct_length():
-    ripl = get_ripl()
-    ripl.assume("number_nodes", 3)
-    prep_ripl(ripl)
-    ripl.execute_program("""
-        assume observation_function = make_observation_function("precomputed_v_structure");""")
-    ripl.observe("observation_function(DAG)", True)
-    list_of_queries = ripl.infer("""return(get_cmi_queries(["node_0", "node_1", "node_2"], "causal_population"))""")
-    assert len(list_of_queries) == 6 
+    n_samples = 10
+    all_samples = [] 
+    for _ in range(n_samples):
+        ripl.infer("mh(default, all, 1)")
+        all_samples.append(ripl.sample("""dag_to_dot_notation(DAG, ["x", "z"])"""))
+    all_samples = np.array(all_samples)
+    count_correct_dag = float(sum(all_samples == "(x)-->(z), ")) + float(sum(all_samples == "(z)-->(x), "))
+    assert (count_correct_dag /n_samples) > 0.7 
