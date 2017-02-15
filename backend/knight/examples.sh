4a558cebdab6a845125e36889efc195e14b9a602
Alexey Radul <axch@mit.edu>
2017-02-15 14:00:26 -0500

    Port remaining examples out of driver.py and into examples.sh.

--- backend/knight/examples.sh
+++ backend/knight/examples.sh
@@ -27,3 +27,69 @@ python -m venture.knight.driver -e '{
 # Running a synthetic SP
 python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
   -e 'normal_normal(0, 1, 1)' # (0, x) where x ~ normal(0, 2)
+
+# Tracing a synthetic SP
+python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
+  -e '{
+  model = () ~> { normal_normal(0, 100, 1) };
+  t1 = get_current_trace();
+  t2 = get_current_trace();
+  _ = regenerate(model, [], t1, t2);
+  list(trace_get(subtrace(subtrace(t2, "app"), "x")), trace_get(subtrace(t2, "app")))
+}' # (0, List(x, y)) where x ~ normal(0, 100) and y ~ normal(x, 1)
+
+# Constraining a synthetic SP
+python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
+  -e '{
+  model = () ~> { normal_normal(0, 1, 1) };
+  t1 = get_current_trace();
+  _ = trace_set(subtrace(t1, "app"), 5);
+  t2 = get_current_trace();
+  res = regenerate(model, [], t1, t2);
+  list(first(res), trace_get(subtrace(subtrace(t2, "app"), "x")), trace_get(subtrace(t2, "app")))
+}' # (0, List(-7.52, x, 5)) where x ~ normal(2.5, 1/sqrt(2))
+
+# Test generic regenerator_of
+python -m venture.knight.driver -e '{
+  t1 = get_current_trace();
+  t2 = get_current_trace();
+  _ = trace_set(t2, 1);
+  res = regenerate(sp(regenerator_of(normal)), [0, 1], t1, t2);
+  list(res, trace_get(t2)) }' # (0, List(List(0 . 1), 1))
+
+# Test tracing a mechanism
+python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
+  -e '{
+  t1 = get_current_trace();
+  t2 = get_current_trace();
+  t3 = get_current_trace();
+  t4 = get_current_trace();
+  _ = regenerate(regenerator_of(normal_normal), [[0, 1, 1], t1, t2], t3, t4);
+  list(trace_get(subtrace(t2, "x")), t4)
+}' # (0, List(x, a trace)) where x ~ normal(0, 1)
+
+# Test another trace of a mechanism
+python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
+  -e '{
+  t1 = get_current_trace();
+  t2 = get_current_trace();
+  _ = trace_set(t1, 5);
+  t3 = get_current_trace();
+  t4 = get_current_trace();
+  res = regenerate(regenerator_of(normal_normal), [[0, 1, 1], t1, t2], t3, t4);
+  list(res, trace_get(subtrace(t2, "x")), t4)
+}' # (0, List(List(0, -7.52 . 5), x, a trace)) where x ~ normal(2.5, 1/sqrt(2))
+
+# Test intervening on a traced mechanism
+# Compare the test case "constraining a synthetic SP"
+python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
+  -e '{
+  t1 = get_current_trace();
+  _ = trace_set(t1, 5);
+  t2 = get_current_trace();
+  t3 = get_current_trace();
+  t4 = get_current_trace();
+  _ = trace_set(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(subtrace(t4, "app"), "app"), "app"), "app"), "app"), "app"), "app"), "app"), "app"), "app"), "app"), "app"), 1), "app"), 7);
+  res = regenerate(regenerator_of(normal_normal), [[0, 1, 1], t1, t2], t3, t4);
+  list(res, trace_get(subtrace(t2, "x")), t4)
+}' # (0, List(List(0, -7.52 . 5), 7, a trace))
