333057c0b135d04e05f1f6f615a267a9712db621
Alexey Radul <axch@mit.edu>
2017-02-24 17:16:32 -0500

    Flush the underscores, now that the parser doesn't require them.

--- backend/knight/examples.sh
+++ backend/knight/examples.sh
@@ -2,7 +2,7 @@ python -m venture.knight.driver -e '((x) -> { x })(2)' # (0, 2)
 python -m venture.knight.driver -e 'normal(2, 1)' # (0, x) where x ~ normal(2, 1)
 python -m venture.knight.driver -e 'get_current_trace()' # (0, An empty trace)
 python -m venture.knight.driver -e 'trace_has(get_current_trace())' # (0, False)
-python -m venture.knight.driver -e '{ t = get_current_trace(); _ = trace_set(t, 5); trace_get(t) }' # (0, 5)
+python -m venture.knight.driver -e '{ t = get_current_trace(); trace_set(t, 5); trace_get(t) }' # (0, 5)
 
 python -m venture.knight.driver -e '{
   t1 = get_current_trace();
@@ -12,7 +12,7 @@ python -m venture.knight.driver -e '{
 
 python -m venture.knight.driver -e '{
   t1 = get_current_trace();
-  _ = trace_set(t1, 1);
+  trace_set(t1, 1);
   t2 = get_current_trace();
   res = regenerate(normal, [0, 1], t1, t2);
   list(res, trace_get(t2)) }' # (0, List(List(-1.41 . 1), 1))
@@ -20,7 +20,7 @@ python -m venture.knight.driver -e '{
 python -m venture.knight.driver -e '{
   t1 = get_current_trace();
   t2 = get_current_trace();
-  _ = trace_set(t2, 1);
+  trace_set(t2, 1);
   res = regenerate(normal, [0, 1], t1, t2);
   list(res, trace_get(t2)) }' # (0, List(List(0 . 1), 1))
 
@@ -34,7 +34,7 @@ python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
   model = () ~> { normal_normal(0, 100, 1) };
   t1 = get_current_trace();
   t2 = get_current_trace();
-  _ = regenerate(model, [], t1, t2);
+  regenerate(model, [], t1, t2);
   subt = subtrace(subtrace(t2, 0), "app");
   list(trace_get(subtrace(subt, "x")), trace_get(subt))
 }' # (0, List(x, y)) where x ~ normal(0, 100) and y ~ normal(x, 1)
@@ -44,7 +44,7 @@ python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
   -e '{
   model = () ~> { normal_normal(0, 1, 1) };
   t1 = get_current_trace();
-  _ = trace_set(subtrace(subtrace(t1, 0), "app"), 5);
+  trace_set(subtrace(subtrace(t1, 0), "app"), 5);
   t2 = get_current_trace();
   res = regenerate(model, [], t1, t2);
   subt = subtrace(subtrace(t2, 0), "app");
@@ -55,7 +55,7 @@ python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
 python -m venture.knight.driver -e '{
   t1 = get_current_trace();
   t2 = get_current_trace();
-  _ = trace_set(t2, 1);
+  trace_set(t2, 1);
   res = regenerate(sp(regenerator_of(normal)), [0, 1], t1, t2);
   list(res, trace_get(t2)) }' # (0, List(List(0 . 1), 1))
 
@@ -66,7 +66,7 @@ python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
   t2 = get_current_trace();
   t3 = get_current_trace();
   t4 = get_current_trace();
-  _ = regenerate(regenerator_of(normal_normal), [[0, 1, 1], t1, t2], t3, t4);
+  regenerate(regenerator_of(normal_normal), [[0, 1, 1], t1, t2], t3, t4);
   list(trace_get(subtrace(t2, "x")), t4)
 }' # (0, List(x, a trace)) where x ~ normal(0, 1)
 
@@ -75,7 +75,7 @@ python -m venture.knight.driver -f backend/knight/normal-normal.vnts \
   -e '{
   t1 = get_current_trace();
   t2 = get_current_trace();
-  _ = trace_set(t1, 5);
+  trace_set(t1, 5);
   t3 = get_current_trace();
   t4 = get_current_trace();
   res = regenerate(regenerator_of(normal_normal), [[0, 1, 1], t1, t2], t3, t4);
