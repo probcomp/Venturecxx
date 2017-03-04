d2521e9343fb96f4bee612bd1fe5d15b8a09ed97
Alexey Radul <axch@mit.edu>
2017-03-03 20:32:07 -0500

    Use the trace literal syntax for the prefix of easy cases.
    Turns out I actually need splicing to cover t1[0, "app"] := 5.

--- backend/knight/examples.sh
+++ backend/knight/examples.sh
@@ -6,23 +6,18 @@ python -m venture.knight.driver -e '{ t = get_current_trace(); t := 5; @t }' # (
 python -m venture.knight.driver -e '{ t = T{ 6 }; @t }' # (0, 6)
 
 python -m venture.knight.driver -e '{
-  t1 = get_current_trace();
-  t2 = get_current_trace();
-  (score, x) = regenerate(normal, [0, 1], t1, t2);
-  (score, x, @t2) }' # (0, List(0, x, x)) where x ~ normal(0, 1)
+  t = T{};
+  (score, x) = regenerate(normal, [0, 1], T{}, t);
+  (score, x, @t) }' # (0, List(0, x, x)) where x ~ normal(0, 1)
 
 python -m venture.knight.driver -e '{
-  t1 = get_current_trace();
-  t1 := 1;
-  t2 = get_current_trace();
-  (score, x) = regenerate(normal, [0, 1], t1, t2);
+  t2 = T{};
+  (score, x) = regenerate(normal, [0, 1], T{1}, t2);
   (score, x, @t2) }' # (0, List(-1.41, 1, 1))
 
 python -m venture.knight.driver -e '{
-  t1 = get_current_trace();
-  t2 = get_current_trace();
-  t2 := 1;
-  (score, x) = regenerate(normal, [0, 1], t1, t2);
+  t2 = T{1};
+  (score, x) = regenerate(normal, [0, 1], T{}, t2);
   (score, x, @t2) }' # (0, List(0, 1, 1))
 
 # Running a synthetic SP
@@ -33,9 +28,8 @@ python -m venture.knight.driver -f backend/knight/prelude.vnts -f backend/knight
 python -m venture.knight.driver -f backend/knight/prelude.vnts -f backend/knight/normal-normal.vnts \
   -e '{
   model = () ~> { normal_normal(0, 100, 1) };
-  t1 = get_current_trace();
-  t2 = get_current_trace();
-  regenerate(model, [], t1, t2);
+  t2 = T{};
+  regenerate(model, [], T{}, t2);
   subt = t2[0, "app"];
   (@subt["x"], @subt)
 }' # (0, List(x, y)) where x ~ normal(0, 100) and y ~ normal(x, 1)
@@ -46,7 +40,7 @@ python -m venture.knight.driver -f backend/knight/prelude.vnts -f backend/knight
   model = () ~> { normal_normal(0, 1, 1) };
   t1 = get_current_trace();
   t1[0, "app"] := 5;
-  t2 = get_current_trace();
+  t2 = T{};
   (score, _) = regenerate(model, [], t1, t2);
   subt = t2[0, "app"];
   (score, @subt["x"], @subt)
