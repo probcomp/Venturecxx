49bf865f6f827b85c88d7333db885a849b7bf4a5
Alexey Radul <axch@mit.edu>
2017-03-08 14:25:01 -0500

    Adapt the mcmc plotting program to rolling-resample chains.
    This is annoying because there is a fence-post discrepancy: The prior is 0 M-H steps, but 1 particle.

--- backend/knight/plotting.py
+++ backend/knight/plotting.py
@@ -18,5 +18,26 @@ def plot_mcmc(filename):
         ax.set_xlabel("weight")
         ax.set_ylabel("num samples")
 
-plot_mcmc('mcmc.txt')
+def plot_particles(filename):
+    with open(filename, 'r') as f:
+        chains = [[float(num) for num in line.strip().strip('[]').split(',')]
+                  for line in f.readlines()]
+
+    num_steps = len(chains[0])
+
+    plot_schedule = [0, int((num_steps-1)/4), int((num_steps-1)/2),
+                     int(3*(num_steps-1)/4), num_steps-1]
+    fig = plt.figure()
+    for i, step in enumerate(plot_schedule):
+        ax = fig.add_subplot(len(plot_schedule), 1, i+1)
+        ax.hist([chain[step] for chain in chains])
+        ax.set_xlim([0,1])
+        if step == 0:
+            ax.set_title("With 1 particle")
+        else:
+            ax.set_title("With %d particles" % (step+1,))
+        ax.set_xlabel("weight")
+        ax.set_ylabel("num samples")
+
+plot_particles('particles.txt')
 plt.show()
