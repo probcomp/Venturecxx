4eb3e2377234ba510f15b5d70d4e0e912734979f
Alexey Radul <axch@mit.edu>
2017-03-08 15:09:32 -0500

    Turns out this check is too strict, since I can sometimes get a numpy object array (!).

--- backend/knight/sp.py
+++ backend/knight/sp.py
@@ -57,7 +57,6 @@ class SPFromLite(SP):
 
   def regenerate(self, args, target, mechanism):
     # type: (List[vv.VentureValue], Trace, Trace) -> Tuple[float, RegenResult]
-    assert isinstance(args, list)
     for a in args:
       assert isinstance(a, vv.VentureValue)
     if target.has():
