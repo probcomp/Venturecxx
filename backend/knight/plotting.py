317adc7968515a7f58b91eb6fc08357c05515b9e
Alexey Radul <axch@mit.edu>
2017-03-08 16:05:29 -0500

    Overlay an outline of the rejection plot over the others, to show how they approach the posterior.

--- backend/knight/plotting.py
+++ backend/knight/plotting.py
@@ -1,4 +1,7 @@
+import numpy as np
 import matplotlib.pyplot as plt
+
+bins = np.linspace(0, 1, 50)
 
 def plot_mcmc(filename):
     with open(filename, 'r') as f:
@@ -12,11 +15,12 @@ def plot_mcmc(filename):
     fig = plt.figure()
     for i, step in enumerate(plot_schedule):
         ax = fig.add_subplot(len(plot_schedule), 1, i+1)
-        ax.hist([chain[step] for chain in chains])
+        ax.hist([chain[step] for chain in chains], bins=bins)
         ax.set_xlim([0,1])
         ax.set_title("M-H step %d" % (step,))
         ax.set_xlabel("weight")
         ax.set_ylabel("num samples")
+        do_plot_rejection('rejection.txt', ax)
 
 def plot_particles(filename):
     with open(filename, 'r') as f:
@@ -30,7 +34,7 @@ def plot_particles(filename):
     fig = plt.figure()
     for i, step in enumerate(plot_schedule):
         ax = fig.add_subplot(len(plot_schedule), 1, i+1)
-        ax.hist([chain[step] for chain in chains])
+        ax.hist([chain[step] for chain in chains], bins=bins)
         ax.set_xlim([0,1])
         if step == 0:
             ax.set_title("With 1 particle")
@@ -38,18 +42,26 @@ def plot_particles(filename):
             ax.set_title("With %d particles" % (step+1,))
         ax.set_xlabel("weight")
         ax.set_ylabel("num samples")
+        do_plot_rejection('rejection.txt', ax)
 
-def plot_rejection(filename):
+def do_plot_rejection(filename, ax=None):
     with open(filename, 'r') as f:
         chains = [[float(num) for num in line.strip().strip('[]').split(',')]
                   for line in f.readlines()]
-    plt.figure()
-    ax = plt.gca()
-    ax.hist([chain[0] for chain in chains])
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+    ax.hist([chain[0] for chain in chains], histtype='step', color='orange', bins=bins)
+    return ax
+
+def plot_rejection(filename):
+    ax = do_plot_rejection(filename)
     ax.set_xlim([0,1])
     ax.set_title("Rejection sampling")
     ax.set_xlabel("weight")
     ax.set_ylabel("num samples")
 
-plot_rejection('rejection.txt')
+# plot_mcmc('mcmc.txt')
+plot_particles('particles.txt')
+# plot_rejection('rejection.txt')
 plt.show()
