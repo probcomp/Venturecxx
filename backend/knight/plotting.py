63671fdd31bfa2b07df8ba5b33cbcb4831255586
Alexey Radul <axch@mit.edu>
2017-03-08 13:21:02 -0500

    Make the distribution evolution plot a bit more presentable.

--- backend/knight/plotting.py
+++ backend/knight/plotting.py
@@ -6,10 +6,15 @@ with open('mcmc.txt', 'r') as f:
 
 num_steps = len(chains[0]) - 1
 
+plot_schedule = [0, int(num_steps/4), int(num_steps/2),
+                 int(3*num_steps/4), num_steps]
 fig = plt.figure()
-for i in range(num_steps+1):
-    ax = fig.add_subplot(num_steps+1, 1, i)
-    ax.hist([chain[i] for chain in chains])
+for i, step in enumerate(plot_schedule):
+    ax = fig.add_subplot(len(plot_schedule), 1, i+1)
+    ax.hist([chain[step] for chain in chains])
     ax.set_xlim([0,1])
+    ax.set_title("M-H step %d" % (step,))
+    ax.set_xlabel("weight")
+    ax.set_ylabel("num samples")
 
 plt.show()
