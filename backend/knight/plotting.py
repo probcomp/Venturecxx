348a4bf0df76dc38c5de0f3fb04c79fe48b89bf4
Alexey Radul <axch@mit.edu>
2017-03-09 19:19:07 -0500

    Generalize name: Exact samples by any means, not just rejection.

--- backend/knight/plotting.py
+++ backend/knight/plotting.py
@@ -4,7 +4,7 @@ import scipy.stats as stats
 
 bins = np.linspace(0, 1, 50)
 
-def plot_mcmc(filename, rejection_file=None):
+def plot_mcmc(filename, exact_file=None):
     with open(filename, 'r') as f:
         chains = [[float(num) for num in line.strip().strip('[]').split(',')]
                   for line in f.readlines()]
@@ -14,11 +14,11 @@ def plot_mcmc(filename, rejection_file=None):
     plot_schedule = [0, int(num_steps/4), int(num_steps/2),
                      int(3*num_steps/4), num_steps]
     fig = plt.figure(figsize=(8,12))
-    if rejection_file is None:
+    if exact_file is None:
         fig.suptitle("%d independent chains" % (len(chains),))
     else:
-        fig.suptitle("%d independent chains, against %d rejection samples (orange outline)" \
-                  % (len(chains), num_samples(rejection_file)))
+        fig.suptitle("%d independent chains, against %d exact samples (orange outline)" \
+                  % (len(chains), num_samples(exact_file)))
     for i, step in enumerate(plot_schedule):
         ax = fig.add_subplot(len(plot_schedule), 1, i+1)
         samples = [chain[step] for chain in chains]
@@ -28,15 +28,15 @@ def plot_mcmc(filename, rejection_file=None):
         title = "M-H step %d" % (step,)
         ax.set_xlabel("weight")
         ax.set_ylabel("% of samples")
-        if rejection_file is not None:
-            (_, rej_samples) = do_plot_rejection(rejection_file, ax)
+        if exact_file is not None:
+            (_, rej_samples) = do_plot_exact(exact_file, ax)
             (D, pval) = stats.ks_2samp(samples, rej_samples)
             title += ", K-S stat %6.4f, p-value %8.6f" % (D, pval)
         ax.set_title(title)
     plt.tight_layout()
     plt.subplots_adjust(top=0.94)
 
-def plot_particles(filename, rejection_file=None):
+def plot_particles(filename, exact_file=None):
     with open(filename, 'r') as f:
         chains = [[float(num) for num in line.strip().strip('[]').split(',')]
                   for line in f.readlines()]
@@ -46,11 +46,11 @@ def plot_particles(filename, rejection_file=None):
     plot_schedule = [0, int((num_steps-1)/4), int((num_steps-1)/2),
                      int(3*(num_steps-1)/4), num_steps-1]
     fig = plt.figure(figsize=(8,12))
-    if rejection_file is None:
+    if exact_file is None:
         fig.suptitle("%d independent replicates" % (len(chains),))
     else:
-        fig.suptitle("%d independent replicates, against %d rejection samples (orange outline)" \
-                  % (len(chains), num_samples(rejection_file)))
+        fig.suptitle("%d independent replicates, against %d exact samples (orange outline)" \
+                  % (len(chains), num_samples(exact_file)))
     for i, step in enumerate(plot_schedule):
         ax = fig.add_subplot(len(plot_schedule), 1, i+1)
         samples = [chain[step] for chain in chains]
@@ -63,8 +63,8 @@ def plot_particles(filename, rejection_file=None):
             title = "With %d particles" % (step+1,)
         ax.set_xlabel("weight")
         ax.set_ylabel("% of samples")
-        if rejection_file is not None:
-            (_, rej_samples) = do_plot_rejection(rejection_file, ax)
+        if exact_file is not None:
+            (_, rej_samples) = do_plot_exact(exact_file, ax)
             (D, pval) = stats.ks_2samp(samples, rej_samples)
             title += ", K-S stat %6.4f, p-value %8.6f" % (D, pval)
         ax.set_title(title)
@@ -75,7 +75,7 @@ def num_samples(filename):
     with open(filename, 'r') as f:
         return len(f.readlines())
 
-def do_plot_rejection(filename, ax=None):
+def do_plot_exact(filename, ax=None):
     with open(filename, 'r') as f:
         chains = [[float(num) for num in line.strip().strip('[]').split(',')]
                   for line in f.readlines()]
@@ -87,14 +87,14 @@ def do_plot_rejection(filename, ax=None):
             bins=bins, weights=[100.0/len(chains)]*len(chains))
     return (ax, samples)
 
-def plot_rejection(filename):
-    (ax, _) = do_plot_rejection(filename)
+def plot_exact(filename):
+    (ax, _) = do_plot_exact(filename)
     ax.set_xlim([0,1])
-    ax.set_title("Rejection sampling")
+    ax.set_title("Exact sampling")
     ax.set_xlabel("weight")
     ax.set_ylabel("num samples")
 
 # plot_mcmc('mcmc.txt')
 # plot_particles('particles.txt')
-# plot_rejection('rejection.txt')
+# plot_exact('exact.txt')
 # plt.show()
