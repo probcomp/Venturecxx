2a6bf207bb4d94850e5f9203ab77bd1568323be1
Alexey Radul <axch@mit.edu>
2017-03-22 05:26:17 -0400

    Avoid a hard dependency on the typing module except when type checking.

--- backend/lite/typing.py
+++ backend/lite/typing.py
@@ -10,4 +10,22 @@
 
 from __future__ import absolute_import
 
-from typing import *
+# Conditionalize trying to import the typing module on whether or not
+# we are typechecking with mypy, as recommended under
+# http://mypy.readthedocs.io/en/latest/common_issues.html#import-cycles
+MYPY = False
+if MYPY:
+    from typing import *
+else:
+    class bogus_container(object):
+        def __getitem__(self, *args, **kwargs): return object
+    Any = None
+    Callable = bogus_container()
+    Dict = bogus_container()
+    Generic = bogus_container()
+    List = bogus_container()
+    Type = None
+    Tuple = bogus_container()
+    Union = bogus_container()
+    TYPE_CHECKING = False
+    def TypeVar(*args, **kwargs): return None
