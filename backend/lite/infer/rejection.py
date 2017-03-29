56c738c3998f7795d97e0682582f642b5a95a4a9
Alexey Radul <axch@mit.edu>
2017-03-29 09:57:42 -0400

    More informative commented debug prints.

--- backend/lite/infer/rejection.py
+++ backend/lite/infer/rejection.py
@@ -84,13 +84,15 @@ class RejectionOperator(InPlaceOperator):
       xiWeight = regenAndAttach(trace, scaffold, False, self.rhoDB, OrderedDict())
       assert xiWeight <= logBound, \
         "Detected regen weight %s not at most weight bound %s" % (xiWeight, logBound)
-      # print xiWeight, logBound
-      accept = trace.py_rng.random() < math.exp(xiWeight - logBound)
+      prob_accept = math.exp(xiWeight - logBound)
+      # print "Attempting candidate with score", xiWeight, \
+      #   "against bound", logBound, "p accept is", prob_accept
+      accept = trace.py_rng.random() < prob_accept
       if not accept:
         detachAndExtract(trace, scaffold)
         attempt += 1
       # else:
-      #   print "Accepted!"
+      #   print "Accepted after %d attempts!" % (attempt,)
     if not accept:
       # Ran out of attempts
       print "Warning: rejection hit attempt bound of %s" % self.trials
