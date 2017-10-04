dbe953605d2c2f6fc3cafcd5ed2a4d8ff77d9816
Alexey Radul <axch@mit.edu>
2017-10-04 16:35:23 -0400

    Pull out the MCDF support into its own module and document it more clearly.

--- python/lib/plots/p_p_plot.py
+++ python/lib/plots/p_p_plot.py
@@ -17,66 +17,15 @@
 
 """Support for making p-p plots."""
 
-import bisect
-import itertools
-
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 
-# Returns a "massive cdf": a function a -> (Double, Double) that
-# returns the total probability mass strictly below a, and the total
-# mass of a itself.  A traditional cdf is equivalent to a massive cdf
-# that always claims the mass at a is zero.
-def empirical_cdf(sample):
-  step = 1.0 / len(sample)
-  ordered = sorted(sample)
-  def mcdf(x):
-    low_ind = bisect.bisect_left(ordered, x)
-    high_ind = bisect.bisect_right(ordered, x)
-    return (low_ind * step, (high_ind - low_ind) * step)
-  return mcdf
-
-def discrete_cdf(rates):
-  # (Eq a, Ord a) => [(a, Double)] -> (a -> (Double, Double))
-  # Convert an explicit discrete distribution to its "massive cdf" (by partial sums).
-  ordered = sorted(rates, key=lambda (v, p): v)
-  def mcdf(x):
-    total = 0
-    for (v, p) in ordered:
-      if v < x: total += p
-      if v == x: return (total, p)
-      if v > x: return (total, 0)
-    return (total, 0)
-  return mcdf
-
-def massless(cdf):
-  # (a -> Double) -> (a -> (Double, Double))
-  # Convert a traditional cdf to a "massive" one, assuming every
-  # object has 0 mass.
-  def mcdf(x):
-    return (cdf(x), 0)
-  return mcdf
-
-# Convert a "massive cdf" into a cdf on an expanded state space.  The
-# expanded state space is the product of the original state space with
-# the interval [0,1], interpreting points with mass as gaining mass
-# linearly over that interval.
-def expand_state_space(mcdf):
-  def expanded((x, portion)):
-    (below, at) = mcdf(x)
-    return below + portion * at
-  return expanded
-
-# Convert a sample with potential duplicates into a sample on the
-# expanded state space without duplicates.  Duplicates in the original
-# sample are spaced out along the extra intervals.
-def deduplicate(sample):
-  def chunk((x, xs)):
-    k = len(list(xs))
-    return [(x, float(i)/k) for i in range(k)]
-  return list(itertools.chain.from_iterable(
-    map(chunk, itertools.groupby(sorted(sample)))))
+from venture.mcdf import empirical_cdf
+from venture.mcdf import discrete_cdf
+from venture.mcdf import massless
+from venture.mcdf import expand_state_space
+from venture.mcdf import deduplicate
 
 def report_ks_stat(expected, observed, ax):
   (K, pval) = stats.kstest(observed, expected)
